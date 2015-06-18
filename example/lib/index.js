c8f0bac128eb1a55538cf6bbce613a1f2ab6e99e
David Baker <dave@matrix.org>
2015-06-18 16:41:34 +0100

    typo

--- example/lib/index.js
+++ example/lib/index.js
@@ -2,7 +2,7 @@ var React = require("react");
 // In normal usage of the module:
 //var MatrixReactSdk = require("matrix-react-sdk");
 // Or to import the source directly from the file system:
-// (This is useful for debugging the SDK as ut seems source
+// (This is useful for debugging the SDK as it seems source
 // maps cannot pass through two stages).
 var MatrixReactSdk = require("../../src/index");
 
