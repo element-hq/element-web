08270b26ee6df015cde22b3b271f032c7870774a
David Baker <dave@matrix.org>
2015-10-22 13:10:02 +0100

    Do room avatars properly.

--- src/skins/vector/views/atoms/RoomAvatar.js
+++ src/skins/vector/views/atoms/RoomAvatar.js
@@ -17,12 +17,30 @@ limitations under the License.
 'use strict';
 
 var React = require('react');
+var MatrixClientPeg = require('matrix-react-sdk/lib/MatrixClientPeg');
 
 var RoomAvatarController = require('matrix-react-sdk/lib/controllers/atoms/RoomAvatar')
 
 module.exports = React.createClass({
     displayName: 'RoomAvatar',
     mixins: [RoomAvatarController],
+
+    getUrlList: function() {
+        return [
+            this.roomAvatarUrl(),
+            this.getOneToOneAvatar(),
+            this.getFallbackAvatar()
+        ];
+    },
+
+    getFallbackAvatar: function() {
+        var images = [ '80cef4', '50e2c2', 'f4c371' ];
+        var total = 0;
+        for (var i = 0; i < this.props.room.roomId.length; ++i) {
+            total += this.props.room.roomId.charCodeAt(i);
+        }
+        return 'img/' + images[total % images.length] + '.png';
+    },
 
     render: function() {
         return (
