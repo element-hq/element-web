731ad26be4dfdf840ca6c9ed9ad9f586a857ebd5
Kegan Dougal <kegan@matrix.org>
2015-10-27 17:45:47 +0000

    Exit full screen programatically when the remote side hangs up

--- src/skins/vector/views/molecules/RoomHeader.js
+++ src/skins/vector/views/molecules/RoomHeader.js
@@ -39,7 +39,7 @@ module.exports = React.createClass({
     },
 
     onFullscreenClick: function() {
-        dis.dispatch({action: 'video_fullscreen'}, true);
+        dis.dispatch({action: 'video_fullscreen', fullscreen: true}, true);
     },
 
     render: function() {
