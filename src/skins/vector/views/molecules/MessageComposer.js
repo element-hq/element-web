02d5154aafea2a69d926ecf6ed36402b6b4b70a4
Matthew Hodgson <matthew@matrix.org>
2015-11-19 17:45:03 +0000

    more alt tags

--- src/skins/vector/views/molecules/MessageComposer.js
+++ src/skins/vector/views/molecules/MessageComposer.js
@@ -76,14 +76,14 @@ module.exports = React.createClass({
                             <textarea ref="textarea" rows="1" onKeyDown={this.onKeyDown} onKeyUp={this.onKeyUp} placeholder="Type a message..." />
                         </div>
                         <div className="mx_MessageComposer_upload" onClick={this.onUploadClick}>
-                            <img src="img/upload.png" width="17" height="22"/>
+                            <img src="img/upload.png" alt="Upload file" title="Upload file" width="17" height="22"/>
                             <input type="file" style={uploadInputStyle} ref="uploadInput" onChange={this.onUploadFileSelected} />
                         </div>
                         <div className="mx_MessageComposer_voicecall" onClick={this.onVoiceCallClick}>
-                            <img src="img/voice.png" width="16" height="26"/>
+                            <img src="img/voice.png" alt="Voice call" title="Voice call" width="16" height="26"/>
                         </div>
                         <div className="mx_MessageComposer_videocall" onClick={this.onCallClick}>
-                            <img src="img/call.png" width="28" height="20"/>
+                            <img src="img/call.png" alt="Video call" title="Video call" width="28" height="20"/>
                         </div>
                     </div>
                 </div>
