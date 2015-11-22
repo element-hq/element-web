fd8864d528b7da817fbae73e68f5e423de325928
Matthew Hodgson <matthew@matrix.org>
2015-11-22 22:35:34 +0000

    STOP PEOPLE SHOUTING

--- src/HtmlUtils.js
+++ src/HtmlUtils.js
@@ -22,7 +22,7 @@ var highlight = require('highlight.js');
 
 var sanitizeHtmlParams = {
     allowedTags: [
-        'h1', 'h2', 'font', // custom to matrix
+        'font', // custom to matrix. deliberately no h1/h2 to stop people shouting.
         'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol',
         'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'code', 'hr', 'br', 'div',
         'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre'
