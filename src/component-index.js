4a144ac03dcf8cb7d392beec29583d055ae4ce50
David Baker <dave@matrix.org>
2015-11-30 17:30:32 +0000

    WIP of new component indexing

---
+++ src/component-index.js
@@ -0,0 +1,42 @@
+/*
+ * THIS FILE IS AUTO-GENERATED
+ * You can edit it you like, but your changes will be overwritten,
+ * so you'd just be trying to swim upstream like a salmon.
+ * You are not a salmon.
+ */
+
+module.exports.components = {};
+module.exports.components['views.avatars.MemberAvatar'] = require('./components/views/avatars/MemberAvatar');
+module.exports.components['views.avatars.RoomAvatar'] = require('./components/views/avatars/RoomAvatar');
+module.exports.components['views.create_room.CreateRoomButton'] = require('./components/views/create_room/CreateRoomButton');
+module.exports.components['views.create_room.Presets'] = require('./components/views/create_room/Presets');
+module.exports.components['views.create_room.RoomAlias'] = require('./components/views/create_room/RoomAlias');
+module.exports.components['views.elements.EditableText'] = require('./components/views/elements/EditableText');
+module.exports.components['views.elements.ProgressBar'] = require('./components/views/elements/ProgressBar');
+module.exports.components['views.elements.UserSelector'] = require('./components/views/elements/UserSelector');
+module.exports.components['views.login.CaptchaForm'] = require('./components/views/login/CaptchaForm');
+module.exports.components['views.login.CasLogin'] = require('./components/views/login/CasLogin');
+module.exports.components['views.login.PasswordLogin'] = require('./components/views/login/PasswordLogin');
+module.exports.components['views.messages.Event'] = require('./components/views/messages/Event');
+module.exports.components['views.messages.Message'] = require('./components/views/messages/Message');
+module.exports.components['views.messages.MessageComposer'] = require('./components/views/messages/MessageComposer');
+module.exports.components['views.messages.MFileMessage'] = require('./components/views/messages/MFileMessage');
+module.exports.components['views.messages.MImageMessage'] = require('./components/views/messages/MImageMessage');
+module.exports.components['views.messages.MRoomMemberEvent'] = require('./components/views/messages/MRoomMemberEvent');
+module.exports.components['views.messages.MVideoMessage'] = require('./components/views/messages/MVideoMessage');
+module.exports.components['views.messages.TextualEvent'] = require('./components/views/messages/TextualEvent');
+module.exports.components['views.messages.TextualMessage'] = require('./components/views/messages/TextualMessage');
+module.exports.components['views.messages.UnknownMessage'] = require('./components/views/messages/UnknownMessage');
+module.exports.components['views.rooms.MemberInfo'] = require('./components/views/rooms/MemberInfo');
+module.exports.components['views.rooms.MemberTile'] = require('./components/views/rooms/MemberTile');
+module.exports.components['views.rooms.RoomHeader'] = require('./components/views/rooms/RoomHeader');
+module.exports.components['views.rooms.RoomSettings'] = require('./components/views/rooms/RoomSettings');
+module.exports.components['views.rooms.RoomTile'] = require('./components/views/rooms/RoomTile');
+module.exports.components['views.settings.ChangeAvatar'] = require('./components/views/settings/ChangeAvatar');
+module.exports.components['views.settings.ChangeDisplayName'] = require('./components/views/settings/ChangeDisplayName');
+module.exports.components['views.settings.ChangePassword'] = require('./components/views/settings/ChangePassword');
+module.exports.components['views.settings.EnableNotificationsButton'] = require('./components/views/settings/EnableNotificationsButton');
+module.exports.components['views.voip.CallView'] = require('./components/views/voip/CallView');
+module.exports.components['views.voip.IncomingCallBox'] = require('./components/views/voip/IncomingCallBox');
+module.exports.components['views.voip.VideoFeed'] = require('./components/views/voip/VideoFeed');
+module.exports.components['views.voip.VideoView'] = require('./components/views/voip/VideoView');
