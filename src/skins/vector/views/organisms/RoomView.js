7636645efcb66a49277953c20030d93989e0280e
Matthew Hodgson <matthew@matrix.org>
2015-11-30 15:25:21 +0000

    rename message components as per RL discussion this morning

--- src/skins/vector/views/organisms/RoomView.js
+++ src/skins/vector/views/organisms/RoomView.js
@@ -109,7 +109,7 @@ module.exports = React.createClass({
 
     render: function() {
         var RoomHeader = sdk.getComponent('rooms.RoomHeader');
-        var MessageComposer = sdk.getComponent('messages.MessageComposer');
+        var MessageComposer = sdk.getComponent('rooms.MessageComposer');
         var CallView = sdk.getComponent("voip.CallView");
         var RoomSettings = sdk.getComponent("rooms.RoomSettings");
         var SearchBar = sdk.getComponent("molecules.SearchBar");
