7c0fffa79bce45662bda198cc2bdd0484284dc66
manuroe <manu@matrix.org>
2016-01-18 18:07:33 +0100

    PushRules settings: Applied easy review remarks

--- src/components/views/settings/Notifications.js
+++ src/components/views/settings/Notifications.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2015, 2016 OpenMarket Ltd
+Copyright 2016 OpenMarket Ltd
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -195,7 +195,7 @@ var VectorPushRulesDefinitions = {
             loud: [
                 "notify",
                 {
-                    "set_tweak": "sound",
+                    "set_tweak": "ring",
                     "value": "default"
                 }
             ]
