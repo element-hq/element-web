1c267c69ac1fe03985432b155da9423734a3a648
Kegan Dougal <kegan@matrix.org>
2016-02-02 16:20:02 +0000

    Include react SDK SHA in filename

--- jenkins.sh
+++ jenkins.sh
@@ -6,4 +6,5 @@ npm install
 (cd node_modules/matrix-react-sdk && npm run build) # npm doesn't do this when dependencies point at github.com >:(
 npm run build # Dumps artificats to /vector
 rm vector-*.tar.gz || true # rm previous artifacts without failing if it doesn't exist
-tar -zcvhf vector-$GIT_COMMIT.tar.gz vector #g[z]ip, [c]reate archive, [v]erbose, [f]ilename, [h]ard-dereference (do not archive symlinks)
+REACT_SHA=$(cd node_modules/matrix-react-sdk && git rev-parse --short=12 HEAD) # use the ACTUAL SHA rather than assume develop
+tar -zcvhf vector-$GIT_COMMIT-react-$REACT_SHA.tar.gz vector #g[z]ip, [c]reate archive, [v]erbose, [f]ilename, [h]ard-dereference (do not archive symlinks)
