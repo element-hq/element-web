2a7b24e9693feef6c0901df7056939d473de9a25
Kegan Dougal <kegan@matrix.org>
2016-02-02 16:09:33 +0000

    Update jenkins.sh to include the git commit

--- jenkins.sh
+++ jenkins.sh
@@ -5,5 +5,5 @@ nvm use 4
 npm install
 (cd node_modules/matrix-react-sdk && npm run build) # npm doesn't do this when dependencies point at github.com >:(
 npm run build # Dumps artificats to /vector
-rm vector-build.tar.gz || true # rm previous artifacts without failing if it doesn't exist
-tar -zcvhf vector-build.tar.gz vector #g[z]ip, [c]reate archive, [v]erbose, [f]ilename, [h]ard-dereference (do not archive symlinks)
+rm vector-*.tar.gz || true # rm previous artifacts without failing if it doesn't exist
+tar -zcvhf vector-$GIT_COMMIT.tar.gz vector #g[z]ip, [c]reate archive, [v]erbose, [f]ilename, [h]ard-dereference (do not archive symlinks)
