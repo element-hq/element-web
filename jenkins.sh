928915873bbbf8d705c99ffbc2d12b2484779f27
Richard van der Hoff <richard@matrix.org>
2016-03-11 14:30:53 +0000

    Use npm to fetch react-sdk and js-sdk
    After some discussion, we've agreed it's less evil to have package.json point
    to git develop, even if you then have to do a manual build step. That avoids
    any future problems where (for example) js-sdk develop gains experimental
    changes which would break for users using a build process which involves a
    manual git checkout.

--- jenkins.sh
+++ jenkins.sh
@@ -8,12 +8,10 @@ nvm use 4
 
 set -x
 
-# install the versions of js-sdk and react-sdk provided to us by jenkins
-npm install ./node_modules/matrix-js-sdk-*.tgz
-npm install ./node_modules/matrix-react-sdk-*.tgz
-
-# install the other dependencies
 npm install
+
+# we may be using a dev branch of react-sdk, in which case we need to build it
+(cd node_modules/matrix-react-sdk && npm run build)
 
 # build our artifacts; dumps them in ./vector
 npm run build
@@ -21,7 +19,12 @@ npm run build
 # gzip up ./vector
 rm vector-*.tar.gz || true # rm previous artifacts without failing if it doesn't exist
 
-REACT_SHA=$(head -c 12 node_modules/matrix-react-sdk/git-revision.txt)
-JSSDK_SHA=$(head -c 12 node_modules/matrix-js-sdk/git-revision.txt)
+ # node_modules deps from 'npm install' don't have a .git dir so can't
+ # rev-parse; but they do set the commit in package.json under 'gitHead' which
+ # we're grabbing here.
+REACT_SHA=$(grep 'gitHead' node_modules/matrix-react-sdk/package.json | cut -d \" -f 4 | head -c 12)
+JSSDK_SHA=$(grep 'gitHead' node_modules/matrix-js-sdk/package.json | cut -d \" -f 4 | head -c 12)
+
 VECTOR_SHA=$(git rev-parse --short=12 HEAD) # use the ACTUAL SHA rather than assume develop
+
 tar -zcvhf vector-$VECTOR_SHA-react-$REACT_SHA-js-$JSSDK_SHA.tar.gz vector #g[z]ip, [c]reate archive, [v]erbose, [f]ilename, [h]ard-dereference (do not archive symlinks)
