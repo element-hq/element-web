f70eec62d18fb282fefc8d037dae88647c82ea95
Matthew Hodgson <matthew@matrix.org>
2016-03-15 18:35:09 +0000

    let registering guests either upgrade or create a new account by specifying a new username. fixes https://github.com/vector-im/vector-web/issues/1161

--- src/components/views/login/RegistrationForm.js
+++ src/components/views/login/RegistrationForm.js
@@ -38,7 +38,6 @@ module.exports = React.createClass({
         defaultUsername: React.PropTypes.string,
         showEmail: React.PropTypes.bool,
         minPasswordLength: React.PropTypes.number,
-        disableUsernameChanges: React.PropTypes.bool,
         onError: React.PropTypes.func,
         onRegisterClick: React.PropTypes.func // onRegisterClick(Object) => ?Promise
     },
@@ -56,7 +55,7 @@ module.exports = React.createClass({
     getInitialState: function() {
         return {
             email: this.props.defaultEmail,
-            username: this.props.defaultUsername,
+            username: null,
             password: null,
             passwordConfirm: null,
             fieldValid: {}
@@ -78,7 +77,7 @@ module.exports = React.createClass({
 
         if (this.allFieldsValid()) {
             var promise = this.props.onRegisterClick({
-                username: this.refs.username.value.trim(),
+                username: this.refs.username.value.trim() || this.props.defaultUsername,
                 password: this.refs.password.value.trim(),
                 email: this.refs.email.value.trim()
             });
@@ -120,13 +119,14 @@ module.exports = React.createClass({
                 break;
             case FIELD_USERNAME:
                 // XXX: SPEC-1
-                if (encodeURIComponent(this.refs.username.value) != this.refs.username.value) {
+                var username = this.refs.username.value.trim() || this.props.defaultUsername;
+                if (encodeURIComponent(username) != username) {
                     this.markFieldValid(
                         field_id,
                         false,
                         "RegistrationForm.ERR_USERNAME_INVALID"
                     );
-                } else if (this.refs.username.value == '') {
+                } else if (username == '') {
                     this.markFieldValid(
                         field_id,
                         false,
@@ -199,7 +199,7 @@ module.exports = React.createClass({
         if (this.props.showEmail) {
             emailSection = (
                 <input className="mx_Login_field" type="text" ref="email"
-                    autoFocus={true} placeholder="Email address"
+                    autoFocus={true} placeholder="Email address (optional)"
                     defaultValue={this.state.email}
                     style={this._styleField(FIELD_EMAIL)}
                     onBlur={function() {self.validateField(FIELD_EMAIL)}} />
@@ -211,17 +211,24 @@ module.exports = React.createClass({
             );
         }
 
+        var placeholderUserName = "User name";
+        if (this.props.defaultUsername) {
+            placeholderUserName += " (default: " + this.props.defaultUsername + ")"
+        }
+
         return (
             <div>
                 <form onSubmit={this.onSubmit}>
                     {emailSection}
                     <br />
                     <input className="mx_Login_field" type="text" ref="username"
-                        placeholder="User name" defaultValue={this.state.username}
+                        placeholder={ placeholderUserName } defaultValue={this.state.username}
                         style={this._styleField(FIELD_USERNAME)}
-                        onBlur={function() {self.validateField(FIELD_USERNAME)}}
-                        disabled={this.props.disableUsernameChanges} />
+                        onBlur={function() {self.validateField(FIELD_USERNAME)}} />
                     <br />
+                    { this.props.defaultUsername ?
+                        <div className="mx_Login_fieldLabel">Setting a user name will create a fresh account</div> : null
+                    }
                     <input className="mx_Login_field" type="password" ref="password"
                         style={this._styleField(FIELD_PASSWORD)}
                         onBlur={function() {self.validateField(FIELD_PASSWORD)}}
