17b2d14ffcce37e11e8cff91ad7b42d8ad8b7154
Richard van der Hoff <richard@matrix.org>
2016-03-23 14:15:22 +0000

    Release script for vector-web

---
+++ release.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# Script to perform a release of vector-web.
+#
+# Requires github-changelog-generator; to install, do 
+#   pip install git+https://github.com/matrix-org/github-changelog-generator.git
+
+set -e
+
+cd `dirname $0`
+
+exec ./node_modules/matrix-js-sdk/release.sh -z -c CHANGES.rst "$@"
