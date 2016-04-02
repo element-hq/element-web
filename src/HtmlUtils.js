4ef148eaec8681a4b8b4f8b3dc0939e98be79562
Aviral Dasgupta <me@aviraldg.com>
2016-04-02 22:15:29 +0530

    whitelist <u> tag (fixes vector-im/vector-web#1339)

--- src/HtmlUtils.js
+++ src/HtmlUtils.js
@@ -27,7 +27,7 @@ var sanitizeHtmlParams = {
         'del', // for markdown
         // deliberately no h1/h2 to stop people shouting.
         'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol',
-        'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'code', 'hr', 'br', 'div',
+        'nl', 'li', 'b', 'i', 'u', 'strong', 'em', 'strike', 'code', 'hr', 'br', 'div',
         'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre'
     ],
     allowedAttributes: {
