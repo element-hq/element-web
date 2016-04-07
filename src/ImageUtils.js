1125c62505d63d99a09ec9c20243cdaf02f4bd94
Matthew Hodgson <matthew@matrix.org>
2016-04-07 18:10:35 +0100

    add comments for thumbHeight

--- src/ImageUtils.js
+++ src/ImageUtils.js
@@ -17,6 +17,21 @@ limitations under the License.
 'use strict';
 
 module.exports = {
+
+    /**
+     * Returns the actual height that an image of dimensions (fullWidth, fullHeight)
+     * will occupy if resized to fit inside a thumbnail bounding box of size
+     * (thumbWidth, thumbHeight).
+     *
+     * If the aspect ratio of the source image is taller than the aspect ratio of
+     * the thumbnail bounding box, then we return the thumbHeight parameter unchanged.
+     * Otherwise we return the thumbHeight parameter scaled down appropriately to
+     * reflect the actual height the scaled thumbnail occupies.
+     *
+     * This is very useful for calculating how much height a thumbnail will actually
+     * consume in the timeline, when performing scroll offset calcuations
+     * (e.g. scroll locking)
+     */
     thumbHeight: function(fullWidth, fullHeight, thumbWidth, thumbHeight) {
         if (!fullWidth || !fullHeight) {
             // Cannot calculate thumbnail height for image: missing w/h in metadata. We can't even
