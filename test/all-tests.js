7a821ce9d143d7193aee449001c1d536ccd3bb39
Richard van der Hoff <richard@matrix.org>
2016-04-07 17:49:39 +0100

    Make it possible to only run one test file each time

--- test/all-tests.js
+++ test/all-tests.js
@@ -1,11 +1,7 @@
-// tests.js
+// all-tests.js
 //
 // Our master test file: uses the webpack require API to find our test files
 // and run them
-
-// this is a handly place to make sure the sdk has been skinned
-var sdk = require("matrix-react-sdk");
-sdk.loadSkin(require('./test-component-index'));
 
 var context = require.context('.', true, /-test\.jsx?$/);
 context.keys().forEach(context);
