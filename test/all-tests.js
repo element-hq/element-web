69ce3c43cf71243c7e115834bbd3d7fe0ce7d787
Richard van der Hoff <richard@matrix.org>
2016-04-13 17:17:45 +0100

    Revert "Merge branch 'develop' into rav/karma"
    The karma tests don't pass yet, and aren't ready to land on develop.
    
    This reverts commit 438453e61af74d38c7de57ac2d63317ed1f4a1ed, reversing
    changes made to 50f94eb0402da92f8d6051ea53e4ee2d63a9663a.

--- test/all-tests.js
+++
@@ -1,7 +0,0 @@
-// all-tests.js
-//
-// Our master test file: uses the webpack require API to find our test files
-// and run them
-
-var context = require.context('./app-tests', true, /\.jsx?$/);
-context.keys().forEach(context);
--- test/skin-sdk.js
+++
@@ -1,8 +0,0 @@
-/*
- * skin-sdk.js
- *
- * Skins the react-sdk with the vector components
- */
-
-var sdk = require('matrix-react-sdk');
-sdk.loadSkin(require('component-index'));
