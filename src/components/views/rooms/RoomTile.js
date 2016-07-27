867b14d2cacd6db7eb5a1e1404b40081d44eb836
wmwragg <wm.wragg@gmail.com>
2016-07-27 18:14:46 +0100

    Reposition context menu now that the styling is final

--- src/components/views/rooms/RoomTile.js
+++ src/components/views/rooms/RoomTile.js
@@ -84,7 +84,7 @@ module.exports = React.createClass({
             var elementRect = e.target.getBoundingClientRect();
             // The window X and Y offsets are to adjust position when zoomed in to page
             var x = elementRect.right + window.pageXOffset;
-            var y = (elementRect.top + (elementRect.height / 2) + window.pageYOffset) - 43;
+            var y = (elementRect.top + (elementRect.height / 2) + window.pageYOffset) - 53;
             var self = this;
             ContextualMenu.createMenu(Menu, {
                 menuWidth: 188,
