5608e977194f84279001e7fe89da5573e790548e
David Baker <dave@matrix.org>
2016-10-12 17:23:01 +0100

    Remove babel-polyfill
    Should be no need for it if we're using transform-runtime (and
    we definitely don't need it twice...)

--- src/vector/index.js
+++ src/vector/index.js
@@ -16,10 +16,6 @@ limitations under the License.
 
 'use strict';
 
-// for ES6 stuff like startsWith() that Safari doesn't handle
-// and babel doesn't do by default
-require('babel-polyfill');
-
 // CSS requires: just putting them here for now as CSS is going to be
 // refactored soon anyway
 require('../../vector/components.css');
