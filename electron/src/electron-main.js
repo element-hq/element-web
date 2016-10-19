da30338aa7f0e7eb9007c332382937c5bf685ac8
David Baker <dave@matrix.org>
2016-10-19 17:48:41 +0100

    BIGGER!
    At the default window size, Riot starts with the left bar folded
    down which is not ideal as a default.

--- electron/src/electron-main.js
+++ electron/src/electron-main.js
@@ -6,6 +6,7 @@ let window = null;
 app.on('ready', () => {
     window = new BrowserWindow({
         icon: `${__dirname}/../../vector/img/logo.png`,
+        width: 1024, height: 768,
     });
     window.loadURL(`file://${__dirname}/../../vector/index.html`);
     window.on('closed', () => {
