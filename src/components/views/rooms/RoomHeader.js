a2eb0a9cf0a5c54360921fc49fce887a34d18ec9
David Baker <dave@matrix.org>
2016-10-26 13:09:53 +0100

    Fix CPU spin on joining rooms
    Ratelimit roomheader's updates and move some other things into
    rate limited functions.

--- src/components/views/rooms/RoomHeader.js
+++ src/components/views/rooms/RoomHeader.js
@@ -21,6 +21,7 @@ var sdk = require('../../../index');
 var MatrixClientPeg = require('../../../MatrixClientPeg');
 var Modal = require("../../../Modal");
 var dis = require("../../../dispatcher");
+var rate_limited_func = require('../../../ratelimitedfunc');
 
 var linkify = require('linkifyjs');
 var linkifyElement = require('linkifyjs/element');
@@ -85,8 +86,12 @@ module.exports = React.createClass({
         }
 
         // redisplay the room name, topic, etc.
-        this.forceUpdate();
+        this._rateLimitedUpdate();
     },
+
+    _rateLimitedUpdate: new rate_limited_func(function() {
+        this.forceUpdate();
+    }, 500),
 
     _onRoomNameChange: function(room) {
         this.forceUpdate();
