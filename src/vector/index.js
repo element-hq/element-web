8fe05fdff3f5e61ea83c6cf3bbb7c3646bd5a975
Richard van der Hoff <richard@matrix.org>
2016-10-26 15:02:34 +0100

    Correct path to components.css
    Fix failure to find components.css which was introduced in e1919c5
    (https://github.com/vector-im/vector-web/pull/2513)

--- src/vector/index.js
+++ src/vector/index.js
@@ -30,7 +30,7 @@ require('babel-polyfill');
 
 // CSS requires: just putting them here for now as CSS is going to be
 // refactored "soon" anyway
-require('../build/components.css');
+require('../../build/components.css');
 require('gemini-scrollbar/gemini-scrollbar.css');
 require('gfm.css/gfm.css');
 require('highlight.js/styles/github.css');
