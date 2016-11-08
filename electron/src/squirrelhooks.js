ccea483f4f74b0b076a1b6874ede50d11ec6b149
David Baker <dave@matrix.org>
2016-11-08 11:20:47 +0000

    Missed files electron windows fixes
    Missed from https://github.com/vector-im/vector-web/pull/2551
    
     * squirrel hooks file for handling install / uninstall events
     * Install 'spinner' gif for windows (ours does not spin)

---
+++ electron/src/squirrelhooks.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const spawn = require('child_process').spawn;
+const app = require('electron').app;
+
+function run_update_exe(args, done) {
+    const updateExe = path.resolve(path.dirname(process.execPath), '..', 'Update.exe');
+    spawn(updateExe, args, {
+      detached: true
+    }).on('close', done);
+};
+
+function check_squirrel_hooks() {
+    if (process.platform != 'win32') return false;
+
+    const cmd = process.argv[1];
+    const target = path.basename(process.execPath);
+    if (cmd === '--squirrel-install' || cmd === '--squirrel-updated') {
+        run_update_exe(['--createShortcut=' + target + ''], app.quit);
+        return true;
+    } else if (cmd === '--squirrel-uninstall') {
+        run_update_exe(['--removeShortcut=' + target + ''], app.quit);
+        return true;
+    } else if (cmd === '--squirrel-obsolete') {
+        app.quit();
+        return true;
+    }
+    return false;
+}
+
+module.exports = check_squirrel_hooks;
