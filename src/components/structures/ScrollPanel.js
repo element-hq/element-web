42fc7b1b66f4dbedf0af82213016fb7713f56fa4
Luke Barnard <lukeb@openmarket.com>
2016-11-22 17:23:06 +0000

    Use UNFILL_REQUEST_DEBOUNCE_MS constant, reset unfillDebouncer timeout reference.

--- src/components/structures/ScrollPanel.js
+++ src/components/structures/ScrollPanel.js
@@ -26,6 +26,9 @@ var DEBUG_SCROLL = false;
 // The amount of extra scroll distance to allow prior to unfilling.
 // See _getExcessHeight.
 const UNPAGINATION_PADDING = 1500;
+// The number of milliseconds to debounce calls to onUnfillRequest, to prevent
+// many scroll events causing many unfilling requests.
+const UNFILL_REQUEST_DEBOUNCE_MS = 200;
 
 if (DEBUG_SCROLL) {
     // using bind means that we get to keep useful line numbers in the console
@@ -367,8 +370,9 @@ module.exports = React.createClass({
                 clearTimeout(this._unfillDebouncer);
             }
             this._unfillDebouncer = setTimeout(() => {
+                this._unfillDebouncer = null;
                 this.props.onUnfillRequest(backwards, markerScrollToken);
-            }, 200);
+            }, UNFILL_REQUEST_DEBOUNCE_MS);
         }
     },
 
