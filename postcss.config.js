c3fa6ff8053f047d6d8dd8270dbb243abed5d587
Richard van der Hoff <richard@matrix.org>
2017-01-19 02:48:17 +0000

    Use the postcss-webpack-loader
    Use postcss-webpack-loader instead of webpack-cli to process the scss. Doing so
    mostly means that we avoid the problem that webpack-dev-server fails to start
    if we haven't already built the CSS. (It also simplifies package.json somewhat,
    which is no bad thing)

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    plugins: [
+        require("postcss-import")(),
+        require("autoprefixer")(),
+        require("postcss-simple-vars")(),
+        require("postcss-extend")(),
+        require("postcss-nested")(),
+        require("postcss-mixins")(),
+        require("postcss-strip-inline-comments")(),
+    ],
+    "parser": "postcss-scss",
+    "local-plugins": true,
+};
