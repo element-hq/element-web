1b6735d7292e7e0ee6a4331f687818b1f5fb4506
Richard van der Hoff <richard@matrix.org>
2017-01-19 12:51:20 +0000

    Configure travis to test riot-web after building

---
+++ .travis-test-riot.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# script which is run by the travis build (after `npm run test`).
+#
+# clones riot-web develop and runs the tests against our version of react-sdk.
+
+set -ev
+
+git clone --depth=1 https://github.com/vector-im/riot-web.git riot-web
+cd riot-web
+mkdir node_modules
+ln -s ../.. node_modules/matrix-react-sdk
+npm install
+(cd node_modules/matrix-js-sdk && npm install)
+npm run test
