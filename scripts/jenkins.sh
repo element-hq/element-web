541eb45e1aeb8f75b8dcd271a23bfdce5b52d7b8
Richard van der Hoff <richard@matrix.org>
2017-02-03 15:06:03 +0000

    Jenkins: use default olm
    ... to stop us making a release and realising we forgot to bump the olm version
    in vector-web.

--- scripts/jenkins.sh
+++ scripts/jenkins.sh
@@ -11,13 +11,18 @@ set -x
 npm install
 
 # apparently npm 3.10.3 on node 6.4.0 doesn't upgrade #develop target with npm install unless explicitly asked.
-npm install matrix-react-sdk matrix-js-sdk
+npm install matrix-react-sdk matrix-js-sdk olm
 
 # install olm. A naive 'npm i ./olm/olm-*.tgz' fails because it uses the url
 # from our package.json (or even matrix-js-sdk's) in preference.
-tar -C olm -xz < olm/olm-*.tgz
-rm -r node_modules/olm
-cp -r olm/package node_modules/olm
+#
+# disabled for now, to avoid the annoying scenario of a release doing something
+# different to /develop. Instead, add it to the 'npm install' list above.
+# -- rav 2016/02/03
+#tar -C olm -xz < olm/olm-*.tgz
+#rm -r node_modules/olm
+#cp -r olm/package node_modules/olm
+
 
 # we may be using dev branches of js-sdk and react-sdk, in which case we need to build them
 (cd node_modules/matrix-js-sdk && npm install)
