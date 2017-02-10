4ac769168aa2a9035e83e6616bd747ba7ebed41c
Luke Barnard <lukeb@openmarket.com>
2017-02-10 17:17:58 +0000

    View /home on registered /w team

--- src/components/structures/login/Registration.js
+++ src/components/structures/login/Registration.js
@@ -227,6 +227,9 @@ module.exports = React.createClass({
                     const teamToken = data.team_token;
                     // Store for use /w welcome pages
                     window.localStorage.setItem('mx_team_token', teamToken);
+                    // Set the team token and view homepage
+                    window.matrixChat._teamToken = teamToken;
+                    window.mxDispatcher.dispatch({action: 'view_home_page'});
 
                     self._rtsClient.getTeam(teamToken).then((team) => {
                         console.log(
