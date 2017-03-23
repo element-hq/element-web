cbcb83052012a90d240f814d7bef1253a4951287
Tess Gadwa <info@yesexactly.com>
2017-03-22 18:40:02 -0700

    High Contrast Light Theme for Riot
    Based on feedback from Alan Day and others, refined the theming submitted in the original PR submitted March 14 to:
    
    - Make body text less heavy
    - Condense vertical spacing of text

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,7 @@ module.exports = {
         // CSS themes
         "theme-light": "./src/skins/vector/css/themes/light.scss",
         "theme-dark": "./src/skins/vector/css/themes/dark.scss",
+        "theme-contrastlight": "./src/skins/vector/css/themes/contrastlight.scss",
     },
     module: {
         preLoaders: [
