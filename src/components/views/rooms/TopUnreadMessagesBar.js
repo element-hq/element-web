106ce90916e8bd4c74af6bef0b179898043cac0b
Luke Barnard <lukeb@openmarket.com>
2017-04-04 16:31:28 +0100

    Change "Unread messages." to "Jump to first unread message."
    Also get rid of the "up" arrow so as not to indiciate direction. This is important because in future the RM will not be based on what has been paginated into the client (but instead RM will be handled server-side) and thus we cannot assert any kind of direction on it relative to the events in the viewport.

--- src/components/views/rooms/TopUnreadMessagesBar.js
+++ src/components/views/rooms/TopUnreadMessagesBar.js
@@ -32,10 +32,7 @@ module.exports = React.createClass({
             <div className="mx_TopUnreadMessagesBar">
                 <div className="mx_TopUnreadMessagesBar_scrollUp"
                         onClick={this.props.onScrollUpClick}>
-                    <img src="img/scrollup.svg" width="24" height="24"
-                        alt="Scroll to unread messages"
-                        title="Scroll to unread messages"/>
-                    Unread messages. <span style={{ textDecoration: 'underline' }} onClick={this.props.onCloseClick}>Mark all read</span>
+                    Jump to first unread message. <span style={{ textDecoration: 'underline' }} onClick={this.props.onCloseClick}>Mark all read</span>
                 </div>
                 <img className="mx_TopUnreadMessagesBar_close"
                     src="img/cancel.svg" width="18" height="18"
