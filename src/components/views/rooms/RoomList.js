ec6a1c4c750f959017cdf823402a6c9d86b16fe2
Matthew Hodgson <matthew@matrix.org>
2017-04-22 01:16:16 +0100

    recalculate roomlist when your invites change

--- src/components/views/rooms/RoomList.js
+++ src/components/views/rooms/RoomList.js
@@ -265,9 +265,16 @@ module.exports = React.createClass({
     },
 
     onRoomStateMember: function(ev, state, member) {
-        constantTimeDispatcher.dispatch(
-            "RoomTile.refresh", member.roomId, {}
-        );
+        if (ev.getStateKey() === MatrixClientPeg.get().credentials.userId && 
+            ev.getPrevContent() && ev.getPrevContent().membership === "invite")
+        {
+            this._delayedRefreshRoomList();
+        }
+        else {
+            constantTimeDispatcher.dispatch(
+                "RoomTile.refresh", member.roomId, {}
+            );
+        }
     },
 
     onRoomMemberName: function(ev, member) {
