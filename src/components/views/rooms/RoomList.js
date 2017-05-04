f86ca5bc97e915ae097f5e4eba459aba9e0973b8
David Baker <dave@matrix.org>
2017-05-04 18:08:04 +0100

    Hide empty tips if collapsed

--- src/components/views/rooms/RoomList.js
+++ src/components/views/rooms/RoomList.js
@@ -578,6 +578,12 @@ module.exports = React.createClass({
     },
 
     _getEmptyContent: function(section) {
+        const RoomDropTarget = sdk.getComponent('rooms.RoomDropTarget');
+
+        if (this.props.collapsed) {
+            return <RoomDropTarget label="" />;
+        }
+
         const RoleButton = sdk.getComponent('elements.RoleButton');
         if (this.state.totalRoomCount === 0) {
             const TintableSvg = sdk.getComponent('elements.TintableSvg');
@@ -598,7 +604,6 @@ module.exports = React.createClass({
                     </div>;
             }
         }
-        const RoomDropTarget = sdk.getComponent('rooms.RoomDropTarget');
 
         const labelText = 'Drop here to ' + (VERBS[section] || 'tag ' + section);
 
