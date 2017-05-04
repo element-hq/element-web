cb478f11941a1dd622e4a114aca8aa82cddaf302
Michael Telatynski <7t3chguy@gmail.com>
2017-05-04 15:50:52 +0100

    no idea why those got in there...
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- src/components/structures/UserSettings.js
+++ src/components/structures/UserSettings.js
@@ -628,8 +628,8 @@ module.exports = React.createClass({
                 <h3>Cryptography</h3>
                 <div className="mx_UserSettings_section mx_UserSettings_cryptoSection">
                     <ul>
-                        <li><label>Device ID:</label>                       <span><code>{deviceId}</code></span></li>
-                        <li><label>Device key:</label>                      <span><code><b>{identityKey}</b></code></span></li>
+                        <li><label>Device ID:</label>             <span><code>{deviceId}</code></span></li>
+                        <li><label>Device key:</label>            <span><code><b>{identityKey}</b></code></span></li>
                     </ul>
                     { importExportButtons }
                 </div>
