f54708227e0eb624dc178992c134519e42c43dfa
Michael Telatynski <7t3chguy@gmail.com>
2017-05-05 16:54:55 +0100

    Proper two package.json format, release.sh bumps 2nd json ver too
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- release.sh
+++ release.sh
@@ -9,4 +9,17 @@ set -e
 
 cd `dirname $0`
 
+
+# bump Electron's package.json first
+release="${1#v}"
+tag="v${release}"
+echo "electron npm version"
+
+cd electron
+npm version --no-git-tag-version "$release"
+git commit package.json -m "$tag"
+
+
+cd ..
+
 exec ./node_modules/matrix-js-sdk/release.sh -z "$@"
