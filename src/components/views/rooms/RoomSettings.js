50092a0f1f432ad8b7aed33120765b264e32e7cb
Michael Telatynski <7t3chguy@gmail.com>
2017-05-10 15:16:49 +0100

    fixes vector-im/riot-web#3881
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- src/components/views/rooms/RoomSettings.js
+++ src/components/views/rooms/RoomSettings.js
@@ -926,7 +926,7 @@ module.exports = React.createClass({
                         <PowerSelector ref="ban" value={ban_level} controlled={false} disabled={!can_change_levels || current_user_level < ban_level} onChange={this.onPowerLevelsChanged}/>
                     </div>
                     <div className="mx_RoomSettings_powerLevel">
-                        <span className="mx_RoomSettings_powerLevelKey">To redact messages, you must be a </span>
+                        <span className="mx_RoomSettings_powerLevelKey">To redact other users' messages, you must be a </span>
                         <PowerSelector ref="redact" value={redact_level} controlled={false} disabled={!can_change_levels || current_user_level < redact_level} onChange={this.onPowerLevelsChanged}/>
                     </div>
 
