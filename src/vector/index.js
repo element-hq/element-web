90628683f98ba92a2c39e5de19f306ab5b5353b1
daniel tygel <dtygel@eita.org.br>
2017-05-22 13:14:16 -0300

    build fallback for themes and add new prop for MatrixChat

--- src/vector/index.js
+++ src/vector/index.js
@@ -228,6 +228,37 @@ function onLoadCompleted() {
     }
 }
 
+function getThemeStyleElements() {
+    // look for the stylesheet elements.
+    // styleElements is a map from style name to HTMLLinkElement.
+    var styleElements = Object.create(null);
+    var i, a;
+    for (i = 0; (a = document.getElementsByTagName("link")[i]); i++) {
+        var href = a.getAttribute("href");
+        // shouldn't we be using the 'title' tag rather than the href?
+        var match = href.match(/^bundles\/.*\/theme-(.*)\.css$/);
+        if (match) {
+            styleElements[match[1]] = a;
+        }
+    }
+    return styleElements;
+}
+
+function filterValidThemes(themes) {
+    if (!themes) {
+        return;
+    }
+    var validThemes = [];
+    
+    var styleElements = getThemeStyleElements();
+    themes.forEach(function(configTheme) {
+	    if (configTheme.value in styleElements) {
+            validThemes.push(configTheme);
+	    }
+    });
+    return validThemes;
+}
+
 
 async function loadApp() {
     const fragparts = parseQsFromFragment(window.location);
@@ -263,10 +294,11 @@ async function loadApp() {
         configError = e;
     }
     
+    configJson.themes = filterValidThemes(configJson.themes);
+    
     if (!configJson.themes) {
         configJson.themes = [{"label":"Light theme", "value": "light"}];
     }
-    configJson.themes = [{"label":"Light theme", "value": "dark"}, {"label":"Light themeeee", "value": "lightttt"}];
 
     if (window.localStorage && window.localStorage.getItem('mx_accepts_unsupported_browser')) {
         console.log('User has previously accepted risks in using an unsupported browser');
@@ -288,6 +320,7 @@ async function loadApp() {
                 makeRegistrationUrl={makeRegistrationUrl}
                 ConferenceHandler={VectorConferenceHandler}
                 config={configJson}
+                themeStyleElements={getThemeStyleElements()}
                 realQueryParams={params}
                 startingFragmentQueryParams={fragparts.params}
                 enableGuest={true}
