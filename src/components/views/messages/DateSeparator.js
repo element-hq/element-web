d688eade43bb1eac7682351f70edf217841a7fa4
David Baker <dave@matrix.org>
2017-05-26 13:34:18 +0100

    Remove commented use strict
    It's even less use in a comment

--- src/components/views/messages/DateSeparator.js
+++ src/components/views/messages/DateSeparator.js
@@ -14,8 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// 'use strict';
-
 import React from 'react';
 import { _t } from 'matrix-react-sdk/lib/languageHandler';
 import DateUtils from 'matrix-react-sdk/lib/DateUtils';
