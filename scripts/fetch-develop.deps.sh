9c94f2ac14e94d6b7a237faec696c7d56ed2d3b9
David Baker <dave@matrix.org>
2017-06-07 10:39:46 +0100

    Use bash magic instead of sed

--- scripts/fetch-develop.deps.sh
+++ scripts/fetch-develop.deps.sh
@@ -28,7 +28,7 @@ fi
 # Chop 'origin' off the start as jenkins ends up using
 # branches on the origin, but this doesn't work if we
 # specify the branch when cloning.
-curbranch=`echo "$curbranch" | sed -e 's/^origin\///'`
+curbranch=${curbranch#origin/}
 
 echo "Determined branch to be $curbranch"
 
