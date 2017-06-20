a5fffe4afa8367b6522036eb7a144a5ad506157a
Michael Telatynski <7t3chguy@googlemail.com>
2017-06-20 20:15:25 +0100

    fix ambiguity

--- src/BasePlatform.js
+++ src/BasePlatform.js
@@ -17,7 +17,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import dis from 'dispatcher';
+import dis from './dispatcher';
 
 /**
  * Base class for classes that provide platform-specific functionality
