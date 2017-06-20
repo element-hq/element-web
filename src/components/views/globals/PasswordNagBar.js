c4a3211cfc42ed42b003c08eda5a524e82a6296e
Luke Barnard <lukeb@openmarket.com>
2017-06-20 16:28:40 +0100

    Use _tJsx for PasswordNagBar (because it has <u>)

--- src/components/views/globals/PasswordNagBar.js
+++ src/components/views/globals/PasswordNagBar.js
@@ -20,7 +20,7 @@ import React from 'react';
 import sdk from 'matrix-react-sdk';
 import Modal from 'matrix-react-sdk/lib/Modal';
 import dis from 'matrix-react-sdk/lib/dispatcher';
-import { _t } from 'matrix-react-sdk/lib/languageHandler';
+import { _t, _tJsx } from 'matrix-react-sdk/lib/languageHandler';
 
 export default React.createClass({
     onUpdateClicked: function() {
@@ -49,7 +49,11 @@ export default React.createClass({
                     alt="Warning"
                 />
                 <div className="mx_MatrixToolbar_content">
-                    { _t("To return to your account in future you need to <u>set a password</u>") }
+                    { _tJsx(
+                        "To return to your account in future you need to <u>set a password</u>",
+                        /<u>(.*?)<\/u>/,
+                        (sub) => { return <u>{ sub }</u>; },
+                    ) }
                 </div>
                 <button className="mx_MatrixToolbar_action">
                     { _t("Set Password") }
