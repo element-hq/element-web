c033d5defd02aafad7ba292f6d8648fdf3065111
David Baker <dave@matrix.org>
2017-06-27 09:58:29 +0100

    Missing React import

--- src/components/structures/GroupView.js
+++ src/components/structures/GroupView.js
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+import React from 'react';
 import MatrixClientPeg from '../../MatrixClientPeg';
 import sdk from '../../index';
 import { sanitizedHtmlNode } from '../../HtmlUtils';
