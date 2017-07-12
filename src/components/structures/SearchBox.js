4fe4e10abb55d4735ecb3159731c77ea4f829981
Luke Barnard <lukeb@openmarket.com>
2017-07-12 13:49:57 +0100

    Implement the focus_room_filter action
    This is for ctrl+k room filtering and switching

--- src/components/structures/SearchBox.js
+++ src/components/structures/SearchBox.js
@@ -46,18 +46,22 @@ module.exports = React.createClass({
     },
 
     onAction: function(payload) {
-        // Disabling this as I find it really really annoying, and was used to the
-        // previous behaviour - see https://github.com/vector-im/riot-web/issues/3348
-/*        
         switch (payload.action) {
+/*      Disabling this as I find it really really annoying, and was used to the
+        previous behaviour - see https://github.com/vector-im/riot-web/issues/3348
             // Clear up the text field when a room is selected.
             case 'view_room':
                 if (this.refs.search) {
                     this._clearSearch();
                 }
                 break;
+*/
+            case 'focus_room_filter':
+                if (this.refs.search) {
+                    this.refs.search.focus();
+                }
+                break;
         }
-*/        
     },
 
     onChange: function() {
