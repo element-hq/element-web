bac22cfb7cdf5f1c78b2ef5d63e92fcd291c7129
Luke Barnard <lukeb@openmarket.com>
2017-07-20 18:04:12 +0100

    Re-add removal of (IRC) suffix

--- src/components/views/rooms/MessageComposerInput.js
+++ src/components/views/rooms/MessageComposerInput.js
@@ -290,7 +290,7 @@ export default class MessageComposerInput extends React.Component {
                 // paths for inserting a user pill is not fun
                 const selection = this.state.editorState.getSelection();
                 const member = this.props.room.getMember(payload.user_id);
-                const completion = member ? member.name : payload.user_id;
+                const completion = member ? member.name.replace(' (IRC)', '') : payload.user_id;
                 this.setDisplayedCompletion({
                     completion,
                     selection,
