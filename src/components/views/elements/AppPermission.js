70824960ee6b7618409031161db3c97629b9cad4
Robert Swain <rob@matrix.org>
2017-08-21 10:23:55 +0200

    AppPermission: Make strings translatable

--- src/components/views/elements/AppPermission.js
+++ src/components/views/elements/AppPermission.js
@@ -50,7 +50,7 @@ export default class AppPermission extends React.Component {
         let e2eWarningText;
         if (this.props.isRoomEncrypted) {
             e2eWarningText =
-                <span className='mx_AppPermissionWarningTextLabel'>NOTE: Apps are not end-to-end encrypted</span>;
+                <span className='mx_AppPermissionWarningTextLabel'>{_t('NOTE: Apps are not end-to-end encrypted')}</span>;
         }
         return (
             <div className='mx_AppPermissionWarning'>
@@ -58,7 +58,7 @@ export default class AppPermission extends React.Component {
                     <img src='img/warning.svg' alt={_t('Warning!')}/>
                 </div>
                 <div className='mx_AppPermissionWarningText'>
-                    <span className='mx_AppPermissionWarningTextLabel'>Do you want to load widget from URL:</span> <span className='mx_AppPermissionWarningTextURL'>{this.state.curlBase}</span>
+                    <span className='mx_AppPermissionWarningTextLabel'>{_t('Do you want to load widget from URL:')}</span> <span className='mx_AppPermissionWarningTextURL'>{this.state.curlBase}</span>
                     {e2eWarningText}
                 </div>
                 <input
