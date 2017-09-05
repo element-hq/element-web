87e1addf1ee72b502048177ae38dc8a08b0ecfbc
David Baker <dave@matrix.org>
2017-09-05 13:34:40 +0100

    rm -r the bundles dir as it might not be empty

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -13,6 +13,7 @@ import os.path
 import subprocess
 import sys
 import tarfile
+import shutil
 
 try:
     # python3
@@ -108,9 +109,9 @@ class Deployer:
             extracted_bundles = os.path.join(extracted_dir, 'bundles')
             move_bundles(source=extracted_bundles, dest=self.bundles_path)
 
-            # replace the (hopefully now empty) extracted_bundles dir with a
-            # symlink to the common dir.
-            os.rmdir(extracted_bundles)
+            # replace the extracted_bundles dir (which may not be empty if some
+            # bundles were skipped) with a symlink to the common dir.
+            shutil.rmtree(extracted_bundles)
             create_relative_symlink(
                 target=self.bundles_path,
                 linkname=extracted_bundles,
