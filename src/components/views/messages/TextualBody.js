876257f4e219acc06bd5097dd4bd71eb5403ebd2
David Baker <dave@matrix.org>
2017-09-10 14:23:33 +0100

    Consolidate the code copy button
    Adding the code code button was done by manipulating the HTML of
    the event body to add a span tag, then adding the onclick handler
    after the thing was mounted. Apart from splitting the code between
    two places, adding the span tag was, according to Chrome's
    profiler, taking up quite a lot of CPU cycles (apparently as soon
    as you set the innerHTML on a div). Instead, just build the whole
    lot together after the component mounts.

--- src/components/views/messages/TextualBody.js
+++ src/components/views/messages/TextualBody.js
@@ -113,14 +113,17 @@ module.exports = React.createClass({
                     }
                 }, 10);
             }
-            // add event handlers to the 'copy code' buttons
-            const buttons = ReactDOM.findDOMNode(this).getElementsByClassName("mx_EventTile_copyButton");
-            for (let i = 0; i < buttons.length; i++) {
-                buttons[i].onclick = (e) => {
-                    const copyCode = buttons[i].parentNode.getElementsByTagName("code")[0];
+
+            // Add 'copy' buttons to pre blocks
+            ReactDOM.findDOMNode(this).querySelectorAll('.mx_EventTile_body pre').forEach((p) => {
+                const button = document.createElement("span");
+                button.className = "mx_EventTile_copyButton";
+                button.onclick = (e) => {
+                    const copyCode = button.parentNode.getElementsByTagName("code")[0];
                     this.copyToClipboard(copyCode.textContent);
                 };
-            }
+                p.appendChild(button);
+            });
         }
     },
 
