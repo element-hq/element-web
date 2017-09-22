0f9f1c85a80232d483403b6820472b25fc0c7124
David Baker <dave@matrix.org>
2017-09-22 14:58:49 +0100

    Remove done todo

--- src/components/views/groups/GroupMemberList.js
+++ src/components/views/groups/GroupMemberList.js
@@ -90,7 +90,6 @@ export default withMatrixClient(React.createClass({
         let memberList = this.state.members;
         if (query) {
             memberList = memberList.filter((m) => {
-                // TODO: add this when we have this info from the API
                 const matchesName = m.displayname.toLowerCase().indexOf(query) !== -1;
                 const matchesId = m.userId.toLowerCase().includes(query);
 
