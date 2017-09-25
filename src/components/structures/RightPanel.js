bfdb92ff6b87caf7762f1379eae8179087d85c1a
Luke Barnard <lukeb@openmarket.com>
2017-09-25 16:13:54 +0100

    Nit: Re-order property

--- src/components/structures/RightPanel.js
+++ src/components/structures/RightPanel.js
@@ -246,8 +246,8 @@ module.exports = React.createClass({
                     analytics={['Right Panel', 'Member List Button', 'click']}
                 />,
                 <HeaderButton key="_filesButton" title={_t('Files')} iconSrc="img/icons-files.svg"
-                    clickPhase={this.Phase.FilePanel}
                     isHighlighted={this.state.phase === this.Phase.FilePanel}
+                    clickPhase={this.Phase.FilePanel}
                     analytics={['Right Panel', 'File List Button', 'click']}
                 />,
                 <HeaderButton key="_notifsButton" title={_t('Notifications')} iconSrc="img/icons-notifications.svg"
