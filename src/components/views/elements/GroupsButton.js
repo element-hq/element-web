c8b215815655397ed831e0052e1692277b473d3b
Luke Barnard <lukeb@openmarket.com>
2017-09-29 10:42:23 +0100

    Lint

--- src/components/views/elements/GroupsButton.js
+++ src/components/views/elements/GroupsButton.js
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import React from 'react';
 import sdk from '../../../index';
 import PropTypes from 'prop-types';
 import { _t } from '../../../languageHandler';
@@ -23,7 +22,7 @@ const GroupsButton = function(props) {
     const ActionButton = sdk.getComponent('elements.ActionButton');
     return (
         <ActionButton action="view_my_groups"
-            label={ _t("Groups") }
+            label={_t("Groups")}
             iconPath="img/icons-groups.svg"
             size={props.size}
             tooltip={props.tooltip}
