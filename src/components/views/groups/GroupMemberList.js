14eee9a37a343f04ed46f524e03c740f85ef79c0
Luke Barnard <lukeb@openmarket.com>
2017-10-16 17:59:53 +0100

    Only show invited section if there are invited group members

--- src/components/views/groups/GroupMemberList.js
+++ src/components/views/groups/GroupMemberList.js
@@ -161,10 +161,11 @@ export default withMatrixClient(React.createClass({
             { this.makeGroupMemberTiles(this.state.searchQuery, this.state.members) }
         </div> : <div />;
 
-        const invited = this.state.invitedMembers ? <div className="mx_MemberList_invited">
-            <h2>{ _t("Invited") }</h2>
-            { this.makeGroupMemberTiles(this.state.searchQuery, this.state.invitedMembers) }
-        </div> : <div />;
+        const invited = (this.state.invitedMembers || this.state.invitedMembers.length > 0) ?
+            <div className="mx_MemberList_invited">
+                <h2>{ _t("Invited") }</h2>
+                { this.makeGroupMemberTiles(this.state.searchQuery, this.state.invitedMembers) }
+            </div> : <div />;
 
         return (
             <div className="mx_MemberList">
