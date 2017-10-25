0561c5bd4f785d1f2f1d7ccc3399823e9205c0ad
Luke Barnard <lukeb@openmarket.com>
2017-10-25 10:29:57 +0100

    Swap ui_opacity for panel_disabled
    Simplify the API for disabling panels in the UI. `mx_fadable_faded` is applied instead of setting opacity.

--- src/components/structures/RightPanel.js
+++ src/components/structures/RightPanel.js
@@ -18,6 +18,7 @@ limitations under the License.
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import { _t } from 'matrix-react-sdk/lib/languageHandler';
 import sdk from 'matrix-react-sdk';
 import dis from 'matrix-react-sdk/lib/dispatcher';
@@ -345,18 +346,16 @@ module.exports = React.createClass({
             );
         }
 
-        let classes = "mx_RightPanel mx_fadable";
-        if (this.props.collapsed) {
-            classes += " collapsed";
-        }
+        let classes = classNames(
+            "mx_RightPanel", "mx_fadable",
+            {
+                "collapsed": this.props.collapsed,
+                "mx_fadable_faded": this.props.disabled,
+            }
+        );
 
         return (
-            <aside className={classes}
-                style={{
-                    opacity: this.props.opacity,
-                    pointerEvents: this.props.opacity < 1.0 ? 'none' : undefined,
-                }}
-            >
+            <aside className={classes}>
                 <div className="mx_RightPanel_header">
                     <div className="mx_RightPanel_headerButtonGroup">
                         {headerButtons}
