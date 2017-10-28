672fbb287316641bdcccf0b6314a40bd165f23a9
Matthew Hodgson <matthew@matrix.org>
2017-10-28 18:33:38 +0100

    hopefully fix NPE on toLowerCase

--- src/components/structures/login/Registration.js
+++ src/components/structures/login/Registration.js
@@ -303,7 +303,9 @@ module.exports = React.createClass({
         } : {};
 
         return this._matrixClient.register(
-            this.state.formVals.username.toLowerCase(),
+            (this.state.formVals.username ?
+                this.state.formVals.username.toLowerCase() : 
+                this.state.formVals.username),
             this.state.formVals.password,
             undefined, // session id: included in the auth dict already
             auth,
