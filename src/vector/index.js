ccf52b2a69ff656c622e266f031f2115186bf4a0
Travis Ralston <travpc@gmail.com>
2017-10-29 17:05:27 -0600

    Convert local settings to granular settings
    Signed-off-by: Travis Ralston <travpc@gmail.com>

--- src/vector/index.js
+++ src/vector/index.js
@@ -67,7 +67,6 @@ sdk.loadSkin(require('../component-index'));
 var VectorConferenceHandler = require('../VectorConferenceHandler');
 import Promise from 'bluebird';
 var request = require('browser-request');
-import * as UserSettingsStore from 'matrix-react-sdk/lib/UserSettingsStore';
 import * as languageHandler from 'matrix-react-sdk/lib/languageHandler';
 // Also import _t directly so we can call it just `_t` as this is what gen-i18n.js expects
 import { _t } from 'matrix-react-sdk/lib/languageHandler';
@@ -78,6 +77,7 @@ import {parseQs, parseQsFromFragment} from './url_utils';
 import Platform from './platform';
 
 import MatrixClientPeg from 'matrix-react-sdk/lib/MatrixClientPeg';
+import SettingsStore from "../../../matrix-react-sdk/src/settings/SettingsStore";
 
 var lastLocationHashSet = null;
 
@@ -328,7 +328,7 @@ async function loadApp() {
 }
 
 async function loadLanguage() {
-    const prefLang = UserSettingsStore.getLocalSetting('language');
+    const prefLang = SettingsStore.getValue("language");
     let langs = [];
 
     if (!prefLang) {
