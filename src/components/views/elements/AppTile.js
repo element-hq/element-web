e87940f63cf2460a0bccfcdf7b31fff5b40a7755
Richard Lewis <richard@smetco.co.uk>
2017-11-14 19:53:32 +0000

    Make app tile title stronger

--- src/components/views/elements/AppTile.js
+++ src/components/views/elements/AppTile.js
@@ -381,7 +381,7 @@ export default React.createClass({
         return (
             <div className={this.props.fullWidth ? "mx_AppTileFullWidth" : "mx_AppTile"} id={this.props.id}>
                 <div ref="menu_bar" className="mx_AppTileMenuBar" onClick={this.onClickMenuBar}>
-                    { this.formatAppTileName() }
+                    <b>{ this.formatAppTileName() }</b>
                     <span className="mx_AppTileMenuBarWidgets">
                         { /* Edit widget */ }
                         { showEditButton && <img
