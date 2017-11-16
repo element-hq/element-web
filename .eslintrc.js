c429b13b05688eb0eba54ad6187d9039e5268b48
Luke Barnard <luke@matrix.org>
2017-11-16 13:18:58 +0000

    Add eslint rule keyword-spacing
    Because we follow it almost all of the time.

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,6 +29,10 @@ module.exports = {
         // so we replace it with a version that is class property aware
         "babel/no-invalid-this": "error",
 
+        // We appear to follow this most of the time, so let's enforce it instead
+        // of occasionally following it (or catching it in review)
+        "keyword-spacing": "error",
+
         /** react **/
         // This just uses the react plugin to help eslint known when
         // variables have been used in JSX
