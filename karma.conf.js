0f3d045e013335ec9d779a56f3d9cab95001cef4
Richard Lewis <richard@smetco.co.uk>
2017-11-16 12:11:41 +0000

    Upgrade karma-webpack and fix karma config.

--- karma.conf.js
+++ karma.conf.js
@@ -33,8 +33,8 @@ const olm_entry = webpack_config.entry['olm'];
 delete webpack_config['entry'];
 
 // add ./test as a search path for js
-webpack_config.module.loaders.unshift({
-    test: /\.js$/, loader: "babel",
+webpack_config.module.rules.unshift({
+    test: /\.js$/, loader: "babel-loader",
     include: [path.resolve('./src'), path.resolve('./test')],
 });
 
@@ -46,7 +46,8 @@ webpack_config.module.noParse.push(/sinon\/pkg\/sinon\.js$/);
 // ?
 webpack_config.resolve.alias['sinon'] = 'sinon/pkg/sinon.js';
 
-webpack_config.resolve.root = [
+webpack_config.resolve.modules = [
+    path.resolve('./node_modules'),
     path.resolve('./test'),
 ];
 
