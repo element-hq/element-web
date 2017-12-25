3f069af0670c4df445d8e833c9a96d3ed09315d3
Travis Ralston <travpc@gmail.com>
2017-12-25 14:33:46 -0700

    Also remove the setting
    Signed-off-by: Travis Ralston <travpc@gmail.com>

--- src/settings/Settings.js
+++ src/settings/Settings.js
@@ -88,12 +88,6 @@ export const SETTINGS = {
         supportedLevels: LEVELS_FEATURE,
         default: false,
     },
-    "feature_presence_management": {
-        isFeature: true,
-        displayName: _td("Presence Management"),
-        supportedLevels: LEVELS_FEATURE,
-        default: false,
-    },
     "MessageComposerInput.dontSuggestEmoji": {
         supportedLevels: LEVELS_ACCOUNT_SETTINGS,
         displayName: _td('Disable Emoji suggestions while typing'),
