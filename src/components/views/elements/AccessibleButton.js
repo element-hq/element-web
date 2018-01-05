cf472c791d580ccc0f5b3e419ec3f69e49ec50e3
Peter Vágner <pvdeejay@gmail.com>
2018-01-05 11:45:45 +0100

    Consume all combinations of space / enter, keyDown / keyUp presses and try to explain this key handling inconsistency with some additional comments as per the review discussion.

--- src/components/views/elements/AccessibleButton.js
+++ src/components/views/elements/AccessibleButton.js
@@ -32,11 +32,19 @@ export default function AccessibleButton(props) {
     // We need to consume enter onKeyDown and space onKeyUp
     // otherwise we are risking also activating other keyboard focusable elements
     // that might receive focus as a result of the AccessibleButtonClick action
+    // It's because we are using html buttons at a few places e.g. inside dialogs
+    // And divs which we report as role button to assistive technologies.
+    // Browsers handle space and enter keypresses differently and we are only adjusting to the
+    // inconsistencies here
     restProps.onKeyDown = function(e) {
         if (e.keyCode === KeyCode.ENTER) {
             e.stopPropagation();
             e.preventDefault();
             return onClick(e);
+        }
+        if (e.keyCode === KeyCode.SPACE) {
+            e.stopPropagation();
+            e.preventDefault();
         }
     };
     restProps.onKeyUp = function(e) {
@@ -44,6 +52,10 @@ export default function AccessibleButton(props) {
             e.stopPropagation();
             e.preventDefault();
             return onClick(e);
+        }
+        if (e.keyCode === KeyCode.ENTER) {
+            e.stopPropagation();
+            e.preventDefault();
         }
     };
     restProps.tabIndex = restProps.tabIndex || "0";
