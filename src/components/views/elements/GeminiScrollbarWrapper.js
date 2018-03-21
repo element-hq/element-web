944940057eb25b9af4a1d1f5628e5251aa9efcb0
Luke Barnard <luke@matrix.org>
2018-03-21 17:23:19 +0000

    Remember that React needs React

--- src/components/views/elements/GeminiScrollbarWrapper.js
+++ src/components/views/elements/GeminiScrollbarWrapper.js
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+import React from 'react';
 import GeminiScrollbar from 'react-gemini-scrollbar';
 
 function GeminiScrollbarWrapper(props) {
