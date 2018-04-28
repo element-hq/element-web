bbf4d3e39f6d52851ed4ca45f9f70d884566f365
Michael Telatynski <7t3chguy@gmail.com>
2018-04-29 00:34:27 +0100

    add comment
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- src/components/structures/RoomView.js
+++ src/components/structures/RoomView.js
@@ -917,6 +917,7 @@ module.exports = React.createClass({
         ContentMessages.sendContentToRoom(
             file, this.state.room.roomId, MatrixClientPeg.get(),
         ).done(() => {
+            // Send message_sent callback, for things like _checkIfAlone because after all a file is still a message.
             dis.dispatch({
                 action: 'message_sent',
             });
