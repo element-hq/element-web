825d610938bcb30e509639782c1b4ce34ebe785d
David Baker <dave@matrix.org>
2018-05-02 11:13:16 +0100

    Bah, no pushd.
    Also this npm install should now be unnecessary

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -6,9 +6,9 @@ scripts/fetchdep.sh matrix-org matrix-js-sdk
 rm -r node_modules/matrix-js-sdk || true
 ln -s matrix-js-sdk node_modules/matrix-js-sdk
 
-pushd node_modules/matrix-js-sdk
+cd node_modules/matrix-js-sdk
 npm install
-popd
+cd ../..
 
 npm run test
 ./.travis-test-riot.sh
