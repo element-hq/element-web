9bccecf449533b5d48552a13f1e845983acc54c1
David Baker <dave@matrix.org>
2018-05-02 11:23:57 +0100

    Get symlink right
    Also No need to cd into the symlink, can just go straight there

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -4,11 +4,11 @@ set -ex
 
 scripts/fetchdep.sh matrix-org matrix-js-sdk
 rm -r node_modules/matrix-js-sdk || true
-ln -s matrix-js-sdk node_modules/matrix-js-sdk
+ln -s ../matrix-js-sdk node_modules/matrix-js-sdk
 
-cd node_modules/matrix-js-sdk
+cd matrix-js-sdk
 npm install
-cd ../..
+cd ..
 
 npm run test
 ./.travis-test-riot.sh
