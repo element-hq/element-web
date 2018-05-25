7fe2bfc2120e4f739e70c43b905829c83a572f90
Matthew Hodgson <matthew@matrix.org>
2018-05-25 16:30:38 +0100

    remove numbers

--- src/components/structures/RoomSubList.js
+++ src/components/structures/RoomSubList.js
@@ -297,13 +297,13 @@ var RoomSubList = React.createClass({
             <div className="mx_RoomSubList_labelContainer" title={ title } ref="header">
                 <AccessibleButton onClick={ this.onClick } className="mx_RoomSubList_label" tabIndex={tabindex}>
                     { this.props.collapsed ? '' : this.props.label }
-                    <div className="mx_RoomSubList_roomCount">{ roomCount }</div>
                     <div className={chevronClasses}></div>
                     { badge }
                     { incomingCall }
                 </AccessibleButton>
             </div>
         );
+        // <div className="mx_RoomSubList_roomCount">{ roomCount }</div>
     },
 
     _createOverflowTile: function(overflowCount, totalCount) {
