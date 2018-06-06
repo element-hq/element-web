bf0d03bce364521e760e37ec8ccdf2b0b16a114e
Akihiko Odaki <nekomanma@pixiv.co.jp>
2018-06-06 16:30:24 +0900

    Import react as React in src/components/views/elements/DNDTagTile.js
    Signed-off-by: Akihiko Odaki <nekomanma@pixiv.co.jp>

--- src/components/views/elements/DNDTagTile.js
+++ src/components/views/elements/DNDTagTile.js
@@ -17,6 +17,7 @@ limitations under the License.
 
 import TagTile from './TagTile';
 
+import React from 'react';
 import { Draggable } from 'react-beautiful-dnd';
 
 export default function DNDTagTile(props) {
