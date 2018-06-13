22bec0d67d6928afedeaca9be43b45c924d3f639
Michael Telatynski <7t3chguy@gmail.com>
2018-06-13 09:13:32 +0100

    allow CreateRoom to scale properly horizontally
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- src/components/views/dialogs/CreateRoomDialog.js
+++ src/components/views/dialogs/CreateRoomDialog.js
@@ -52,8 +52,8 @@ export default React.createClass({
                         <div className="mx_CreateRoomDialog_label">
                             <label htmlFor="textinput"> { _t('Room name (optional)') } </label>
                         </div>
-                        <div>
-                            <input id="textinput" ref="textinput" className="mx_CreateRoomDialog_input" autoFocus={true} size="64" />
+                        <div className="mx_CreateRoomDialog_input_container">
+                            <input id="textinput" ref="textinput" className="mx_CreateRoomDialog_input" autoFocus={true} />
                         </div>
                         <br />
 
