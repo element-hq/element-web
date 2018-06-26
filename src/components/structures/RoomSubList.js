abd37d141e371a53d9a0c85f03a8e18de17ddb5d
Michael Telatynski <7t3chguy@gmail.com>
2018-06-26 07:54:38 +0100

    clear up if statement to make it more readable
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- src/components/structures/RoomSubList.js
+++ src/components/structures/RoomSubList.js
@@ -389,16 +389,14 @@ const RoomSubList = React.createClass({
                 </Droppable> : subListContent;
         } else {
             const Loader = sdk.getComponent("elements.Spinner");
-            if (this.props.showSpinner && !this.state.hidden) {
+            if (this.props.showSpinner) {
                 content = <Loader />;
-            } else if (this.state.hidden) {
-                content = undefined;
-            } // else show content calculated above
+            }
 
             return (
                 <div className="mx_RoomSubList">
                     {this.props.alwaysShowHeader ? this._getHeaderJsx() : undefined}
-                    { content }
+                    { this.state.hidden ? undefined : content }
                 </div>
             );
         }
