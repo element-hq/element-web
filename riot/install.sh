7ecd7d387325fe809f1b0bc2c19e2450333b1e05
Bruno Windels <brunow@matrix.org>
2018-07-20 18:50:05 +0200

    add template config file for riot installation

--- riot/install.sh
+++ riot/install.sh
@@ -6,6 +6,7 @@ curl -L https://github.com/vector-im/riot-web/archive/${RIOT_BRANCH}.zip --outpu
 unzip riot.zip
 rm riot.zip
 mv riot-web-${RIOT_BRANCH} riot-web
+cp config-template/config.json riot-web/
 pushd riot-web
 npm install
 npm run build
