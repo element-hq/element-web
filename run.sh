410b32ff859bcb63427d6365d78f896cbeda19d6
Bruno Windels <brunow@matrix.org>
2018-07-20 19:00:27 +0200

    make script runnable in one terminal, without server output garbling up test results. This won't work well on CI server but makes it clear to run locally

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
-sh synapse/clear.sh
-sh synapse/start.sh
-sh riot/start.sh
-node start.js
-sh riot/stop.sh
-sh synapse/stop.sh
+tmux \
+	new-session "sh riot/stop.sh; sh synapse/stop.sh; sh synapse/clear.sh; sh synapse/start.sh; sh riot/start.sh; read"\; \
+	split-window "sleep 5; node start.js; sh riot/stop.sh; sh synapse/stop.sh; read"\; \
+	select-layout even-vertical
