048a3670811ea30cf8863b45ef65a440ffd74c1b
Bruno Windels <brunow@matrix.org>
2018-07-23 10:21:38 +0200

    use in-memory database, faster and no need to clear before every run

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 tmux \
-	new-session "sh riot/stop.sh; sh synapse/stop.sh; sh synapse/clear.sh; sh synapse/start.sh; sh riot/start.sh; read"\; \
+	new-session "sh riot/stop.sh; sh synapse/stop.sh; sh synapse/start.sh; sh riot/start.sh; read"\; \
 	split-window "sleep 5; node start.js; sh riot/stop.sh; sh synapse/stop.sh; read"\; \
 	select-layout even-vertical
--- synapse/clear.sh
+++
@@ -1,6 +0,0 @@
-BASE_DIR=$(realpath $(dirname $0))
-pushd $BASE_DIR
-pushd installations/consent
-rm homeserver.db
-popd
-popd
