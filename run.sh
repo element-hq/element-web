b3473a7220236f9af0ac0fcf4fc74b281ed89b5c
Bruno Windels <brunow@matrix.org>
2018-07-27 13:43:38 +0200

    with no logs polluting the output, we dont need tmux anymore to split the terminal

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
-tmux \
-	new-session "sh riot/stop.sh; sh synapse/stop.sh; sh synapse/start.sh; sh riot/start.sh; read"\; \
-	split-window "sleep 5; node start.js; sh riot/stop.sh; sh synapse/stop.sh; read"\; \
-	select-layout even-vertical
+sh synapse/start.sh
+sh riot/start.sh
+node start.js
+sh riot/stop.sh
+sh synapse/stop.sh
