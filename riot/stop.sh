758da7865941d4243ceacaa44f055cb7678c28bc
Bruno Windels <brunow@matrix.org>
2018-07-27 18:43:40 +0200

    dont fail when trying to stop riot and its not running

--- riot/stop.sh
+++ riot/stop.sh
@@ -2,5 +2,8 @@
 BASE_DIR=$(readlink -f $(dirname $0))
 cd $BASE_DIR
 PIDFILE=riot.pid
-kill $(cat $PIDFILE)
-rm $PIDFILE
+if [ -f $PIDFILE ]; then
+	echo "stopping riot server ..."
+	kill $(cat $PIDFILE)
+	rm $PIDFILE
+fi
