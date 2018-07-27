29d688543d34554c5b56c8ce78eb7491f8f51429
Bruno Windels <brunow@matrix.org>
2018-07-27 18:44:01 +0200

    stop servers on error in run script

--- riot/start.sh
+++ riot/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PORT=5000
-echo "running riot on http://localhost:$PORT..."
+echo "running riot on http://localhost:$PORT ..."
 BASE_DIR=$(readlink -f $(dirname $0))
 cd $BASE_DIR/
 pushd riot-web/webapp/ > /dev/null
--- run.sh
+++ run.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-set -e
+
+stop_servers() {
+	./riot/stop.sh
+	./synapse/stop.sh
+}
+
+handle_error() {
+	EXIT_CODE=$?
+	stop_servers
+	exit $EXIT_CODE
+}
+
+trap 'handle_error' ERR
+
 ./synapse/start.sh
 ./riot/start.sh
 node start.js
-./riot/stop.sh
-./synapse/stop.sh
+stop_servers
