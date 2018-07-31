97fa7e03d13bd73726bcbd0e6c556a8e336163a4
Bruno Windels <brunow@matrix.org>
2018-07-31 14:45:14 +0200

    dont swallow synapse startup errors

--- synapse/start.sh
+++ synapse/start.sh
@@ -3,4 +3,10 @@ BASE_DIR=$(readlink -f $(dirname $0))
 cd $BASE_DIR
 cd installations/consent
 source env/bin/activate
-./synctl start 2> /dev/null
+LOGFILE=$(mktemp)
+./synctl start 2> $LOGFILE
+EXIT_CODE=$?
+if [ $EXIT_CODE -ne 0 ]; then
+	cat $LOGFILE
+fi
+exit $EXIT_CODE
