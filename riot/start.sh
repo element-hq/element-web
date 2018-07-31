31fcf08fece786f7fc7c3fd96770037c0926dc2e
Bruno Windels <brunow@matrix.org>
2018-07-31 11:41:01 +0200

    only allow one riot server instance simultaneously

--- riot/start.sh
+++ riot/start.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 PORT=5000
-echo "running riot on http://localhost:$PORT ..."
 BASE_DIR=$(readlink -f $(dirname $0))
+PIDFILE=riot.pid
+CONFIG_BACKUP=config.e2etests_backup.json
+
 cd $BASE_DIR/
+
+if [ -f $PIDFILE ]; then
+	exit
+fi
+
+echo "running riot on http://localhost:$PORT ..."
 pushd riot-web/webapp/ > /dev/null
 python -m SimpleHTTPServer $PORT > /dev/null 2>&1 &
 PID=$!
 popd > /dev/null
-echo $PID > riot.pid
+echo $PID > $PIDFILE
