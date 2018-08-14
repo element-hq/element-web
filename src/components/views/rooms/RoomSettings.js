c736ae7efbe76c290e1fea43d5b338c5891485ee
David Baker <dave@matrix.org>
2018-08-14 16:53:34 +0100

    Copyright

--- src/components/views/rooms/RoomSettings.js
+++ src/components/views/rooms/RoomSettings.js
@@ -1,6 +1,7 @@
 /*
 Copyright 2015, 2016 OpenMarket Ltd
 Copyright 2017 Vector Creations Ltd
+Copyright 2018 New Vector Ltd
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
