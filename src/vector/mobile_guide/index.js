e23195fd8648d1ce2ed6790df9a0a6b135eb3f72
David Baker <dave@matrix.org>
2018-09-04 10:08:52 +0100

    Conditionally show custom server bit

--- src/vector/mobile_guide/index.js
+++ src/vector/mobile_guide/index.js
@@ -9,11 +9,15 @@ async function initPage() {
     document.getElementById('back_to_riot_button').onclick = onBackToRiotClick;
 
     const config = await getVectorConfig('..');
-    let hsUrl = 'https://matrix.org/';
+    let hsUrl;
     if (config && config['default_hs_url']) {
         hsUrl = config['default_hs_url'];
     }
-    document.getElementById('hs_url').innerHTML = hsUrl;
+    if (hsUrl && hsUrl !== 'https://matrix.org/') {
+        document.getElementById('step2_container').style.display = 'block';
+        document.getElementById('hs_url').innerHTML = hsUrl;
+        document.getElementById('step_login_header').innerHTML= 'Step 3: Register or Log in';
+    }
 }
 
 initPage();
