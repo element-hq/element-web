34d27954cfd6049c993431bcf0673f1f3ba600d1
David Baker <dbkr@users.noreply.github.com>
2018-09-05 18:03:48 +0100

    Revert "Don't rely on room members to query power levels"

--- src/components/views/rooms/MessageComposer.js
+++ src/components/views/rooms/MessageComposer.js
@@ -309,8 +309,8 @@ export default class MessageComposer extends React.Component {
                 </div>;
         }
 
-        const canSendMessages = !this.state.tombstone &&
-            this.props.room.maySendMessage();
+        const canSendMessages = !this.state.tombstone && this.props.room.currentState.maySendMessage(
+            MatrixClientPeg.get().credentials.userId);
 
         if (canSendMessages) {
             // This also currently includes the call buttons. Really we should
