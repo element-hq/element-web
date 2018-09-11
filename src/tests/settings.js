abc7c4c3acb334d6b300a1baa10f7338e0c3e89c
Bruno Windels <brunow@matrix.org>
2018-09-11 18:30:57 +0200

    join use cases that touch settings in one file, as selectors are similar

---
+++ src/tests/settings.js
@@ -0,0 +1,43 @@
+/*
+Copyright 2018 New Vector Ltd
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+const assert = require('assert');
+
+module.exports.enableLazyLoading = async function(session) {
+    session.log.step(`enables lazy loading of members in the lab settings`);
+    const settingsButton = await session.query('.mx_BottomLeftMenu_settings');
+    await settingsButton.click();
+    const llCheckbox = await session.waitAndQuery("#feature_lazyloading");
+    await llCheckbox.click();
+    await session.waitForReload();
+    const closeButton = await session.waitAndQuery(".mx_RoomHeader_cancelButton");
+    await closeButton.click();
+    session.log.done();
+}
+
+module.exports.getE2EDeviceFromSettings = async function(session) {
+    session.log.step(`gets e2e device/key from settings`);
+    const settingsButton = await session.query('.mx_BottomLeftMenu_settings');
+    await settingsButton.click();
+    const deviceAndKey = await session.waitAndQueryAll(".mx_UserSettings_section.mx_UserSettings_cryptoSection code");
+    assert.equal(deviceAndKey.length, 2);
+    const id = await (await deviceAndKey[0].getProperty("innerText")).jsonValue();
+    const key = await (await deviceAndKey[1].getProperty("innerText")).jsonValue();
+    const closeButton = await session.query(".mx_RoomHeader_cancelButton");
+    await closeButton.click();
+    session.log.done();
+    return {id, key};
+}
