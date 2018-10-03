1e5101aa0cee74917a64dcc3bddba34f5b15605a
Will Hunt <will@half-shot.uk>
2018-10-03 19:39:47 +0100

    The comment can go

--- src/autocomplete/UserProvider.js
+++ src/autocomplete/UserProvider.js
@@ -105,7 +105,7 @@ export default class UserProvider extends AutocompleteProvider {
         // Don't search if the query is a single "@"
         if (fullMatch && fullMatch !== '@') {
             completions = this.matcher.match(fullMatch).map((user) => {
-                const displayName = (user.name || user.userId || ''); // FIXME when groups are done
+                const displayName = (user.name || user.userId || '');
                 return {
                     // Length of completion should equal length of text in decorator. draft-js
                     // relies on the length of the entity === length of the text in the decoration.
