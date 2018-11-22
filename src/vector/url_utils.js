f701e2601a5ca3556d6502f7330a736857f95650
David Baker <dbkr@users.noreply.github.com>
2018-11-22 18:17:09 +0000

    Revert "Run lint on travis builds and use modern node versions"

--- src/vector/url_utils.js
+++ src/vector/url_utils.js
@@ -23,16 +23,16 @@ import qs from 'querystring';
 export function parseQsFromFragment(location) {
     // if we have a fragment, it will start with '#', which we need to drop.
     // (if we don't, this will return '').
-    const fragment = location.hash.substring(1);
+    var fragment = location.hash.substring(1);
 
     // our fragment may contain a query-param-like section. we need to fish
     // this out *before* URI-decoding because the params may contain ? and &
     // characters which are only URI-encoded once.
-    const hashparts = fragment.split('?');
+    var hashparts = fragment.split('?');
 
-    const result = {
+    var result = {
         location: decodeURIComponent(hashparts[0]),
-        params: {},
+        params: {}
     };
 
     if (hashparts.length > 1) {
