f6bbfd7e0d117a0fc79e0306210e2c871c0946f5
David Baker <dave@matrix.org>
2019-01-25 16:25:15 +0000

    New text/caption for key backup by verifying device
    https://github.com/vector-im/riot-web/issues/8072

--- src/components/views/rooms/RoomRecoveryReminder.js
+++ src/components/views/rooms/RoomRecoveryReminder.js
@@ -133,17 +133,29 @@ export default class RoomRecoveryReminder extends React.PureComponent {
         const AccessibleButton = sdk.getComponent("views.elements.AccessibleButton");
 
         let body;
+        let primaryCaption;
         if (this.state.error) {
             body = <div className="error">
                 {_t("Unable to load key backup status")}
             </div>;
+            primaryCaption = _t("Set up");
         } else if (this.state.unverifiedDevice) {
             // A key backup exists for this account, but the creating device is not
             // verified.
-            body = _t(
-                "To view your secure message history and ensure you can view new " +
-                "messages on future devices, set up Secure Message Recovery.",
-            );
+            body = <div>
+                <p>{_t(
+                    "Secure Message Recovery has been set up on another device: <deviceName></deviceName>",
+                    {},
+                    {
+                        deviceName: () => <i>{this.state.unverifiedDevice.unsigned.device_display_name}</i>,
+                    },
+                )}</p>
+                <p>{_t(
+                    "To view your secure message history and ensure you can view new " +
+                    "messages on future devices, verify that device now.",
+                )}</p>
+            </div>;
+            primaryCaption = _t("Verify device");
         } else {
             // The default case assumes that a key backup doesn't exist for this account.
             // (This component doesn't currently check that itself.)
@@ -167,7 +179,7 @@ export default class RoomRecoveryReminder extends React.PureComponent {
                     </AccessibleButton>
                     <AccessibleButton className="mx_RoomRecoveryReminder_button"
                         onClick={this.onSetupClick}>
-                        { _t("Set up") }
+                        {primaryCaption}
                     </AccessibleButton>
                 </div>
             </div>
