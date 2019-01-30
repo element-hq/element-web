f050323f980cbf8d30c770c3be84a406436b9851
Travis Ralston <travpc@gmail.com>
2019-01-29 19:18:13 -0700

    Inherit font size from other elements for push notifications table

--- src/components/views/settings/tabs/NotificationSettingsTab.js
+++ src/components/views/settings/tabs/NotificationSettingsTab.js
@@ -28,7 +28,7 @@ export default class NotificationSettingsTab extends React.Component {
         return (
             <div className="mx_SettingsTab">
                 <div className="mx_SettingsTab_heading">{_t("Notifications")}</div>
-                <div className="mx_SettingsTab_section">
+                <div className="mx_SettingsTab_section mx_SettingsTab_subsectionText">
                     <Notifications />
                 </div>
             </div>
