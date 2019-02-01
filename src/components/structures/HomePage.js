3bfd603dabefc7391258b95ba294c3c168085a0f
J. Ryan Stinnett <jryans@gmail.com>
2019-02-01 15:24:07 -0600

    Remove guest warning bar

--- src/components/structures/HomePage.js
+++ src/components/structures/HomePage.js
@@ -24,7 +24,6 @@ import { _t } from '../../languageHandler';
 import sanitizeHtml from 'sanitize-html';
 import sdk from '../../index';
 import { MatrixClient } from 'matrix-js-sdk';
-import dis from '../../dispatcher';
 
 class HomePage extends React.Component {
     static displayName = 'HomePage';
@@ -80,51 +79,10 @@ class HomePage extends React.Component {
         this._unmounted = true;
     }
 
-    onLoginClick(ev) {
-        ev.preventDefault();
-        ev.stopPropagation();
-        dis.dispatch({ action: 'start_login' });
-    }
-
-    onRegisterClick(ev) {
-        ev.preventDefault();
-        ev.stopPropagation();
-        dis.dispatch({ action: 'start_registration' });
-    }
-
     render() {
-        let guestWarning = "";
-        if (this.context.matrixClient.isGuest()) {
-            guestWarning = (
-                <div className="mx_HomePage_guest_warning">
-                    <img src={require("../../../res/img/warning.svg")} width="24" height="23" />
-                    <div>
-                        <div>
-                            { _t("You are currently using Riot anonymously as a guest.") }
-                        </div>
-                        <div>
-                            { _t(
-                                'If you would like to create a Matrix account you can <a>register</a> now.',
-                                {},
-                                { 'a': (sub) => <a href="#" onClick={this.onRegisterClick}>{ sub }</a> },
-                            ) }
-                        </div>
-                        <div>
-                            { _t(
-                                'If you already have a Matrix account you can <a>log in</a> instead.',
-                                {},
-                                { 'a': (sub) => <a href="#" onClick={this.onLoginClick}>{ sub }</a> },
-                            ) }
-                        </div>
-                    </div>
-                </div>
-            );
-        }
-
         if (this.state.iframeSrc) {
             return (
                 <div className="mx_HomePage">
-                    { guestWarning }
                     <iframe src={ this.state.iframeSrc } />
                 </div>
             );
@@ -132,7 +90,6 @@ class HomePage extends React.Component {
             const GeminiScrollbarWrapper = sdk.getComponent("elements.GeminiScrollbarWrapper");
             return (
                 <GeminiScrollbarWrapper autoshow={true} className="mx_HomePage">
-                    { guestWarning }
                     <div className="mx_HomePage_body" dangerouslySetInnerHTML={{ __html: this.state.page }}>
                     </div>
                 </GeminiScrollbarWrapper>
