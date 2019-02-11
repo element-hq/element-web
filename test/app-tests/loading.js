50baa09f5e7251049cebc336165a98c45a53c041
J. Ryan Stinnett <jryans@gmail.com>
2019-02-11 10:06:44 +0000

    Revert "Disable even more tests"
    This was only done because CI was missing
    matrix-org/matrix-react-sdk@5977859dd98960189e65f34abc2832d281ec48de.
    
    This reverts commit e55ceef9055a82f9dcf706dc92db856ce4dadb5f.

--- test/app-tests/loading.js
+++ test/app-tests/loading.js
@@ -222,8 +222,7 @@ describe('loading:', function() {
             }).done(done, done);
         });
 
-        // TODO: Repair this test in https://github.com/vector-im/riot-web/issues/8468
-        /* it('should follow the original link after successful login', function(done) {
+        it('should follow the original link after successful login', function(done) {
             loadApp({
                 uriFragment: "#/room/!room:id",
             });
@@ -258,10 +257,9 @@ describe('loading:', function() {
                 expect(localStorage.getItem('mx_hs_url')).toEqual(DEFAULT_HS_URL);
                 expect(localStorage.getItem('mx_is_url')).toEqual(DEFAULT_IS_URL);
             }).done(done, done);
-        }); */
+        });
 
-        // TODO: Repair this test in https://github.com/vector-im/riot-web/issues/8468
-        /* it('should not register as a guest when using a #/login link', function() {
+        it('should not register as a guest when using a #/login link', function() {
             loadApp({
                 uriFragment: "#/login",
             });
@@ -288,7 +286,7 @@ describe('loading:', function() {
                     matrixChat, sdk.getComponent('structures.EmbeddedPage'));
                 expect(windowLocation.hash).toEqual("#/home");
             });
-        }); */
+        });
     });
 
     describe("MatrixClient rehydrated from stored credentials:", function() {
@@ -300,8 +298,7 @@ describe('loading:', function() {
             localStorage.setItem("mx_last_room_id", "!last_room:id");
         });
 
-        // TODO: Repair this test in https://github.com/vector-im/riot-web/issues/8468
-        /* it('shows the last known room by default', function() {
+        it('shows the last known room by default', function() {
             httpBackend.when('GET', '/pushrules').respond(200, {});
             httpBackend.when('POST', '/filter').respond(200, { filter_id: 'fid' });
 
@@ -357,7 +354,7 @@ describe('loading:', function() {
                 httpBackend.verifyNoOutstandingExpectation();
                 expect(windowLocation.hash).toEqual("#/room/!room:id");
             }).done(done, done);
-        }); */
+        });
 
         describe('/#/login link:', function() {
             beforeEach(function() {
@@ -387,8 +384,7 @@ describe('loading:', function() {
                 }
             });
 
-            // TODO: Repair this test in https://github.com/vector-im/riot-web/issues/8468
-            /* it('shows the homepage after login', function() {
+            it('shows the homepage after login', function() {
                 return completeLogin(matrixChat).then(() => {
                     // we should see a home page, even though we previously had
                     // a stored mx_last_room_id
@@ -396,7 +392,7 @@ describe('loading:', function() {
                         matrixChat, sdk.getComponent('structures.EmbeddedPage'));
                     expect(windowLocation.hash).toEqual("#/home");
                 });
-            }); */
+            });
         });
     });
 
