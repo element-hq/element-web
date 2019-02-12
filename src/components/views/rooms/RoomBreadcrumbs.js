229d9058359c941552ac4029e192c7600f96723b
Bruno Windels <brunow@matrix.org>
2019-02-12 12:21:40 +0100

    copyright

--- src/components/views/rooms/RoomBreadcrumbs.js
+++ src/components/views/rooms/RoomBreadcrumbs.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2019 Vector Creations Ltd
+Copyright 2019 New Vector Ltd
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
