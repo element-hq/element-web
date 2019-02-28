0978ab3da0e0d2b2872f4970e21f173f9d7a2b45
Travis Ralston <travpc@gmail.com>
2019-02-28 15:55:58 -0700

    Support stacking dialogs to prevent unmounting
    Fixes https://github.com/vector-im/riot-web/issues/8371

--- src/components/structures/MatrixChat.js
+++ src/components/structures/MatrixChat.js
@@ -584,7 +584,8 @@ export default React.createClass({
                 break;
             case 'view_user_settings': {
                 const UserSettingsDialog = sdk.getComponent("dialogs.UserSettingsDialog");
-                Modal.createTrackedDialog('User settings', '', UserSettingsDialog, {}, 'mx_SettingsDialog');
+                Modal.createTrackedDialog('User settings', '', UserSettingsDialog, {}, 'mx_SettingsDialog',
+                    /*isPriority=*/false, /*isStatic=*/true);
 
                 // View the welcome or home page if we need something to look at
                 this._viewSomethingBehindModal();
