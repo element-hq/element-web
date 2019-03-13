618b2b1ae67645e9676a770a8e30f9ab8acb5965
Travis Ralston <travpc@gmail.com>
2019-03-13 17:42:05 -0600

    Add basic BuildKite :pipeline:

--- scripts/ci/unit-tests.sh
+++ scripts/ci/unit-tests.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
-# script which is run by the travis build (after `yarn test`).
+# script which is run by the CI build (after `yarn test`).
 #
 # clones riot-web develop and runs the tests against our version of react-sdk.
 
 set -ev
 
-scripts/travis/build.sh
+scripts/ci/build.sh
 CHROME_BIN='/usr/bin/google-chrome-stable' yarn test
