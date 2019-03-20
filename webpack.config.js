fde42fcec4e31f771040a144442b4596356e107a
MTRNord <mtrnord1@gmail.com>
2019-03-20 16:55:19 +0100

    [webpack4-optimizations] remove matrix-react-sdk alias
    Took 8 minutes

--- webpack.config.js
+++ webpack.config.js
@@ -166,9 +166,6 @@ module.exports = {
             "matrix-js-sdk": path.resolve('./node_modules/matrix-js-sdk'),
 
             "$webapp": path.resolve('./webapp'),
-
-            // We need to overwrite the main file of react-sdk to point to the src version
-            "matrix-react-sdk": path.resolve('./node_modules/matrix-react-sdk/src/index.js'),
         },
     },
     plugins: [
