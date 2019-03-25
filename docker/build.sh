82a07019c3ceb2875e2c3159e1aac0011b2e822f
8sd <git@sebdu.de>
2019-03-25 16:53:04 +0100

    add custom config

--- docker/build.sh
+++ docker/build.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-# get riot
+# Get Riot
 wget https://github.com/vector-im/riot-web/releases/download/v$1/riot-v$1.tar.gz;
 tar axf riot-v$1.tar.gz;
 rm -f riot-v$1.tar.gz;
 mv riot-v$1 riot;
+
+# Add custom config, if existing
+if [ -f ./config.json ] ; then
+	cp ./config.json riot/config.json
+fi
 
 # Build image
 docker build . -t riot:latest;
