146549a66a9c2257617591a3613284e8637fcb3d
Bruno Windels <brunow@matrix.org>
2019-04-03 16:20:26 +0200

    keep complexhttpserver installation within riot folder and gitignore leftovers

--- riot/install.sh
+++ riot/install.sh
@@ -8,23 +8,24 @@ if [ -d $BASE_DIR/riot-web ]; then
     exit
 fi
 
+cd $BASE_DIR
 # Install ComplexHttpServer (a drop in replacement for Python's SimpleHttpServer
 # but with support for multiple threads) into a virtualenv.
 (
-    virtualenv $BASE_DIR/env
-    source $BASE_DIR/env/bin/activate
+    virtualenv env
+    source env/bin/activate
 
     # Having been bitten by pip SSL fail too many times, I don't trust the existing pip
     # to be able to --upgrade itself, so grab a new one fresh from source.
     curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
     python get-pip.py
+    rm get-pip.py
 
     pip install ComplexHttpServer
 
     deactivate
 )
 
-cd $BASE_DIR
 curl -L https://github.com/vector-im/riot-web/archive/${RIOT_BRANCH}.zip --output riot.zip
 unzip -q riot.zip
 rm riot.zip
