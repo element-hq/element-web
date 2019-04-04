406196e11c3e3f998f77d06acb946202aea4576f
Travis Ralston <travpc@gmail.com>
2019-04-04 15:10:17 -0600

    Move import to avoid future merge conflicts

--- src/components/views/rooms/RoomBreadcrumbs.js
+++ src/components/views/rooms/RoomBreadcrumbs.js
@@ -18,13 +18,13 @@ limitations under the License.
 import React from "react";
 import dis from "../../../dispatcher";
 import MatrixClientPeg from "../../../MatrixClientPeg";
+import SettingsStore, {SettingLevel} from "../../../settings/SettingsStore";
 import AccessibleButton from '../elements/AccessibleButton';
 import RoomAvatar from '../avatars/RoomAvatar';
 import classNames from 'classnames';
 import sdk from "../../../index";
 import * as RoomNotifs from '../../../RoomNotifs';
 import * as FormattingUtils from "../../../utils/FormattingUtils";
-import SettingsStore, {SettingLevel} from "../../../settings/SettingsStore";
 
 const MAX_ROOMS = 20;
 
