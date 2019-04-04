b7e557e49a7c5b8363ec2990f1944a5369a17eea
Travis Ralston <travpc@gmail.com>
2019-04-04 16:27:00 -0600

    Autohide the scrollbar on breadcrumbs
    Fixes https://github.com/vector-im/riot-web/issues/9349

--- src/components/views/rooms/RoomBreadcrumbs.js
+++ src/components/views/rooms/RoomBreadcrumbs.js
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-'use strict';
 import React from "react";
 import dis from "../../../dispatcher";
 import MatrixClientPeg from "../../../MatrixClientPeg";
