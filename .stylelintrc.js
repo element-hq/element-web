eb6f3f60085612aed9805525de079561b9f2c1ee
David Baker <dave@matrix.org>
2019-04-09 15:03:13 +0100

    Fix up some CSS things stylelint was complaining about
    And also disable a load of rules that feel like they're probably
    causing more headaches than they fix.

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,5 +3,13 @@ module.exports = {
     "rules": {
         "indentation": 4,
         "comment-empty-line-before": null,
+        "declaration-empty-line-before": null,
+        "length-zero-no-unit": null,
+        "rule-empty-line-before": null,
+        "color-hex-length": null,
+        "max-empty-lines": null,
+        "number-no-trailing-zeros": null,
+        "number-leading-zero": null,
+        "selector-list-comma-newline-after": null,
     }
 }
