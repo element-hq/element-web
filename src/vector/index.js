51941eae49edf4639e59b793c091c3a9513ba854
Travis Ralston <travpc@gmail.com>
2019-04-17 10:28:37 -0600

    Add note to self

--- src/vector/index.js
+++ src/vector/index.js
@@ -448,6 +448,9 @@ async function loadLanguage() {
 async function verifyServerConfig() {
     console.log("Verifying homeserver configuration");
 
+    // TODO: Handle query string arguments for hs_url and is_url
+    // We probably don't want to handle them unless the user is logged out though?
+
     const config = SdkConfig.get();
     let wkConfig = config['default_server_config']; // overwritten later under some conditions
     const serverName = config['default_server_name'];
