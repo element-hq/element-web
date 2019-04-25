1ffe0d1a24b52fd1e09cda145239fadb5a513d18
J. Ryan Stinnett <jryans@gmail.com>
2019-04-25 14:23:51 +0100

    Report location of Synpase log

--- synapse/start.sh
+++ synapse/start.sh
@@ -6,6 +6,7 @@ cd $BASE_DIR
 cd installations/consent
 source env/bin/activate
 LOGFILE=$(mktemp)
+echo "Synapse log file at $LOGFILE"
 ./synctl start 2> $LOGFILE
 EXIT_CODE=$?
 if [ $EXIT_CODE -ne 0 ]; then
