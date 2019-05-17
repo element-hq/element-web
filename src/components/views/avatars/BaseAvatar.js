f1aa2875e1e9bf6c7a8d87f93b8f8f050568b0d6
Travis Ralston <travpc@gmail.com>
2019-05-17 15:25:59 -0600

    Hide avatars from screen readers by default
    To avoid having them read out the user's ID

--- src/components/views/avatars/BaseAvatar.js
+++ src/components/views/avatars/BaseAvatar.js
@@ -189,7 +189,7 @@ module.exports = React.createClass({
             const imgNode = (
                 <img className="mx_BaseAvatar_image" src={imageUrl}
                     alt="" title={title} onError={this.onError}
-                    width={width} height={height} />
+                    width={width} height={height} aria-hidden="true" />
             );
             if (onClick != null) {
                 return (
