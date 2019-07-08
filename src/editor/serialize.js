888e4d1ec258f93f9fdcae94c56379a265b4cf7d
Bruno Windels <brunow@matrix.org>
2019-07-08 15:25:44 +0200

    preserve reply fallback when editing

--- src/editor/serialize.js
+++ src/editor/serialize.js
@@ -33,10 +33,10 @@ export function mdSerialize(model) {
     }, "");
 }
 
-export function htmlSerializeIfNeeded(model) {
+export function htmlSerializeIfNeeded(model, forceHtml = false) {
     const md = mdSerialize(model);
     const parser = new Markdown(md);
-    if (!parser.isPlainText()) {
+    if (!parser.isPlainText() || forceHtml) {
         return parser.toHTML();
     }
 }
