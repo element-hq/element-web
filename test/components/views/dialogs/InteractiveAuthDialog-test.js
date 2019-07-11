a9419135aade987d613d0669cbb7276f3df11413
Travis Ralston <travpc@gmail.com>
2019-07-11 14:38:28 -0600

    Fix tests to use UIA proper

--- test/components/views/dialogs/InteractiveAuthDialog-test.js
+++ test/components/views/dialogs/InteractiveAuthDialog-test.js
@@ -97,11 +97,14 @@ describe('InteractiveAuthDialog', function() {
             ReactTestUtils.Simulate.submit(formNode, {});
 
             expect(doRequest.callCount).toEqual(1);
-            expect(doRequest.calledWithExactly({
+            expect(doRequest.calledWithMatch({
                 session: "sess",
                 type: "m.login.password",
                 password: "s3kr3t",
-                user: "@user:id",
+                identifier: {
+                    type: "m.id.user",
+                    user: "@user:id",
+                },
             })).toBe(true);
             // let the request complete
             return Promise.delay(1);
