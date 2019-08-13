a44d61fb75a9b6b4068ec415f1e09bc2d2def18e
Michael Telatynski <7t3chguy@gmail.com>
2019-08-13 18:17:42 +0100

    add copyright
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- src/components/views/elements/ReplyThread.js
+++ src/components/views/elements/ReplyThread.js
@@ -1,5 +1,6 @@
 /*
 Copyright 2017 New Vector Ltd
+Copyright 2019 Michael Telatynski <7t3chguy@gmail.com>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
