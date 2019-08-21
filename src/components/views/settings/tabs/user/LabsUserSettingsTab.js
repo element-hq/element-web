5bf0587cc51cb94833d6b39651fd543b739b1c8c
Travis Ralston <travpc@gmail.com>
2019-08-21 08:55:19 -0600

    Don't double translate labs settings
    SettingsStore.getDisplayName() already calls _t() for us.
    
    Fixes https://github.com/vector-im/riot-web/issues/10586

--- src/components/views/settings/tabs/user/LabsUserSettingsTab.js
+++ src/components/views/settings/tabs/user/LabsUserSettingsTab.js
@@ -32,7 +32,7 @@ export class LabsSettingToggle extends React.Component {
     };
 
     render() {
-        const label = _t(SettingsStore.getDisplayName(this.props.featureId));
+        const label = SettingsStore.getDisplayName(this.props.featureId);
         const value = SettingsStore.isFeatureEnabled(this.props.featureId);
         return <LabelledToggleSwitch value={value} label={label} onChange={this._onChange} />;
     }
