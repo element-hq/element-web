0e65f71a375fbdee100828dcd84cd6c22a448796
Bruno Windels <brunow@matrix.org>
2019-08-26 16:00:56 +0200

    support incrementing/decrementing doc positions with predicate

--- src/editor/position.js
+++ src/editor/position.js
@@ -35,4 +35,73 @@ export default class DocumentPosition {
             return this._index - otherPos._index;
         }
     }
+
+    iteratePartsBetween(other, model, callback) {
+        if (this.index === -1 || other.index === -1) {
+            return;
+        }
+        const [startPos, endPos] = this.compare(other) < 0 ? [this, other] : [other, this];
+        if (startPos.index === endPos.index) {
+            callback(model.parts[this.index], startPos.offset, endPos.offset);
+        } else {
+            const firstPart = model.parts[startPos.index];
+            callback(firstPart, startPos.offset, firstPart.text.length);
+            for (let i = startPos.index + 1; i < endPos.index; ++i) {
+                const part = model.parts[i];
+                callback(part, 0, part.text.length);
+            }
+            const lastPart = model.parts[endPos.index];
+            callback(lastPart, 0, endPos.offset);
+        }
+    }
+
+    forwardsWhile(model, predicate) {
+        if (this.index === -1) {
+            return this;
+        }
+
+        let {index, offset} = this;
+        const {parts} = model;
+        while (index < parts.length) {
+            const part = parts[index];
+            while (offset < part.text.length) {
+                if (!predicate(index, offset, part)) {
+                    return new DocumentPosition(index, offset);
+                }
+                offset += 1;
+            }
+            // end reached
+            if (index === (parts.length - 1)) {
+                return new DocumentPosition(index, offset);
+            } else {
+                index += 1;
+                offset = 0;
+            }
+        }
+    }
+
+    backwardsWhile(model, predicate) {
+        if (this.index === -1) {
+            return this;
+        }
+
+        let {index, offset} = this;
+        const parts = model.parts;
+        while (index >= 0) {
+            const part = parts[index];
+            while (offset > 0) {
+                if (!predicate(index, offset - 1, part)) {
+                    return new DocumentPosition(index, offset);
+                }
+                offset -= 1;
+            }
+            // start reached
+            if (index === 0) {
+                return new DocumentPosition(index, offset);
+            } else {
+                index -= 1;
+                offset = parts[index].text.length;
+            }
+        }
+    }
 }
