8e66d382deee95f1e2bb82a1171d23846e39aeae
Bruno Windels <brunow@matrix.org>
2019-08-27 16:18:22 +0200

    don't crash on race with room members and initial composer render
    not ideal, but for now this prevents a crash at startup
    when a user-pill is persisted in local storage

--- src/editor/parts.js
+++ src/editor/parts.js
@@ -284,6 +284,9 @@ class UserPillPart extends PillPart {
     }
 
     setAvatar(node) {
+        if (!this._member) {
+            return;
+        }
         const name = this._member.name || this._member.userId;
         const defaultAvatarUrl = Avatar.defaultAvatarUrlForString(this._member.userId);
         let avatarUrl = Avatar.avatarUrlForMember(
