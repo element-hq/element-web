07a26be2ec29b394ded21f67c221b158eeb93e50
TeeMak <bunth667@gmail.com>
2019-08-28 10:43:01 -0400

    ActivityPanel - add main controller for Blast

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,50 @@
-const path = require('path');
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 let og_image_url = process.env.RIOT_OG_IMAGE_URL;
-if (!og_image_url) og_image_url = 'https://riot.im/app/themes/riot/img/logos/riot-im-logo-black-text.png';
+if (!og_image_url)
+    og_image_url =
+        "https://riot.im/app/themes/riot/img/logos/riot-im-logo-black-text.png";
 
 module.exports = {
+    watch: true,
     entry: {
         // Load babel-polyfill first to avoid issues where some imports (namely react)
         // are potentially loaded before babel-polyfill.
-        "bundle": ["babel-polyfill", "./src/vector/index.js"],
+        bundle: ["babel-polyfill", "./src/vector/index.js"],
         "indexeddb-worker": "./src/vector/indexeddb-worker.js",
 
-        "mobileguide": "./src/vector/mobile_guide/index.js",
+        mobileguide: "./src/vector/mobile_guide/index.js",
 
         // CSS themes
-        "theme-light": "./node_modules/matrix-react-sdk/res/themes/light/css/light.scss",
-        "theme-dark": "./node_modules/matrix-react-sdk/res/themes/dark/css/dark.scss",
+        "theme-light":
+            "./node_modules/matrix-react-sdk/res/themes/light/css/light.scss",
+        "theme-dark":
+            "./node_modules/matrix-react-sdk/res/themes/dark/css/dark.scss"
     },
     module: {
         rules: [
-            { enforce: 'pre', test: /\.js$/, use: "source-map-loader", exclude: /node_modules/, },
-            { test: /\.js$/, use: "babel-loader", include: path.resolve(__dirname, 'src') },
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                use: "source-map-loader",
+                exclude: /node_modules/
+            },
+            {
+                test: /\.js$/,
+                use: "babel-loader",
+                include: path.resolve(__dirname, "src")
+            },
             {
                 test: /\.wasm$/,
                 loader: "file-loader",
                 type: "javascript/auto", // https://github.com/webpack/webpack/issues/6725
                 options: {
-                    name: '[name].[hash:7].[ext]',
-                    outputPath: '.',
-                },
+                    name: "[name].[hash:7].[ext]",
+                    outputPath: "."
+                }
             },
             {
                 test: /\.scss$/,
@@ -43,32 +57,32 @@ module.exports = {
                     use: [
                         "css-loader",
                         {
-                            loader: 'postcss-loader',
+                            loader: "postcss-loader",
                             options: {
                                 config: {
-                                    path: './postcss.config.js',
-                                },
-                            },
-                        },
-                    ],
-                }),
+                                    path: "./postcss.config.js"
+                                }
+                            }
+                        }
+                    ]
+                })
             },
             {
                 // this works similarly to the scss case, without postcss.
                 test: /\.css$/,
                 use: ExtractTextPlugin.extract({
-                    use: "css-loader",
-                }),
+                    use: "css-loader"
+                })
             },
             {
                 // cache-bust languages.json file placed in
                 // riot-web/webapp/i18n during build by copy-res.js
                 test: /\.*languages.json$/,
                 type: "javascript/auto",
-                loader: 'file-loader',
+                loader: "file-loader",
                 options: {
-                    name: 'i18n/[name].[hash:7].[ext]',
-                },
+                    name: "i18n/[name].[hash:7].[ext]"
+                }
             },
             {
                 test: /\.(gif|png|svg|ttf|woff|woff2|xml|ico)$/,
@@ -78,33 +92,41 @@ module.exports = {
                     {
                         // Assets referenced in CSS files
                         issuer: /\.(scss|css)$/,
-                        loader: 'file-loader',
+                        loader: "file-loader",
                         options: {
-                            name: '[name].[hash:7].[ext]',
+                            name: "[name].[hash:7].[ext]",
                             outputPath: getImgOutputPath,
                             publicPath: function(url, resourcePath) {
                                 // CSS image usages end up in the `bundles/[hash]` output
                                 // directory, so we adjust the final path to navigate up
                                 // twice.
-                                const outputPath = getImgOutputPath(url, resourcePath);
-                                return toPublicPath(path.join("../..", outputPath));
-                            },
-                        },
+                                const outputPath = getImgOutputPath(
+                                    url,
+                                    resourcePath
+                                );
+                                return toPublicPath(
+                                    path.join("../..", outputPath)
+                                );
+                            }
+                        }
                     },
                     {
                         // Assets referenced in HTML and JS files
-                        loader: 'file-loader',
+                        loader: "file-loader",
                         options: {
-                            name: '[name].[hash:7].[ext]',
+                            name: "[name].[hash:7].[ext]",
                             outputPath: getImgOutputPath,
                             publicPath: function(url, resourcePath) {
-                                const outputPath = getImgOutputPath(url, resourcePath);
+                                const outputPath = getImgOutputPath(
+                                    url,
+                                    resourcePath
+                                );
                                 return toPublicPath(outputPath);
-                            },
-                        },
-                    },
-                ],
-            },
+                            }
+                        }
+                    }
+                ]
+            }
         ],
         noParse: [
             // for cross platform compatibility use [\\\/] as the path separator
@@ -118,8 +140,8 @@ module.exports = {
 
             // olm takes ages for webpack to process, and it's already heavily
             // optimised, so there is little to gain by us uglifying it.
-            /olm[\\\/](javascript[\\\/])?olm\.js$/,
-        ],
+            /olm[\\\/](javascript[\\\/])?olm\.js$/
+        ]
     },
     output: {
         path: path.join(__dirname, "webapp"),
@@ -142,71 +164,75 @@ module.exports = {
             // trees smashed together, but this fixes everything being under
             // various levels of '.' and '..'
             // Also, sometimes the resource path is absolute.
-            return path.relative(process.cwd(), info.resourcePath).replace(/^[\/\.]*/, '');
-        },
+            return path
+                .relative(process.cwd(), info.resourcePath)
+                .replace(/^[\/\.]*/, "");
+        }
     },
     resolve: {
         alias: {
             // alias any requires to the react module to the one in our path,
             // otherwise we tend to get the react source included twice when
             // using `npm link` / `yarn link`.
-            "react": path.resolve('./node_modules/react'),
-            "react-dom": path.resolve('./node_modules/react-dom'),
-            "react-addons-perf": path.resolve('./node_modules/react-addons-perf'),
+            react: path.resolve("./node_modules/react"),
+            "react-dom": path.resolve("./node_modules/react-dom"),
+            "react-addons-perf": path.resolve(
+                "./node_modules/react-addons-perf"
+            ),
 
             // same goes for js-sdk
-            "matrix-js-sdk": path.resolve('./node_modules/matrix-js-sdk'),
+            "matrix-js-sdk": path.resolve("./node_modules/matrix-js-sdk"),
 
-            "$webapp": path.resolve('./webapp'),
-        },
+            $webapp: path.resolve("./webapp")
+        }
     },
     plugins: [
         new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-            },
+            "process.env": {
+                NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+            }
         }),
         new ExtractTextPlugin("bundles/[hash]/[name].css", {
-            allChunks: true,
+            allChunks: true
         }),
 
         new HtmlWebpackPlugin({
-            template: './src/vector/index.html',
+            template: "./src/vector/index.html",
 
             // we inject the links ourselves via the template, because
             // HtmlWebpackPlugin wants to put the script tags either at the
             // bottom of <head> or the bottom of <body>, and I'm a bit scared
             // about moving them.
             inject: false,
-            excludeChunks: ['mobileguide'],
+            excludeChunks: ["mobileguide"],
             vars: {
-                og_image_url: og_image_url,
-            },
+                og_image_url: og_image_url
+            }
         }),
         new HtmlWebpackPlugin({
-            template: './src/vector/mobile_guide/index.html',
-            filename: 'mobile_guide/index.html',
-            chunks: ['mobileguide'],
-        }),
+            template: "./src/vector/mobile_guide/index.html",
+            filename: "mobile_guide/index.html",
+            chunks: ["mobileguide"]
+        })
     ],
-    devtool: 'source-map',
+    devtool: "source-map",
 
     // configuration for the webpack-dev-server
     devServer: {
         // serve unwebpacked assets from webapp.
-        contentBase: './webapp',
+        contentBase: "./webapp",
 
         stats: {
             // don't fill the console up with a mahoosive list of modules
-            chunks: false,
+            chunks: false
         },
 
         // hot mdule replacement doesn't work (I think we'd need react-hot-reload?)
         // so webpack-dev-server reloads the page on every update which is quite
         // tedious in Riot since that can take a while.
         hot: false,
-        inline: false,
-    },
+        inline: false
+    }
 };
 
 /**
@@ -230,5 +256,5 @@ function getImgOutputPath(url, resourcePath) {
  * @param {string} path Some path to a file.
  */
 function toPublicPath(path) {
-    return path.replace(/\\/g, '/');
+    return path.replace(/\\/g, "/");
 }
