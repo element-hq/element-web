2ff2ff0e75fbd0c0d75683bc95054e0df4261107
Bruno Windels <brunow@matrix.org>
2019-08-29 17:43:18 +0200

    support autocomplete replacing text with multiple parts
    and append ": " to user pills

--- src/editor/model.js
+++ src/editor/model.js
@@ -47,6 +47,7 @@ export default class EditorModel {
         this._activePartIdx = null;
         this._autoComplete = null;
         this._autoCompletePartIdx = null;
+        this._autoCompletePartCount = 0;
         this._transformCallback = null;
         this.setUpdateCallback(updateCallback);
     }
@@ -219,6 +220,7 @@ export default class EditorModel {
                         // make sure that react picks up the difference between both acs
                         this._autoComplete = ac;
                         this._autoCompletePartIdx = index;
+                        this._autoCompletePartCount = 1;
                     }
                 }
             }
@@ -230,23 +232,24 @@ export default class EditorModel {
             this._activePartIdx = null;
             this._autoComplete = null;
             this._autoCompletePartIdx = null;
+            this._autoCompletePartCount = 0;
         }
         return Promise.resolve();
     }
 
-    _onAutoComplete = ({replacePart, caretOffset, close}) => {
+    _onAutoComplete = ({replaceParts, close}) => {
         let pos;
-        if (replacePart) {
-            this._replacePart(this._autoCompletePartIdx, replacePart);
-            const index = this._autoCompletePartIdx;
-            if (caretOffset === undefined) {
-                caretOffset = replacePart.text.length;
-            }
-            pos = new DocumentPosition(index, caretOffset);
+        if (replaceParts) {
+            this._parts.splice(this._autoCompletePartIdx, this._autoCompletePartCount, ...replaceParts);
+            this._autoCompletePartCount = replaceParts.length;
+            const lastPart = replaceParts[replaceParts.length - 1];
+            const lastPartIndex = this._autoCompletePartIdx + replaceParts.length - 1;
+            pos = new DocumentPosition(lastPartIndex, lastPart.text.length);
         }
         if (close) {
             this._autoComplete = null;
             this._autoCompletePartIdx = null;
+            this._autoCompletePartCount = 0;
         }
         // rerender even if editor contents didn't change
         // to make sure the MessageEditor checks
