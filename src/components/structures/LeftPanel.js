4ffa8c7b4aa876cd8b438ab672b0751ad11ace76
Bruno Windels <brunow@matrix.org>
2019-09-10 10:53:55 +0200

    add explore button next to filter field

--- src/components/structures/LeftPanel.js
+++ src/components/structures/LeftPanel.js
@@ -218,6 +218,7 @@ const LeftPanel = React.createClass({
         const TopLeftMenuButton = sdk.getComponent('structures.TopLeftMenuButton');
         const SearchBox = sdk.getComponent('structures.SearchBox');
         const CallPreview = sdk.getComponent('voip.CallPreview');
+        const AccessibleButton = sdk.getComponent('elements.AccessibleButton');
 
         const tagPanelEnabled = SettingsStore.getValue("TagPanel.enableTagPanel");
         let tagPanelContainer;
@@ -241,6 +242,15 @@ const LeftPanel = React.createClass({
             },
         );
 
+        let exploreButton;
+        if (!this.props.collapsed) {
+            exploreButton = (
+                <div className={classNames("mx_LeftPanel_explore", {"mx_LeftPanel_explore_hidden": this.state.searchFocused})}>
+                    <AccessibleButton onClick={() => dis.dispatch({action: 'view_room_directory'})}>{_t("Explore")}</AccessibleButton>
+                </div>
+            );
+        }
+
         const searchBox = (<SearchBox
             enableRoomSearchFocus={true}
             placeholder={ _t('Filter room names') }
@@ -259,7 +269,10 @@ const LeftPanel = React.createClass({
                 <aside className={"mx_LeftPanel dark-panel"} onKeyDown={ this._onKeyDown } onFocus={ this._onFocus } onBlur={ this._onBlur }>
                     <TopLeftMenuButton collapsed={ this.props.collapsed } />
                     { breadcrumbs }
-                    { searchBox }
+                    <div className="mx_LeftPanel_exploreAndFilterRow">
+                        { exploreButton }
+                        { searchBox }
+                    </div>
                     <CallPreview ConferenceHandler={VectorConferenceHandler} />
                     <RoomList
                         ref={this.collectRoomList}
