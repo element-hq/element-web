634141637bfa6978d1d21e32916f4be3f406718f
Nad Chishtie <n.chishtie@gmail.com>
2019-09-11 16:08:24 -0700

    Remove extraneous content & styles. Style improvements.

--- src/vector/mobile_guide/index.js
+++ src/vector/mobile_guide/index.js
@@ -95,9 +95,9 @@ async function initPage() {
         document.getElementById('configure_riot_button').href =
             "https://riot.im/config/config?hs_url=" + encodeURIComponent(hsUrl) +
             "&is_url=" + encodeURIComponent(isUrl);
+        document.getElementById('step1_heading').innerHTML= '1: Install the app';
         document.getElementById('step2_container').style.display = 'block';
         document.getElementById('hs_url').innerText = hsUrl;
-        document.getElementById('step_login_header').innerHTML= '3: Launch the app';
 
         if (isUrl && isUrl !== "https://vector.im/") {
             document.getElementById('default_is').style.display = 'none';
