8d1d3090f36963c0f3921968f09b20d680810d80
Michael Telatynski <7t3chguy@gmail.com>
2019-09-27 09:00:54 +0100

    Improve keyboard accessibility using :focus-visible CSS polyfill
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- src/components/structures/MatrixChat.js
+++ src/components/structures/MatrixChat.js
@@ -41,7 +41,9 @@ import * as Rooms from '../../Rooms';
 import linkifyMatrix from "../../linkify-matrix";
 import * as Lifecycle from '../../Lifecycle';
 // LifecycleStore is not used but does listen to and dispatch actions
-require('../../stores/LifecycleStore');
+import '../../stores/LifecycleStore';
+// focus-visible is a Polyfill for the :focus-visible CSS pseudo-attribute used by _AccessibleButton.scss
+import 'focus-visible';
 import PageTypes from '../../PageTypes';
 import { getHomePageUrl } from '../../utils/pages';
 
