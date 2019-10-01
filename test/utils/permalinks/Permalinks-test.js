0862ad029da494554caba4ab6f1a5f870199340a
Travis Ralston <travpc@gmail.com>
2019-09-30 21:15:31 -0600

    Fix permalinks test

--- test/utils/permalinks/Permalinks-test.js
+++ test/utils/permalinks/Permalinks-test.js
@@ -1,5 +1,7 @@
 /*
 Copyright 2018 New Vector Ltd
+Copyright 2019 The Matrix.org Foundation C.I.C.
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -11,7 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import expect from 'expect/build/index';
+import expect from 'expect';
 import peg from '../../../src/MatrixClientPeg';
 import {
     makeGroupPermalink,
@@ -62,7 +64,7 @@ function mockRoom(roomId, members, serverACL) {
     };
 }
 
-describe('matrix-to', function() {
+describe('Permalinks', function() {
     let sandbox;
 
     beforeEach(function() {
