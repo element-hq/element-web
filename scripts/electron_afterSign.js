ff70339aadaac760281c85ea53be8b86420847a9
David Baker <dave@matrix.org>
2019-10-09 16:29:24 +0100

    Working branch for notarisation

---
+++ scripts/electron_afterSign.js
@@ -0,0 +1,25 @@
+const { notarize } = require('electron-notarize');
+
+exports.default = async function(context) {
+    const { electronPlatformName, appOutDir } = context;  
+    if (electronPlatformName !== 'darwin') {
+        return;
+    }
+
+    // We get the password from keychain. The keychain stores
+    // user IDs too, but apparently altool can't get the user ID
+    // from the keychain, so we need to get it from the environment.
+    const userId = process.env.NOTARIZE_APPLE_ID;
+    if (userId === undefined) {
+        throw new Exception("User ID not found. Set NOTARIZE_APPLE_ID.");
+    }
+ 
+    const appName = context.packager.appInfo.productFilename;
+ 
+    return await notarize({
+        appBundleId: 'im.riot.app',
+        appPath: `${appOutDir}/${appName}.app`,
+        appleId: userId,
+        appleIdPassword: '@keychain:NOTARIZE_CREDS,
+    });
+};
