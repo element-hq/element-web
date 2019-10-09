5b9bfae320de98b7ea3dd2540d1a69c6c67e2fe7
Bruno Windels <brunow@matrix.org>
2019-10-09 17:03:01 +0200

    first attempt at running local e2e tests from CI

--- scripts/ci/end-to-end-tests.sh
+++ scripts/ci/end-to-end-tests.sh
@@ -28,9 +28,7 @@ REACT_SDK_DIR=`pwd`
 echo "--- Building Riot"
 scripts/ci/build.sh
 # run end to end tests
-echo "--- Fetching end-to-end tests from master"
-scripts/fetchdep.sh matrix-org matrix-react-end-to-end-tests master
-pushd matrix-react-end-to-end-tests
+pushd test/end-to-end-tests
 ln -s $REACT_SDK_DIR/$RIOT_WEB_DIR riot/riot-web
 # PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true ./install.sh
 # CHROME_PATH=$(which google-chrome-stable) ./run.sh
