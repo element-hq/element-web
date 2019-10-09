59cc36ca652ce71b59194166e6192e1609aa6f49
Bruno Windels <brunow@matrix.org>
2019-10-09 17:34:40 +0200

    don't fetch riot/master by default when installing e2e tests

--- test/end-to-end-tests/install.sh
+++ test/end-to-end-tests/install.sh
@@ -2,5 +2,7 @@
 # run with PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true sh install.sh if chrome is already installed
 set -e
 ./synapse/install.sh
-./riot/install.sh
+# both CI and local testing don't need a riot fetched from master,
+# so not installing this by default anymore
+# ./riot/install.sh
 yarn install
