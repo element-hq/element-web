75bcc3f84913c61960b9734cd60929df3d4e369b
Tulir Asokan <tulir@maunium.net>
2019-10-18 19:58:55 +0300

    Update src/editor/deserialize.js
    Co-Authored-By: Bruno Windels <bruno@windels.cloud>

--- src/editor/deserialize.js
+++ src/editor/deserialize.js
@@ -109,7 +109,8 @@ function parseElement(n, partCreator, lastNode, state) {
             const indent = "  ".repeat(state.listDepth - 1);
             if (n.parentElement.nodeName === "OL") {
                 // The markdown parser doesn't do nested indexed lists at all, but this supports it anyway.
-                let index = state.listIndex[state.listIndex.length - 1]++;
+                let index = state.listIndex[state.listIndex.length - 1];
+                state.listIndex[state.listIndex.length - 1] += 1;
                 return partCreator.plain(`${indent}${index}. `);
             } else {
                 return partCreator.plain(`${indent}- `);
