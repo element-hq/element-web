8a028029eea3a2e0b797ff746823bcd648744317
Bruno Windels <brunow@matrix.org>
2019-10-18 12:28:04 +0000

    Update test/end-to-end-tests/install.sh
    Co-Authored-By: J. Ryan Stinnett <jryans@gmail.com>

--- test/end-to-end-tests/install.sh
+++ test/end-to-end-tests/install.sh
@@ -2,7 +2,7 @@
 # run with PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true sh install.sh if chrome is already installed
 set -e
 ./synapse/install.sh
-# both CI and local testing don't need a riot fetched from master,
+# both CI and local testing don't need a Riot fetched from master,
 # so not installing this by default anymore
 # ./riot/install.sh
 yarn install
