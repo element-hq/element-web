b0eb54541cbe18c67c91f9017ec609478bf53ce1
Travis Ralston <travpc@gmail.com>
2019-11-20 19:50:13 -0700

    Rip out options to change your integration manager
    We are not supporting this due to the complexity involved in switching integration managers. We still support custom ones under the hood, just not to the common user. A later sprint on integrations will consider re-adding the option alongside fixing the various bugs out there.

--- src/components/views/settings/SetIntegrationManager.js
+++ src/components/views/settings/SetIntegrationManager.js
@@ -16,13 +16,7 @@ limitations under the License.
 
 import React from 'react';
 import {_t} from "../../../languageHandler";
-import sdk from '../../../index';
-import Field from "../elements/Field";
 import {IntegrationManagers} from "../../../integrations/IntegrationManagers";
-import MatrixClientPeg from "../../../MatrixClientPeg";
-import {SERVICE_TYPES} from "matrix-js-sdk";
-import {IntegrationManagerInstance} from "../../../integrations/IntegrationManagerInstance";
-import Modal from "../../../Modal";
 
 export default class SetIntegrationManager extends React.Component {
     constructor() {
@@ -32,136 +26,10 @@ export default class SetIntegrationManager extends React.Component {
 
         this.state = {
             currentManager,
-            url: "", // user-entered text
-            error: null,
-            busy: false,
-            checking: false,
         };
     }
 
-    _onUrlChanged = (ev) => {
-        const u = ev.target.value;
-        this.setState({url: u});
-    };
-
-    _getTooltip = () => {
-        if (this.state.checking) {
-            const InlineSpinner = sdk.getComponent('views.elements.InlineSpinner');
-            return <div>
-                <InlineSpinner />
-                { _t("Checking server") }
-            </div>;
-        } else if (this.state.error) {
-            return <span className="warning">{this.state.error}</span>;
-        } else {
-            return null;
-        }
-    };
-
-    _canChange = () => {
-        return !!this.state.url && !this.state.busy;
-    };
-
-    _continueTerms = async (manager) => {
-        try {
-            await IntegrationManagers.sharedInstance().overwriteManagerOnAccount(manager);
-            this.setState({
-                busy: false,
-                error: null,
-                currentManager: IntegrationManagers.sharedInstance().getPrimaryManager(),
-                url: "", // clear input
-            });
-        } catch (e) {
-            console.error(e);
-            this.setState({
-                busy: false,
-                error: _t("Failed to update integration manager"),
-            });
-        }
-    };
-
-    _setManager = async (ev) => {
-        // Don't reload the page when the user hits enter in the form.
-        ev.preventDefault();
-        ev.stopPropagation();
-
-        this.setState({busy: true, checking: true, error: null});
-
-        let offline = false;
-        let manager: IntegrationManagerInstance;
-        try {
-            manager = await IntegrationManagers.sharedInstance().tryDiscoverManager(this.state.url);
-            offline = !manager; // no manager implies offline
-        } catch (e) {
-            console.error(e);
-            offline = true; // probably a connection error
-        }
-        if (offline) {
-            this.setState({
-                busy: false,
-                checking: false,
-                error: _t("Integration manager offline or not accessible."),
-            });
-            return;
-        }
-
-        // Test the manager (causes terms of service prompt if agreement is needed)
-        // We also cancel the tooltip at this point so it doesn't collide with the dialog.
-        this.setState({checking: false});
-        try {
-            const client = manager.getScalarClient();
-            await client.connect();
-        } catch (e) {
-            console.error(e);
-            this.setState({
-                busy: false,
-                error: _t("Terms of service not accepted or the integration manager is invalid."),
-            });
-            return;
-        }
-
-        // Specifically request the terms of service to see if there are any.
-        // The above won't trigger a terms of service check if there are no terms to
-        // sign, so when there's no terms at all we need to ensure we tell the user.
-        let hasTerms = true;
-        try {
-            const terms = await MatrixClientPeg.get().getTerms(SERVICE_TYPES.IM, manager.trimmedApiUrl);
-            hasTerms = terms && terms['policies'] && Object.keys(terms['policies']).length > 0;
-        } catch (e) {
-            // Assume errors mean there are no terms. This could be a 404, 500, etc
-            console.error(e);
-            hasTerms = false;
-        }
-        if (!hasTerms) {
-            this.setState({busy: false});
-            const QuestionDialog = sdk.getComponent("views.dialogs.QuestionDialog");
-            Modal.createTrackedDialog('No Terms Warning', '', QuestionDialog, {
-                title: _t("Integration manager has no terms of service"),
-                description: (
-                    <div>
-                        <span className="warning">
-                            {_t("The integration manager you have chosen does not have any terms of service.")}
-                        </span>
-                        <span>
-                            &nbsp;{_t("Only continue if you trust the owner of the server.")}
-                        </span>
-                    </div>
-                ),
-                button: _t("Continue"),
-                onFinished: async (confirmed) => {
-                    if (!confirmed) return;
-                    this._continueTerms(manager);
-                },
-            });
-            return;
-        }
-
-        this._continueTerms(manager);
-    };
-
     render() {
-        const AccessibleButton = sdk.getComponent('views.elements.AccessibleButton');
-
         const currentManager = this.state.currentManager;
         let managerName;
         let bodyText;
@@ -181,7 +49,7 @@ export default class SetIntegrationManager extends React.Component {
         }
 
         return (
-            <form className="mx_SettingsTab_section mx_SetIntegrationManager" onSubmit={this._setManager}>
+            <div className='mx_SetIntegrationManager'>
                 <div className="mx_SettingsTab_heading">
                     <span>{_t("Integration Manager")}</span>
                     <span className="mx_SettingsTab_subheading">{managerName}</span>
@@ -189,24 +57,7 @@ export default class SetIntegrationManager extends React.Component {
                 <span className="mx_SettingsTab_subsectionText">
                     {bodyText}
                 </span>
-                <Field
-                    label={_t("Enter a new integration manager")}
-                    id="mx_SetIntegrationManager_newUrl"
-                    type="text" value={this.state.url}
-                    autoComplete="off"
-                    onChange={this._onUrlChanged}
-                    tooltipContent={this._getTooltip()}
-                    tooltipClassName="mx_SetIntegrationManager_tooltip"
-                    disabled={this.state.busy}
-                    flagInvalid={!!this.state.error}
-                />
-                <AccessibleButton
-                    kind="primary_sm"
-                    type="submit"
-                    disabled={!this._canChange()}
-                    onClick={this._setManager}
-                >{_t("Change")}</AccessibleButton>
-            </form>
+            </div>
         );
     }
 }
