a84de0bae268de9f6477b5eea8bb84e63da57eb6
David Baker <dave@matrix.org>
2019-11-26 15:24:43 +0000

    Sign all of the Windows executable files
    We can actually just supply a custom signing module here to do our
    signing rather than manually signing things in the afterSign hook.
    This means all 4 executable files get signed (the main exe, the
    stub exe, Update.exe and the installer).

--- scripts/electron_afterSign.js
+++ scripts/electron_afterSign.js
@@ -1,7 +1,4 @@
 const { notarize } = require('electron-notarize');
-const { exec, execFile } = require('child_process');
-const fs = require('fs');
-const shellescape = require('shell-escape');
 
 exports.default = async function(context) {
     const { electronPlatformName, appOutDir } = context;
@@ -22,55 +19,6 @@ exports.default = async function(context) {
             appPath: `${appOutDir}/${appName}.app`,
             appleId: userId,
             appleIdPassword: '@keychain:NOTARIZE_CREDS',
-        });
-    } else if (electronPlatformName === 'win32') {
-        // This signs the actual Riot executable
-        const appName = context.packager.appInfo.productFilename;
-
-        // get the token passphrase from the keychain
-        const tokenPassphrase = await new Promise((resolve, reject) => {
-            execFile(
-                'security',
-                ['find-generic-password', '-s', 'riot_signing_token', '-w'],
-                {},
-                (err, stdout) => {
-                    if (err) {
-                        reject(err);
-                    } else {
-                        resolve(stdout.trim());
-                    }
-                },
-            );
-        });
-
-        return new Promise((resolve, reject) => {
-            let cmdLine = 'osslsigncode sign ';
-            if (process.env.OSSLSIGNCODE_SIGNARGS) {
-                cmdLine += process.env.OSSLSIGNCODE_SIGNARGS + ' ';
-            }
-            const tmpFile = 'tmp_' + Math.random().toString(36).substring(2, 15) + '.exe';
-            cmdLine += shellescape([
-                '-pass', tokenPassphrase,
-                '-in', `${appOutDir}/${appName}.exe`,
-                '-out', `${appOutDir}/${tmpFile}`,
-            ]);
-
-            const signproc = exec(cmdLine, {}, (error, stdout) => {
-                console.log(stdout);
-            });
-            signproc.on('exit', (code) => {
-                if (code !== 0) {
-                    reject("osslsigncode failed with code " + code);
-                    return;
-                }
-                fs.rename(`${appOutDir}/${tmpFile}`, `${appOutDir}/${appName}.exe`, (err) => {
-                    if (err) {
-                        reject(err);
-                    } else {
-                        resolve();
-                    }
-                });
-            });
         });
     }
 };
