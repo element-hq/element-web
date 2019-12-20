7b7ede4fb0ddfe06e5cd7ff53933e4951dfdb787
Travis Ralston <travpc@gmail.com>
2019-12-19 19:09:31 -0700

    Don't resolve matrix-react-sdk ourselves

--- webpack.config.js
+++ webpack.config.js
@@ -62,7 +62,7 @@ module.exports = {
             // same goes for js-sdk, but we also want to point at the source
             // of each SDK so we can compile it for ourselves.
             "matrix-js-sdk": path.resolve(__dirname, 'node_modules/matrix-js-sdk'),
-            "matrix-react-sdk": path.resolve(__dirname, 'node_modules/matrix-react-sdk/src'),
+            //"matrix-react-sdk": path.resolve(__dirname, 'node_modules/matrix-react-sdk/src'),
 
             "$webapp": path.resolve(__dirname, 'webapp'),
         },
