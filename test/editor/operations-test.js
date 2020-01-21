9a530a72f6d27dd67d338533fe3823c880d7ff77
Michael Telatynski <7t3chguy@gmail.com>
2020-01-21 15:36:25 +0000

    delint

--- test/editor/operations-test.js
+++ test/editor/operations-test.js
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import {getLineAndNodePosition} from "../../src/editor/caret";
 import EditorModel from "../../src/editor/model";
 import {createPartCreator, createRenderer} from "./mock";
 import {toggleInlineFormat} from "../../src/editor/operations";
