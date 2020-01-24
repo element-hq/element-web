99252e08b0eb3800b9aa79be9e000f803b04aeb0
David Baker <dave@matrix.org>
2020-01-24 09:37:43 +0000

    Unintentional commit

--- src/components/views/settings/KeyBackupPanel.js
+++ src/components/views/settings/KeyBackupPanel.js
@@ -127,7 +127,7 @@ export default class KeyBackupPanel extends React.PureComponent {
         Modal.createTrackedDialogAsync('Key Backup', 'Key Backup',
             import('../../../async-components/views/dialogs/keybackup/CreateKeyBackupDialog'),
             {
-                secureSecretStorage: false,
+                secureSecretStorage: SettingsStore.isFeatureEnabled("feature_cross_signing"),
                 onFinished: () => {
                     this._loadBackupStatus();
                 },
