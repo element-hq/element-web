ecfecfe559e2eda9c8cc326913a5103d19fb9ea0
Damir Jelić <poljar@termina.org.uk>
2020-01-24 12:07:03 +0100

    EventIndex: Fix a small style issue.

--- src/indexing/EventIndex.js
+++ src/indexing/EventIndex.js
@@ -172,7 +172,7 @@ export default class EventIndex {
         }
 
         const jsonEvent = ev.toJSON();
-        const e = ev.isEncrypted() ? jsonEvent.decrypted: jsonEvent;
+        const e = ev.isEncrypted() ? jsonEvent.decrypted : jsonEvent;
 
         const profile = {
             displayname: ev.sender.rawDisplayName,
@@ -308,7 +308,7 @@ export default class EventIndex {
             // consume.
             const events = filteredEvents.map((ev) => {
                 const jsonEvent = ev.toJSON();
-                const e = ev.isEncrypted() ? jsonEvent.decrypted: jsonEvent;
+                const e = ev.isEncrypted() ? jsonEvent.decrypted : jsonEvent;
 
                 let profile = {};
                 if (e.sender in profiles) profile = profiles[e.sender];
