bfaa9d56fbf368eb3ffb4c985794633fd8014833
Half-Shot <will@half-shot.uk>
2020-01-28 16:05:27 +0000

    prefixes

--- src/components/views/settings/BridgeTile.js
+++ src/components/views/settings/BridgeTile.js
@@ -105,7 +105,7 @@ export default class BridgeTile extends React.PureComponent {
                 <p className={metadataClassname}>
                     {creator} {bot}
                 </p>
-                <AccessibleButton className="showMore" kind="secondary" onClick={this._toggleVisible.bind(this)}>
+                <AccessibleButton className="mx_showMore" kind="secondary" onClick={this._toggleVisible.bind(this)}>
                     { this.state.visible ? _t("Show less") : _t("Show more") }
                 </AccessibleButton>
             </div>
