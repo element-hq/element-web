cff4ad9e8273d3560485b8e459d689088a629d9d
Travis Ralston <travpc@gmail.com>
2020-01-28 14:21:26 +0000

    Remove legacy and stop using deprecated things

--- babel.config.js
+++ babel.config.js
@@ -2,18 +2,16 @@ module.exports = {
     "sourceMaps": true,
     "presets": [
         ["@babel/preset-env", {
-            "targets": {
-                "browsers": [
-                    "last 2 Chrome versions", "last 2 Firefox versions", "last 2 Safari versions"
-                ]
-            },
+            "targets": [
+                "last 2 Chrome versions", "last 2 Firefox versions", "last 2 Safari versions"
+            ],
         }],
         "@babel/preset-typescript",
         "@babel/preset-flow",
         "@babel/preset-react"
     ],
     "plugins": [
-        ["@babel/plugin-proposal-decorators", {"legacy": false, decoratorsBeforeExport: true}],
+        ["@babel/plugin-proposal-decorators", {decoratorsBeforeExport: true}],
         "@babel/plugin-proposal-export-default-from",
         "@babel/plugin-proposal-numeric-separator",
         "@babel/plugin-proposal-class-properties",
