3f2dcf2113f017470b568d5deac3d368532eb470
J. Ryan Stinnett <jryans@gmail.com>
2020-02-14 11:07:05 +0000

    Add dep version checks for release

--- release.sh
+++ release.sh
@@ -9,4 +9,21 @@ set -e
 
 cd `dirname $0`
 
+for i in matrix-js-sdk
+do
+    depver=`cat package.json | jq -r .dependencies[\"$i\"]`
+    latestver=`yarn info -s $i dist-tags.next`
+    if [ "$depver" != "$latestver" ]
+    then
+        echo "The latest version of $i is $latestver but package.json depends on $depver"
+        echo -n "Type 'Yes' to continue anyway: "
+        read resp
+        if [ "$resp" != "Yes" ]
+        then
+            echo "OK, never mind."
+            exit 1
+        fi
+    fi
+done
+
 exec ./node_modules/matrix-js-sdk/release.sh -z "$@"
