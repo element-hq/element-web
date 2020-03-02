178c61dbf749a0d5aa23498f249d31d6859fcb2b
Michael Telatynski <7t3chguy@gmail.com>
2020-03-02 15:01:06 +0000

    fix typo

--- src/vector/platform/ElectronPlatform.js
+++ src/vector/platform/ElectronPlatform.js
@@ -5,7 +5,7 @@ Copyright 2016 Aviral Dasgupta
 Copyright 2016 OpenMarket Ltd
 Copyright 2018 New Vector Ltd
 Copyright 2019 Michael Telatynski <7t3chguy@gmail.com>
-`Copyright 2020 The Matrix.org Foundation C.I.C.
+Copyright 2020 The Matrix.org Foundation C.I.C.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
