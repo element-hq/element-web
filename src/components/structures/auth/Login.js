88f351d5ca8e113b38f1b126d4b7e2ad5d544e3a
J. Ryan Stinnett <jryans@gmail.com>
2020-03-03 10:40:58 +0000

    Keep sign in button disabled on success
    This changes password login to keep the sign in button locked when proceeding
    successfully to avoid the possibility of double device sign in.
    
    Part of https://github.com/vector-im/riot-web/issues/12546

--- src/components/structures/auth/Login.js
+++ src/components/structures/auth/Login.js
@@ -245,19 +245,13 @@ export default createReactClass({
             }
 
             this.setState({
+                busy: false,
                 errorText: errorText,
                 // 401 would be the sensible status code for 'incorrect password'
                 // but the login API gives a 403 https://matrix.org/jira/browse/SYN-744
                 // mentions this (although the bug is for UI auth which is not this)
                 // We treat both as an incorrect password
                 loginIncorrect: error.httpStatus === 401 || error.httpStatus === 403,
-            });
-        }).finally(() => {
-            if (this._unmounted) {
-                return;
-            }
-            this.setState({
-                busy: false,
             });
         });
     },
