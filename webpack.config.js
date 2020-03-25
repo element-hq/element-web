a808d26764396e083b7ba0b98e9cde009dfc8c08
Michael Telatynski <7t3chguy@gmail.com>
2020-03-25 12:41:33 +0000

    Enable webpack reuseExistingChunk
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -56,6 +56,9 @@ module.exports = (env, argv) => {
                         enforce: true,
                         // Do not add `chunks: 'all'` here because you'll break the app entry point.
                     },
+                    default: {
+                        reuseExistingChunk: true,
+                    },
                 },
             },
 
