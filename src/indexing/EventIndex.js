c6fcaf86ffafe79dde4b55423999028d98bfc9f9
Damir Jelić <poljar@termina.org.uk>
2020-04-08 11:15:13 +0200

    EventIndex: Log if we had all events in a checkpoint but are continuing.

--- src/indexing/EventIndex.js
+++ src/indexing/EventIndex.js
@@ -530,6 +530,10 @@ export default class EventIndex extends EventEmitter {
                                 "added, stopping the crawl", checkpoint);
                     await indexManager.removeCrawlerCheckpoint(newCheckpoint);
                 } else {
+                    if (eventsAlreadyAdded === true) {
+                        console.log("EventIndex: Checkpoint had already all events",
+                                    "added, but continuing due to a full crawl", checkpoint);
+                    }
                     this.crawlerCheckpoints.push(newCheckpoint);
                 }
             } catch (e) {
