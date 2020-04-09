4510499987b8244f24b41e958dc1990f39bebf7c
Travis Ralston <travpc@gmail.com>
2020-04-09 15:25:11 -0600

    Fix widgets for all other sources too

--- src/widgets/WidgetType.ts
+++ src/widgets/WidgetType.ts
@@ -16,11 +16,22 @@ limitations under the License.
 
 export class WidgetType {
     public static readonly JITSI = new WidgetType("m.jitsi", "jitsi");
+    public static readonly CUSTOM = new WidgetType("m.custom", "m.custom");
 
     constructor(public readonly preferred: string, public readonly legacy: string) {
     }
 
     public matches(type: string): boolean {
         return type === this.preferred || type === this.legacy;
+    }
+
+    static fromString(type: string): WidgetType {
+        // First try and match it against something we're already aware of
+        const known = Object.values(WidgetType).filter(v => v instanceof WidgetType);
+        const knownMatch = known.find(w => w.matches(type));
+        if (knownMatch) return knownMatch;
+
+        // If that fails, invent a new widget type
+        return new WidgetType(type, type);
     }
 }
