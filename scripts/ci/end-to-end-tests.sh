22533d3e6a3fb17c1a124e10a6324157782fbdb6
Travis Ralston <travpc@gmail.com>
2020-04-16 12:46:34 -0600

    Disable scripts in CI too

--- scripts/ci/end-to-end-tests.sh
+++ scripts/ci/end-to-end-tests.sh
@@ -13,25 +13,29 @@ handle_error() {
 
 trap 'handle_error' ERR
 
+echo "Tests are disabled, see https://github.com/vector-im/riot-web/issues/13226"
+exit 0
 
-echo "--- Building Riot"
-scripts/ci/layered-riot-web.sh
-cd ../riot-web
-riot_web_dir=`pwd`
-CI_PACKAGE=true yarn build
-cd ../matrix-react-sdk
-# run end to end tests
-pushd test/end-to-end-tests
-ln -s $riot_web_dir riot/riot-web
-# PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true ./install.sh
-# CHROME_PATH=$(which google-chrome-stable) ./run.sh
-echo "--- Install synapse & other dependencies"
-./install.sh
-# install static webserver to server symlinked local copy of riot
-./riot/install-webserver.sh
-rm -r logs || true
-mkdir logs
-echo "+++ Running end-to-end tests"
-TESTS_STARTED=1
-./run.sh --no-sandbox --log-directory logs/
-popd
+#TODO: Uncomment all of this in https://github.com/vector-im/riot-web/issues/13226
+
+#echo "--- Building Riot"
+#scripts/ci/layered-riot-web.sh
+#cd ../riot-web
+#riot_web_dir=`pwd`
+#CI_PACKAGE=true yarn build
+#cd ../matrix-react-sdk
+## run end to end tests
+#pushd test/end-to-end-tests
+#ln -s $riot_web_dir riot/riot-web
+## PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true ./install.sh
+## CHROME_PATH=$(which google-chrome-stable) ./run.sh
+#echo "--- Install synapse & other dependencies"
+#./install.sh
+## install static webserver to server symlinked local copy of riot
+#./riot/install-webserver.sh
+#rm -r logs || true
+#mkdir logs
+#echo "+++ Running end-to-end tests"
+#TESTS_STARTED=1
+#./run.sh --no-sandbox --log-directory logs/
+#popd
