475cea8b8acf7ccb1924c96a8541dc0e80f2e0b9
Travis Ralston <travpc@gmail.com>
2020-04-22 10:58:11 -0600

    Take encrypted message search out of labs
    Fixes https://github.com/vector-im/riot-web/issues/13262
    
    This is part of the cross-signing shipping master plan. Known issues relating to this feature are:
    * https://github.com/vector-im/riot-web/issues/12896
    * https://github.com/vector-im/riot-web/issues/12385
    * https://github.com/vector-im/riot-web/issues/11831
    * https://github.com/vector-im/riot-web/issues/11155
    
    In theory, these are issues we're comfortable with shipping as we're already enabling it by default. This just makes it easier on everyone by removing the flag (making it still enabled by default).

--- src/settings/Settings.js
+++ src/settings/Settings.js
@@ -158,12 +158,6 @@ export const SETTINGS = {
         supportedLevels: ['device', 'config'], // we shouldn't use LEVELS_FEATURE for non-features, so copy it here.
         default: true,
     },
-    "feature_event_indexing": {
-        isFeature: true,
-        supportedLevels: LEVELS_FEATURE,
-        displayName: _td("Enable local event indexing and E2EE search (requires restart)"),
-        default: false,
-    },
     "feature_bridge_state": {
         isFeature: true,
         supportedLevels: LEVELS_FEATURE,
