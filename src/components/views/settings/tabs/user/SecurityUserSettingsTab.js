ef1e40c83995a6495678d86a4c67d59982b0e0da
David Baker <dave@matrix.org>
2020-04-27 14:35:32 +0100

    Update to (almost) match design

--- src/components/views/settings/tabs/user/SecurityUserSettingsTab.js
+++ src/components/views/settings/tabs/user/SecurityUserSettingsTab.js
@@ -292,11 +292,19 @@ export default class SecurityUserSettingsTab extends React.Component {
         return (
             <div className="mx_SettingsTab mx_SecurityUserSettingsTab">
                 <div className="mx_SettingsTab_heading">{_t("Security & Privacy")}</div>
-                <a href={"#/user/" + MatrixClientPeg.get().getUserId()} onClick={this._onGoToUserProfileClick}>
-                    {_t("Verify your devices in your User Profile")}
-                </a>
                 <div className="mx_SettingsTab_section">
-                    <span className="mx_SettingsTab_subheading">{_t("Sessions")}</span>
+                    <span className="mx_SettingsTab_subheading">{_t("Where you’re logged in")}</span>
+                    <span>
+                        {_t(
+                            "Manage the names of and sign out of your sessions below or " +
+                            "<a>verify them in your User Profile</a>.", {},
+                            {
+                                a: sub => <a href={"#/user/" + MatrixClientPeg.get().getUserId()}
+                                    onClick={this._onGoToUserProfileClick}
+                                >{sub}</a>
+                            }
+                        )}
+                    </span>
                     <div className='mx_SettingsTab_subsectionText'>
                         {_t("A session's public name is visible to people you communicate with")}
                         <DevicesPanel />
