4e98b5b4b60e150f4ed594b280a3fd733803ee40
Bruno Windels <brunow@matrix.org>
2020-04-28 17:41:10 +0200

    dont enable e2ee when inviting a 3pid

--- src/components/views/dialogs/InviteDialog.js
+++ src/components/views/dialogs/InviteDialog.js
@@ -577,10 +577,13 @@ export default class InviteDialog extends React.PureComponent {
         if (SettingsStore.getValue("feature_cross_signing")) {
             // Check whether all users have uploaded device keys before.
             // If so, enable encryption in the new room.
-            const client = MatrixClientPeg.get();
-            const allHaveDeviceKeys = await canEncryptToAllUsers(client, targetIds);
-            if (allHaveDeviceKeys) {
-                createRoomOptions.encryption = true;
+            const containsNonMatrixUsers = targets.some(t => !(t instanceof DirectoryMember));
+            if (!containsNonMatrixUsers) {
+                const client = MatrixClientPeg.get();
+                const allHaveDeviceKeys = await canEncryptToAllUsers(client, targetIds);
+                if (allHaveDeviceKeys) {
+                    createRoomOptions.encryption = true;
+                }
             }
         }
 
