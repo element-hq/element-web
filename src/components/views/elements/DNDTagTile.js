fc5a5c411f41c004fae10241f3dc4d4c013e307a
Agusti Bau <agustibau@gmail.com>
2020-05-02 13:09:04 +0200

    provided.placeholder has no effect

--- src/components/views/elements/DNDTagTile.js
+++ src/components/views/elements/DNDTagTile.js
@@ -54,7 +54,6 @@ export default function DNDTagTile(props) {
                                menuDisplayed={menuDisplayed}
                                openMenu={openMenu}
                             />
-                        { provided.placeholder }
                         </div>
                 ) }
             </Draggable>
