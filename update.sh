6168ee2a1fa40eb4f558fdbd040395c212d8dc03
Abhishek Gupta <a@navgurukul.org>
2020-05-20 07:47:39 +0530

    updated update file

--- update.sh
+++ update.sh
@@ -5,3 +5,4 @@ git push
 # Random Comment
 # Random Comment
 # Random Comment
+# Random Comment
--- update_sdk.sh
+++ update_sdk.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-echo "# Random Comment" >> update.sh
+cd ../matrix-react-sdk;
+git pull main develop;
+git push;
 
+cd -;
+
+echo "# Random Comment" >> update.sh
 git commit -am 'updated update file'
+
 git pull main develop
 git push
