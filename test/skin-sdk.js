47bfa16a2db6d0c4086f4c9834cca0b15f983c06
Abhishek Gupta <a@navgurukul.org>
2020-05-25 18:25:36 +0530

    ok

--- test/skin-sdk.js
+++
@@ -1,9 +0,0 @@
-/*
- * skin-sdk.js
- *
- * Skins the react-sdk with the vector components
- */
-
-import * as sdk from "matrix-react-sdk";
-import * as skin from "../src/component-index";
-sdk.loadSkin(skin);
