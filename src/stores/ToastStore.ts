3dade4e759bc4d01af126e1f3a5d64c4057b1d20
Michael Telatynski <7t3chguy@gmail.com>
2020-05-27 11:48:48 +0100

    tweak
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- src/stores/ToastStore.ts
+++ src/stores/ToastStore.ts
@@ -70,13 +70,15 @@ export default class ToastStore extends EventEmitter {
 
     dismissToast(key) {
         console.log("DEBUG dismissToast", key);
+        if (this.toasts[0] && this.toasts[0].key === key) {
+            this.countSeen++;
+        }
+
         const length = this.toasts.length;
         this.toasts = this.toasts.filter(t => t.key !== key);
         if (length !== this.toasts.length) {
             if (this.toasts.length === 0) {
                 this.countSeen = 0;
-            } else {
-                this.countSeen++;
             }
 
             this.emit('update');
