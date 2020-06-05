0c32daa162e7f3b273f7b09efc1ce3b230398c21
Michael Telatynski <7t3chguy@gmail.com>
2020-06-05 23:34:04 +0100

    label ZWJ as such
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- src/emoji.ts
+++ src/emoji.ts
@@ -62,6 +62,8 @@ export const DATA_BY_CATEGORY = {
     "flags": [],
 };
 
+const ZERO_WIDTH_JOINER = "\u200D";
+
 // Store various mappings from unicode/emoticon/shortcode to the Emoji objects
 EMOJIBASE.forEach((emoji: IEmojiWithFilterString) => {
     const categoryId = EMOJIBASE_GROUP_ID_TO_CATEGORY[emoji.group];
@@ -70,7 +72,7 @@ EMOJIBASE.forEach((emoji: IEmojiWithFilterString) => {
     }
     // This is used as the string to match the query against when filtering emojis
     emoji.filterString = `${emoji.annotation}\n${emoji.shortcodes.join('\n')}}\n${emoji.emoticon || ''}\n` +
-        `${emoji.unicode.split("\u200D").join("\n")}`.toLowerCase();
+        `${emoji.unicode.split(ZERO_WIDTH_JOINER).join("\n")}`.toLowerCase();
 
     // Add mapping from unicode to Emoji object
     // The 'unicode' field that we use in emojibase has either
