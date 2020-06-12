fd029e8e8021cdf475d95ed13f65058900695d82
Travis Ralston <travpc@gmail.com>
2020-06-11 21:24:25 -0600

    Dumb down list algorithms in favour of smarter tags
    This commit is a bit involved, as it factors the tag specific handling out of `/list-ordering` (and moves the `Algorithm` class one higher as a result), leaving it in the `Algorithm`. The algorithms for list ordering now only know how to handle a single tag, and this is managed by the `Algorithm` class - which is also no longer the base class for the list ordering. The list ordering algorithms now inherit from a generic `OrderingAlgorithm` base class which handles some rudimentary things.
    
    Overall the logic hasn't changed much: the tag-specific stuff has been moved into the `Algorithm`, and the list ordering algorithms essentially just removed the iteration on tags. The `RoomListStore2` still shovels a bunch of information over to the `Algorithm`, which can lead to an awkward code flow however this commit is meant to keep the number of surfaces touched to a minimum.
    
    The RoomListStore has also gained the ability to set per-list (tag) ordering and sorting, which is required for the new room list. The assumption that it defaults from the account-level settings is not reviewed by design, yet. This decision is deferred.

--- src/stores/room-list/algorithms/models.ts
+++ src/stores/room-list/algorithms/models.ts
@@ -16,6 +16,7 @@ limitations under the License.
 
 import { TagID } from "../models";
 import { Room } from "matrix-js-sdk/src/models/room";
+import { OrderingAlgorithm } from "./list-ordering/OrderingAlgorithm";
 
 export enum SortAlgorithm {
     Manual = "MANUAL",
@@ -34,6 +35,16 @@ export enum ListAlgorithm {
 export interface ITagSortingMap {
     // @ts-ignore - TypeScript really wants this to be [tagId: string] but we know better.
     [tagId: TagID]: SortAlgorithm;
+}
+
+export interface IListOrderingMap {
+    // @ts-ignore - TypeScript really wants this to be [tagId: string] but we know better.
+    [tagId: TagID]: ListAlgorithm;
+}
+
+export interface IOrderingAlgorithmMap {
+    // @ts-ignore - TypeScript really wants this to be [tagId: string] but we know better.
+    [tagId: TagID]: OrderingAlgorithm;
 }
 
 export interface ITagMap {
