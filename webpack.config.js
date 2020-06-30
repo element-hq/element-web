c9c402310ade542edce52ae771d9d9ecafb18288
Bruno Windels <brunow@matrix.org>
2020-06-30 13:27:58 +0200

    element theme webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,7 @@ module.exports = (env, argv) => {
             "usercontent": "./node_modules/matrix-react-sdk/src/usercontent/index.js",
 
             // CSS themes
+            "theme-element": "./node_modules/matrix-react-sdk/res/themes/element/css/element.scss",
             "theme-light": "./node_modules/matrix-react-sdk/res/themes/light/css/light.scss",
             "theme-dark": "./node_modules/matrix-react-sdk/res/themes/dark/css/dark.scss",
             "theme-light-custom": "./node_modules/matrix-react-sdk/res/themes/light-custom/css/light-custom.scss",
