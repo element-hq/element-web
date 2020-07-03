b370e3f0788b889e24adafafcee7e5d361081705
Bruno Windels <brunow@matrix.org>
2020-07-03 16:52:01 +0200

    don't need to set width with javascript?

--- src/components/structures/LeftPanel2.tsx
+++ src/components/structures/LeftPanel2.tsx
@@ -119,13 +119,11 @@ export default class LeftPanel2 extends React.Component<IProps, IState> {
             if (slRect.top + headerHeight > bottom && !gotBottom) {
                 header.classList.add("mx_RoomSublist2_headerContainer_sticky");
                 header.classList.add("mx_RoomSublist2_headerContainer_stickyBottom");
-                header.style.width = `${headerStickyWidth}px`;
                 header.style.top = `unset`;
                 gotBottom = true;
             } else if (slRect.top < top) {
                 header.classList.add("mx_RoomSublist2_headerContainer_sticky");
                 header.classList.add("mx_RoomSublist2_headerContainer_stickyTop");
-                header.style.width = `${headerStickyWidth}px`;
                 header.style.top = `${rlRect.top}px`;
                 if (lastTopHeader) {
                     lastTopHeader.style.display = "none";
@@ -137,7 +135,6 @@ export default class LeftPanel2 extends React.Component<IProps, IState> {
                 header.classList.remove("mx_RoomSublist2_headerContainer_sticky");
                 header.classList.remove("mx_RoomSublist2_headerContainer_stickyTop");
                 header.classList.remove("mx_RoomSublist2_headerContainer_stickyBottom");
-                header.style.width = `unset`;
                 header.style.top = `unset`;
             }
         }
