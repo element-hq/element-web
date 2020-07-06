8fd9dbf1d889defb37c70532048b342c5332fcfd
Bruno Windels <brunow@matrix.org>
2020-07-06 18:28:54 +0200

    moar icon changes

--- src/components/views/context_menus/TagTileContextMenu.js
+++ src/components/views/context_menus/TagTileContextMenu.js
@@ -60,7 +60,7 @@ export default class TagTileContextMenu extends React.Component {
             <MenuItem className="mx_TagTileContextMenu_item" onClick={this._onViewCommunityClick}>
                 <TintableSvg
                     className="mx_TagTileContextMenu_item_icon"
-                    src={require("../../../../res/img/icons-groups.svg")}
+                    src={require("../../../../res/img/element-icons/view-community.svg")}
                     width="15"
                     height="15"
                 />
