cfc39dc4a93a62638d0c4f28f13bbebb58f5ff43
Travis Ralston <travpc@gmail.com>
2020-07-10 16:10:05 -0600

    Remove now-dead code from sublist resizing

--- src/stores/room-list/ListLayout.ts
+++ src/stores/room-list/ListLayout.ts
@@ -18,11 +18,6 @@ import { TagID } from "./models";
 
 const TILE_HEIGHT_PX = 44;
 
-// this comes from the CSS where the show more button is
-// mathematically this percent of a tile when floating.
-//const RESIZER_BOX_FACTOR = 0.78;
-const RESIZER_BOX_FACTOR = 0;
-
 interface ISerializedListLayout {
     numTiles: number;
     showPreviews: boolean;
@@ -82,36 +77,12 @@ export class ListLayout {
     }
 
     public get minVisibleTiles(): number {
-        return 1 + RESIZER_BOX_FACTOR;
+        return 1;
     }
 
     public get defaultVisibleTiles(): number {
         // This number is what "feels right", and mostly subject to design's opinion.
-        return 5 + RESIZER_BOX_FACTOR;
-    }
-
-    public setVisibleTilesWithin(newVal: number, maxPossible: number) {
-        maxPossible = maxPossible + RESIZER_BOX_FACTOR;
-        if (newVal > maxPossible) {
-            this.visibleTiles = maxPossible;
-        } else {
-            this.visibleTiles = newVal;
-        }
-    }
-
-    public calculateTilesToPixelsMin(maxTiles: number, n: number, possiblePadding: number): number {
-        // Only apply the padding if we're about to use maxTiles as we need to
-        // plan for the padding. If we're using n, the padding is already accounted
-        // for by the resizing stuff.
-        let padding = 0;
-        if (maxTiles < n) {
-            padding = possiblePadding;
-        }
-        return this.tilesToPixels(Math.min(maxTiles, n)) + padding;
-    }
-
-    public tilesWithResizerBoxFactor(n: number): number {
-        return n + RESIZER_BOX_FACTOR;
+        return 5;
     }
 
     public tilesWithPadding(n: number, paddingPx: number): number {
