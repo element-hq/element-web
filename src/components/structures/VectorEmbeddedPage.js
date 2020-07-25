f21352f55817eeee49f392405f00284ea615aef8
Matthew Hodgson <matthew@matrix.org>
2020-07-25 02:25:08 +0100

    spell Matrix correctly

--- src/components/structures/VectorEmbeddedPage.js
+++ src/components/structures/VectorEmbeddedPage.js
@@ -30,7 +30,7 @@ export default class VectorEmbeddedPage extends EmbeddedPage {
         s = sanitizeHtml(_t(s));
         // ugly fix for https://github.com/vector-im/riot-web/issues/4243
         // eslint-disable-next-line max-len
-        s = s.replace(/\[matrix\]/, '<a href="https://matrix.org" target="_blank" rel="noreferrer noopener"><img width="79" height="34" alt="[matrix]" style="padding-left: 1px;vertical-align: middle" src="welcome/images/matrix.svg"/></a>');
+        s = s.replace(/\[matrix\]/, '<a href="https://matrix.org" target="_blank" rel="noreferrer noopener"><img width="79" height="34" alt="Matrix" style="padding-left: 1px;vertical-align: middle" src="welcome/images/matrix.svg"/></a>');
         return s;
     }
 }
