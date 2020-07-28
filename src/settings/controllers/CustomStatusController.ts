d4fd3b32625677d05af42fa9aee174442ef57a4b
Travis Ralston <travpc@gmail.com>
2020-07-28 15:17:01 -0600

    Convert CustomStatusController to TS

--- src/settings/controllers/CustomStatusController.ts
+++ src/settings/controllers/CustomStatusController.ts
@@ -1,5 +1,6 @@
 /*
 Copyright 2019 New Vector Ltd
+Copyright 2020 The Matrix.org Foundation C.I.C.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -16,9 +17,10 @@ limitations under the License.
 
 import SettingController from "./SettingController";
 import dis from "../../dispatcher/dispatcher";
+import { SettingLevel } from "../SettingLevel";
 
 export default class CustomStatusController extends SettingController {
-    onChange(level, roomId, newValue) {
+    public onChange(level: SettingLevel, roomId: string, newValue: any) {
         // Dispatch setting change so that some components that are still visible when the
         // Settings page is open (such as RoomTiles) can reflect the change.
         dis.dispatch({
