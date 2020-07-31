02ca5011a3ccf62bdc2afd277f9f2b175c0ecdeb
thomcatdotrocks <37344783+thomcatdotrocks@users.noreply.github.com>
2020-07-31 15:59:13 -0500

    Updating package.sh
    This should correct the tarball structure from `riot-$version` to `element-$version`.

--- scripts/package.sh
+++ scripts/package.sh
@@ -16,20 +16,20 @@ yarn build
 cp config.sample.json webapp/
 
 mkdir -p dist
-cp -r webapp riot-$version
+cp -r webapp element-$version
 
 # Just in case you have a local config, remove it before packaging
-rm riot-$version/config.json || true
+rm element-$version/config.json || true
 
 # if $version looks like semver with leading v, strip it before writing to file
 if [[ ${version} =~ ^v[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+(-.+)?$ ]]; then
-    echo ${version:1} > riot-$version/version
+    echo ${version:1} > element-$version/version
 else
-    echo ${version} > riot-$version/version
+    echo ${version} > element-$version/version
 fi
 
-tar chvzf dist/riot-$version.tar.gz riot-$version
-rm -r riot-$version
+tar chvzf dist/element-$version.tar.gz element-$version
+rm -r element-$version
 
 echo
-echo "Packaged dist/riot-$version.tar.gz"
+echo "Packaged dist/element-$version.tar.gz"
