2698a12a6fc61e8e9a1d992b7054f116d355651f
J. Ryan Stinnett <jryans@gmail.com>
2020-10-09 16:59:56 +0100

    Convert `src/SecurityManager.js` to TypeScript
    This includes a few small API tweaks as well, only in cases where TS revealed we
    were doing something confusing or wrong.
    
    Part of https://github.com/vector-im/element-web/issues/15350

--- src/MatrixClientPeg.ts
+++ src/MatrixClientPeg.ts
@@ -17,6 +17,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+import { ICreateClientOpts } from 'matrix-js-sdk/src/matrix';
 import {MatrixClient} from 'matrix-js-sdk/src/client';
 import {MemoryStore} from 'matrix-js-sdk/src/store/memory';
 import * as utils from 'matrix-js-sdk/src/utils';
@@ -249,8 +250,7 @@ class _MatrixClientPeg implements IMatrixClientPeg {
     }
 
     private createClient(creds: IMatrixClientCreds): void {
-        // TODO: Make these opts typesafe with the js-sdk
-        const opts = {
+        const opts: ICreateClientOpts = {
             baseUrl: creds.homeserverUrl,
             idBaseUrl: creds.identityServerUrl,
             accessToken: creds.accessToken,
