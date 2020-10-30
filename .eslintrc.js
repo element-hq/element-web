30a28853cc8fe40a6fb71ce2a34698f4079e02c8
cs-allan-johnson <allan.johnson.d.leal@concrete.com.br>
2020-10-30 19:24:45 -0300

    adding favIcon and update package

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,23 @@
 module.exports = {
-    "extends": ["matrix-org", "matrix-org/react"],
     "env": {
         "browser": true,
-        "node": true,
+        "es2021": true
     },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "node"
+    ],
     "rules": {
-        "quotes": "off",
-    },
-    "overrides": [{
-        "files": ["src/**/*.{ts,tsx}"],
-        "extends": ["matrix-org/ts", "matrix-org/react"],
-        "env": {
-            "browser": true,
-        },
-        "rules": {
-            "quotes": "off",
-            // While converting to ts we allow this
-            "@typescript-eslint/no-explicit-any": "off",
-            "prefer-promise-reject-errors": "off",
-        },
-    }],
+    }
 };
