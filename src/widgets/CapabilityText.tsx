7e0cf5c7832a46243f4b145a06c7ac821eda4e14
Travis Ralston <travpc@gmail.com>
2020-12-29 12:35:48 -0700

    Wire up MSC2931 widget navigation
    Fixes https://github.com/vector-im/element-web/issues/16006
    **Requires https://github.com/matrix-org/matrix-widget-api/pull/27**

--- src/widgets/CapabilityText.tsx
+++ src/widgets/CapabilityText.tsx
@@ -60,6 +60,9 @@ export class CapabilityText {
         [ElementWidgetCapabilities.CanChangeViewedRoom]: {
             [GENERIC_WIDGET_KIND]: _td("Change which room you're viewing"),
         },
+        [MatrixCapabilities.MSC2931Navigate]: {
+            [GENERIC_WIDGET_KIND]: _td("Change which room you're viewing and use permalinks"),
+        },
     };
 
     private static stateSendRecvCaps: ISendRecvStaticCapText = {
