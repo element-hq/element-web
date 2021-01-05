bb7e76f69e9cf901acf041b00e12b0c2542278d6
Šimon Brandner <simon.bra.ag@gmail.com>
2021-01-05 20:36:54 +0100

    Hopefully fixed copyrights
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- res/css/views/elements/_DesktopCapturerSourcePicker.scss
+++ res/css/views/elements/_DesktopCapturerSourcePicker.scss
@@ -1,12 +1,11 @@
 /*
-Copyright 2015, 2016 OpenMarket Ltd
-Copyright 2019 Michael Telatynski <7t3chguy@gmail.com>
+Copyright 2021 Šimon Brandner <simon.bra.ag@gmail.com>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
