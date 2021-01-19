ab51404421f9bb060962f200b0af28a0afb34e54
Travis Ralston <travpc@gmail.com>
2021-01-18 21:07:44 -0700

    Appease the tests?

--- src/stores/room-list/RoomListStore.ts
+++ src/stores/room-list/RoomListStore.ts
@@ -114,7 +114,7 @@ export class RoomListStoreClass extends AsyncStoreWithClient<IState> {
     // Public for test usage. Do not call this.
     public async makeReady(forcedClient?: MatrixClient) {
         if (forcedClient) {
-            super.readyStore.useUnitTestClient(forcedClient);
+            this.readyStore.useUnitTestClient(forcedClient);
         }
 
         this.checkLoggingEnabled();
