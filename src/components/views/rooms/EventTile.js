90ad3360b628136f4e5dfc69db2c4fd81e07f0aa
Šimon Brandner <simon.bra.ag@gmail.com>
2021-01-24 09:15:11 +0100

    Fixed read receipts?
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/components/views/rooms/EventTile.js
+++ src/components/views/rooms/EventTile.js
@@ -950,9 +950,6 @@ export default class EventTile extends React.Component {
                 return (
                     <div className={classes} tabIndex={-1} aria-live={ariaLive} aria-atomic="true">
                         { ircTimestamp }
-                        <div className="mx_EventTile_msgOption">
-                            { readAvatars }
-                        </div>
                         { sender }
                         { ircPadlock }
                         <div className="mx_EventTile_line">
@@ -970,6 +967,9 @@ export default class EventTile extends React.Component {
                             { keyRequestInfo }
                             { reactionsRow }
                             { actionBar }
+                        </div>
+                        <div className="mx_EventTile_msgOption">
+                            { readAvatars }
                         </div>
                         {
                             // The avatar goes after the event tile as it's absolutely positioned to be over the
