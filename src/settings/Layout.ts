20193ad7fed0f8ccb54b56424820ea431a9fc1e5
Šimon Brandner <simon.bra.ag@gmail.com>
2021-02-05 08:16:06 +0100

    Added LayoutPropType
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/settings/Layout.ts
+++ src/settings/Layout.ts
@@ -14,8 +14,13 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+import PropTypes from 'prop-types';
+
 /* TODO: This should be later reworked into something more generic */
 export enum Layout {
     IRC = "irc",
     Group = "group"
 }
+
+/* We need this because multiple components are still using JavaScript */
+export const LayoutPropType = PropTypes.oneOf(Object.values(Layout));
