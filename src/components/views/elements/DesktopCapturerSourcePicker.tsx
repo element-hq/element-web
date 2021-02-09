27b2b5b65dbe14c30e24db2fc7aaeede57d6e37f
Šimon Brandner <simon.bra.ag@gmail.com>
2021-02-09 13:19:12 +0100

    Make componentDidMount async
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/components/views/elements/DesktopCapturerSourcePicker.tsx
+++ src/components/views/elements/DesktopCapturerSourcePicker.tsx
@@ -84,14 +84,12 @@ export default class DesktopCapturerSourcePicker extends React.Component<
         };
     }
 
-    componentDidMount() {
+    async componentDidMount() {
         // setInterval() first waits and then executes, therefore
         // we call getDesktopCapturerSources() here without any delay.
         // Otherwise the dialog would be left empty for some time.
-        getDesktopCapturerSources().then((result) => {
-            this.setState({
-                sources: result,
-            });
+        this.setState({
+            sources: await getDesktopCapturerSources(),
         });
 
         // We update the sources every 500ms to get newer thumbnails
