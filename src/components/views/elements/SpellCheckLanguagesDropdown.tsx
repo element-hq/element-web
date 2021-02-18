5de99c7708c58b2808d288160ce8fb69f4f5027c
Šimon Brandner <simon.bra.ag@gmail.com>
2021-02-18 19:40:24 +0100

    Fix licenses
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/components/views/elements/SpellCheckLanguagesDropdown.tsx
+++ src/components/views/elements/SpellCheckLanguagesDropdown.tsx
@@ -1,6 +1,5 @@
 /*
-Copyright 2017 Marcel Radzio (MTRNord)
-Copyright 2017 Vector Creations Ltd.
+Copyright 2021 Šimon Brandner <simon.bra.ag@gmail.com>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
