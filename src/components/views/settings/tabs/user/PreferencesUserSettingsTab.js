6fcb4c7cd2ddf0a5fc64ff1796d079389a3d8482
Šimon Brandner <simon.bra.ag@gmail.com>
2021-03-02 07:37:00 +0100

    Fix quote
    Co-authored-by: Travis Ralston <travpc@gmail.com>

--- src/components/views/settings/tabs/user/PreferencesUserSettingsTab.js
+++ src/components/views/settings/tabs/user/PreferencesUserSettingsTab.js
@@ -48,7 +48,7 @@ export default class PreferencesUserSettingsTab extends React.Component {
         'showRedactions',
         'enableSyntaxHighlightLanguageDetection',
         'expandCodeByDefault',
-        `scrollToBottomOnMessageSent`,
+        'scrollToBottomOnMessageSent',
         'showCodeLineNumbers',
         'showJoinLeaves',
         'showAvatarChanges',
