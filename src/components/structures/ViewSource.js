6d792cc08cec87e5ad6a856c5e4c9e593ac974df
Panagiotis <27917356+panoschal@users.noreply.github.com>
2021-03-03 23:48:39 +0200

    feat: use <details> to hide encrypted block

--- src/components/structures/ViewSource.js
+++ src/components/structures/ViewSource.js
@@ -36,29 +36,42 @@ export default class ViewSource extends React.Component {
     render() {
         const BaseDialog = sdk.getComponent('views.dialogs.BaseDialog');
 
-        const DecryptedSection = <>
-            <div className="mx_ViewSource_separator" />
-            <div className="mx_ViewSource_heading">{_t("Decrypted event source")}</div>
-            <SyntaxHighlight className="json">
-                { JSON.stringify(this.props.decryptedContent, null, 2) }
-            </SyntaxHighlight>
-        </>;
-
-        const OriginalSection = <>
-            <div className="mx_ViewSource_separator" />
-            <div className="mx_ViewSource_heading">{_t("Original event source")}</div>
-            <SyntaxHighlight className="json">
-                { JSON.stringify(this.props.content, null, 2) }
-            </SyntaxHighlight>
-        </>;
+        let content;
+        if (this.props.isEncrypted) {
+            content = <>
+                <details open className="mx_ViewSource_details">
+                    <summary>
+                        <span className="mx_ViewSource_heading">{_t("Decrypted event source")}</span>
+                    </summary>
+                    <SyntaxHighlight className="json">
+                        { JSON.stringify(this.props.decryptedContent, null, 2) }
+                    </SyntaxHighlight>
+                </details>
+                <details className="mx_ViewSource_details">
+                    <summary>
+                        <span className="mx_ViewSource_heading">{_t("Original event source")}</span>
+                    </summary>
+                    <SyntaxHighlight className="json">
+                        { JSON.stringify(this.props.content, null, 2) }
+                    </SyntaxHighlight>
+                </details>
+            </>;
+        } else {
+            content = <>
+                <div className="mx_ViewSource_heading">{_t("Original event source")}</div>
+                <SyntaxHighlight className="json">
+                    { JSON.stringify(this.props.content, null, 2) }
+                </SyntaxHighlight>
+            </>;
+        }
 
         return (
             <BaseDialog className="mx_ViewSource" onFinished={this.props.onFinished} title={_t('View Source')}>
                 <div className="mx_Dialog_content">
                     <div className="mx_ViewSource_label_left">Room ID: { this.props.roomId }</div>
                     <div className="mx_ViewSource_label_left">Event ID: { this.props.eventId }</div>
-                    { this.props.isEncrypted && DecryptedSection }
-                    { OriginalSection }
+                    <div className="mx_ViewSource_separator" />
+                    { content }
                 </div>
             </BaseDialog>
         );
