1c7cf8645e177fdf5b9232a0feb166e44d61845e
Travis Ralston <travpc@gmail.com>
2021-03-09 12:06:59 -0700

    Remove a couple useless 'use strict' calls

--- src/components/views/auth/VectorAuthHeaderLogo.js
+++ src/components/views/auth/VectorAuthHeaderLogo.js
@@ -15,8 +15,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-'use strict';
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import SdkConfig from 'matrix-react-sdk/src/SdkConfig';
