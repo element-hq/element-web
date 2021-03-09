375ffafda647a47f9390b272f93b84ca2d8571a0
Travis Ralston <travpc@gmail.com>
2021-03-09 10:13:16 -0700

    Fix tests failing to load skin
    We should find a better way to do this, but this works well enough.

--- test/editor/deserialize-test.js
+++ test/editor/deserialize-test.js
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+import '../skinned-sdk'; // Must be first for skinning to work
 import {parseEvent} from "../../src/editor/deserialize";
 import {createPartCreator} from "./mock";
 
