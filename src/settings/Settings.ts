69f90ee97eef64af7b23a8b91d1c53536b810012
Travis Ralston <travpc@gmail.com>
2021-03-16 23:55:07 -0600

    Label labs flag as in development

--- src/settings/Settings.ts
+++ src/settings/Settings.ts
@@ -130,7 +130,7 @@ export const SETTINGS: {[setting: string]: ISetting} = {
     },
     "feature_voice_messages": {
         isFeature: true,
-        displayName: _td("Send and receive voice messages"),
+        displayName: _td("Send and receive voice messages (in development)"),
         supportedLevels: LEVELS_FEATURE,
         default: false,
     },
