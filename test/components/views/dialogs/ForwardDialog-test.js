400917623ce24f37b4eb12fda5769531f7c635c5
Robin Townsend <robin@robin.town>
2021-05-24 08:33:28 -0400

    Make myself the copyright holder for forward dialog code
    Signed-off-by: Robin Townsend <robin@robin.town>

--- test/components/views/dialogs/ForwardDialog-test.js
+++ test/components/views/dialogs/ForwardDialog-test.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 The Matrix.org Foundation C.I.C.
+Copyright 2021 Robin Townsend.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
