a6ca8f797dd851552608b5ea0c8355d34bb9d25c
Michael Telatynski <7t3chguy@gmail.com>
2021-05-25 15:34:44 +0100

    Fix useAsyncMemo out-of-order resolutions

--- src/hooks/useAsyncMemo.ts
+++ src/hooks/useAsyncMemo.ts
@@ -21,7 +21,15 @@ type Fn<T> = () => Promise<T>;
 export const useAsyncMemo = <T>(fn: Fn<T>, deps: DependencyList, initialValue?: T): T => {
     const [value, setValue] = useState<T>(initialValue);
     useEffect(() => {
-        fn().then(setValue);
+        let discard = false;
+        fn().then(v => {
+            if (!discard) {
+                setValue(v);
+            }
+        });
+        return () => {
+            discard = true;
+        };
     }, deps); // eslint-disable-line react-hooks/exhaustive-deps
     return value;
 };
