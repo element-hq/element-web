527723c63045fa108953be195d379725f690db83
Šimon Brandner <simon.bra.ag@gmail.com>
2021-06-01 11:51:05 +0200

    Remove unused import
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/TextForEvent.js
+++ src/TextForEvent.js
@@ -13,7 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-import {MatrixClientPeg} from './MatrixClientPeg';
 import { _t } from './languageHandler';
 import * as Roles from './Roles';
 import {isValid3pidInvite} from "./RoomInvite";
