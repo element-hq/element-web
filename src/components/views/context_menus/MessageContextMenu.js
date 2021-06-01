c78167977a49421156f62d995cf5aab97801a758
Robin Townsend <robin@robin.town>
2021-06-01 17:57:26 -0400

    Remove unused class
    Signed-off-by: Robin Townsend <robin@robin.town>

--- src/components/views/context_menus/MessageContextMenu.js
+++ src/components/views/context_menus/MessageContextMenu.js
@@ -161,7 +161,7 @@ export default class MessageContextMenu extends React.Component {
             matrixClient: MatrixClientPeg.get(),
             event: this.props.mxEvent,
             permalinkCreator: this.props.permalinkCreator,
-        }, 'mx_Dialog_forwardMessage');
+        });
         this.closeMenu();
     };
 
