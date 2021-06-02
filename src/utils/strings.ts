f3431feaffe913ad792152f631e2cd7bb9f89b2e
Germain Souquet <germain@souquet.com>
2021-06-02 11:25:32 +0100

    Remove unused memoize import

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -14,8 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import { memoize } from "lodash";
-
 /**
  * Copy plaintext to user's clipboard
  * It will overwrite user's selection range
