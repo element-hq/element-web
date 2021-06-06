e891d18fa264491fe81fbbcd893fa5c9fdc1d51a
Robin Townsend <robin@robin.town>
2021-06-05 21:41:28 -0400

    Add my email to my copyright notices
    Signed-off-by: Robin Townsend <robin@robin.town>

--- test/components/views/dialogs/ForwardDialog-test.js
+++ test/components/views/dialogs/ForwardDialog-test.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 Robin Townsend.
+Copyright 2021 Robin Townsend <robin@robin.town>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
