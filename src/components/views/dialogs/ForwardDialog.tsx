b00ad63a35fa2aa18c0ca04b803b34e252fa270c
Robin Townsend <robin@robin.town>
2021-06-07 19:16:00 -0400

    Fix whitespace nitpick
    Signed-off-by: Robin Townsend <robin@robin.town>

--- src/components/views/dialogs/ForwardDialog.tsx
+++ src/components/views/dialogs/ForwardDialog.tsx
@@ -201,7 +201,7 @@ const ForwardDialog: React.FC<IProps> = ({ matrixClient: cli, event, permalinkCr
         onFinished={onFinished}
         fixedWidth={false}
     >
-        <h3>{_t("Message preview")}</h3>
+        <h3>{ _t("Message preview") }</h3>
         <div className={classnames("mx_ForwardDialog_preview", {
             "mx_IRCLayout": previewLayout == Layout.IRC,
             "mx_GroupLayout": previewLayout == Layout.Group,
