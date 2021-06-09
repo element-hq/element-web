b43315c6c1c555ebce7a032dcc2f7950e999ed83
J. Ryan Stinnett <jryans@gmail.com>
2021-06-09 15:46:10 +0100

    Restore read receipt animation from event to event
    This restores expected read receipt animation by always including the positioned
    parent of read receipts. I imagine there's something smarter we could be doing,
    but for now, at least least get back to expected behaviour.
    
    Fixes https://github.com/vector-im/element-web/issues/17561

--- src/components/views/rooms/EventTile.tsx
+++ src/components/views/rooms/EventTile.tsx
@@ -644,7 +644,18 @@ export default class EventTile extends React.Component<IProps, IState> {
 
         // return early if there are no read receipts
         if (!this.props.readReceipts || this.props.readReceipts.length === 0) {
-            return null;
+            // We currently must include `mx_EventTile_readAvatars` in the DOM
+            // of all events, as it is the positioned parent of the animated
+            // read receipts. We can't let it unmount when a receipt moves
+            // events, so for now we mount it for all events. With out it, the
+            // animation will start from the top of the timeline (because it
+            // lost its container).
+            // See also https://github.com/vector-im/element-web/issues/17561
+            return (
+                <div className="mx_EventTile_msgOption">
+                    <span className="mx_EventTile_readAvatars" />
+                </div>
+            );
         }
 
         const ReadReceiptMarker = sdk.getComponent('rooms.ReadReceiptMarker');
