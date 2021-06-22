9dc8493a5c845336dafc774193353d08e1ef5971
Michael Telatynski <7t3chguy@gmail.com>
2021-06-22 22:07:42 +0100

    Hide communities invites and the community autocompleter when Spaces Beta is enabled

--- src/components/views/rooms/RoomList.tsx
+++ src/components/views/rooms/RoomList.tsx
@@ -466,6 +466,7 @@ export default class RoomList extends React.PureComponent<IProps, IState> {
     }
 
     private renderCommunityInvites(): ReactComponentElement<typeof ExtraTile>[] {
+        if (SettingsStore.getValue("feature_spaces")) return [];
         // TODO: Put community invites in a more sensible place (not in the room list)
         // See https://github.com/vector-im/element-web/issues/14456
         return MatrixClientPeg.get().getGroups().filter(g => {
