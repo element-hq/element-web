593f14beae8ba0cb412f2b9b62c2a41eef88eff9
Jaiwanth <jaiwanth2011@gmail.com>
2021-06-25 15:01:14 +0530

    Append media text to text for event

--- src/utils/exportUtils/PlainTextExport.ts
+++ src/utils/exportUtils/PlainTextExport.ts
@@ -75,7 +75,7 @@ export default class PlainTextExporter extends Exporter {
             } else mediaText = ` (${this.mediaOmitText})`;
         }
         if (this.isReply(mxEv)) return senderDisplayName + ": " + this.textForReplyEvent(mxEv) + mediaText;
-        else return textForEvent(mxEv);
+        else return textForEvent(mxEv) + mediaText;
     }
 
     protected async createOutput(events: MatrixEvent[]) {
