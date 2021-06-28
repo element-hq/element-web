bf556722636fbdb2c0777036dd940983e9c1af5e
Tulir Asokan <tulir@maunium.net>
2021-06-28 21:07:58 +0300

    Remove unused import
    Signed-off-by: Tulir Asokan <tulir@maunium.net>

--- src/Unread.ts
+++ src/Unread.ts
@@ -16,7 +16,7 @@ limitations under the License.
 
 import { Room } from "matrix-js-sdk/src/models/room";
 import { MatrixEvent } from "matrix-js-sdk/src/models/event";
-import { EventType, MsgType } from "matrix-js-sdk/src/@types/event";
+import { EventType } from "matrix-js-sdk/src/@types/event";
 
 import { MatrixClientPeg } from "./MatrixClientPeg";
 import shouldHideEvent from './shouldHideEvent';
