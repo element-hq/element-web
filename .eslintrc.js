573698789e46e0a0cd993ae1615ae05859fe1971
J. Ryan Stinnett <jryans@gmail.com>
2021-06-30 13:24:16 +0100

    Migrate to `eslint-plugin-matrix-org`
    This migrates to the new plugin form of our custom ESLint configs. As part of
    this, some packages are de-duplicated, configs streamlined, etc.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,30 @@
 module.exports = {
-    extends: ["matrix-org", "matrix-org/react"],
+    plugins: ["matrix-org"],
+    extends: [
+        "plugin:matrix-org/babel",
+        "plugin:matrix-org/react",
+    ],
     env: {
         browser: true,
         node: true,
     },
     rules: {
+        // Things we do that break the ideal style
         "quotes": "off",
     },
     overrides: [{
         files: ["src/**/*.{ts,tsx}"],
-        extends: ["matrix-org/ts", "matrix-org/react"],
-        env: {
-            browser: true,
-        },
+        extends: [
+            "plugin:matrix-org/typescript",
+            "plugin:matrix-org/react",
+        ],
         rules: {
-            "quotes": "off",
-            // While converting to ts we allow this
-            "@typescript-eslint/no-explicit-any": "off",
+            // Things we do that break the ideal style
             "prefer-promise-reject-errors": "off",
+            "quotes": "off",
+
+            // We disable this while we're transitioning
+            "@typescript-eslint/no-explicit-any": "off",
         },
     }],
 };
