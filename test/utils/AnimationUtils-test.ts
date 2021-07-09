a90b8f32f19eab28b9c0f7baa9b96712e77eeb91
Šimon Brandner <simon.bra.ag@gmail.com>
2021-07-09 16:45:04 +0200

    Add some tests
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- test/utils/AnimationUtils-test.ts
+++ test/utils/AnimationUtils-test.ts
@@ -14,19 +14,22 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import { clamp } from "lodash";
+import { lerp } from "../../src/utils/AnimationUtils";
 
-/**
- * This method linearly interpolates between two points (start, end). This is
- * most commonly used to find a point some fraction of the way along a line
- * between two endpoints (e.g. to move an object gradually between those
- * points).
- * @param {number} start the starting point
- * @param {number} end the ending point
- * @param {number} amt the interpolant
- * @returns
- */
-export function lerp(start: number, end: number, amt: number) {
-    amt = clamp(amt, 0, 1);
-    return (1 - amt) * start + amt * end;
-}
+describe("lerp", () => {
+    it("correctly interpolates", () => {
+        expect(lerp(0, 100, 0.5)).toBe(50);
+        expect(lerp(50, 100, 0.5)).toBe(75);
+        expect(lerp(0, 1, 0.1)).toBe(0.1);
+    });
+
+    it("clamps the interpolant", () => {
+        expect(lerp(0, 100, 50)).toBe(100);
+        expect(lerp(0, 100, -50)).toBe(0);
+    });
+
+    it("handles negative numbers", () => {
+        expect(lerp(-100, 0, 0.5)).toBe(-50);
+        expect(lerp(100, -100, 0.5)).toBe(0);
+    });
+});
