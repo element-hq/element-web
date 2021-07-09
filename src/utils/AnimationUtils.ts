d9b8f0d540d1506b1fc885b6e0b87adb8374cf8a
Šimon Brandner <simon.bra.ag@gmail.com>
2021-07-09 15:58:35 +0200

    Add docs
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/utils/AnimationUtils.ts
+++ src/utils/AnimationUtils.ts
@@ -14,6 +14,16 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+/**
+ * This method linearly interpolates between two points (start, end). This is
+ * most commonly used to find a point some fraction of the way along a line
+ * between two endpoints (e.g. to move an object gradually between those
+ * points).
+ * @param {number} start the starting point
+ * @param {number} end the ending point
+ * @param {number} amt the interpolant
+ * @returns
+ */
 export function lerp(start: number, end: number, amt: number) {
     return (1 - amt) * start + amt * end;
 }
