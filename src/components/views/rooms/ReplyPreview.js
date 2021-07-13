ae5e10ff0cefa79c22b584d018cea6738eeb833f
Šimon Brandner <simon.bra.ag@gmail.com>
2021-07-13 08:45:36 +0200

    Burn sdk.getComponent
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/components/views/rooms/ReplyPreview.js
+++ src/components/views/rooms/ReplyPreview.js
@@ -16,12 +16,12 @@ limitations under the License.
 
 import React from 'react';
 import dis from '../../../dispatcher/dispatcher';
-import * as sdk from '../../../index';
 import { _t } from '../../../languageHandler';
 import RoomViewStore from '../../../stores/RoomViewStore';
 import PropTypes from "prop-types";
 import { RoomPermalinkCreator } from "../../../utils/permalinks/Permalinks";
 import { replaceableComponent } from "../../../utils/replaceableComponent";
+import ReplyTile from './ReplyTile';
 
 function cancelQuoting() {
     dis.dispatch({
@@ -68,8 +68,6 @@ export default class ReplyPreview extends React.Component {
 
     render() {
         if (!this.state.event) return null;
-
-        const ReplyTile = sdk.getComponent('rooms.ReplyTile');
 
         return <div className="mx_ReplyPreview">
             <div className="mx_ReplyPreview_section">
