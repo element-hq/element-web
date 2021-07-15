316b21408dc42c81b1933df05ac5d4cbe8839322
Michael Telatynski <7t3chguy@gmail.com>
2021-07-15 10:59:52 +0100

    Fix tests

--- test/editor/caret-test.js
+++ test/editor/caret-test.js
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+import "../skinned-sdk"; // Must be first for skinning to work
 import { getLineAndNodePosition } from "../../src/editor/caret";
 import EditorModel from "../../src/editor/model";
 import { createPartCreator } from "./mock";
