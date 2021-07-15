f65231268dd9df273104b0ef9bfe0dc1da571c46
Michael Telatynski <7t3chguy@gmail.com>
2021-07-15 08:26:50 +0100

    Cache value of feature_spaces* flags as they cause page refresh so are immutable

--- src/vector/platform/ElectronPlatform.tsx
+++ src/vector/platform/ElectronPlatform.tsx
@@ -50,7 +50,7 @@ import { showToast as showUpdateToast } from "matrix-react-sdk/src/toasts/Update
 import { CheckUpdatesPayload } from "matrix-react-sdk/src/dispatcher/payloads/CheckUpdatesPayload";
 import ToastStore from "matrix-react-sdk/src/stores/ToastStore";
 import GenericExpiringToast from "matrix-react-sdk/src/components/views/toasts/GenericExpiringToast";
-import SettingsStore from 'matrix-react-sdk/src/settings/SettingsStore';
+import SpaceStore from "matrix-react-sdk/src/stores/SpaceStore";
 import { IMatrixProfile, IEventWithRoomId as IMatrixEvent, IResultRoomEvents } from "matrix-js-sdk/src/@types/search";
 
 import VectorBasePlatform from './VectorBasePlatform';
@@ -600,7 +600,7 @@ export default class ElectronPlatform extends VectorBasePlatform {
             case "8":
             case "9":
             case "0":
-                if (SettingsStore.getValue("feature_spaces") && isOnlyCtrlOrCmdKeyEvent(ev)) {
+                if (SpaceStore.spacesEnabled && isOnlyCtrlOrCmdKeyEvent(ev)) {
                     this.navigateToSpace(parseInt(ev.key, 10));
                     handled = true;
                 }
