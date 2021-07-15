13c5adbb6ca050a3130996646fe10e09885665f9
Germain Souquet <germain@souquet.com>
2021-07-15 15:22:02 +0200

    Migrate AuthHeaderLogo to TypeScript

--- src/components/views/auth/AuthHeaderLogo.tsx
+++ src/components/views/auth/AuthHeaderLogo.tsx
@@ -19,7 +19,7 @@ import { replaceableComponent } from "../../../utils/replaceableComponent";
 
 @replaceableComponent("views.auth.AuthHeaderLogo")
 export default class AuthHeaderLogo extends React.PureComponent {
-    render() {
+    public render(): React.ReactNode {
         return <div className="mx_AuthHeaderLogo">
             Matrix
         </div>;
