45fb0e927f9c2bd330ca34eda56918d99987d5dd
David Baker <dave@matrix.org>
2021-07-16 22:35:53 +0100

    Mention nested labels in comments

--- src/components/views/elements/StyledRadioButton.tsx
+++ src/components/views/elements/StyledRadioButton.tsx
@@ -21,8 +21,8 @@ import { replaceableComponent } from "../../../utils/replaceableComponent";
 interface IProps extends React.InputHTMLAttributes<HTMLInputElement> {
     outlined?: boolean;
     // If true (default), the children will be contained within a <label> element
-    // If false, they'll be in a div. Putting interactive components like Fields in labels can
-    // cause strange bugs like https://github.com/vector-im/element-web/issues/18031
+    // If false, they'll be in a div. Putting interactive components that have labels
+    // themselves in labels can cause strange bugs like https://github.com/vector-im/element-web/issues/18031
     childrenInLabel?: boolean;
 }
 
