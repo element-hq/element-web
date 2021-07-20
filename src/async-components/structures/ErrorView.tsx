4b5b0e92449660218ccad29b489d16ba88e9a6f4
Michael Telatynski <7t3chguy@gmail.com>
2021-07-20 09:27:12 +0100

    delint

--- src/async-components/structures/ErrorView.tsx
+++ src/async-components/structures/ErrorView.tsx
@@ -40,9 +40,9 @@ const ErrorView: React.FC<IProps> = ({ title, messages }) => {
                 <div className="mx_HomePage_row">
                     <div>
                         <h2 id="step1_heading">{ title }</h2>
-                        {messages && messages.map(msg => <p key={msg}>
+                        { messages && messages.map(msg => <p key={msg}>
                             { msg }
-                        </p>)}
+                        </p>) }
                     </div>
                 </div>
             </div>
