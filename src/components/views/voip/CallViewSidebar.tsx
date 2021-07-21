74649f1f920670b603b649b7eef9507f9ef1c008
Šimon Brandner <simon.bra.ag@gmail.com>
2021-07-21 16:21:52 +0200

    Correct feed ordering
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/components/views/voip/CallViewSidebar.tsx
+++ src/components/views/voip/CallViewSidebar.tsx
@@ -27,11 +27,7 @@ interface IProps {
 
 export default class CallViewSidebar extends React.Component<IProps> {
     render() {
-        const feeds = this.props.feeds.sort((a, b) => {
-            if (!a.isLocal() && b.isLocal()) return -1;
-            else if (a.isLocal() && !b.isLocal()) return 1;
-            return 0;
-        }).map((feed) => {
+        const feeds = this.props.feeds.map((feed) => {
             // Hide local video feed if video is off
             if (
                 this.props.call.isLocalVideoMuted()
