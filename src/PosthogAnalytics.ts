2a48d3c9bc83e9083c3a9d7366c008f3909c9f6d
James Salter <iteration@gmail.com>
2021-07-21 07:40:39 +0100

    First pass at a PosthogAnalytics class

---
+++ src/PosthogAnalytics.ts
@@ -0,0 +1,82 @@
+import posthog from 'posthog-js';
+import SdkConfig from './SdkConfig';
+
+export interface IEvent {
+    key: string;
+    properties: {}
+}
+
+export interface IOnboardingLoginBegin extends IEvent {
+    key: "onboarding_login_begin",
+}
+
+const hashHex = async (input: string): Promise<string> => {
+    const buf = new TextEncoder().encode(input);
+    const digestBuf = await window.crypto.subtle.digest("sha-256", buf);
+    return [...new Uint8Array(digestBuf)].map((b: number) => b.toString(16).padStart(2, "0")).join("");
+};
+
+export class PosthogAnalytics {
+    private onlyTrackAnonymousEvents = false;
+    private initialised = false;
+    private posthog = null;
+
+    private static _instance = null;
+
+    public static instance(): PosthogAnalytics {
+        if (!this.instance) {
+            this._instance = new PosthogAnalytics(posthog);
+        }
+        return this._instance;
+    }
+
+    constructor(posthog) {
+        this.posthog = posthog;
+    }
+
+    public init(onlyTrackAnonymousEvents: boolean) {
+        if (Boolean(navigator.doNotTrack === "1")) {
+            this.initialised = false;
+            return;
+        }
+        this.onlyTrackAnonymousEvents = onlyTrackAnonymousEvents;
+        const posthogConfig = SdkConfig.get()["posthog"];
+        if (posthogConfig) {
+            console.log(`Initialising Posthog for ${posthogConfig.apiHost}`);
+            this.posthog.init(posthogConfig.projectApiKey, { api_host: posthogConfig.apiHost });
+            this.initialised = true;
+        }
+    }
+
+    public isInitialised(): boolean {
+        return this.initialised;
+    }
+
+    public setOnlyTrackAnonymousEvents(enabled: boolean) {
+        this.onlyTrackAnonymousEvents = enabled;
+    }
+
+    public track<E extends IEvent>(
+        key: E["key"],
+        properties: E["properties"],
+        anonymous = false,
+    ) {
+        if (!this.initialised) return;
+        if (this.onlyTrackAnonymousEvents && !anonymous) return;
+
+        this.posthog.capture(key, properties);
+    }
+
+    public async trackRoomEvent<E extends IEvent>(
+        key: E["key"],
+        roomId: string,
+        properties: E["properties"],
+        ...args
+    ) {
+        const updatedProperties = {
+            ...properties,
+            hashedRoomId: roomId ? await hashHex(roomId) : null,
+        };
+        this.track(key, updatedProperties, ...args);
+    }
+}
---
+++ test/PosthogAnalytics-test.ts
@@ -0,0 +1,84 @@
+import { IEvent, PosthogAnalytics } from '../src/PosthogAnalytics';
+import SdkConfig from '../src/SdkConfig';
+const crypto = require('crypto');
+
+class FakePosthog {
+    public capture;
+    public init;
+
+    constructor() {
+        this.capture = jest.fn();
+        this.init = jest.fn();
+    }
+}
+
+export interface ITestEvent extends IEvent {
+    key: "jest_test_event",
+    properties: {
+        foo: string
+    }
+}
+
+describe("PosthogAnalytics", () => {
+    let analytics: PosthogAnalytics;
+    let fakePosthog: FakePosthog;
+
+    beforeEach(() => {
+        fakePosthog = new FakePosthog();
+        analytics = new PosthogAnalytics(fakePosthog);
+        window.crypto = {
+            subtle: crypto.webcrypto.subtle,
+        };
+    });
+
+    afterEach(() => {
+        navigator.doNotTrack = null;
+        window.crypto = null;
+    });
+
+    it("Should not initialise if DNT is enabled", () => {
+        navigator.doNotTrack = "1";
+        analytics.init(false);
+        expect(analytics.isInitialised()).toBe(false);
+    });
+
+    it("Should not initialise if config is not set", () => {
+        jest.spyOn(SdkConfig, "get").mockReturnValue({});
+        analytics.init(false);
+        expect(analytics.isInitialised()).toBe(false);
+    });
+
+    it("Should initialise if config is set", () => {
+        jest.spyOn(SdkConfig, "get").mockReturnValue({
+            posthog: {
+                projectApiKey: "foo",
+                apiHost: "bar",
+            },
+        });
+        analytics.init(false);
+        expect(analytics.isInitialised()).toBe(true);
+    });
+
+    it("Should pass track() to posthog", () => {
+        analytics.init(false);
+        analytics.track<ITestEvent>("jest_test_event", {
+            foo: "bar",
+        });
+        expect(fakePosthog.capture.mock.calls[0][0]).toBe("jest_test_event");
+        expect(fakePosthog.capture.mock.calls[0][1]).toEqual({ foo: "bar" });
+    });
+
+    it("Should pass trackRoomEvent to posthog", () => {
+        analytics.init(false);
+        const roomId = "42";
+        return analytics.trackRoomEvent<ITestEvent>("jest_test_event", roomId, {
+            foo: "bar",
+        }).then(() => {
+            expect(fakePosthog.capture.mock.calls[0][0]).toBe("jest_test_event");
+            expect(fakePosthog.capture.mock.calls[0][1]).toEqual({
+                foo: "bar",
+                hashedRoomId: "73475cb40a568e8da8a045ced110137e159f890ac4da883b6b17dc651b3a8049",
+            });
+        });
+    });
+});
