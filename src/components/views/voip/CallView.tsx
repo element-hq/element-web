50c27ebed7744423ee4799bcf2d4d597d90810b7
Šimon Brandner <simon.bra.ag@gmail.com>
2021-07-25 08:16:55 +0200

    Add myself to the license header as I need to re-run the CI
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/components/views/voip/CallView.tsx
+++ src/components/views/voip/CallView.tsx
@@ -1,6 +1,7 @@
 /*
 Copyright 2015, 2016 OpenMarket Ltd
 Copyright 2019, 2020 The Matrix.org Foundation C.I.C.
+Copyright 2021 Šimon Brandner <simon.bra.ag@gmail.com>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
