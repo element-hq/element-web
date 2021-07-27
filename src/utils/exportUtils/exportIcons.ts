6f8f0e831408b1896755666e169edb2e7d3655b9
Jaiwanth <jaiwanth2011@gmail.com>
2021-07-27 19:29:22 +0530

    Change copyright year

--- src/utils/exportUtils/exportIcons.ts
+++ src/utils/exportUtils/exportIcons.ts
@@ -1,7 +1,7 @@
-import Trash from "!!raw-loader!../../../res/img/element-icons/trashcan.svg";
-import Attach from "!!raw-loader!../../../res/img/element-icons/room/composer/attach.svg";
+import trashSVG from "!!raw-loader!../../../res/img/element-icons/trashcan.svg";
+import attachSVG from "!!raw-loader!../../../res/img/element-icons/room/composer/attach.svg";
 
 export default {
-    "trash.svg": Trash,
-    "attach.svg": Attach,
+    "trash.svg": trashSVG,
+    "attach.svg": attachSVG,
 };
