a76fa5d62479030fb84575e63f0e26ae090547db
Timo <16718859+toger5@users.noreply.github.com>
2021-11-04 16:30:07 +0100

    add feature flag for maximised widgets (#7084)

--- src/settings/Settings.tsx
+++ src/settings/Settings.tsx
@@ -172,6 +172,12 @@ export const SETTINGS: {[setting: string]: ISetting} = {
         supportedLevels: LEVELS_FEATURE,
         default: false,
     },
+    "feature_maximised_widgets": {
+        isFeature: true,
+        displayName: _td("Maximised widgets"),
+        supportedLevels: LEVELS_FEATURE,
+        default: false,
+    },
     "feature_thread": {
         isFeature: true,
         // Requires a reload as we change an option flag on the `js-sdk`
