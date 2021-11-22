358a97c0905a477a81494ed1ed1046f36bfe7bd1
James Salter <iteration@gmail.com>
2021-11-23 10:41:42 +1100

    Strip leading v from version, matching package.sh

--- src/vector/platform/WebPlatform.ts
+++ src/vector/platform/WebPlatform.ts
@@ -129,7 +129,15 @@ export default class WebPlatform extends VectorBasePlatform {
     }
 
     getAppVersion(): Promise<string> {
-        return Promise.resolve(process.env.VERSION);
+        let ver = process.env.VERSION;
+
+        // if version looks like semver with leading v, strip it
+        // (matches scripts/package.sh)
+        const semVerRegex = new RegExp("^v[0-9]+.[0-9]+.[0-9]+(-.+)?$");
+        if (semVerRegex.test(process.env.VERSION)) {
+            ver = process.env.VERSION.substr(1);
+        }
+        return Promise.resolve(ver);
     }
 
     startUpdater() {
