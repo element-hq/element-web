5e19009e02a931ad1d601948e45c385043ca12c6
Šimon Brandner <simon.bra.ag@gmail.com>
2021-12-06 15:32:15 +0100

    Fix jump to bottom on message send (#7280)
    Co-authored-by: Germain <germain@souquet.com>

--- src/components/structures/RoomView.tsx
+++ src/components/structures/RoomView.tsx
@@ -929,7 +929,7 @@ export class RoomView extends React.Component<IRoomProps, IRoomState> {
             }
 
             case "scroll_to_bottom":
-                if (payload.timelineRenderingType === this.context.timelineRenderingType) {
+                if (payload.timelineRenderingType === TimelineRenderingType.Room) {
                     this.messagePanel?.jumpToLiveTimeline();
                 }
                 break;
