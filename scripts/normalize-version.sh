0f4f4ac338080bb4f461bfca473a72f688fd6f2f
James Salter <iteration@gmail.com>
2021-12-07 14:47:14 +1100

    clarify comment

--- scripts/normalize-version.sh
+++ scripts/normalize-version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# If $1 looks like v1.2.3, strip the leading v, then print it to stdout
+# If $1 looks like v1.2.3 or v1.2.3-foo, strip the leading v, then print it to stdout
 if [[ $1 =~ ^v[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+(-.+)?$ ]]; then
     echo ${1:1}
 else
