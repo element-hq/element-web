d25d325387ee9f2bee94ebfefaec268196fcbf02
Michael Telatynski <7t3chguy@gmail.com>
2021-12-21 14:59:59 +0000

    Don't show `Testing small changes` without UIFeature.Feedback (#7427)

--- src/components/structures/MatrixChat.tsx
+++ src/components/structures/MatrixChat.tsx
@@ -1523,7 +1523,7 @@ export default class MatrixChat extends React.PureComponent<IProps, IState> {
                 showNotificationsToast(false);
             }
 
-            if (!localStorage.getItem("mx_seen_ia_1.1_changes_toast")) {
+            if (!localStorage.getItem("mx_seen_ia_1.1_changes_toast") && SettingsStore.getValue(UIFeature.Feedback)) {
                 const key = "IA_1.1_TOAST";
                 ToastStore.sharedInstance().addOrReplaceToast({
                     key,
