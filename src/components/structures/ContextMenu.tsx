f3776f89448b4bbec3020281fc95c3976ada824f
Michael Telatynski <7t3chguy@gmail.com>
2021-12-22 10:53:17 +0000

    Prevent keyboard propagation out of context menus (#7437)

--- src/components/structures/ContextMenu.tsx
+++ src/components/structures/ContextMenu.tsx
@@ -188,6 +188,8 @@ export default class ContextMenu extends React.PureComponent<IProps, IState> {
     // We now only handle closing the ContextMenu in this keyDown handler.
     // All of the item/option navigation is delegated to RovingTabIndex.
     private onKeyDown = (ev: React.KeyboardEvent) => {
+        ev.stopPropagation(); // prevent keyboard propagating out of the context menu, we're focus-locked
+
         // If someone is managing their own focus, we will only exit for them with Escape.
         // They are probably using props.focusLock along with this option as well.
         if (!this.props.managed) {
