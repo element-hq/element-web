6d9d9a56b4bef9501439d847da59420c241902e0
Michael Telatynski <7t3chguy@gmail.com>
2022-01-13 16:42:32 +0000

    Apply border-radius onto linear gradient in bubble layout (#7536)

--- src/settings/enums/Layout.ts
+++ src/settings/enums/Layout.ts
@@ -15,14 +15,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import PropTypes from 'prop-types';
-
 /* TODO: This should be later reworked into something more generic */
 export enum Layout {
     IRC = "irc",
     Group = "group",
     Bubble = "bubble",
 }
-
-/* We need this because multiple components are still using JavaScript */
-export const LayoutPropType = PropTypes.oneOf(Object.values(Layout));
