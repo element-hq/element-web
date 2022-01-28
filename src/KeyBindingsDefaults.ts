db09d162050f33da261c63980f16cd5b6f8d26a3
Šimon Brandner <simon.bra.ag@gmail.com>
2022-01-28 16:32:30 +0100

    Fix shift-enter repeating last character (#7665)

--- src/KeyBindingsDefaults.ts
+++ src/KeyBindingsDefaults.ts
@@ -140,6 +140,13 @@ const messageComposerBindings = (): KeyBinding<MessageComposerAction>[] => {
                 key: Key.ENTER,
             },
         });
+        bindings.push({
+            action: MessageComposerAction.NewLine,
+            keyCombo: {
+                key: Key.ENTER,
+                shiftKey: true,
+            },
+        });
     } else {
         bindings.push({
             action: MessageComposerAction.Send,
