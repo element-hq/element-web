78373e86ea6deba0ad251b9bf1708d2ee8ac5f29
Michael Telatynski <7t3chguy@gmail.com>
2022-02-04 10:20:20 +0000

    Truncate long server names on login/register screen (#7702)
    Co-authored-by: Aaron Raimist <aaron@raim.ist>

--- src/components/views/elements/ServerPicker.tsx
+++ src/components/views/elements/ServerPicker.tsx
@@ -86,7 +86,9 @@ const ServerPicker = ({ title, dialogTitle, serverConfig, onServerConfigChange }
     return <div className="mx_ServerPicker">
         <h3>{ title || _t("Homeserver") }</h3>
         { !disableCustomUrls ? <AccessibleButton className="mx_ServerPicker_help" onClick={onHelpClick} /> : null }
-        <span className="mx_ServerPicker_server">{ serverName }</span>
+        <span className="mx_ServerPicker_server" title={typeof serverName === "string" ? serverName : undefined}>
+            { serverName }
+        </span>
         { editBtn }
         { desc }
     </div>;
