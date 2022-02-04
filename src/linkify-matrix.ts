7f3f18604417d9567c109201673e60a5f24a30ce
J. Ryan Stinnett <jryans@gmail.com>
2022-02-04 17:21:03 +0000

    Switch back to upstream Linkify (#7677)

--- src/linkify-matrix.ts
+++ src/linkify-matrix.ts
@@ -15,11 +15,11 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import * as linkifyjs from '@matrix-org/linkifyjs';
-import linkifyElement from '@matrix-org/linkify-element';
-import linkifyString from '@matrix-org/linkify-string';
+import * as linkifyjs from 'linkifyjs';
+import { registerCustomProtocol, registerPlugin } from 'linkifyjs';
+import linkifyElement from 'linkify-element';
+import linkifyString from 'linkify-string';
 import { RoomMember } from 'matrix-js-sdk/src/models/room-member';
-import { registerCustomProtocol, registerPlugin } from '@matrix-org/linkifyjs';
 
 import { baseUrl } from "./utils/permalinks/MatrixToPermalinkConstructor";
 import {
