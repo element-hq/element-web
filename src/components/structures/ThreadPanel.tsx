3a82163127ba3ab2fc5f746a87f852041f2655b4
Germain <germains@element.io>
2022-03-02 10:39:22 +0000

    Disable pending events in thread panel event timeline set (#7874)

--- src/components/structures/ThreadPanel.tsx
+++ src/components/structures/ThreadPanel.tsx
@@ -86,7 +86,9 @@ export async function getThreadTimelineSet(
         // Filter creation fails if HomeServer does not support the new relation
         // filter fields. We fallback to the threads that have been discovered in
         // the main timeline
-        const timelineSet = new EventTimelineSet(room, {});
+        const timelineSet = new EventTimelineSet(room, {
+            pendingEvents: false,
+        });
 
         Array.from(room.threads)
             .sort(([, threadA], [, threadB]) => threadA.replyToEvent.getTs() - threadB.replyToEvent.getTs())
