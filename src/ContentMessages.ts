457fc542da321d41858d763ea0560168eb549ed1
Hugh Nimmo-Smith <hughns@users.noreply.github.com>
2022-04-05 18:29:27 +0100

    Replace browser-encrypt-attachment with matrix-encrypt-attachment (#7795)
    * Replace browser-encrypt-attachment with matrix-encrypt-attachment
    
    matrix-encrypt-attachment is maintained and includes TS typings
    
    * Upgrade matrix-encrypt-attachment to latest with default exports

--- src/ContentMessages.ts
+++ src/ContentMessages.ts
@@ -19,7 +19,7 @@ limitations under the License.
 import { MatrixClient } from "matrix-js-sdk/src/client";
 import { IUploadOpts } from "matrix-js-sdk/src/@types/requests";
 import { MsgType } from "matrix-js-sdk/src/@types/event";
-import encrypt from "browser-encrypt-attachment";
+import encrypt from "matrix-encrypt-attachment";
 import extractPngChunks from "png-chunks-extract";
 import { IAbortablePromise, IImageInfo } from "matrix-js-sdk/src/@types/partials";
 import { logger } from "matrix-js-sdk/src/logger";
