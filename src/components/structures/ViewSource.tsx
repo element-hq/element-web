7b2f1e22e17d8008d60fca6ee804120c8557e281
olivialivia <94136530+olivialivia@users.noreply.github.com>
2022-04-11 19:37:52 +0100

    Add copy button to View Source screen (#8278)
    Co-authored-by: Michael Telatynski <7t3chguy@gmail.com>

--- src/components/structures/ViewSource.tsx
+++ src/components/structures/ViewSource.tsx
@@ -29,6 +29,7 @@ import BaseDialog from "../views/dialogs/BaseDialog";
 import { DevtoolsContext } from "../views/dialogs/devtools/BaseTool";
 import { StateEventEditor } from "../views/dialogs/devtools/RoomState";
 import { stringify, TimelineEventEditor } from "../views/dialogs/devtools/Event";
+import CopyableText from "../views/elements/CopyableText";
 
 interface IProps extends IDialogProps {
     mxEvent: MatrixEvent; // the MatrixEvent associated with the context menu
@@ -63,29 +64,58 @@ export default class ViewSource extends React.Component<IProps, IState> {
         // @ts-ignore
         const decryptedEventSource = mxEvent.clearEvent; // FIXME: clearEvent is private
         const originalEventSource = mxEvent.event;
-
+        const copyOriginalFunc = (): string => {
+            return stringify(originalEventSource);
+        };
         if (isEncrypted) {
+            const copyDecryptedFunc = (): string => {
+                return stringify(decryptedEventSource);
+            };
             return (
                 <>
                     <details open className="mx_ViewSource_details">
                         <summary>
-                            <span className="mx_ViewSource_heading">{ _t("Decrypted event source") }</span>
+                            <span className="mx_ViewSource_heading">
+                                { _t("Decrypted event source") }
+                            </span>
                         </summary>
-                        <SyntaxHighlight language="json">{ stringify(decryptedEventSource) }</SyntaxHighlight>
+                        <div className="mx_ViewSource_container">
+                            <CopyableText getTextToCopy={copyDecryptedFunc}>
+                                <SyntaxHighlight language="json">
+                                    { stringify(decryptedEventSource) }
+                                </SyntaxHighlight>
+                            </CopyableText>
+                        </div>
                     </details>
                     <details className="mx_ViewSource_details">
                         <summary>
-                            <span className="mx_ViewSource_heading">{ _t("Original event source") }</span>
+                            <span className="mx_ViewSource_heading">
+                                { _t("Original event source") }
+                            </span>
                         </summary>
-                        <SyntaxHighlight language="json">{ stringify(originalEventSource) }</SyntaxHighlight>
+                        <div className="mx_ViewSource_container">
+                            <CopyableText getTextToCopy={copyOriginalFunc}>
+                                <SyntaxHighlight language="json">
+                                    { stringify(originalEventSource) }
+                                </SyntaxHighlight>
+                            </CopyableText>
+                        </div>
                     </details>
                 </>
             );
         } else {
             return (
                 <>
-                    <div className="mx_ViewSource_heading">{ _t("Original event source") }</div>
-                    <SyntaxHighlight language="json">{ stringify(originalEventSource) }</SyntaxHighlight>
+                    <div className="mx_ViewSource_heading">
+                        { _t("Original event source") }
+                    </div>
+                    <div className="mx_ViewSource_container">
+                        <CopyableText getTextToCopy={copyOriginalFunc}>
+                            <SyntaxHighlight language="json">
+                                { stringify(originalEventSource) }
+                            </SyntaxHighlight>
+                        </CopyableText>
+                    </div>
                 </>
             );
         }
