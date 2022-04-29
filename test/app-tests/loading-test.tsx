a5d21f01e4bbf35abeacab0472b9a5d4459403fb
Reshma <reshmashaik3011@gmail.com>
2022-04-29 18:01:22 +0530

    Fixed the unmount() error

--- test/app-tests/loading-test.tsx
+++ test/app-tests/loading-test.tsx
@@ -75,7 +75,7 @@ describe('loading:', function() {
     afterEach(async function() {
         console.log(`${Date.now()}: loading: afterEach`);
         if (parentDiv) {
-            parentDiv.unmount();
+            createRoot(parentDiv!).unmount();
             parentDiv.remove();
             parentDiv = null;
         }
