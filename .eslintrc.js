150c5308507e325b1ad9cc778c30a1638493f3f1
Travis Ralston <travisr@matrix.org>
2022-05-30 00:21:55 -0600

    Disable no-non-null assertions lint rule (#8699)
    This *allows* us to use `variable!.prop` to ensure `variable` is not null/undefined.

--- .eslintrc.js
+++ .eslintrc.js
@@ -115,6 +115,8 @@ module.exports = {
                 "@typescript-eslint/no-explicit-any": "off",
                 // We'd rather not do this but we do
                 "@typescript-eslint/ban-ts-comment": "off",
+                // We're okay with assertion errors when we ask for them
+                "@typescript-eslint/no-non-null-assertion": "off",
             },
         },
         // temporary override for offending icon require files
