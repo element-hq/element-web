c0168155621ededeec9fb57940edfc48579d9f1a
Suguru Hirahara <luixxiul@users.noreply.github.com>
2022-06-08 09:53:58 +0000

    Normalize the name of buttons on MessageActionBar (#8781)
    Signed-off-by: Suguru Hirahara <luixxiul@users.noreply.github.com>

--- src/components/views/rooms/EventTile.tsx
+++ src/components/views/rooms/EventTile.tsx
@@ -1372,13 +1372,13 @@ export class UnwrappedEventTile extends React.Component<IProps, IState> {
                         </div>
                         <Toolbar className="mx_MessageActionBar" aria-label={_t("Message Actions")} aria-live="off">
                             <RovingAccessibleTooltipButton
-                                className="mx_MessageActionBar_maskButton mx_MessageActionBar_viewInRoom"
+                                className="mx_MessageActionBar_maskButton mx_MessageActionBar_viewInRoomButton"
                                 onClick={this.viewInRoom}
                                 title={_t("View in room")}
                                 key="view_in_room"
                             />
                             <RovingAccessibleTooltipButton
-                                className="mx_MessageActionBar_maskButton mx_MessageActionBar_copyLinkToThread"
+                                className="mx_MessageActionBar_maskButton mx_MessageActionBar_copyLinkButton"
                                 onClick={this.copyLinkToThread}
                                 title={_t("Copy link to thread")}
                                 key="copy_link_to_thread"
