18d7f7c525684d2f165f8b14f44a03515900c6ec
Suguru Hirahara <luixxiul@users.noreply.github.com>
2022-06-12 14:52:55 +0000

    Use mx_SettingsTab_subsectionText for the text under the top header of appearance user settings tab (#8822)

--- src/components/views/settings/tabs/user/AppearanceUserSettingsTab.tsx
+++ src/components/views/settings/tabs/user/AppearanceUserSettingsTab.tsx
@@ -143,7 +143,7 @@ export default class AppearanceUserSettingsTab extends React.Component<IProps, I
         return (
             <div className="mx_SettingsTab mx_AppearanceUserSettingsTab">
                 <div className="mx_SettingsTab_heading">{ _t("Customise your appearance") }</div>
-                <div className="mx_SettingsTab_SubHeading">
+                <div className="mx_SettingsTab_subsectionText">
                     { _t("Appearance Settings only affect this %(brand)s session.", { brand }) }
                 </div>
                 <ThemeChoicePanel />
