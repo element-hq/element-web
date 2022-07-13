93873cfd080982f5ecd96fb72a47a94152ceda27
Michael Telatynski <7t3chguy@gmail.com>
2022-07-13 15:08:55 +0100

    Attempt to wire up dockerhub building in GHA

--- scripts/docker-link-repos.sh
+++ scripts/docker-link-repos.sh
@@ -20,14 +20,14 @@ then
 fi
 
 echo "Linking js-sdk"
-git clone --depth 1 --branch $JS_SDK_BRANCH $JS_SDK_REPO js-sdk
+git clone --depth 1 --branch $JS_SDK_BRANCH "$JS_SDK_REPO" js-sdk
 cd js-sdk
 yarn link
 yarn --network-timeout=100000 install
 cd ../
 
 echo "Linking react-sdk"
-git clone --depth 1 --branch $REACT_SDK_BRANCH $REACT_SDK_REPO react-sdk
+git clone --depth 1 --branch $REACT_SDK_BRANCH "$REACT_SDK_REPO" react-sdk
 cd react-sdk
 yarn link
 yarn link matrix-js-sdk
