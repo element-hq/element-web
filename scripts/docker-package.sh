0e9a9fbc9977aed8ff0353851b08738331673864
Michael Telatynski <7t3chguy@gmail.com>
2022-07-29 11:38:16 +0100

    Update comment

--- scripts/docker-package.sh
+++ scripts/docker-package.sh
@@ -7,11 +7,7 @@ DIST_VERSION=$(git describe --abbrev=0 --tags)
 
 DIR=$(dirname "$0")
 
-# If the branch comes out as HEAD then we're probably checked out to a tag, so if the thing is *not*
-# coming out as HEAD then we're on a branch. When we're on a branch, we want to resolve ourselves to
-# a few SHAs rather than a version.
-# Docker Hub doesn't always check out the tag and sometimes checks out the branch, so we should look
-# for an appropriately tagged branch as well (heads/v1.2.3).
+# If we're not using custom SDKs and on a branch other than master, generate a version akin go develop.element.io
 if [[ $USE_CUSTOM_SDKS == false ]] && [[ $BRANCH != 'master' ]]
 then
     DIST_VERSION=$("$DIR"/get-version-from-git.sh)
