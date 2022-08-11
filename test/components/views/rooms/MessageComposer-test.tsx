964d538db9e71d497e6dd38acf494c7d1e6465b0
Michael Weimann <michaelw@matrix.org>
2022-08-11 17:51:24 +0200

    Hide sticker picker for local rooms (#9174)
    * Disable stickers for local rooms
    
    * Use prop

--- test/components/views/rooms/MessageComposer-test.tsx
+++ test/components/views/rooms/MessageComposer-test.tsx
@@ -28,48 +28,66 @@ import RoomContext from "../../../../src/contexts/RoomContext";
 import { IRoomState } from "../../../../src/components/structures/RoomView";
 import ResizeNotifier from "../../../../src/utils/ResizeNotifier";
 import { RoomPermalinkCreator } from "../../../../src/utils/permalinks/Permalinks";
+import { LocalRoom } from "../../../../src/models/LocalRoom";
+import MessageComposerButtons from "../../../../src/components/views/rooms/MessageComposerButtons";
 
 describe("MessageComposer", () => {
     stubClient();
     const cli = createTestClient();
-    const room = mkStubRoom("!roomId:server", "Room 1", cli);
 
-    it("Renders a SendMessageComposer and MessageComposerButtons by default", () => {
-        const wrapper = wrapAndRender({ room });
+    describe("for a Room", () => {
+        const room = mkStubRoom("!roomId:server", "Room 1", cli);
 
-        expect(wrapper.find("SendMessageComposer")).toHaveLength(1);
-        expect(wrapper.find("MessageComposerButtons")).toHaveLength(1);
+        it("Renders a SendMessageComposer and MessageComposerButtons by default", () => {
+            const wrapper = wrapAndRender({ room });
+
+            expect(wrapper.find("SendMessageComposer")).toHaveLength(1);
+            expect(wrapper.find("MessageComposerButtons")).toHaveLength(1);
+        });
+
+        it("Does not render a SendMessageComposer or MessageComposerButtons when user has no permission", () => {
+            const wrapper = wrapAndRender({ room }, false);
+
+            expect(wrapper.find("SendMessageComposer")).toHaveLength(0);
+            expect(wrapper.find("MessageComposerButtons")).toHaveLength(0);
+            expect(wrapper.find(".mx_MessageComposer_noperm_error")).toHaveLength(1);
+        });
+
+        it("Does not render a SendMessageComposer or MessageComposerButtons when room is tombstoned", () => {
+            const wrapper = wrapAndRender({ room }, true, mkEvent({
+                event: true,
+                type: "m.room.tombstone",
+                room: room.roomId,
+                user: "@user1:server",
+                skey: "",
+                content: {},
+                ts: Date.now(),
+            }));
+
+            expect(wrapper.find("SendMessageComposer")).toHaveLength(0);
+            expect(wrapper.find("MessageComposerButtons")).toHaveLength(0);
+            expect(wrapper.find(".mx_MessageComposer_roomReplaced_header")).toHaveLength(1);
+        });
     });
 
-    it("Does not render a SendMessageComposer or MessageComposerButtons when user has no permission", () => {
-        const wrapper = wrapAndRender({ room }, false);
+    describe("for a LocalRoom", () => {
+        const localRoom = new LocalRoom("!room:example.com", cli, cli.getUserId());
 
-        expect(wrapper.find("SendMessageComposer")).toHaveLength(0);
-        expect(wrapper.find("MessageComposerButtons")).toHaveLength(0);
-        expect(wrapper.find(".mx_MessageComposer_noperm_error")).toHaveLength(1);
-    });
-
-    it("Does not render a SendMessageComposer or MessageComposerButtons when room is tombstoned", () => {
-        const wrapper = wrapAndRender({ room }, true, mkEvent({
-            event: true,
-            type: "m.room.tombstone",
-            room: room.roomId,
-            user: "@user1:server",
-            skey: "",
-            content: {},
-            ts: Date.now(),
-        }));
-
-        expect(wrapper.find("SendMessageComposer")).toHaveLength(0);
-        expect(wrapper.find("MessageComposerButtons")).toHaveLength(0);
-        expect(wrapper.find(".mx_MessageComposer_roomReplaced_header")).toHaveLength(1);
+        it("should pass the sticker picker disabled prop", () => {
+            const wrapper = wrapAndRender({ room: localRoom });
+            expect(wrapper.find(MessageComposerButtons).props().showStickersButton).toBe(false);
+        });
     });
 });
 
-function wrapAndRender(props = {}, canSendMessages = true, tombstone?: MatrixEvent): ReactWrapper {
+function wrapAndRender(
+    props: Partial<React.ComponentProps<typeof MessageComposer>> = {},
+    canSendMessages = true,
+    tombstone?: MatrixEvent,
+): ReactWrapper {
     const mockClient = MatrixClientPeg.get();
     const roomId = "myroomid";
-    const room: any = {
+    const room: any = props.room || {
         currentState: undefined,
         roomId,
         client: mockClient,
