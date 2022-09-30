c6d996755a3508662966bc19c6bf565f7932a3d7
Michael Telatynski <7t3chguy@gmail.com>
2022-09-30 13:18:57 +0100

    Add snapshot tests for auth view component overrides

--- src/components/views/auth/VectorAuthHeaderLogo.tsx
+++ src/components/views/auth/VectorAuthHeaderLogo.tsx
@@ -15,7 +15,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import React from 'react';
+import * as React from 'react';
 import SdkConfig from 'matrix-react-sdk/src/SdkConfig';
 
 export default class VectorAuthHeaderLogo extends React.PureComponent {
