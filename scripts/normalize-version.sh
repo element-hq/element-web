341b0b469ea62cd6a77c9b275ba3aa2483e3d3f9
Michael Telatynski <7t3chguy@gmail.com>
2022-10-12 16:15:51 +0100

    Make bash scripts exit on error rather than continue (#23467)

--- scripts/normalize-version.sh
+++ scripts/normalize-version.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # If $1 looks like v1.2.3 or v1.2.3-foo, strip the leading v, then print it to stdout
 if [[ $1 =~ ^v[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+(-.+)?$ ]]; then
     echo ${1:1}
