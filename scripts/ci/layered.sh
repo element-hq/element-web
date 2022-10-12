96213d644b879b5a1f1a2b851eecb3ec2bad49c5
Michael Telatynski <7t3chguy@gmail.com>
2022-10-12 16:15:57 +0100

    Make bash scripts exit on error rather than continue (#9396)

--- scripts/ci/layered.sh
+++ scripts/ci/layered.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -ex
+
 # Creates a layered environment with the full repo for the app and SDKs cloned
 # and linked. This gives an element-web dev environment ready to build with
 # the current react-sdk branch and any matching branches of react-sdk's dependencies
