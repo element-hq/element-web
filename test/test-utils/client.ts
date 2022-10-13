970d1912e52c9357481538a4f1a9e1a906a15f57
Germain <germains@element.io>
2022-10-13 18:22:25 +0100

    Add mock for canSupport (#9414)
    * Add mock for canSupport
    
    * dead code

--- test/test-utils/client.ts
+++ test/test-utils/client.ts
@@ -16,6 +16,7 @@ limitations under the License.
 
 import EventEmitter from "events";
 import { MethodKeysOf, mocked, MockedObject, PropertyKeysOf } from "jest-mock";
+import { Feature, ServerSupport } from "matrix-js-sdk/src/feature";
 import { MatrixClient, User } from "matrix-js-sdk/src/matrix";
 
 import { MatrixClientPeg } from "../../src/MatrixClientPeg";
@@ -50,6 +51,11 @@ export const getMockClientWithEventEmitter = (
     const mock = mocked(new MockClientWithEventEmitter(mockProperties) as unknown as MatrixClient);
 
     jest.spyOn(MatrixClientPeg, 'get').mockReturnValue(mock);
+
+    mock.canSupport = new Map();
+    Object.keys(Feature).forEach(feature => {
+        mock.canSupport.set(feature as Feature, ServerSupport.Stable);
+    });
     return mock;
 };
 
