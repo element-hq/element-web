67dbb360260bea1da55fe518e50a50424d816abc
kegsay <kegan@matrix.org>
2022-10-18 13:44:45 +0100

    Listen for and update the notification state when they change (#9438)
    * Listen for and update the notification state when they change
    
    * Remove unnecessary listeners: justify each listener left remaining
    
    * Update removeListener too

--- src/stores/notifications/RoomNotificationState.ts
+++ src/stores/notifications/RoomNotificationState.ts
@@ -32,16 +32,15 @@ import { ThreadsRoomNotificationState } from "./ThreadsRoomNotificationState";
 export class RoomNotificationState extends NotificationState implements IDestroyable {
     constructor(public readonly room: Room, private readonly threadsState?: ThreadsRoomNotificationState) {
         super();
-        this.room.on(RoomEvent.Receipt, this.handleReadReceipt);
-        this.room.on(RoomEvent.Timeline, this.handleRoomEventUpdate);
-        this.room.on(RoomEvent.Redaction, this.handleRoomEventUpdate);
-        this.room.on(RoomEvent.MyMembership, this.handleMembershipUpdate);
-        this.room.on(RoomEvent.LocalEchoUpdated, this.handleLocalEchoUpdated);
+        this.room.on(RoomEvent.Receipt, this.handleReadReceipt); // for unread indicators
+        this.room.on(RoomEvent.MyMembership, this.handleMembershipUpdate); // for redness on invites
+        this.room.on(RoomEvent.LocalEchoUpdated, this.handleLocalEchoUpdated); // for redness on unsent messages
+        this.room.on(RoomEvent.UnreadNotifications, this.handleNotificationCountUpdate); // for server-sent counts
         if (threadsState) {
             threadsState.on(NotificationStateEvents.Update, this.handleThreadsUpdate);
         }
-        MatrixClientPeg.get().on(MatrixEventEvent.Decrypted, this.onEventDecrypted);
-        MatrixClientPeg.get().on(ClientEvent.AccountData, this.handleAccountDataUpdate);
+        MatrixClientPeg.get().on(MatrixEventEvent.Decrypted, this.onEventDecrypted); // for local count calculation
+        MatrixClientPeg.get().on(ClientEvent.AccountData, this.handleAccountDataUpdate); // for push rules
         this.updateNotificationState();
     }
 
@@ -52,10 +51,9 @@ export class RoomNotificationState extends NotificationState implements IDestroy
     public destroy(): void {
         super.destroy();
         this.room.removeListener(RoomEvent.Receipt, this.handleReadReceipt);
-        this.room.removeListener(RoomEvent.Timeline, this.handleRoomEventUpdate);
-        this.room.removeListener(RoomEvent.Redaction, this.handleRoomEventUpdate);
         this.room.removeListener(RoomEvent.MyMembership, this.handleMembershipUpdate);
         this.room.removeListener(RoomEvent.LocalEchoUpdated, this.handleLocalEchoUpdated);
+        this.room.removeListener(RoomEvent.UnreadNotifications, this.handleNotificationCountUpdate);
         if (this.threadsState) {
             this.threadsState.removeListener(NotificationStateEvents.Update, this.handleThreadsUpdate);
         }
@@ -83,14 +81,12 @@ export class RoomNotificationState extends NotificationState implements IDestroy
         this.updateNotificationState();
     };
 
-    private onEventDecrypted = (event: MatrixEvent) => {
-        if (event.getRoomId() !== this.room.roomId) return; // ignore - not for us or notifications timeline
-
+    private handleNotificationCountUpdate = () => {
         this.updateNotificationState();
     };
 
-    private handleRoomEventUpdate = (event: MatrixEvent, room: Room | null) => {
-        if (room?.roomId !== this.room.roomId) return; // ignore - not for us or notifications timeline
+    private onEventDecrypted = (event: MatrixEvent) => {
+        if (event.getRoomId() !== this.room.roomId) return; // ignore - not for us or notifications timeline
 
         this.updateNotificationState();
     };
