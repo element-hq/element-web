be3a66b0e605ab0481beae4ca3c1ce7b137e8741
Florian Duros <florianduros@element.io>
2022-12-05 16:17:25 +0100

    Fix edition

--- src/components/views/rooms/wysiwyg_composer/components/Editor.tsx
+++ src/components/views/rooms/wysiwyg_composer/components/Editor.tsx
@@ -61,7 +61,7 @@ export const Editor = memo(
                     onBlur={onBlur}
                     />
                 </div>
-                { rightComponent(selectPreviousSelection) }
+                { rightComponent?.(selectPreviousSelection) }
             </div>;
         },
     ),
