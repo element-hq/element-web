75c2c1a572fa45d1ea1d1a96e9e36e303332ecaa
László Várady <3130044+MrAnno@users.noreply.github.com>
2022-12-05 17:19:50 +0100

    Honor advanced audio processing settings when recording voice messages (#9610)
    * VoiceRecordings: honor advanced audio processing settings
    
    Audio processing settings introduced in #8759 is now taken into account
    when recording a voice message.
    
    Signed-off-by: László Várady <laszlo.varady@protonmail.com>
    
    * VoiceRecordings: add higher-quality audio recording
    
    When recording non-voice audio (e.g. music, FX), a different Opus encoder
    application should be specified. It is also recommended to increase the
    bitrate to 64-96 kb/s for musical use.
    
    Note: the HQ mode is currently activated when noise suppression is
    turned off. This is a very arbitrary condition.
    
    Signed-off-by: László Várady <laszlo.varady@protonmail.com>
    
    * RecorderWorklet: fix type mismatch
    
    src/audio/VoiceRecording.ts:129:67 - Argument of type 'null' is not
    assignable to parameter of type 'string | URL'.
    
    Signed-off-by: László Várady <laszlo.varady@protonmail.com>
    
    * VoiceRecording: test audio settings
    
    Signed-off-by: László Várady <laszlo.varady@protonmail.com>
    
    * Fix typos
    
    Signed-off-by: László Várady <laszlo.varady@protonmail.com>
    
    * VoiceRecording: refactor using destructuring assignment
    
    Signed-off-by: László Várady <laszlo.varady@protonmail.com>
    
    * VoiceRecording: add comments about constants and non-trivial conditions
    
    Signed-off-by: László Várady <laszlo.varady@protonmail.com>
    
    Signed-off-by: László Várady <laszlo.varady@protonmail.com>

--- test/audio/VoiceRecording-test.ts
+++ test/audio/VoiceRecording-test.ts
@@ -14,7 +14,24 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import { VoiceRecording } from "../../src/audio/VoiceRecording";
+import { mocked } from 'jest-mock';
+// @ts-ignore
+import Recorder from 'opus-recorder/dist/recorder.min.js';
+
+import { VoiceRecording, voiceRecorderOptions, highQualityRecorderOptions } from "../../src/audio/VoiceRecording";
+import { createAudioContext } from '../..//src/audio/compat';
+import MediaDeviceHandler from "../../src/MediaDeviceHandler";
+
+jest.mock('opus-recorder/dist/recorder.min.js');
+const RecorderMock = mocked(Recorder);
+
+jest.mock('../../src/audio/compat', () => ({
+    createAudioContext: jest.fn(),
+}));
+const createAudioContextMock = mocked(createAudioContext);
+
+jest.mock("../../src/MediaDeviceHandler");
+const MediaDeviceHandlerMock = mocked(MediaDeviceHandler);
 
 /**
  * The tests here are heavily using access to private props.
@@ -43,6 +60,7 @@ describe("VoiceRecording", () => {
         // @ts-ignore
         recording.observable = {
             update: jest.fn(),
+            close: jest.fn(),
         };
         jest.spyOn(recording, "stop").mockImplementation();
         recorderSecondsSpy = jest.spyOn(recording, "recorderSeconds", "get");
@@ -50,6 +68,56 @@ describe("VoiceRecording", () => {
 
     afterEach(() => {
         jest.resetAllMocks();
+    });
+
+    describe("when starting a recording", () => {
+        beforeEach(() => {
+            const mockAudioContext = {
+                createMediaStreamSource: jest.fn().mockReturnValue({
+                    connect: jest.fn(),
+                    disconnect: jest.fn(),
+                }),
+                createScriptProcessor: jest.fn().mockReturnValue({
+                    connect: jest.fn(),
+                    disconnect: jest.fn(),
+                    addEventListener: jest.fn(),
+                    removeEventListener: jest.fn(),
+                }),
+                destination: {},
+                close: jest.fn(),
+            };
+            createAudioContextMock.mockReturnValue(mockAudioContext as unknown as AudioContext);
+        });
+
+        afterEach(async () => {
+            await recording.stop();
+        });
+
+        it("should record high-quality audio if voice processing is disabled", async () => {
+            MediaDeviceHandlerMock.getAudioNoiseSuppression.mockReturnValue(false);
+            await recording.start();
+
+            expect(navigator.mediaDevices.getUserMedia).toHaveBeenCalledWith(expect.objectContaining({
+                audio: expect.objectContaining({ noiseSuppression: { ideal: false } }),
+            }));
+            expect(RecorderMock).toHaveBeenCalledWith(expect.objectContaining({
+                encoderBitRate: highQualityRecorderOptions.bitrate,
+                encoderApplication: highQualityRecorderOptions.encoderApplication,
+            }));
+        });
+
+        it("should record normal-quality voice if voice processing is enabled", async () => {
+            MediaDeviceHandlerMock.getAudioNoiseSuppression.mockReturnValue(true);
+            await recording.start();
+
+            expect(navigator.mediaDevices.getUserMedia).toHaveBeenCalledWith(expect.objectContaining({
+                audio: expect.objectContaining({ noiseSuppression: { ideal: true } }),
+            }));
+            expect(RecorderMock).toHaveBeenCalledWith(expect.objectContaining({
+                encoderBitRate: voiceRecorderOptions.bitrate,
+                encoderApplication: voiceRecorderOptions.encoderApplication,
+            }));
+        });
     });
 
     describe("when recording", () => {
