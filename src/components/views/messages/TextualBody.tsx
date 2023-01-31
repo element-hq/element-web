97f6431d60ff5e3f9168948a306036402c316fa1
Harnoor Singh <hsingh9@student.gsu.edu>
2023-01-31 04:48:34 -0500

    Migrate highlight.js API call in TextualBody (#9923)
    Co-authored-by: Michael Weimann <michaelw@matrix.org>
    Fixes https://github.com/vector-im/element-web/issues/22233

--- src/components/views/messages/TextualBody.tsx
+++ src/components/views/messages/TextualBody.tsx
@@ -265,7 +265,7 @@ export default class TextualBody extends React.Component<IBodyProps, IState> {
             // We don't use highlightElement here because we can't force language detection
             // off. It should use the one we've found in the CSS class but we'd rather pass
             // it in explicitly to make sure.
-            code.innerHTML = highlight.highlight(advertisedLang, code.textContent).value;
+            code.innerHTML = highlight.highlight(code.textContent, { language: advertisedLang }).value;
         } else if (
             SettingsStore.getValue("enableSyntaxHighlightLanguageDetection") &&
             code.parentElement instanceof HTMLPreElement
