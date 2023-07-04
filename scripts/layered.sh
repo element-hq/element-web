9dd65fc1db6ca17558952c6d05ad9cdd4a40284a
Michael Telatynski <7t3chguy@gmail.com>
2023-07-04 09:47:28 +0100

    Iterate

--- scripts/layered.sh
+++ scripts/layered.sh
@@ -29,11 +29,13 @@ popd
 
 # Also set up matrix-analytics-events for branch with matching name
 node_modules/matrix-react-sdk/scripts/fetchdep.sh matrix-org matrix-analytics-events
-pushd matrix-analytics-events
-yarn link
-yarn install --frozen-lockfile
-yarn build:ts
-popd
+if [ -d matrix-analytics-events ]; then
+    pushd matrix-analytics-events
+    yarn link
+    yarn install --frozen-lockfile
+    yarn build:ts
+    popd
+fi
 
 # Now set up the react-sdk
 node_modules/matrix-react-sdk/scripts/fetchdep.sh matrix-org matrix-react-sdk develop
