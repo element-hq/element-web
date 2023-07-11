6e99b2fca3ed99359bfdf58eafe797fc8f0c6c49
Michael Telatynski <7t3chguy@gmail.com>
2023-07-11 08:25:51 +0100

    Linkify mxc:// URLs as links to your media repo (#11213)

--- src/linkify-matrix.ts
+++ src/linkify-matrix.ts
@@ -19,7 +19,7 @@ import * as linkifyjs from "linkifyjs";
 import { EventListeners, Opts, registerCustomProtocol, registerPlugin } from "linkifyjs";
 import linkifyElement from "linkify-element";
 import linkifyString from "linkify-string";
-import { User } from "matrix-js-sdk/src/matrix";
+import { getHttpUriForMxc, User } from "matrix-js-sdk/src/matrix";
 
 import {
     parsePermalink,
@@ -186,6 +186,11 @@ export const options: Opts = {
 
     formatHref: function (href: string, type: Type | string): string {
         switch (type) {
+            case "url":
+                if (href.startsWith("mxc://") && MatrixClientPeg.get()) {
+                    return getHttpUriForMxc(MatrixClientPeg.get()!.baseUrl, href);
+                }
+            // fallthrough
             case Type.RoomAlias:
             case Type.UserId:
             default: {
@@ -269,6 +274,8 @@ PERMITTED_URL_SCHEMES.forEach((scheme) => {
         registerCustomProtocol(scheme, optionalSlashProtocols.includes(scheme));
     }
 });
+
+registerCustomProtocol("mxc", false);
 
 export const linkify = linkifyjs;
 export const _linkifyElement = linkifyElement;
