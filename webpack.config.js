c0268792379223505c083d9c4802b0af5590a06d
Germain <germain@souquet.com>
2023-08-18 11:56:07 +0100

    Fix path separator for Windows based systems (#25997)

--- webpack.config.js
+++ webpack.config.js
@@ -275,7 +275,7 @@ module.exports = (env, argv) => {
                         // either webpack or our babel setup.
                         // When we do get to upgrade our current setup, this should
                         // probably be removed.
-                        if (f.includes("@vector-im/compound-web")) return true;
+                        if (f.includes(path.join("@vector-im", "compound-web"))) return true;
 
                         // but we can't run all of our dependencies through babel (many of them still
                         // use module.exports which breaks if babel injects an 'include' for its
