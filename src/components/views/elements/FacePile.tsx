93ccccc03d3911c525d31f8e5bb8ad3d483b2b55
Michael Telatynski <7t3chguy@gmail.com>
2023-09-05 10:11:26 +0100

    Fix regression around FacePile with overflow (#11527)
    * Work around compound-web AvatarStack not applying overlap to non-Avatars
    
    * Fix FacePile overflow tile not being layed out correctly

--- src/components/views/elements/FacePile.tsx
+++ src/components/views/elements/FacePile.tsx
@@ -57,7 +57,8 @@ const FacePile: FC<IProps> = ({
 
     const pileContents = (
         <>
-            {overflow ? <span className="mx_FacePile_more" /> : null}
+            {/* XXX: The margin-left is a workaround for Compound's styling excluding this element and being overly specific */}
+            {overflow ? <span className="mx_FacePile_more" style={{ marginLeft: `calc(${size} * -0.2)` }} /> : null}
             {faces}
         </>
     );
