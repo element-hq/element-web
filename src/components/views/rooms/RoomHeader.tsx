cda17d55b4618b6a3fb378513fecded752de03d8
Robin <robin@robin.town>
2023-11-13 12:45:11 -0500

    Use the correct video call icon variant (#11859)
    This regression happened because of breaking changes in the most recent compound-design-tokens release.

--- src/components/views/rooms/RoomHeader.tsx
+++ src/components/views/rooms/RoomHeader.tsx
@@ -16,7 +16,7 @@ limitations under the License.
 
 import React, { useEffect, useMemo, useState } from "react";
 import { Body as BodyText, IconButton, Tooltip } from "@vector-im/compound-web";
-import { Icon as VideoCallIcon } from "@vector-im/compound-design-tokens/icons/video-call.svg";
+import { Icon as VideoCallIcon } from "@vector-im/compound-design-tokens/icons/video-call-solid.svg";
 import { Icon as VoiceCallIcon } from "@vector-im/compound-design-tokens/icons/voice-call.svg";
 import { Icon as ThreadsIcon } from "@vector-im/compound-design-tokens/icons/threads-solid.svg";
 import { Icon as NotificationsIcon } from "@vector-im/compound-design-tokens/icons/notifications-solid.svg";
