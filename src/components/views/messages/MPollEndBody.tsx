2f0eb8fb05a6f856ec9e38612200fd42b2a225eb
Michael Telatynski <7t3chguy@gmail.com>
2023-11-20 17:27:54 +0000

    Fix ended polls overlapping event bubbles (#11895)
    * Fix ended polls overlapping event bubbles
    
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>
    
    * Update snapshot
    
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>
    
    ---------
    
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- src/components/views/messages/MPollEndBody.tsx
+++ src/components/views/messages/MPollEndBody.tsx
@@ -108,7 +108,7 @@ export const MPollEndBody = React.forwardRef<any, IBodyProps>(({ mxEvent, ...pro
     }
 
     return (
-        <div ref={ref}>
+        <div className="mx_MPollEndBody" ref={ref}>
             <Caption>{_t("timeline|m.poll.end|ended")}</Caption>
             <MPollBody mxEvent={pollStartEvent} {...props} />
         </div>
