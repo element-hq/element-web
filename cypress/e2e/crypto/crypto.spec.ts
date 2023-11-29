27b138b52d58db79b8f75fef8d8a6aa421312076
Richard van der Hoff <1389908+richvdh@users.noreply.github.com>
2023-11-29 17:21:11 +0000

    Enable disabled cypress test (#11979)
    I can't for the life of me make this fail locally, so let's have a go on CI.

--- cypress/e2e/crypto/crypto.spec.ts
+++ cypress/e2e/crypto/crypto.spec.ts
@@ -277,7 +277,6 @@ describe("Cryptography", function () {
     });
 
     it("should allow verification when there is no existing DM", function (this: CryptoTestContext) {
-        skipIfRustCrypto(); // https://github.com/vector-im/element-web/issues/26447
         cy.bootstrapCrossSigning(aliceCredentials);
         autoJoin(this.bob);
 
