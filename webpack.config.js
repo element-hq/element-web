ffbcc18221adcb32689c4d3c33de8a51bb40ee31
Nya Candy <dev@candinya.com>
2023-12-06 15:25:26 +0800

    fix: move missing site-specified configuration files

--- webpack.config.js
+++ webpack.config.js
@@ -736,6 +736,7 @@ module.exports = (env, argv) => {
                     { from: "media/**", context: path.resolve(__dirname, "node_modules/matrix-react-sdk/res/") },
                     "node_modules/@matrix-org/olm/olm_legacy.js",
                     { from: "config.json", noErrorOnMissing: true },
+                    { from: "*.json", context: path.resolve(__dirname, "conf.d"), noErrorOnMissing: true },
                     "contribute.json",
                 ],
             }),
