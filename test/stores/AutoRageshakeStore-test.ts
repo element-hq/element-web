element-hq/element-web

commit c30b2637c56962e107153164eaa5e8877c1f4241

Changes for v3.69.0

src/stores/AutoRageshakeStore.ts
@@ -135,9 +135,10 @@ export default class AutoRageshakeStore extends AsyncStoreWithClient<IState> {
                 ...eventInfo,
                 recipient_rageshake: rageshakeURL,
             };
-            this.matrixClient.sendToDevice(AUTO_RS_REQUEST, {
-                [messageContent.user_id]: { [messageContent.device_id]: messageContent },
-            });
+            this.matrixClient.sendToDevice(
+                AUTO_RS_REQUEST,
+                new Map([["messageContent.user_id", new Map([[messageContent.device_id, messageContent]])]]),
+            );
         }
     }
 

test/stores/AutoRageshakeStore-test.ts
@@ -79,9 +79,9 @@ describe("AutoRageshakeStore", () => {
                     [
                       [
                         "im.vector.auto_rs_request",
-                        {
-                          "@userId:matrix.org": {
-                            "undefined": {
+                        Map {
+                          "messageContent.user_id" => Map {
+                            undefined => {
                               "device_id": undefined,
                               "event_id": "utd_event_id",
                               "recipient_rageshake": undefined,

src/components/structures/MessagePanel.tsx
@@ -235,7 +235,7 @@ export default class MessagePanel extends React.Component<IProps, IState> {
     // This is recomputed on each render. It's only stored on the component
     // for ease of passing the data around since it's computed in one pass
     // over all events.
-    private readReceiptsByEvent: Record<string, IReadReceiptProps[]> = {};
+    private readReceiptsByEvent: Map<string, IReadReceiptProps[]> = new Map();
 
     // Track read receipts by user ID. For each user ID we've ever shown a
     // a read receipt for, we store an object:
@@ -254,7 +254,7 @@ export default class MessagePanel extends React.Component<IProps, IState> {
     // This is recomputed on each render, using the data from the previous
     // render as our fallback for any user IDs we can't match a receipt to a
     // displayed event in the current render cycle.
-    private readReceiptsByUserId: Record<string, IReadReceiptForUser> = {};
+    private readReceiptsByUserId: Map<string, IReadReceiptForUser> = new Map();
 
     private readonly _showHiddenEvents: boolean;
     private isMounted = false;
@@ -624,7 +624,7 @@ export default class MessagePanel extends React.Component<IProps, IState> {
         // Note: the EventTile might still render a "sent/sending receipt" independent of
         // this information. When not providing read receipt information, the tile is likely
         // to assume that sent receipts are to be shown more often.
-        this.readReceiptsByEvent = {};
+        this.readReceiptsByEvent = new Map();
         if (this.props.showReadReceipts) {
             this.readReceiptsByEvent = this.getReadReceiptsByShownEvent();
         }
@@ -727,7 +727,7 @@ export default class MessagePanel extends React.Component<IProps, IState> {
         const eventId = mxEv.getId();
         const highlight = eventId === this.props.highlightedEventId;
 
-        const readReceipts = this.readReceiptsByEvent[eventId];
+        const readReceipts = this.readReceiptsByEvent.get(eventId);
 
         let isLastSuccessful = false;
         const isSentState = (s: EventStatus): boolean => !s || s === EventStatus.SENT;
@@ -846,17 +846,11 @@ export default class MessagePanel extends React.Component<IProps, IState> {
     // Get an object that maps from event ID to a list of read receipts that
     // should be shown next to that event. If a hidden event has read receipts,
     // they are folded into the receipts of the last shown event.
-    private getReadReceiptsByShownEvent(): Record<string, IReadReceiptProps[]> {
-        const receiptsByEvent: Record<string, IReadReceiptProps[]> = {};
-        const receiptsByUserId: Record<
-            string,
-            {
-                lastShownEventId: string;
-                receipt: IReadReceiptProps;
-            }
-        > = {};
+    private getReadReceiptsByShownEvent(): Map<string, IReadReceiptProps[]> {
+        const receiptsByEvent: Map<string, IReadReceiptProps[]> = new Map();
+        const receiptsByUserId: Map<string, IReadReceiptForUser> = new Map();
 
-        let lastShownEventId;
+        let lastShownEventId: string;
         for (const event of this.props.events) {
             if (this.shouldShowEvent(event)) {
                 lastShownEventId = event.getId();
@@ -865,9 +859,9 @@ export default class MessagePanel extends React.Component<IProps, IState> {
                 continue;
             }
 
-            const existingReceipts = receiptsByEvent[lastShownEventId] || [];
+            const existingReceipts = receiptsByEvent.get(lastShownEventId) || [];
             const newReceipts = this.getReadReceiptsForEvent(event);
-            receiptsByEvent[lastShownEventId] = existingReceipts.concat(newReceipts);
+            receiptsByEvent.set(lastShownEventId, existingReceipts.concat(newReceipts));
 
             // Record these receipts along with their last shown event ID for
             // each associated user ID.
@@ -885,21 +879,21 @@ export default class MessagePanel extends React.Component<IProps, IState> {
         // someone which had one in the last. By looking through our previous
         // mapping of receipts by user ID, we can cover recover any receipts
         // that would have been lost by using the same event ID from last time.
-        for (const userId in this.readReceiptsByUserId) {
-            if (receiptsByUserId[userId]) {
+        for (const userId of this.readReceiptsByUserId.keys()) {
+            if (receiptsByUserId.get(userId)) {
                 continue;
             }
-            const { lastShownEventId, receipt } = this.readReceiptsByUserId[userId];
-            const existingReceipts = receiptsByEvent[lastShownEventId] || [];
-            receiptsByEvent[lastShownEventId] = existingReceipts.concat(receipt);
-            receiptsByUserId[userId] = { lastShownEventId, receipt };
+            const { lastShownEventId, receipt } = this.readReceiptsByUserId.get(userId);
+            const existingReceipts = receiptsByEvent.get(lastShownEventId) || [];
+            receiptsByEvent.set(lastShownEventId, existingReceipts.concat(receipt));
+            receiptsByUserId.set(userId, { lastShownEventId, receipt });
         }
         this.readReceiptsByUserId = receiptsByUserId;
 
         // After grouping receipts by shown events, do another pass to sort each
         // receipt list.
-        for (const eventId in receiptsByEvent) {
-            receiptsByEvent[eventId].sort((r1, r2) => {
+        for (const receipts of receiptsByEvent.values()) {
+            receipts.sort((r1, r2) => {
                 return r2.ts - r1.ts;
             });
         }

src/components/views/dialogs/devtools/AccountData.tsx
@@ -51,7 +51,7 @@ export const RoomAccountDataEventEditor: React.FC<IEditorProps> = ({ mxEvent, on
 };
 
 interface IProps extends IDevtoolsProps {
-    events: Record<string, MatrixEvent>;
+    events: Map<string, MatrixEvent>;
     Editor: React.FC<IEditorProps>;
     actionLabel: string;
 }
@@ -74,7 +74,7 @@ const BaseAccountDataExplorer: React.FC<IProps> = ({ events, Editor, actionLabel
     return (
         <BaseTool onBack={onBack} actionLabel={actionLabel} onAction={onAction}>
             <FilteredList query={query} onChange={setQuery}>
-                {Object.entries(events).map(([eventType, ev]) => {
+                {Array.from(events.entries()).map(([eventType, ev]) => {
                     const onClick = (): void => {
                         setEvent(ev);
                     };

src/languageHandler.tsx
@@ -21,6 +21,7 @@ import counterpart from "counterpart";
 import React from "react";
 import { logger } from "matrix-js-sdk/src/logger";
 import { Optional } from "matrix-events-sdk";
+import { MapWithDefault, safeSet } from "matrix-js-sdk/src/utils";
 
 import SettingsStore from "./settings/SettingsStore";
 import PlatformPeg from "./PlatformPeg";
@@ -629,21 +630,16 @@ export class CustomTranslationOptions {
 function doRegisterTranslations(customTranslations: ICustomTranslations): void {
     // We convert the operator-friendly version into something counterpart can
     // consume.
-    const langs: {
-        // same structure, just flipped key order
-        [lang: string]: {
-            [str: string]: string;
-        };
-    } = {};
+    // Map: lang → Record: string → translation
+    const langs: MapWithDefault<string, Record<string, string>> = new MapWithDefault(() => ({}));
     for (const [str, translations] of Object.entries(customTranslations)) {
         for (const [lang, newStr] of Object.entries(translations)) {
-            if (!langs[lang]) langs[lang] = {};
-            langs[lang][str] = newStr;
+            safeSet(langs.getOrCreate(lang), str, newStr);
         }
     }
 
     // Finally, tell counterpart about our translations
-    for (const [lang, translations] of Object.entries(langs)) {
+    for (const [lang, translations] of langs) {
         counterpart.registerTranslations(lang, translations);
     }
 }

src/modules/ModuleRunner.ts
@@ -14,11 +14,13 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+import { safeSet } from "matrix-js-sdk/src/utils";
 import { TranslationStringsObject } from "@matrix-org/react-sdk-module-api/lib/types/translations";
 import { AnyLifecycle } from "@matrix-org/react-sdk-module-api/lib/lifecycles/types";
 
 import { AppModule } from "./AppModule";
 import { ModuleFactory } from "./ModuleFactory";
+
 import "./ModuleComponents";
 
 /**
@@ -53,9 +55,10 @@ export class ModuleRunner {
             if (!i18n) continue;
 
             for (const [lang, strings] of Object.entries(i18n)) {
-                if (!merged[lang]) merged[lang] = {};
+                safeSet(merged, lang, merged[lang] || {});
+
                 for (const [str, val] of Object.entries(strings)) {
-                    merged[lang][str] = val;
+                    safeSet(merged[lang], str, val);
                 }
             }
         }

src/settings/handlers/RoomDeviceSettingsHandler.ts
@@ -15,6 +15,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+import { safeSet } from "matrix-js-sdk/src/utils";
+
 import { SettingLevel } from "../SettingLevel";
 import { WatchManager } from "../WatchManager";
 import AbstractLocalStorageSettingsHandler from "./AbstractLocalStorageSettingsHandler";
@@ -48,7 +50,7 @@ export default class RoomDeviceSettingsHandler extends AbstractLocalStorageSetti
             let value = this.read("mx_local_settings");
             if (!value) value = {};
             if (!value["blacklistUnverifiedDevicesPerRoom"]) value["blacklistUnverifiedDevicesPerRoom"] = {};
-            value["blacklistUnverifiedDevicesPerRoom"][roomId] = newValue;
+            safeSet(value["blacklistUnverifiedDevicesPerRoom"], roomId, newValue);
             this.setObject("mx_local_settings", value);
             this.watchers.notifyUpdate(settingName, roomId, SettingLevel.ROOM_DEVICE, newValue);
             return Promise.resolve();

src/stores/widgets/StopGapWidgetDriver.ts
@@ -275,7 +275,7 @@ export class StopGapWidgetDriver extends WidgetDriver {
                         if (deviceId === "*") {
                             // Send the message to all devices we have keys for
                             await client.encryptAndSendToDevices(
-                                Object.values(deviceInfoMap[userId]).map((deviceInfo) => ({
+                                Array.from(deviceInfoMap.get(userId).values()).map((deviceInfo) => ({
                                     userId,
                                     deviceInfo,
                                 })),
@@ -284,7 +284,7 @@ export class StopGapWidgetDriver extends WidgetDriver {
                         } else {
                             // Send the message to a specific device
                             await client.encryptAndSendToDevices(
-                                [{ userId, deviceInfo: deviceInfoMap[userId][deviceId] }],
+                                [{ userId, deviceInfo: deviceInfoMap.get(userId).get(deviceId) }],
                                 content,
                             );
                         }

commit 5c8b14c53e17b321da7f3cae7cc0cd0c28949cb1

Fix requests for senders to submit auto-rageshakes (#12304)
If auto-rageshake-on-UISI is enabled, then when we get a UISI we send the sender a to-device message asking them to also rageshake. Unfortunately, the logic to do so has been broken since c30b263.

Also a few other minor cleanups while we're here.

src/stores/AutoRageshakeStore.ts
@@ -14,8 +14,17 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import { ClientEvent, MatrixEvent, MatrixEventEvent, SyncStateData, SyncState } from "matrix-js-sdk/src/matrix";
+import {
+    ClientEvent,
+    MatrixEvent,
+    MatrixEventEvent,
+    SyncStateData,
+    SyncState,
+    ToDeviceMessageId,
+} from "matrix-js-sdk/src/matrix";
 import { sleep } from "matrix-js-sdk/src/utils";
+import { v4 as uuidv4 } from "uuid";
+import { logger } from "matrix-js-sdk/src/logger";
 
 import SdkConfig from "../SdkConfig";
 import sendBugReport from "../rageshake/submit-rageshake";
@@ -108,20 +117,28 @@ export default class AutoRageshakeStore extends AsyncStoreWithClient<IState> {
 
             const now = new Date().getTime();
             if (now - this.state.lastRageshakeTime < RAGESHAKE_INTERVAL) {
+                logger.info(
+                    `Not sending recipient-side autorageshake for event ${ev.getId()}/session ${sessionId}: last rageshake was too recent`,
+                );
                 return;
             }
 
             await this.updateState({ lastRageshakeTime: now });
 
+            const senderUserId = ev.getSender()!;
             const eventInfo = {
                 event_id: ev.getId(),
                 room_id: ev.getRoomId(),
                 session_id: sessionId,
                 device_id: wireContent.device_id,
-                user_id: ev.getSender()!,
+                user_id: senderUserId,
                 sender_key: wireContent.sender_key,
             };
 
+            logger.info(`Sending recipient-side autorageshake for event ${ev.getId()}/session ${sessionId}`);
+            // XXX: the rageshake server returns the URL for the github issue... which is typically absent for
+            //   auto-uisis, because we've disabled creation of GH issues for them. So the `recipient_rageshake`
+            //   field is broken.
             const rageshakeURL = await sendBugReport(SdkConfig.get().bug_report_endpoint_url, {
                 userText: "Auto-reporting decryption error (recipient)",
                 sendLogs: true,
@@ -133,10 +150,11 @@ export default class AutoRageshakeStore extends AsyncStoreWithClient<IState> {
             const messageContent = {
                 ...eventInfo,
                 recipient_rageshake: rageshakeURL,
+                [ToDeviceMessageId]: uuidv4(),
             };
             this.matrixClient?.sendToDevice(
                 AUTO_RS_REQUEST,
-                new Map([["messageContent.user_id", new Map([[messageContent.device_id, messageContent]])]]),
+                new Map([[senderUserId, new Map([[messageContent.device_id, messageContent]])]]),
             );
         }
     }
@@ -158,6 +176,9 @@ export default class AutoRageshakeStore extends AsyncStoreWithClient<IState> {
         const now = new Date().getTime();
         if (now - this.state.lastRageshakeTime > RAGESHAKE_INTERVAL) {
             await this.updateState({ lastRageshakeTime: now });
+            logger.info(
+                `Sending sender-side autorageshake for event ${messageContent["event_id"]}/session ${messageContent["session_id"]}`,
+            );
             await sendBugReport(SdkConfig.get().bug_report_endpoint_url, {
                 userText: `Auto-reporting decryption error (sender)\nRecipient rageshake: ${recipientRageshake}`,
                 sendLogs: true,
@@ -168,6 +189,10 @@ export default class AutoRageshakeStore extends AsyncStoreWithClient<IState> {
                     auto_uisi: JSON.stringify(messageContent),
                 },
             });
+        } else {
+            logger.info(
+                `Not sending sender-side autorageshake for event ${messageContent["event_id"]}/session ${messageContent["session_id"]}: last rageshake was too recent`,
+            );
         }
     }
 

test/stores/AutoRageshakeStore-test.ts
@@ -32,6 +32,8 @@ jest.mock("../../src/rageshake/submit-rageshake");
 jest.mock("../../src/stores/WidgetStore");
 jest.mock("../../src/stores/widgets/WidgetLayoutStore");
 
+const TEST_SENDER = "@sender@example.com";
+
 describe("AutoRageshakeStore", () => {
     const roomId = "!room:example.com";
     let client: MatrixClient;
@@ -59,7 +61,7 @@ describe("AutoRageshakeStore", () => {
             event: true,
             content: {},
             room: roomId,
-            user: client.getSafeUserId(),
+            user: TEST_SENDER,
             type: EventType.RoomMessage,
         });
         jest.spyOn(utdEvent, "isDecryptionFailure").mockReturnValue(true);
@@ -81,29 +83,32 @@ describe("AutoRageshakeStore", () => {
                 jest.advanceTimersByTime(5500);
             });
 
-            it("should send a rageshake", () => {
-                expect(mocked(client).sendToDevice.mock.calls).toMatchInlineSnapshot(
-                    `
+            it("should send a to-device message", () => {
+                expect(mocked(client).sendToDevice.mock.calls).toEqual([
                     [
-                      [
                         "im.vector.auto_rs_request",
-                        Map {
-                          "messageContent.user_id" => Map {
-                            undefined => {
-                              "device_id": undefined,
-                              "event_id": "utd_event_id",
-                              "recipient_rageshake": undefined,
-                              "room_id": "!room:example.com",
-                              "sender_key": undefined,
-                              "session_id": undefined,
-                              "user_id": "@userId:matrix.org",
-                            },
-                          },
-                        },
-                      ],
-                    ]
-                `.replace("utd_event_id", utdEvent.getId()!),
-                );
+                        new Map([
+                            [
+                                TEST_SENDER,
+                                new Map([
+                                    [
+                                        undefined,
+                                        {
+                                            "device_id": undefined,
+                                            "event_id": utdEvent.getId(),
+                                            "org.matrix.msgid": expect.any(String),
+                                            "recipient_rageshake": undefined,
+                                            "room_id": "!room:example.com",
+                                            "sender_key": undefined,
+                                            "session_id": undefined,
+                                            "user_id": TEST_SENDER,
+                                        },
+                                    ],
+                                ]),
+                            ],
+                        ]),
+                    ],
+                ]);
             });
         });
     });