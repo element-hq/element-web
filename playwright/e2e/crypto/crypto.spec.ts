bf0fed59f20dbe3a7288f9a624b58e03a8c334b1
Florian Duros <florian.duros@ormaz.fr>
2024-04-18 15:25:00 +0200

    Use focus instead of hover

--- playwright/e2e/crypto/crypto.spec.ts
+++ playwright/e2e/crypto/crypto.spec.ts
@@ -392,7 +392,7 @@ test.describe("Cryptography", function () {
             await bobSecondDevice.sendMessage(testRoomId, "test encrypted from unverified");
             await expect(lastTile).toContainText("test encrypted from unverified");
             await expect(lastTileE2eIcon).toHaveClass(/mx_EventTile_e2eIcon_warning/);
-            await lastTileE2eIcon.hover();
+            await lastTileE2eIcon.focus();
             await expect(page.getByRole("tooltip")).toContainText("Encrypted by a device not verified by its owner.");
 
             /* Should show a grey padlock for a message from an unknown device */
@@ -431,7 +431,7 @@ test.describe("Cryptography", function () {
             } else {
                 await expect(lastE2eIcon).toHaveClass(/mx_EventTile_e2eIcon_normal/);
             }
-            await lastE2eIcon.hover();
+            await lastE2eIcon.focus();
             await expect(page.getByRole("tooltip")).toContainText("Encrypted by an unknown or deleted device.");
         });
 
