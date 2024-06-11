c7b2ca04fcc51fd0282b63981b2bc8385b645e30
Reza Mohseni <reza.mohseni@rosberg.com>
2024-06-11 16:59:58 +0200

    feat: Enable "UIFeature.AddSubSpace" feature in SpaceContextMenu. added test

--- src/settings/UIFeature.ts
+++ src/settings/UIFeature.ts
@@ -34,6 +34,7 @@ export const enum UIFeature {
     TimelineEnableRelativeDates = "UIFeature.timelineEnableRelativeDates",
     BulkUnverifiedSessionsReminder = "UIFeature.BulkUnverifiedSessionsReminder",
     ShowCreateSpaceButton = "UIFeature.showCreateSpaceButton",
+    AddSubSpace = "UIFeature.addSubSpace",
 }
 
 export enum UIComponent {
