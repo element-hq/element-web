f9e2425ac700b50a8c2600cc212f2a957115259c
Michael Telatynski <7t3chguy@gmail.com>
2024-06-18 16:12:12 +0100

    Remove stray setState which caused encryption state shields to flicker (#12632)
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- src/components/structures/RoomView.tsx
+++ src/components/structures/RoomView.tsx
@@ -1539,7 +1539,6 @@ export class RoomView extends React.Component<IRoomProps, IRoomState> {
         this.setState({ e2eStatus });
 
         if (this.context.client.isCryptoEnabled()) {
-            this.setState({ e2eStatus: E2EStatus.Normal });
             /* At this point, the user has encryption on and cross-signing on */
             e2eStatus = await shieldStatusForRoom(this.context.client, room);
             RoomView.e2eStatusCache.set(room.roomId, e2eStatus);
