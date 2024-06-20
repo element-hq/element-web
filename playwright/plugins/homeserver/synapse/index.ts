d5e06c95cb86887f010b930dd30a6a3decce9d45
ElementRobot <releases@riot.im>
2024-06-20 10:42:39 +0100

    [create-pull-request] automated change (#12655)
    Co-authored-by: github-merge-queue <github-merge-queue@users.noreply.github.com>

--- playwright/plugins/homeserver/synapse/index.ts
+++ playwright/plugins/homeserver/synapse/index.ts
@@ -28,7 +28,7 @@ import { randB64Bytes } from "../../utils/rand";
 // Docker tag to use for `matrixdotorg/synapse` image.
 // We target a specific digest as every now and then a Synapse update will break our CI.
 // This digest is updated by the playwright-image-updates.yaml workflow periodically.
-const DOCKER_TAG = "develop@sha256:6ef71b95a6ff5c5b1bdcc052e0acc6ac1ef28f6904772a7077f5e0ede260e422";
+const DOCKER_TAG = "develop@sha256:54998edd6a29399e53c541cb698ce63569a47fde8680212acf23b0d7b795db95";
 
 async function cfgDirFromTemplate(opts: StartHomeserverOpts): Promise<Omit<HomeserverConfig, "dockerUrl">> {
     const templateDir = path.join(__dirname, "templates", opts.template);
