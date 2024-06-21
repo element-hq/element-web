4bbc57da254a1117348621b5d453be8b32e2f0cd
Reza Mohseni <reza.mohseni@rosberg.com>
2024-06-21 08:24:04 +0200

    feat: Added 'UIFeature.ShowExploreRoomsButton' to LeftPanel. created tests

--- src/settings/UIFeature.ts
+++ src/settings/UIFeature.ts
@@ -34,6 +34,7 @@ export const enum UIFeature {
     TimelineEnableRelativeDates = "UIFeature.timelineEnableRelativeDates",
     BulkUnverifiedSessionsReminder = "UIFeature.BulkUnverifiedSessionsReminder",
     ShowCreateSpaceButton = "UIFeature.showCreateSpaceButton",
+    ShowExploreRoomsButton = "UIFeature.showExploreRoomsButton",
 }
 
 export enum UIComponent {
