f33eec70edd8a65f4102242276df1a2072f25c6f
Michael Telatynski <7t3chguy@gmail.com>
2024-08-01 09:59:28 +0100

    Fix babel config
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- babel.config.js
+++ babel.config.js
@@ -19,7 +19,6 @@ module.exports = {
     plugins: [
         "@babel/plugin-proposal-export-default-from",
         "@babel/plugin-transform-numeric-separator",
-        "@babel/plugin-transform-class-properties",
         "@babel/plugin-transform-object-rest-spread",
         "@babel/plugin-syntax-dynamic-import",
         "@babel/plugin-transform-runtime",
