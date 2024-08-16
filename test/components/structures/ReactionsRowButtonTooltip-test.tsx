72f5defa99fd48deb68ef9603e4a16280beb809b
Thor Arne Johansen <taj@verji.com>
2024-08-16 15:04:57 +0200

    fix issues after syncing with  upstream 3.100.0

--- test/components/structures/ReactionsRowButtonTooltip-test.tsx
+++ test/components/structures/ReactionsRowButtonTooltip-test.tsx
@@ -1,133 +1,87 @@
 /*
-Copyright 2023 Beeper
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+    Copyright 2024 Verji Tech AS. All rights reserved.
+    Unauthorized copying or distribution of this file, via any medium, is strictly prohibited.
 */
 
 import React from "react";
-import { IContent, MatrixEvent, Room } from "matrix-js-sdk/src/matrix";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import { MatrixClient, MatrixEvent, Room } from "matrix-js-sdk/src/matrix";
+import {
+    CustomComponentLifecycle,
+    CustomComponentOpts,
+} from "@matrix-org/react-sdk-module-api/lib/lifecycles/CustomComponentLifecycle";
 
-import MatrixClientContext from "../../../../src/contexts/MatrixClientContext";
-import { getMockClientWithEventEmitter } from "../../../test-utils";
-import ReactionsRowButton, { IProps } from "../../../../src/components/views/messages/ReactionsRowButton";
+import ReactionsRowButtonTooltip from "../../../src/components/views/messages/ReactionsRowButtonTooltip";
+import { getMockClientWithEventEmitter } from "../../test-utils";
+import MatrixClientContext from "../../../src/contexts/MatrixClientContext";
+import { ModuleRunner } from "../../../src/modules/ModuleRunner";
 
-describe("ReactionsRowButton", () => {
-    const userId = "@alice:server";
-    const roomId = "!randomcharacters:aser.ver";
+import { Tooltip } from "@vector-im/compound-web";
+
+
+describe("ReactionsRowButtonTooltip", () => {
+    const content = "Hello world!";
+    const reactionEvents = [] as any;
+    const roomId = "myRoomId";
     const mockClient = getMockClientWithEventEmitter({
         mxcUrlToHttp: jest.fn().mockReturnValue("https://not.a.real.url"),
         getRoom: jest.fn(),
     });
+    const userId = "@alice:server";
     const room = new Room(roomId, mockClient, userId);
 
-    const createProps = (relationContent: IContent): IProps => ({
-        mxEvent: new MatrixEvent({
-            room_id: roomId,
-            event_id: "$test:example.com",
-            content: { body: "test" },
-        }),
-        content: relationContent["m.relates_to"]?.key || "",
-        count: 2,
-        reactionEvents: [
-            new MatrixEvent({
-                type: "m.reaction",
-                sender: "@user1:example.com",
-                content: relationContent,
-            }),
-            new MatrixEvent({
-                type: "m.reaction",
-                sender: "@user2:example.com",
-                content: relationContent,
-            }),
-        ],
-        customReactionImagesEnabled: true,
-    });
+    const customReactionImagesEnabled = true;
 
-    beforeEach(function () {
+    const mxEvent = {
+        getRoomId: jest.fn().mockReturnValue(roomId),
+        pushDetails: {},
+        _replacingEvent: null,
+        _localRedactionEvent: null,
+        _isCancelled: false,
+    } as unknown as MatrixEvent;
+
+    const getComp = () =>
+        render(
+            <MatrixClientContext.Provider
+                value={{ getRoom: jest.fn().mockReturnValue(room) } as unknown as MatrixClient}
+            >
+                <ReactionsRowButtonTooltip
+                    mxEvent={mxEvent}
+                    content={content}
+                    reactionEvents={reactionEvents}
+                    customReactionImagesEnabled={customReactionImagesEnabled}
+                />
+            </MatrixClientContext.Provider>,
+        );
+
+    beforeEach(() => {
         jest.clearAllMocks();
-        mockClient.credentials = { userId: userId };
-        mockClient.getRoom.mockImplementation((roomId: string): Room | null => {
-            return roomId === room.roomId ? room : null;
-        });
     });
 
-    it("renders reaction row button emojis correctly", () => {
-        const props = createProps({
-            "m.relates_to": {
-                event_id: "$user2:example.com",
-                key: "👍",
-                rel_type: "m.annotation",
-            },
-        });
-        const root = render(
-            <MatrixClientContext.Provider value={mockClient}>
-                <ReactionsRowButton {...props} />
-            </MatrixClientContext.Provider>,
-        );
-        expect(root.asFragment()).toMatchSnapshot();
-
-        // Try hover and make sure that the ReactionsRowButtonTooltip works
-        const reactionButton = root.getByRole("button");
-        const event = new MouseEvent("mouseover", {
-            bubbles: true,
-            cancelable: true,
-        });
-        reactionButton.dispatchEvent(event);
-
-        expect(root.asFragment()).toMatchSnapshot();
+    it("should render", () => {
+        const { asFragment } = getComp();
+        screen.debug();
+        expect(asFragment()).toMatchSnapshot();
     });
 
-    it("renders reaction row button custom image reactions correctly", () => {
-        const props = createProps({
-            "com.beeper.reaction.shortcode": ":test:",
-            "shortcode": ":test:",
-            "m.relates_to": {
-                event_id: "$user1:example.com",
-                key: "mxc://example.com/123456789",
-                rel_type: "m.annotation",
-            },
+    describe("wrap the ReactionsRowButtonTooltip with a React.Fragment", () => {
+        it("should wrap the ReactionsRowButtonTooltip with a React.Fragment", () => {
+            jest.spyOn(ModuleRunner.instance, "invoke").mockImplementation((lifecycleEvent, opts) => {
+                if (lifecycleEvent === CustomComponentLifecycle.ReactionsRowButtonTooltip) {
+                    (opts as CustomComponentOpts).CustomComponent = ({ children }) => {
+                        return (
+                            <Tooltip label={"r1, r2"} caption={"caption"} placement="right">
+                                <React.Fragment>
+                                    <div data-testid="test-header">Header</div>
+                                </React.Fragment>
+                            </Tooltip>
+                        );
+                    };
+                }
+            });
+
+            getComp();
+            expect(screen.getByTestId("test-header")).toBeDefined();
         });
-
-        const root = render(
-            <MatrixClientContext.Provider value={mockClient}>
-                <ReactionsRowButton {...props} />
-            </MatrixClientContext.Provider>,
-        );
-        expect(root.asFragment()).toMatchSnapshot();
-
-        // Try hover and make sure that the ReactionsRowButtonTooltip works
-        const reactionButton = root.getByRole("button");
-        const event = new MouseEvent("mouseover", {
-            bubbles: true,
-            cancelable: true,
-        });
-        reactionButton.dispatchEvent(event);
-
-        expect(root.asFragment()).toMatchSnapshot();
-    });
-
-    it("renders without a room", () => {
-        mockClient.getRoom.mockImplementation(() => null);
-
-        const props = createProps({});
-
-        const root = render(
-            <MatrixClientContext.Provider value={mockClient}>
-                <ReactionsRowButton {...props} />
-            </MatrixClientContext.Provider>,
-        );
-
-        expect(root.asFragment()).toMatchSnapshot();
     });
 });
