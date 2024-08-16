b96c426bf245df989979a64d838c99ae3799f365
John Tore Simonsen <john.tore.simonsen@rosberg.com>
2024-08-16 10:16:29 +0200

    modify install-deps to install matrix-react-sdk-module-api from verji-develop

--- scripts/ci/install-deps.sh
+++ scripts/ci/install-deps.sh
@@ -9,13 +9,14 @@
 
 set -ex
 
-scripts/fetchdep.sh verji matrix-js-sdk verji-develop
+scripts/fetchdep.sh verji matrix-js-sdk verji-develop # VERJI HARDCODE PARAMS
 pushd matrix-js-sdk
 [ -n "$JS_SDK_GITHUB_BASE_REF" ] && git fetch --depth 1 origin $JS_SDK_GITHUB_BASE_REF && git checkout $JS_SDK_GITHUB_BASE_REF
 yarn link
 yarn install --frozen-lockfile $@
 popd
 
+#VERJI COMMENT remove matrix-analytics-event
 #scripts/fetchdep.sh matrix-org matrix-analytics-events
 # We don't pass a default branch so cloning may fail when we are not in a PR
 # This is expected as this project does not share a release cycle but we still branch match it
@@ -26,7 +27,17 @@ popd
 #    yarn build:ts
 #    popd
 #fi
+# END VERJI COMMENT
 
+# VERJI ADD custom module-api
+scripts/fetchdep.sh verji matrix-react-sdk-module-api verji-main # VERJI HARDCODE PARAMS
+pushd matrix-js-sdk
+[ -n "$JS_SDK_GITHUB_BASE_REF" ] && git fetch --depth 1 origin $JS_SDK_GITHUB_BASE_REF && git checkout $JS_SDK_GITHUB_BASE_REF
+yarn link
+yarn install --frozen-lockfile $@
+popd
+
+yarn link @matrix-org/react-sdk-module-api
 yarn link matrix-js-sdk
 [ -d matrix-analytics-events ] && yarn link @matrix-org/analytics-events
 yarn install --frozen-lockfile $@
