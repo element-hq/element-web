26b0e83ac4d479df9b03514619b76dec1d5a1ffc
Michael Telatynski <7t3chguy@gmail.com>
2024-10-04 13:42:23 +0100

    Replace composer icons with Compound variants (#123)
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- src/components/views/elements/Pill.tsx
+++ src/components/views/elements/Pill.tsx
@@ -10,6 +10,7 @@ import React, { ReactElement } from "react";
 import classNames from "classnames";
 import { Room, RoomMember } from "matrix-js-sdk/src/matrix";
 import { Tooltip } from "@vector-im/compound-web";
+import LinkIcon from "@vector-im/compound-design-tokens/assets/web/icons/link";
 
 import { MatrixClientPeg } from "../../../MatrixClientPeg";
 import MatrixClientContext from "../../../contexts/MatrixClientContext";
@@ -17,7 +18,6 @@ import { usePermalink } from "../../../hooks/usePermalink";
 import RoomAvatar from "../avatars/RoomAvatar";
 import MemberAvatar from "../avatars/MemberAvatar";
 import { _t } from "../../../languageHandler";
-import { Icon as LinkIcon } from "../../../../res/img/element-icons/room/composer/link.svg";
 import { Icon as UserIcon } from "../../../../res/img/compound/user.svg";
 
 export enum PillType {
