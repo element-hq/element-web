837e27ed4229642f29468d3e67872995b1587fd3
Michael Telatynski <7t3chguy@gmail.com>
2024-10-16 16:49:44 +0100

    Iterate
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- src/components/views/rooms/Stickerpicker.tsx
+++ src/components/views/rooms/Stickerpicker.tsx
@@ -26,7 +26,6 @@ import ScalarAuthClient from "../../../ScalarAuthClient";
 import GenericElementContextMenu from "../context_menus/GenericElementContextMenu";
 import RightPanelStore from "../../../stores/right-panel/RightPanelStore";
 import { UPDATE_EVENT } from "../../../stores/AsyncStore";
-import StickerpackPlaceholderSvg from "../../../../res/img/stickerpack-placeholder.png";
 
 // This should be below the dialog level (4000), but above the rest of the UI (1000-2000).
 // We sit in a context menu, so this should be given to the context menu.
@@ -203,11 +202,13 @@ export default class Stickerpicker extends React.PureComponent<IProps, IState> {
     };
 
     private defaultStickerpickerContent(): JSX.Element {
+        // eslint-disable-next-line @typescript-eslint/no-require-imports
+        const imgSrc = require("../../../../res/img/stickerpack-placeholder.png");
         return (
             <AccessibleButton onClick={this.launchManageIntegrations} className="mx_Stickers_contentPlaceholder">
                 <p>{_t("stickers|empty")}</p>
                 <p className="mx_Stickers_addLink">{_t("stickers|empty_add_prompt")}</p>
-                <img src={StickerpackPlaceholderSvg} alt="" />
+                <img src={imgSrc} alt="" />
             </AccessibleButton>
         );
     }
