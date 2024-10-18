416ffa951bd9e186c3d289e1bbf12b4060ab18fb
Michael Telatynski <7t3chguy@gmail.com>
2024-10-18 10:02:21 +0100

    Remove dead code
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- src/components/structures/ContextMenu.tsx
+++ src/components/structures/ContextMenu.tsx
@@ -611,35 +611,6 @@ export const useContextMenu = <T extends any = HTMLElement>(inputRef?: RefObject
     return [button.current ? isOpen : false, button, open, close, setIsOpen];
 };
 
-// XXX: Deprecated, used only for dynamic Tooltips. Avoid using at all costs.
-export function createMenu(
-    ElementClass: typeof React.Component,
-    props: Record<string, any>,
-): { close: (...args: any[]) => void } {
-    const onFinished = function (...args: any[]): void {
-        ReactDOM.unmountComponentAtNode(getOrCreateContainer());
-        props?.onFinished?.apply(null, args);
-    };
-
-    const menu = (
-        <TooltipProvider>
-            <ContextMenu
-                {...props}
-                mountAsChild={true}
-                hasBackground={false}
-                onFinished={onFinished} // eslint-disable-line react/jsx-no-bind
-                windowResize={onFinished} // eslint-disable-line react/jsx-no-bind
-            >
-                <ElementClass {...props} onFinished={onFinished} />
-            </ContextMenu>
-        </TooltipProvider>
-    );
-
-    ReactDOM.render(menu, getOrCreateContainer());
-
-    return { close: onFinished };
-}
-
 // re-export the semantic helper components for simplicity
 export { ContextMenuButton } from "../../accessibility/context_menu/ContextMenuButton";
 export { ContextMenuTooltipButton } from "../../accessibility/context_menu/ContextMenuTooltipButton";
