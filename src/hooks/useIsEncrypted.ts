08617d330ceb909d90d0664e3483b0d7c918a806
Florian Duros <florian.duros@ormaz.fr>
2024-10-23 19:18:35 +0200

    Replace `MatrixClient.isRoomEncrypted` by `MatrixClient.CryptoApi.isEncryptionEnabledInRoom` in `useIsEncrypted`

--- src/hooks/useIsEncrypted.ts
+++ src/hooks/useIsEncrypted.ts
@@ -6,19 +6,24 @@ SPDX-License-Identifier: AGPL-3.0-only OR GPL-3.0-only
 Please see LICENSE files in the repository root for full details.
 */
 
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { MatrixClient, MatrixEvent, Room, RoomStateEvent, EventType } from "matrix-js-sdk/src/matrix";
 
 import { useTypedEventEmitter } from "./useEventEmitter";
 
-// Hook to simplify watching whether a Matrix room is encrypted, returns undefined if room is undefined
+// Hook to simplify watching whether a Matrix room is encrypted, returns undefined if room is undefined or when isRoomEncrypted is computed
 export function useIsEncrypted(cli: MatrixClient, room?: Room): boolean | undefined {
-    const [isEncrypted, setIsEncrypted] = useState(room ? cli.isRoomEncrypted(room.roomId) : undefined);
+    const [isEncrypted, setIsEncrypted] = useState<boolean>();
 
+    useEffect(() => {
+        const func = async (): Promise<void> =>
+            room && setIsEncrypted(await cli.getCrypto()?.isEncryptionEnabledInRoom(room.roomId));
+        func();
+    }, [cli, room]);
     const update = useCallback(
-        (event: MatrixEvent) => {
+        async (event: MatrixEvent) => {
             if (room && event.getType() === EventType.RoomEncryption) {
-                setIsEncrypted(cli.isRoomEncrypted(room.roomId));
+                setIsEncrypted(await cli.getCrypto()?.isEncryptionEnabledInRoom(room.roomId));
             }
         },
         [cli, room],
