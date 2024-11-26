7adccb2e9484505b35a2b0ee04f63905ffa90c11
Michael Telatynski <7t3chguy@gmail.com>
2024-11-26 17:24:52 +0000

    Fix imports
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- src/vector/platform/WebPlatform.ts
+++ src/vector/platform/WebPlatform.ts
@@ -7,7 +7,7 @@ SPDX-License-Identifier: AGPL-3.0-only OR GPL-3.0-only
 Please see LICENSE files in the repository root for full details.
 */
 
-import UAParser from "ua-parser-js";
+import { UAParser } from "ua-parser-js";
 import { logger } from "matrix-js-sdk/src/logger";
 
 import { MatrixClientPeg } from "../../MatrixClientPeg";
