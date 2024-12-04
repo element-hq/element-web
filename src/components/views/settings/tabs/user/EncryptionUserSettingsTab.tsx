ae623f8319f04b8f4104648c172086786ce4078b
Florian Duros <florian.duros@ormaz.fr>
2024-12-04 16:38:54 +0100

    Add encryption tab

---
+++ src/components/views/settings/tabs/user/EncryptionUserSettingsTab.tsx
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2024 New Vector Ltd.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only OR GPL-3.0-only
+ * Please see LICENSE files in the repository root for full details.
+ */
+
+import React, { JSX } from "react";
+
+import SettingsTab from "../SettingsTab";
+
+export function EncryptionUserSettingsTab(): JSX.Element {
+    return <SettingsTab />;
+}
