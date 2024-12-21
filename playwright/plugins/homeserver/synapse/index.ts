ab80ec2d700f3c0c8310ddd2a81ebe48b2c1802e
t3chguy <2403652+t3chguy@users.noreply.github.com>
2024-12-21 06:04:07 +0000

    [create-pull-request] automated change

--- playwright/plugins/homeserver/synapse/index.ts
+++ playwright/plugins/homeserver/synapse/index.ts
@@ -20,7 +20,7 @@ import { randB64Bytes } from "../../utils/rand";
 // Docker tag to use for synapse docker image.
 // We target a specific digest as every now and then a Synapse update will break our CI.
 // This digest is updated by the playwright-image-updates.yaml workflow periodically.
-const DOCKER_TAG = "develop@sha256:17cc0a301447430624afb860276e5c13270ddeb99a3f6d1c6d519a20b1a8f650";
+const DOCKER_TAG = "develop@sha256:cde72497d6d096445ce4eaf4b6797f871c67bc7ff7ea567a97e10676fb488796";
 
 async function cfgDirFromTemplate(opts: StartHomeserverOpts): Promise<Omit<HomeserverConfig, "dockerUrl">> {
     const templateDir = path.join(__dirname, "templates", opts.template);
