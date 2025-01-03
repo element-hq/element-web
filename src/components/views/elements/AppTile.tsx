52d32410e5c7b34e52570a3504e47b205933f78a
David Langley <davidl@element.io>
2025-01-03 14:44:27 +0000

    Update only spdx id

--- src/components/views/elements/AppTile.tsx
+++ src/components/views/elements/AppTile.tsx
@@ -5,7 +5,7 @@ Copyright 2019 Michael Telatynski <7t3chguy@gmail.com>
 Copyright 2018 New Vector Ltd
 Copyright 2017 Vector Creations Ltd
 
-SPDX-License-Identifier: AGPL-3.0-only OR GPL-3.0-only
+SPDX-License-Identifier: AGPL-3.0-only OR GPL-3.0-only OR LicenseRef-Element-Commercial
 Please see LICENSE files in the repository root for full details.
 */
 
--- src/components/views/room_settings/UrlPreviewSettings.tsx
+++ src/components/views/room_settings/UrlPreviewSettings.tsx
@@ -5,7 +5,7 @@ Copyright 2018, 2019 New Vector Ltd
 Copyright 2017 Travis Ralston
 Copyright 2016 OpenMarket Ltd
 
-SPDX-License-Identifier: AGPL-3.0-only OR GPL-3.0-only
+SPDX-License-Identifier: AGPL-3.0-only OR GPL-3.0-only OR LicenseRef-Element-Commercial
 Please see LICENSE files in the repository root for full details.
 */
 
