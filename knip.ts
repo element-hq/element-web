2348557e0cf872eb8be924fd48169a3255804cb3
Michael Telatynski <7t3chguy@gmail.com>
2025-01-09 14:51:40 +0000

    Update knip config
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- knip.ts
+++ knip.ts
@@ -10,13 +10,13 @@ export default {
         "playwright/**",
         "test/**",
         "res/decoder-ring/**",
+        "res/jitsi_external_api.min.js",
+        "docs/**",
+        // Used by jest
+        "__mocks__/maplibre-gl.js",
     ],
     project: ["**/*.{js,ts,jsx,tsx}"],
     ignore: [
-        "docs/**",
-        "res/jitsi_external_api.min.js",
-        // Used by jest
-        "__mocks__/maplibre-gl.js",
         // Keep for now
         "src/hooks/useLocalStorageState.ts",
         "src/components/views/elements/InfoTooltip.tsx",
@@ -37,13 +37,8 @@ export default {
         // False positive
         "sw.js",
         // Used by webpack
-        "buffer",
         "process",
         "util",
-        // Used by workflows
-        "ts-prune",
-        // Required due to bug in bloom-filters https://github.com/Callidon/bloom-filters/issues/75
-        "@types/seedrandom",
     ],
     ignoreBinaries: [
         // Used in scripts & workflows
