9826a8851d6dbcdc34f85fadb095e7bea00387c6
ElementRobot <releases@riot.im>
2025-01-09 09:09:33 +0000

    [create-pull-request] automated change (#28867)
    Co-authored-by: t3chguy <2403652+t3chguy@users.noreply.github.com>

--- playwright/testcontainers/synapse.ts
+++ playwright/testcontainers/synapse.ts
@@ -17,7 +17,7 @@ import { Credentials } from "../plugins/homeserver";
 import { deepCopy } from "../plugins/utils/object.ts";
 import { HomeserverContainer, StartedHomeserverContainer } from "./HomeserverContainer.ts";
 
-const TAG = "develop@sha256:17cc0a301447430624afb860276e5c13270ddeb99a3f6d1c6d519a20b1a8f650";
+const TAG = "develop@sha256:b69222d98abe9625d46f5d3cb01683d5dc173ae339215297138392cfeec935d9";
 
 const DEFAULT_CONFIG = {
     server_name: "localhost",
