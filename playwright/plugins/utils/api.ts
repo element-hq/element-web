cae93faec6c45a50c09223fd01d97f988b066148
Michael Telatynski <7t3chguy@gmail.com>
2025-01-13 17:50:28 +0000

    Iterate
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- playwright/plugins/utils/api.ts
+++ playwright/plugins/utils/api.ts
@@ -1,5 +1,5 @@
 /*
-Copyright 2024 New Vector Ltd.
+Copyright 2025 New Vector Ltd.
 
 SPDX-License-Identifier: AGPL-3.0-only OR GPL-3.0-only OR LicenseRef-Element-Commercial
 Please see LICENSE files in the repository root for full details.
@@ -7,7 +7,7 @@ Please see LICENSE files in the repository root for full details.
 
 import { APIRequestContext } from "@playwright/test";
 
-import { Credentials } from "../plugins/homeserver";
+import { Credentials } from "../homeserver";
 
 export type Verb = "GET" | "POST" | "PUT" | "DELETE";
 
--- playwright/testcontainers/synapse.ts
+++ playwright/testcontainers/synapse.ts
@@ -17,7 +17,7 @@ import { Credentials } from "../plugins/homeserver";
 import { deepCopy } from "../plugins/utils/object.ts";
 import { HomeserverContainer, StartedHomeserverContainer } from "./HomeserverContainer.ts";
 import { StartedMatrixAuthenticationServiceContainer } from "./mas.ts";
-import { Api, ClientServerApi, Verb } from "./utils.ts";
+import { Api, ClientServerApi, Verb } from "../plugins/utils/api.ts";
 
 const TAG = "develop@sha256:b69222d98abe9625d46f5d3cb01683d5dc173ae339215297138392cfeec935d9";
 
