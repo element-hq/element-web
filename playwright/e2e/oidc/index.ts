9df36659c459f9625e30225ea91514172b1e39f5
Michael Telatynski <7t3chguy@gmail.com>
2025-01-27 13:06:10 +0000

    delint
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- playwright/e2e/oidc/index.ts
+++ playwright/e2e/oidc/index.ts
@@ -6,7 +6,7 @@ SPDX-License-Identifier: AGPL-3.0-only OR GPL-3.0-only OR LicenseRef-Element-Com
 Please see LICENSE files in the repository root for full details.
 */
 
-import { MailpitClient, MailpitMessagesSummaryResponse } from "mailpit-api";
+import { MailpitClient } from "mailpit-api";
 import { Page } from "@playwright/test";
 
 import { expect } from "../../element-web-test";
