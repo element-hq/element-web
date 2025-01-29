e1c395024aca0dbd44e260ca4f2db3b70d881705
Valere <bill.carson@valrsoft.com>
2025-01-29 17:05:37 +0100

    review: comments on types and inline some const

--- src/components/viewmodels/rooms/UserIdentityWarningViewModel.tsx
+++ src/components/viewmodels/rooms/UserIdentityWarningViewModel.tsx
@@ -6,8 +6,8 @@ Please see LICENSE files in the repository root for full details.
 */
 
 import { useCallback, useEffect, useMemo, useState } from "react";
-import { EventType, MatrixEvent, Room, RoomStateEvent, RoomMember } from "matrix-js-sdk/src/matrix";
-import { CryptoEvent, CryptoApi } from "matrix-js-sdk/src/crypto-api";
+import { EventType, MatrixEvent, Room, RoomMember, RoomStateEvent } from "matrix-js-sdk/src/matrix";
+import { CryptoApi, CryptoEvent } from "matrix-js-sdk/src/crypto-api";
 import { throttle } from "lodash";
 import { logger } from "matrix-js-sdk/src/logger";
 
@@ -17,16 +17,34 @@ import { ButtonEvent } from "../../views/elements/AccessibleButton.tsx";
 
 export type ViolationType = "PinViolation" | "VerificationViolation";
 
+/**
+ * Represents a prompt to the user about a violation in the room.
+ * The type of violation and the member it relates to are included.
+ * If the type is "VerificationViolation", the warning is critical and should be reported with more urgency.
+ */
 export type ViolationPrompt = {
     member: RoomMember;
     type: ViolationType;
 };
 
+/**
+ * The state of the UserIdentityWarningViewModel.
+ * This includes the current prompt to show to the user and a callback to handle button clicks.
+ * If currentPrompt is undefined, there are no violations to show.
+ */
 export interface UserIdentityWarningState {
     currentPrompt?: ViolationPrompt;
     onButtonClick: (ev: ButtonEvent) => void;
 }
 
+/**
+ * Maps a list of room members to a list of violations.
+ * Checks for all members in the room to see if they have any violations.
+ * If no violations are found, an empty list is returned.
+ *
+ * @param cryptoApi
+ * @param members - The list of room members to check for violations.
+ */
 async function mapToViolations(cryptoApi: CryptoApi, members: RoomMember[]): Promise<ViolationPrompt[]> {
     const violationList = new Array<ViolationPrompt>();
     for (const member of members) {
@@ -85,51 +103,57 @@ export function useUserIdentityWarningViewModel(room: Room, key: string): UserId
     );
 
     // We need to listen for changes to the members list
-    const onRoomStateEvent = useCallback(
-        async (event: MatrixEvent): Promise<void> => {
-            if (!crypto || event.getRoomId() !== room.roomId) {
-                return;
-            }
-            let shouldRefresh = false;
+    useTypedEventEmitter(
+        cli,
+        RoomStateEvent.Events,
+        useCallback(
+            async (event: MatrixEvent): Promise<void> => {
+                if (!crypto || event.getRoomId() !== room.roomId) {
+                    return;
+                }
+                let shouldRefresh = false;
 
-            const eventType = event.getType();
+                const eventType = event.getType();
 
-            if (eventType === EventType.RoomEncryption && event.getStateKey() === "") {
-                // Room is now encrypted, so we can initialise the component.
-                shouldRefresh = true;
-            } else if (eventType == EventType.RoomMember) {
-                // We're processing an m.room.member event
-                // Something has changed in membership, someone joined or someone left or
-                // someone changed their display name. Anyhow let's refresh.
-                const userId = event.getStateKey();
-                shouldRefresh = !!userId;
-            }
+                if (eventType === EventType.RoomEncryption && event.getStateKey() === "") {
+                    // Room is now encrypted, so we can initialise the component.
+                    shouldRefresh = true;
+                } else if (eventType == EventType.RoomMember) {
+                    // We're processing an m.room.member event
+                    // Something has changed in membership, someone joined or someone left or
+                    // someone changed their display name. Anyhow let's refresh.
+                    const userId = event.getStateKey();
+                    shouldRefresh = !!userId;
+                }
 
-            if (shouldRefresh) {
-                loadViolations().catch((e) => {
-                    logger.error("Error refreshing UserIdentityWarningViewModel:", e);
-                });
-            }
-        },
-        [crypto, room, loadViolations],
+                if (shouldRefresh) {
+                    loadViolations().catch((e) => {
+                        logger.error("Error refreshing UserIdentityWarningViewModel:", e);
+                    });
+                }
+            },
+            [crypto, room, loadViolations],
+        ),
     );
-    useTypedEventEmitter(cli, RoomStateEvent.Events, onRoomStateEvent);
 
     // We need to listen for changes to the verification status of the members to refresh violations
-    const onUserVerificationStatusChanged = useCallback(
-        (userId: string): void => {
-            if (members.find((m) => m.userId == userId)) {
-                // This member is tracked, we need to refresh.
-                // refresh all for now?
-                // As a later optimisation we could store the current violations and only update the relevant one.
-                loadViolations().catch((e) => {
-                    logger.error("Error refreshing UserIdentityWarning:", e);
-                });
-            }
-        },
-        [loadViolations, members],
+    useTypedEventEmitter(
+        cli,
+        CryptoEvent.UserTrustStatusChanged,
+        useCallback(
+            (userId: string): void => {
+                if (members.find((m) => m.userId == userId)) {
+                    // This member is tracked, we need to refresh.
+                    // refresh all for now?
+                    // As a later optimisation we could store the current violations and only update the relevant one.
+                    loadViolations().catch((e) => {
+                        logger.error("Error refreshing UserIdentityWarning:", e);
+                    });
+                }
+            },
+            [loadViolations, members],
+        ),
     );
-    useTypedEventEmitter(cli, CryptoEvent.UserTrustStatusChanged, onUserVerificationStatusChanged);
 
     useEffect(() => {
         loadViolations().catch((e) => {
@@ -151,7 +175,7 @@ export function useUserIdentityWarningViewModel(room: Room, key: string): UserId
                 });
             } else if (currentPrompt.type === "PinViolation") {
                 crypto.pinCurrentUserIdentity(currentPrompt.member.userId).catch((e) => {
-                    logger.error("Error withdrawing verification requirement:", e);
+                    logger.error("Error pinning user identity:", e);
                 });
             }
         };
