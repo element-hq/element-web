af435b4f1a5bae2398403867c5e01dd6be22c0ca
Michael Telatynski <7t3chguy@gmail.com>
2025-01-30 15:14:30 +0000

    Delint

--- packages/element-web-module-api/src/loader.spec.ts
+++ packages/element-web-module-api/src/loader.spec.ts
@@ -5,7 +5,7 @@ SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
 Please see LICENSE files in the repository root for full details.
 */
 
-import { expect, test, describe, vi, beforeEach, afterEach } from "vitest";
+import { expect, test, describe, vi, beforeEach } from "vitest";
 
 import { Api, ModuleIncompatibleError, ModuleLoader } from ".";
 
