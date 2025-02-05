1b99071dfc6c6edfdbf2a8c565db7bd064c0ffec
David Baker <dbkr@users.noreply.github.com>
2025-02-05 16:07:36 +0100

    Fix test

--- test/unit-tests/components/views/settings/encryption/DeleteKeyStoragePanel-test.tsx
+++ test/unit-tests/components/views/settings/encryption/DeleteKeyStoragePanel-test.tsx
@@ -10,13 +10,19 @@ import { MatrixClient } from "matrix-js-sdk/src/matrix";
 import { render, screen, waitFor } from "jest-matrix-react";
 import userEvent from "@testing-library/user-event";
 import { mocked } from "jest-mock";
-import { isEnabled } from "@sentry/core";
 
 import { createTestClient, withClientContextRenderOptions } from "../../../../../test-utils";
 import { DeleteKeyStoragePanel } from "../../../../../../src/components/views/settings/encryption/DeleteKeyStoragePanel";
 import { useKeyStoragePanelViewModel } from "../../../../../../src/components/viewmodels/settings/encryption/KeyStoragePanelViewModel";
 
-jest.mock("../../../../../../src/components/viewmodels/settings/encryption/KeyStoragePanelViewModel");
+jest.mock("../../../../../../src/components/viewmodels/settings/encryption/KeyStoragePanelViewModel", () => ({
+    useKeyStoragePanelViewModel: jest.fn().mockReturnValue({
+        setEnabled: jest.fn(),
+        isEnabled: true,
+        loading: false,
+        busy: false,
+    }),
+}));
 
 describe("<DeleteKeyStoragePanel />", () => {
     let matrixClient: MatrixClient;
