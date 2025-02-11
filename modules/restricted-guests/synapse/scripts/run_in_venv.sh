3e107737aad827a56014d250b06e1b9732c62e82
Andrew Ferrazzutti <andrewf@element.io>
2025-02-11 14:00:07 -0500

    Remove "(C)" from copyright lines
    for consistency with other copyright lines, and readability

--- modules/restricted-guests/synapse/scripts/run_in_venv.sh
+++ modules/restricted-guests/synapse/scripts/run_in_venv.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-# Copyright (C) 2025 New Vector Ltd.
+# Copyright 2025 New Vector Ltd.
 #
 # SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
 # Please see LICENSE files in the repository root for full details.
