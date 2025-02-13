0608f823f03e24920847691edfbc062dbc80757f
David Baker <dbkr@users.noreply.github.com>
2025-02-13 18:35:19 +0000

    Unused import

--- playwright/e2e/sliding-sync/sliding-sync.spec.ts
+++ playwright/e2e/sliding-sync/sliding-sync.spec.ts
@@ -6,7 +6,7 @@ SPDX-License-Identifier: AGPL-3.0-only OR GPL-3.0-only OR LicenseRef-Element-Com
 Please see LICENSE files in the repository root for full details.
 */
 
-import { type Page, type Request } from "@playwright/test";
+import { type Page } from "@playwright/test";
 import { GenericContainer, type StartedTestContainer, Wait } from "testcontainers";
 
 import { test as base, expect } from "../../element-web-test";
