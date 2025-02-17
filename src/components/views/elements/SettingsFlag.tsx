deca7a4bf4a6d6d4ae6592ab22da747798a83f1c
Half-Shot <will@half-shot.uk>
2025-02-17 11:24:04 +0000

    Remove unused checkbox setting.

--- src/components/views/elements/SettingsFlag.tsx
+++ src/components/views/elements/SettingsFlag.tsx
@@ -13,7 +13,6 @@ import { secureRandomString } from "matrix-js-sdk/src/randomstring";
 import SettingsStore from "../../../settings/SettingsStore";
 import { _t } from "../../../languageHandler";
 import ToggleSwitch from "./ToggleSwitch";
-import StyledCheckbox from "./StyledCheckbox";
 import { type SettingLevel } from "../../../settings/SettingLevel";
 import { type BooleanSettingKey, defaultWatchManager } from "../../../settings/Settings";
 
@@ -24,8 +23,6 @@ interface IProps {
     roomId?: string; // for per-room settings
     label?: string;
     isExplicit?: boolean;
-    // XXX: once design replaces all toggles make this the default
-    useCheckbox?: boolean;
     hideIfCannotSet?: boolean;
     onChange?(checked: boolean): void;
 }
@@ -80,10 +77,6 @@ export default class SettingsFlag extends React.Component<IProps, IState> {
         this.props.onChange?.(checked);
     };
 
-    private checkBoxOnChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-        this.onChange(e.target.checked);
-    };
-
     private save = async (val?: boolean): Promise<void> => {
         await SettingsStore.setValue(
             this.props.name,
@@ -101,45 +94,36 @@ export default class SettingsFlag extends React.Component<IProps, IState> {
         const label = this.props.label ?? SettingsStore.getDisplayName(this.props.name, this.props.level);
         const description = SettingsStore.getDescription(this.props.name);
         const shouldWarn = SettingsStore.shouldHaveWarning(this.props.name);
-
-        if (this.props.useCheckbox) {
-            return (
-                <StyledCheckbox checked={this.state.value} onChange={this.checkBoxOnChange} disabled={disabled}>
-                    {label}
-                </StyledCheckbox>
-            );
-        } else {
-            return (
-                <div className="mx_SettingsFlag">
-                    <label className="mx_SettingsFlag_label" htmlFor={this.id}>
-                        <span className="mx_SettingsFlag_labelText">{label}</span>
-                        {description && (
-                            <div className="mx_SettingsFlag_microcopy">
-                                {shouldWarn
-                                    ? _t(
-                                          "settings|warning",
-                                          {},
-                                          {
-                                              w: (sub) => (
-                                                  <span className="mx_SettingsTab_microcopy_warning">{sub}</span>
-                                              ),
-                                              description,
-                                          },
-                                      )
-                                    : description}
-                            </div>
-                        )}
-                    </label>
-                    <ToggleSwitch
-                        id={this.id}
-                        checked={this.state.value}
-                        onChange={this.onChange}
-                        disabled={disabled}
-                        tooltip={disabled ? SettingsStore.disabledMessage(this.props.name) : undefined}
-                        title={label ?? undefined}
-                    />
-                </div>
-            );
-        }
+        return (
+            <div className="mx_SettingsFlag">
+                <label className="mx_SettingsFlag_label" htmlFor={this.id}>
+                    <span className="mx_SettingsFlag_labelText">{label}</span>
+                    {description && (
+                        <div className="mx_SettingsFlag_microcopy">
+                            {shouldWarn
+                                ? _t(
+                                        "settings|warning",
+                                        {},
+                                        {
+                                            w: (sub) => (
+                                                <span className="mx_SettingsTab_microcopy_warning">{sub}</span>
+                                            ),
+                                            description,
+                                        },
+                                    )
+                                : description}
+                        </div>
+                    )}
+                </label>
+                <ToggleSwitch
+                    id={this.id}
+                    checked={this.state.value}
+                    onChange={this.onChange}
+                    disabled={disabled}
+                    tooltip={disabled ? SettingsStore.disabledMessage(this.props.name) : undefined}
+                    title={label ?? undefined}
+                />
+            </div>
+        );
     }
 }
