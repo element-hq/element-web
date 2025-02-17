35700c6feca141e400a7e3cfc71b3c0d7c8998b0
Half-Shot <will@half-shot.uk>
2025-02-17 09:12:43 +0000

    Remove unused import.

--- src/components/views/settings/SetIdServer.tsx
+++ src/components/views/settings/SetIdServer.tsx
@@ -9,7 +9,7 @@ Please see LICENSE files in the repository root for full details.
 import React, { type ReactNode } from "react";
 import { logger } from "matrix-js-sdk/src/logger";
 import { type IThreepid } from "matrix-js-sdk/src/matrix";
-import { EditInPlace, ErrorMessage, TooltipProvider } from "@vector-im/compound-web";
+import { EditInPlace, ErrorMessage } from "@vector-im/compound-web";
 
 import { _t } from "../../../languageHandler";
 import { MatrixClientPeg } from "../../../MatrixClientPeg";
