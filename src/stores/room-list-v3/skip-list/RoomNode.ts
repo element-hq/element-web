98bc8e5f9606269113237d35c3b675e0fb2e4c22
R Midhun Suresh <hi@midhun.dev>
2025-02-24 15:34:43 +0530

    Implement a skip list for storing rooms
    This data structure stores rooms in a given sorted order and allows for
    very fast insertions and deletions.

---
+++ src/stores/room-list-v3/skip-list/RoomNode.ts
@@ -0,0 +1,29 @@
+/*
+Copyright 2025 New Vector Ltd.
+
+SPDX-License-Identifier: AGPL-3.0-only OR GPL-3.0-only OR LicenseRef-Element-Commercial
+Please see LICENSE files in the repository root for full details.
+*/
+
+import type { Room } from "matrix-js-sdk/src/matrix";
+
+/**
+ * Room skip list stores room nodes.
+ * These hold the actual room object and provides references to other nodes
+ * in different levels.
+ */
+export class RoomNode {
+    public constructor(public readonly room: Room) {}
+
+    /**
+     * This array holds references to the next node in a given level.
+     * eg: next[i] gives the next room node from this room node in level i.
+     */
+    public next: RoomNode[] = [];
+
+    /**
+     * This array holds references to the previous node in a given level.
+     * eg: previous[i] gives the next room node from this room node in level i.
+     */
+    public previous: RoomNode[] = [];
+}
