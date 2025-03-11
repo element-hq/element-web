72c2a3eb079dea6a176b7115078ea292bbcd9eb7
Half-Shot <will@half-shot.uk>
2025-03-11 11:32:26 +0000

    Add an action button to hide settings.

--- src/components/views/messages/HideActionButton.tsx
+++ src/components/views/messages/HideActionButton.tsx
@@ -7,65 +7,54 @@ Please see LICENSE files in the repository root for full details.
 */
 
 import { type MatrixEvent } from "matrix-js-sdk/src/matrix";
-import React from "react";
+import React, { useCallback } from "react";
 import classNames from "classnames";
 import { VisibilityOffIcon } from "@vector-im/compound-design-tokens/assets/web/icons";
-
-import { type MediaEventHelper } from "../../../utils/MediaEventHelper";
 import { RovingAccessibleButton } from "../../../accessibility/RovingTabIndex";
-import Spinner from "../elements/Spinner";
-import { _t, _td, type TranslationKey } from "../../../languageHandler";
+import { _t, _td } from "../../../languageHandler";
+import { SettingLevel } from "../../../settings/SettingLevel";
+import { useSettingsValueWithSetter } from "../../../hooks/useSettings";
 
 interface IProps {
     mxEvent: MatrixEvent;
-
-    // XXX: It can take a cycle or two for the MessageActionBar to have all the props/setup
-    // required to get us a MediaEventHelper, so we use a getter function instead to prod for
-    // one.
-    mediaEventHelperGet: () => MediaEventHelper | undefined;
 }
 
-interface IState {
-    loading: boolean;
-    blob?: Blob;
-    tooltip: TranslationKey;
-}
-
-export default class HideActionButton extends React.PureComponent<IProps, IState> {
-    public constructor(props: IProps) {
-        super(props);
-
-        this.state = {
-            loading: false,
-            tooltip: _td("timeline|download_action_downloading"),
-        };
-    }
-
-    private onClick = async (): Promise<void> => {};
-
-    public render(): React.ReactNode {
-        let spinner: JSX.Element | undefined;
-        if (this.state.loading) {
-            spinner = <Spinner w={18} h={18} />;
+const HideActionButton: React.FC<IProps> = ({mxEvent}) => {
+    const eventId = mxEvent.getId();
+    let spinner: JSX.Element | undefined;
+    const [events, setEventIds] = useSettingsValueWithSetter("showMediaEventIds", SettingLevel.DEVICE);
+    const onClick = useCallback(() => {
+        if (!eventId) {
+            return;
         }
-
-        const classes = classNames({
-            mx_MessageActionBar_iconButton: true,
-            mx_MessageActionBar_downloadButton: true,
-            mx_MessageActionBar_downloadSpinnerButton: !!spinner,
+        setEventIds({
+            ...events,
+            [eventId]: false,
         });
+    }, [eventId, events]);
 
-        return (
-            <RovingAccessibleButton
-                className={classes}
-                title={spinner ? _t(this.state.tooltip) : _t("action|download")}
-                onClick={this.onClick}
-                disabled={!!spinner}
-                placement="left"
-            >
-                <VisibilityOffIcon />
-                {spinner}
-            </RovingAccessibleButton>
-        );
+    const classes = classNames({
+        mx_MessageActionBar_iconButton: true,
+        mx_MessageActionBar_downloadButton: true,
+        mx_MessageActionBar_downloadSpinnerButton: !!spinner,
+    });
+
+    if (!eventId || events[eventId] === false) {
+        return;
     }
+
+    return (
+        <RovingAccessibleButton
+            className={classes}
+            title={_t("action|hide")}
+            onClick={onClick}
+            disabled={!!spinner}
+            placement="left"
+        >
+            <VisibilityOffIcon />
+            {spinner}
+        </RovingAccessibleButton>
+    );
 }
+
+export default HideActionButton;
