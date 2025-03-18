e1b75347122dd9f87ec610ec4a28519dd7633b4d
Michael Telatynski <7t3chguy@gmail.com>
2025-03-18 23:42:58 +0000

    Update react imports
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- src/components/views/dialogs/RegistrationEmailPromptDialog.tsx
+++ src/components/views/dialogs/RegistrationEmailPromptDialog.tsx
@@ -6,8 +6,7 @@ SPDX-License-Identifier: AGPL-3.0-only OR GPL-3.0-only OR LicenseRef-Element-Com
 Please see LICENSE files in the repository root for full details.
 */
 
-import * as React from "react";
-import { type SyntheticEvent, useRef, useState } from "react";
+import React, { type SyntheticEvent, useRef, useState } from "react";
 
 import { _t, _td } from "../../../languageHandler";
 import type Field from "../elements/Field";
