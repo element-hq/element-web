a3d104270882f78e9aa48d99c05d3c2125103918
Michael Telatynski <7t3chguy@gmail.com>
2025-03-21 08:45:30 +0000

    Reuse PushProcessor from MatrixClient
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- src/LegacyCallHandler.tsx
+++ src/LegacyCallHandler.tsx
@@ -596,7 +596,7 @@ export default class LegacyCallHandler extends TypedEventEmitter<LegacyCallHandl
 
         switch (newState) {
             case CallState.Ringing: {
-                const incomingCallPushRule = new PushProcessor(MatrixClientPeg.safeGet()).getPushRuleById(
+                const incomingCallPushRule = MatrixClientPeg.safeGet().pushProcessor.getPushRuleById(
                     RuleId.IncomingCall,
                 );
                 const pushRuleEnabled = incomingCallPushRule?.enabled;
