1602cf41f966f8be26b29db59e611969baa9fcfc
Florian Duros <florian.duros@ormaz.fr>
2025-04-03 17:16:04 +0200

    fix(room list item menu): fix color of check icon

--- src/components/views/rooms/RoomListPanel/RoomListItemMenuView.tsx
+++ src/components/views/rooms/RoomListPanel/RoomListItemMenuView.tsx
@@ -173,6 +173,8 @@ interface NotificationMenuProps {
 function NotificationMenu({ vm, setMenuOpen }: NotificationMenuProps): JSX.Element {
     const [open, setOpen] = useState(false);
 
+    const checkComponent = <CheckIcon width="24px" height="24px" color="var(--cpd-color-icon-primary)" />;
+
     return (
         <Menu
             open={open}
@@ -192,7 +194,7 @@ function NotificationMenu({ vm, setMenuOpen }: NotificationMenuProps): JSX.Eleme
                 onSelect={() => vm.setRoomNotifState(RoomNotifState.AllMessages)}
                 onClick={(evt) => evt.stopPropagation()}
             >
-                {vm.isNotificationAllMessage && <CheckIcon width="24px" height="24px" />}
+                {vm.isNotificationAllMessage && checkComponent}
             </MenuItem>
             <MenuItem
                 aria-selected={vm.isNotificationAllMessageLoud}
@@ -201,7 +203,7 @@ function NotificationMenu({ vm, setMenuOpen }: NotificationMenuProps): JSX.Eleme
                 onSelect={() => vm.setRoomNotifState(RoomNotifState.AllMessagesLoud)}
                 onClick={(evt) => evt.stopPropagation()}
             >
-                {vm.isNotificationAllMessageLoud && <CheckIcon width="24px" height="24px" />}
+                {vm.isNotificationAllMessageLoud && checkComponent}
             </MenuItem>
             <MenuItem
                 aria-selected={vm.isNotificationMentionOnly}
@@ -210,7 +212,7 @@ function NotificationMenu({ vm, setMenuOpen }: NotificationMenuProps): JSX.Eleme
                 onSelect={() => vm.setRoomNotifState(RoomNotifState.MentionsOnly)}
                 onClick={(evt) => evt.stopPropagation()}
             >
-                {vm.isNotificationMentionOnly && <CheckIcon width="24px" height="24px" />}
+                {vm.isNotificationMentionOnly && checkComponent}
             </MenuItem>
             <MenuItem
                 aria-selected={vm.isNotificationMute}
@@ -219,7 +221,7 @@ function NotificationMenu({ vm, setMenuOpen }: NotificationMenuProps): JSX.Eleme
                 onSelect={() => vm.setRoomNotifState(RoomNotifState.Mute)}
                 onClick={(evt) => evt.stopPropagation()}
             >
-                {vm.isNotificationMute && <CheckIcon width="24px" height="24px" />}
+                {vm.isNotificationMute && checkComponent}
             </MenuItem>
         </Menu>
     );
