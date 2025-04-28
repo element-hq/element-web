f7f04242966e1c5a075cb4cfecd84cf110a48b8d
Michael Telatynski <7t3chguy@gmail.com>
2025-04-28 12:43:17 +0100

    Iterate

--- modules/opendesk/element-web/tests/opendesk.spec.ts
+++ modules/opendesk/element-web/tests/opendesk.spec.ts
@@ -5,7 +5,7 @@ SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
 Please see LICENSE files in the repository root for full details.
 */
 
-import { type SynapseContainer } from "@element-hq/element-web-playwright-common/lib/testcontainers";
+import { type SynapseContainer } from "@element-hq/element-web-playwright-common/lib/testcontainers/index.js";
 
 import { test, expect } from "../../../../playwright/element-web-test.ts";
 
