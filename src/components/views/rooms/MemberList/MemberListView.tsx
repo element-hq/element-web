c764fc8f2e053441ee9e5bc444a34e2f8371b5a9
langleyd <langley.dave@gmail.com>
2025-05-02 10:16:19 +0100

    implement basic scrolling and keyboard navigation

--- src/components/views/rooms/MemberList/MemberListView.tsx
+++ src/components/views/rooms/MemberList/MemberListView.tsx
@@ -6,9 +6,7 @@ Please see LICENSE files in the repository root for full details.
 */
 
 import { Form } from "@vector-im/compound-web";
-import React, { type JSX } from "react";
-import { List, type ListRowProps } from "react-virtualized/dist/commonjs/List";
-import { AutoSizer } from "react-virtualized";
+import React, { useRef, type JSX } from "react";
 
 import { Flex } from "../../../utils/Flex";
 import {
@@ -21,7 +19,7 @@ import { ThreePidInviteTileView } from "./tiles/ThreePidInviteTileView";
 import { MemberListHeaderView } from "./MemberListHeaderView";
 import BaseCard from "../../right_panel/BaseCard";
 import { _t } from "../../../../languageHandler";
-import { RovingTabIndexProvider } from "../../../../accessibility/RovingTabIndex";
+import { Virtuoso, VirtuosoHandle } from "react-virtuoso";
 
 interface IProps {
     roomId: string;
@@ -63,21 +61,48 @@ const MemberListView: React.FC<IProps> = (props: IProps) => {
             return 56;
         }
     };
+    const ref = useRef<VirtuosoHandle | null>(null);
+    const [currentItemIndex, setCurrentItemIndex] = React.useState(-1)
+    const listRef = useRef<HTMLButtonElement | null>(null);
 
-    const rowRenderer = ({ key, index, style }: ListRowProps): JSX.Element => {
-        if (index === totalRows) {
-            // We've rendered all the members,
-            // now we render an empty div to add some space to the end of the list.
-            return <div key={key} style={style} />;
-        }
-        const item = vm.members[index];
-        return (
-            <div key={key} style={style}>
-                {getRowComponent(item)}
-            </div>
-        );
-    };
+    const keyDownCallback = React.useCallback(
+        (e: any) => {
+            let nextIndex = null
+            console.log("keydown")
+            console.log(e.code)
+            if (e.code === 'ArrowUp') {
+                nextIndex = Math.max(0, currentItemIndex - 1)
+            } else if (e.code === 'ArrowDown') {
+                nextIndex = Math.min(totalRows, currentItemIndex + 1)
+            } else if (e.code === 'enter') {
+                nextIndex = Math.min(totalRows, currentItemIndex + 1)
+            }
 
+            if (nextIndex !== null) {
+                ref?.current?.scrollIntoView({
+                    index: nextIndex,
+                    behavior: 'auto',
+                    done: () => {
+                        setCurrentItemIndex(nextIndex)
+                    },
+                })
+                e.preventDefault()
+            }
+        },
+        [currentItemIndex, ref, setCurrentItemIndex]
+    )
+
+    const scrollerRef = React.useCallback(
+        (element: any) => {
+            if (element) {
+                element.addEventListener('keydown', keyDownCallback)
+                listRef.current = element
+            } else {
+                listRef?.current?.removeEventListener('keydown', keyDownCallback)
+            }
+        },
+        [keyDownCallback]
+    )
     return (
         <BaseCard
             id="memberlist-panel"
@@ -87,34 +112,35 @@ const MemberListView: React.FC<IProps> = (props: IProps) => {
             header={_t("common|people")}
             onClose={props.onClose}
         >
-            <RovingTabIndexProvider handleUpDown scrollIntoView>
-                {({ onKeyDownHandler }) => (
-                    <Flex
-                        align="stretch"
-                        direction="column"
-                        className="mx_MemberListView_container"
-                        onKeyDown={onKeyDownHandler}
-                    >
-                        <Form.Root>
-                            <MemberListHeaderView vm={vm} />
-                        </Form.Root>
-                        <AutoSizer>
-                            {({ height, width }) => (
-                                <List
-                                    rowRenderer={rowRenderer}
-                                    rowHeight={getRowHeight}
-                                    // The +1 refers to the additional empty div that we render at the end of the list.
-                                    rowCount={totalRows + 1}
-                                    // Subtract the height of MemberlistHeaderView so that the parent div does not overflow.
-                                    height={height - 113}
-                                    width={width}
-                                    overscanRowCount={15}
-                                />
-                            )}
-                        </AutoSizer>
-                    </Flex>
-                )}
-            </RovingTabIndexProvider>
+            <Flex
+                align="stretch"
+                direction="column"
+                className="mx_MemberListView_container"
+            >
+                <Form.Root>
+                    <MemberListHeaderView vm={vm} />
+                </Form.Root>
+
+                <Virtuoso
+                    ref={ref}
+                    style={{ height: '100%' }}
+                    scrollerRef={scrollerRef}
+                    context={{ currentItemIndex }}
+                    data={vm.members}
+                    itemContent={(index, member) => (
+                        <div
+                            style={{
+                                borderColor: index === currentItemIndex ? 'var(--border)' : 'transparent',
+                                borderWidth: '1px',
+                                borderStyle: 'solid',
+                            }}
+                        >
+                            {getRowComponent(member)}
+                        </div>
+
+                    )}
+                />
+            </Flex>
         </BaseCard>
     );
 };
