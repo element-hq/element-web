bb4a8b170e4d285e65c1b181a851cf1d961fa9ef
Michael Telatynski <7t3chguy@gmail.com>
2025-05-20 13:43:25 +0100

    Extract svg

--- modules/banner/element-web/vite.config.ts
+++ modules/banner/element-web/vite.config.ts
@@ -11,6 +11,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 import externalGlobals from "rollup-plugin-external-globals";
+import svgr from "vite-plugin-svgr";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -31,6 +32,7 @@ export default defineConfig({
     },
     plugins: [
         react(),
+        svgr(),
         nodePolyfills({
             include: ["events"],
         }),
