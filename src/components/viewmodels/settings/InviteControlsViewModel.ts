caa29f3af180763c73f27eb3e281cc87bcf94313
Half-Shot <will@half-shot.uk>
2025-06-02 15:57:29 +0100

    mvvm

---
+++ src/components/viewmodels/settings/InviteControlsViewModel.ts
@@ -0,0 +1,75 @@
+import { AccountDataEvents } from "matrix-js-sdk/src/types";
+import { useState, useMemo, useCallback } from "react";
+import { useMatrixClientContext } from "../../../contexts/MatrixClientContext";
+import { useAccountData } from "../../../hooks/useAccountData";
+import { useAsyncMemo } from "../../../hooks/useAsyncMemo";
+import { logger } from "matrix-js-sdk/src/logger";
+
+interface InviteControlsViewModel {
+    isSupported: boolean | undefined;
+    isEnforcedServerside: boolean | undefined;
+    globalInvitesEnabled: boolean;
+    busy: boolean;
+    error: boolean;
+    toggleGlobalInvites: () => void;
+}
+
+export function useInviteControlsViewModel(): InviteControlsViewModel {
+    const client = useMatrixClientContext();
+    const [hasError, setHasError] = useState(false);
+    const [busy, setBusy] = useState(false);
+    const inviteConfig = useAccountData<AccountDataEvents["org.matrix.msc4155.invite_permission_config"]>(
+        client,
+        "org.matrix.msc4155.invite_permission_config",
+    );
+
+    // Is supported by the server.
+    const isSupported = useAsyncMemo(async () => {
+        return await client.doesServerSupportUnstableFeature("org.matrix.msc4155");
+    }, [client]);
+
+    // Is enforced by the server.
+    const isEnforcedServerside = useAsyncMemo(async () => {
+        const caps = await client.getCapabilities();
+        return caps["org.matrix.msc4155.invite_permission_config_enforced"]?.enabled;
+    }, [client]);
+
+    // This implements a very basic version of MSC4155 that simply allows
+    // or disallows all invites by setting a simple glob.
+    // Keep in mind that users may configure more powerful rules on other
+    // clients and we should keep those intact.
+    const globalInvitesEnabled = useMemo(() => {
+        if (!inviteConfig) {
+            return false;
+        }
+        return inviteConfig["blocked_users"]?.includes("*") !== true;
+    }, [inviteConfig]);
+
+    const toggleGlobalInvites = useCallback(async () => {
+        setHasError(false);
+        setBusy(true);
+        const newConfig = { ...inviteConfig };
+        if (newConfig.blocked_users?.includes("*")) {
+            newConfig.blocked_users = newConfig.blocked_users.filter((u) => u !== "*");
+        } else {
+            newConfig.blocked_users = [...new Set([...(newConfig.blocked_users ?? []), "*"])];
+        }
+        try {
+            await client.setAccountData("org.matrix.msc4155.invite_permission_config", newConfig);
+        } catch (ex) {
+            logger.error("Could not change input config", ex);
+            setHasError(true);
+        } finally {
+            setBusy(false);
+        }
+    }, [client, inviteConfig]);
+
+    return {
+        globalInvitesEnabled,
+        isSupported,
+        isEnforcedServerside,
+        busy,
+        error: hasError,
+        toggleGlobalInvites,
+    };
+}
--- src/components/views/settings/InviteControlsPanel.tsx
+++ src/components/views/settings/InviteControlsPanel.tsx
@@ -5,68 +5,27 @@ SPDX-License-Identifier: AGPL-3.0-only OR GPL-3.0-only OR LicenseRef-Element-Com
 Please see LICENSE files in the repository root for full details.
 */
 
-import React, { type ChangeEventHandler, type FC, useCallback, useEffect, useMemo, useState } from "react";
-import { type AccountDataEvents } from "matrix-js-sdk/src/types";
+import React, { type ChangeEventHandler, type FC, useCallback } from "react";
 import { ErrorMessage, InlineField, Label, Root, ToggleInput, Tooltip } from "@vector-im/compound-web";
-import { logger } from "matrix-js-sdk/src/logger";
 
 import { SettingsSubsection } from "./shared/SettingsSubsection";
 import { _t } from "../../../languageHandler";
-import { useAccountData } from "../../../hooks/useAccountData";
-import { useMatrixClientContext } from "../../../contexts/MatrixClientContext";
+import { useInviteControlsViewModel } from "../../viewmodels/settings/InviteControlsViewModel";
 
 export const InviteControlsPanel: FC = () => {
-    const client = useMatrixClientContext();
-    const [hasError, setHasError] = useState(false);
-    const [busy, setBusy] = useState(false);
-    const [canUse, setCanUse] = useState<boolean>();
-    const inviteConfig = useAccountData<AccountDataEvents["org.matrix.msc4155.invite_permission_config"]>(
-        client,
-        "org.matrix.msc4155.invite_permission_config",
-    );
-
-    useEffect(() => {
-        (async () => {
-            setCanUse(await client.doesServerSupportUnstableFeature("org.matrix.msc4155"));
-        })();
-    }, [client]);
-
-    // This implements a very basic version of MSC4155 that simply allows
-    // or disallows all invites by setting a simple glob.
-    // Keep in mind that users may configure more powerful rules on other
-    // clients and we should keep those intact.
-    const isBlockingAll = useMemo(() => {
-        if (!inviteConfig) {
-            return false;
-        }
-        return inviteConfig["blocked_users"]?.includes("*") === true;
-    }, [inviteConfig]);
+    const { busy, globalInvitesEnabled, toggleGlobalInvites, isEnforcedServerside, isSupported, error } =
+        useInviteControlsViewModel();
 
     const setValue = useCallback<ChangeEventHandler<HTMLInputElement>>(
-        async (e) => {
+        (e) => {
             e.preventDefault();
-            setHasError(false);
-            setBusy(true);
-            const newConfig = { ...inviteConfig };
-            if (newConfig["blocked_users"]?.includes("*")) {
-                newConfig.blocked_users = newConfig.blocked_users.filter((u) => u !== "*");
-            } else {
-                newConfig.blocked_users = [...new Set([...(newConfig.blocked_users ?? []), "*"])];
-            }
-            try {
-                await client.setAccountData("org.matrix.msc4155.invite_permission_config", newConfig);
-            } catch (ex) {
-                logger.error("Could not change input config", ex);
-                setHasError(true);
-            } finally {
-                setBusy(false);
-            }
+            toggleGlobalInvites();
         },
-        [client, inviteConfig],
+        [toggleGlobalInvites],
     );
 
     let content;
-    if (canUse) {
+    if (isSupported) {
         content = (
             <>
                 <InlineField
@@ -74,23 +33,25 @@ export const InviteControlsPanel: FC = () => {
                     control={
                         <ToggleInput
                             id="mx_invite_controls_default"
-                            disabled={busy || !canUse}
+                            disabled={busy}
                             onChange={setValue}
-                            checked={!isBlockingAll}
+                            checked={globalInvitesEnabled}
                         />
                     }
                 >
                     <Label htmlFor="mx_invite_controls_default">{_t("settings|invite_controls|default_label")}</Label>
-                    {hasError && <ErrorMessage>{_t("settings|invite_controls|error_message")}</ErrorMessage>}
+                    {error && <ErrorMessage>{_t("settings|invite_controls|error_message")}</ErrorMessage>}
                 </InlineField>
             </>
         );
-    } else if (canUse === false) {
+    } else if (isSupported === false || isEnforcedServerside === false) {
         content = (
             <Tooltip description={_t("settings|invite_controls|not_supported")}>
                 <InlineField
                     name="default"
-                    control={<ToggleInput id="mx_invite_controls_default" disabled={true} checked={!isBlockingAll} />}
+                    control={
+                        <ToggleInput id="mx_invite_controls_default" disabled={true} checked={globalInvitesEnabled} />
+                    }
                 >
                     <Label htmlFor="mx_invite_controls_default">{_t("settings|invite_controls|default_label")}</Label>
                 </InlineField>
