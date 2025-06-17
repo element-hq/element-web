e2a89888c29029d9c6d2b34aab553d7acafa35b7
Half-Shot <will@half-shot.uk>
2025-06-17 14:02:41 +0100

    Mark MatrixEvent as @alpha and document.

--- packages/element-web-module-api/src/models/event.ts
+++ packages/element-web-module-api/src/models/event.ts
@@ -6,18 +6,43 @@ Please see LICENSE files in the repository root for full details.
 */
 
 /**
- * Representation of a Matrix event.
- * @beta
+ * Representation of a Matrix event, as specified by the client server specification.
+ * @alpha Subject to change.
+ * @see https://spec.matrix.org/v1.14/client-server-api/#room-event-format
  */
 export interface MatrixEvent {
+    /**
+     * The event ID of this event.
+     */
     eventId: string;
-    roomId?: string;
+    /**
+     * The room ID which contains this event.
+     */
+    roomId: string;
+    /**
+     * The Matrix ID of the user who sent this event.
+     */
     sender: string;
+    /**
+     * The content of the event.
+     * If the event was encrypted, this is the decrypted content.
+     */
     content: Record<string, unknown>;
+    /**
+     * Contains optional extra information about the event.
+     */
     unsigned: Record<string, unknown>;
+    /**
+     * The type of the event.
+     */
     type: string;
+    /**
+     * The state key of the event.
+     * If this key is set, including `""` then the event is a state event.
+     */
     stateKey?: string;
+    /**
+     * Timestamp (in milliseconds since the unix epoch) on originating homeserver when this event was sent.
+     */
     originServerTs: number;
-    redacts?: string;
-    age?: number;
 }
