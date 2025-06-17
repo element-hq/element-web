294857209d544b3ab3842cce4b828e502970c5c8
Half-Shot <will@half-shot.uk>
2025-06-17 09:22:13 +0100

    lint

--- playwright/e2e/modules/custom-component.spec.ts
+++ playwright/e2e/modules/custom-component.spec.ts
@@ -6,6 +6,7 @@ Please see LICENSE files in the repository root for full details.
 */
 
 import { type Page } from "@playwright/test";
+
 import { test, expect } from "../../element-web-test";
 
 const screenshotOptions = (page: Page) => ({
