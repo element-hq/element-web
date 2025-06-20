f36a69f1d7e6bfb0b56bde61e1b043f389d225f0
Florian Duros <florian.duros@ormaz.fr>
2025-06-20 14:20:26 +0200

    chore: setup storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,16 @@
+import type { Preview } from "@storybook/react-webpack5";
+
+import "../res/css/shared.pcss"; // Import global styles
+
+const preview: Preview = {
+    parameters: {
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
+        },
+    },
+};
+
+export default preview;
