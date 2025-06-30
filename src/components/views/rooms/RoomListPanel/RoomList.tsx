a5e2667bf7ea727ea72944ff36c237e7ba4d6440
David Langley <davidl@element.io>
2025-06-30 20:28:28 +0100

    Use new virtuoso api

--- src/components/views/rooms/RoomListPanel/RoomList.tsx
+++ src/components/views/rooms/RoomListPanel/RoomList.tsx
@@ -12,13 +12,6 @@ import { type Room } from "matrix-js-sdk/src/matrix";
 import { type RoomListViewState } from "../../../viewmodels/roomlist/RoomListViewModel";
 import { _t } from "../../../../languageHandler";
 import { RoomListItemView } from "./RoomListItemView";
-import { RovingTabIndexProvider } from "../../../../accessibility/RovingTabIndex";
-import { getKeyBindingsManager } from "../../../../KeyBindingsManager";
-import { KeyBindingAction } from "../../../../accessibility/KeyboardShortcuts";
-import { Landmark, LandmarkNavigation } from "../../../../accessibility/LandmarkNavigation";
-import { useEventEmitterState } from "../../../../hooks/useEventEmitter";
-import { type SpaceKey, UPDATE_SELECTED_SPACE } from "../../../../stores/spaces";
-import SpaceStore from "../../../../stores/spaces/SpaceStore";
 
 interface RoomListProps {
     /**
@@ -27,10 +20,17 @@ interface RoomListProps {
     vm: RoomListViewState;
 }
 
+
+type RoomListState = {
+    currentRoomIndex: number | undefined;
+    rooms: Room[];
+    spaceId: string;
+};
+
 /**
  * A virtualized list of rooms.
  */
-export function RoomList({ vm: { rooms, activeIndex: currentRoomIndex } }: RoomListProps): JSX.Element {
+export function RoomList({ vm: { rooms, activeIndex: currentRoomIndex, spaceId } }: RoomListProps): JSX.Element {
     // To follow the grid pattern (https://www.w3.org/WAI/ARIA/apg/patterns/grid/), we need to set the first element of the list as a row.
     // The virtuoso component is set to role="grid" and the items are set to role="gridcell".
     // TODO REPLACE WITH A CUSToM LIST COMPONENT
@@ -41,74 +41,46 @@ export function RoomList({ vm: { rooms, activeIndex: currentRoomIndex } }: RoomL
     }, []);
 
     const virtuosoRef = useRef<VirtuosoHandle | null>(null);
-    // UNCOMMENT IF YOU WANT SCROLLING TO THE CURRENT ROOM INDEX
-    // USE THIS OR THE KEY ON VIRTUOSO, NOT BOTH
-    // useEffect(() => {
-    //     console.log("virtuoso useEffect: currentRoomIndex changed to", currentRoomIndex, "rooms", rooms.length);
-    //     if (!virtuosoRef.current) return;
-    //
-    //     console.log("virtuoso scrolling to current room index:", currentRoomIndex);
-    //
-    //     virtuosoRef.current?.scrollIntoView({
-    //         index: currentRoomIndex || 0,
-    //         behavior: "auto",
-    //     });
-    //
-    //     // DATA AND INDEX CHANGES AT THE SAME TIME, WE NEED TO WAIT TO SET THE DATA FIRST
-    //     // setTimeout(() => {
-    //     //     virtuosoRef.current?.scrollIntoView({
-    //     //         index: currentRoomIndex || 0,
-    //     //         behavior: "auto",
-    //     //     });
-    //     // }, 0);
-    // }, [rooms, currentRoomIndex]);
+    const [roomListState, setRoomListState] = useState<RoomListState>({ currentRoomIndex, rooms, spaceId});
 
-    // Mound and unmount the virtuoso component when the space changes.
-    // We listen the rooms array to detect changes in the space because we want the rooms to be updated when the space changes.
-    // If only the room changes, the space key will not change, so we can keep the same virtuoso component.
-    // MOUNT AND UNMOUNT CREATE A VISUAL GLITCH
-    const currentSpaceRef = useRef(SpaceStore.instance.activeSpace);
-    const [currentSpaceKey, setCurrentSpaceKey] = useState(SpaceStore.instance.activeSpace);
     useEffect(() => {
-        const newSpace = SpaceStore.instance.activeSpace;
-        if (currentSpaceRef.current !== newSpace) {
-            setCurrentSpaceKey(newSpace);
-            currentSpaceRef.current = newSpace;
+        // TODO: this logic is not correct, but was introduced so that we change the spaceId and the rooms at the same time.
+        // Ideally the props should be updated in a valid way(we don't have a mismatched spaceId/rooms being passed in).
+        if(roomListState.spaceId !== spaceId && roomListState.rooms.length !== rooms.length) {
+            virtuosoRef.current?.scrollIntoView({
+                align: `start`,
+                index: currentRoomIndex || 0,
+                behavior: "auto",
+                targetsNextRefresh: true,
+            });
+            setRoomListState({ ...roomListState, rooms, spaceId, currentRoomIndex});
         }
-    }, [rooms]);
-
-    console.log("virtuoso key:", currentSpaceKey, "currentRoomIndex:", currentRoomIndex, "rooms:", rooms.length);
-
-    useEffect(() => {
-        console.log("mounted virtuoso with spaceKey:", currentSpaceKey);
-        return () => {
-            console.log("unmounted virtuoso with spaceKey:", currentSpaceKey);
-        };
-    }, [currentSpaceKey]);
-
+    
+    }, [spaceId, rooms, currentRoomIndex, roomListState]);
+   
     return (
         //   <RovingTabIndexProvider handleHomeEnd={true} handleUpDown={true}>
         //  {({ onKeyDownHandler }) => (
         <Virtuoso
-            key={currentSpaceKey}
+            // key={spaceId}
             data-testid="room-list"
             role="grid"
             aria-label={_t("room_list|list_title")}
             aria-rowcount={1}
             aria-colcount={1}
-            style={{ flex: 1 }}
-            totalCount={rooms.length}
+            totalCount={roomListState.rooms.length}
+            data={roomListState.rooms}
             fixedItemHeight={48}
-            context={{ currentRoomIndex }}
-            itemContent={(index, _room, { currentRoomIndex }) => (
+            context={{ currentRoomIndex: roomListState.currentRoomIndex }}
+            itemContent={(index, room, { currentRoomIndex }) => (
                 <RoomListItemView
-                    room={rooms[index]}
+                    room={room}
                     isSelected={currentRoomIndex === index}
                     aria-colindex={index}
                     role="gridcell"
                 />
             )}
-            computeItemKey={(index) => rooms[index].roomId}
+            computeItemKey={(index, item ) => item.roomId}
             /* 240px = 5 rows */
             increaseViewportBy={240}
             initialTopMostItemIndex={currentRoomIndex ?? 0}
