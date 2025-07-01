a9364332b3bbc6324589904a472492f051ad1f3b
David Baker <dbkr@users.noreply.github.com>
2025-07-01 14:58:15 +0100

    Change import

--- src/shared-components/event-tiles/TextualEvent/TextualEvent.test.tsx
+++ src/shared-components/event-tiles/TextualEvent/TextualEvent.test.tsx
@@ -5,7 +5,7 @@ SPDX-License-Identifier: AGPL-3.0-only OR GPL-3.0-only OR LicenseRef-Element-Com
 Please see LICENSE files in the repository root for full details.
 */
 
-import { composeStories } from "@storybook/react";
+import { composeStories } from "@storybook/react-webpack5";
 import { render } from "jest-matrix-react";
 import React from "react";
 
