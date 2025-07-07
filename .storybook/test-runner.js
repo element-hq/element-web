7e4ff895977cc4f1c89e6b64559b1f9cdba4826e
David Baker <dbkr@users.noreply.github.com>
2025-07-07 16:23:38 +0100

    Hopefully do screenshot testing...

---
+++ .storybook/test-runner.js
@@ -0,0 +1,28 @@
+import { waitForPageReady } from '@storybook/test-runner';
+import { toMatchImageSnapshot } from 'jest-image-snapshot';
+
+const customSnapshotsDir = `${process.cwd()}/playwright/shared-component-snapshots`;
+const customReceivedDir = `${process.cwd()}/playwright/shared-component-received`;
+
+/**
+ * @type {import('@storybook/test-runner').TestRunnerConfig}
+ */
+const config = {
+  setup(page) {
+    expect.extend({ toMatchImageSnapshot });
+  },
+  async postVisit(page, context) {
+    await waitForPageReady(page);
+
+    // If you want to take screenshot of multiple browsers, use
+    // page.context().browser().browserType().name() to get the browser name to prefix the file name
+    const image = await page.screenshot();
+    expect(image).toMatchImageSnapshot({
+      customSnapshotsDir,
+      customSnapshotIdentifier: context.id,
+      customReceivedDir,
+    });
+  },
+};
+
+export default config;
