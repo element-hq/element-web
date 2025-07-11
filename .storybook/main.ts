292e8b72e2f9a78c4e3ab59d0145a3318a9fba0c
Florian Duros <florian.duros@ormaz.fr>
2025-07-11 11:55:11 +0200

    feat: add language selector to storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,9 +6,17 @@ Please see LICENSE files in the repository root for full details.
 */
 
 import type { StorybookConfig } from "@storybook/react-vite";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+import { mergeConfig } from "vite";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const config: StorybookConfig = {
     stories: ["../src/shared-components/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+    staticDirs: ["../webapp"],
     addons: ["@storybook/addon-docs", "@storybook/addon-designs"],
     framework: "@storybook/react-vite",
     core: {
@@ -16,6 +24,18 @@ const config: StorybookConfig = {
     },
     typescript: {
         reactDocgen: "react-docgen-typescript",
+    },
+    async viteFinal(config) {
+        return mergeConfig(config, {
+            resolve: {
+                alias: {
+                    // Alias used by i18n.tsx
+                    $webapp: path.resolve(__dirname, "../webapp"),
+                },
+            },
+            // Needed for counterpart to work
+            plugins: [nodePolyfills({ include: ["process", "util"] })],
+        });
     },
 };
 export default config;
