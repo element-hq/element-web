3bda3c80eeb2db6cfa29250b2a3dd06bdbd012c0
Florian Duros <florian.duros@ormaz.fr>
2025-07-16 16:47:22 +0200

    fix: make visual test works in CI

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,12 +7,8 @@ Please see LICENSE files in the repository root for full details.
 
 import type { StorybookConfig } from "@storybook/react-vite";
 import path from "node:path";
-import { fileURLToPath } from "node:url";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 import { mergeConfig } from "vite";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const config: StorybookConfig = {
     stories: ["../src/shared-components/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -30,7 +26,7 @@ const config: StorybookConfig = {
             resolve: {
                 alias: {
                     // Alias used by i18n.tsx
-                    $webapp: path.resolve(__dirname, "../webapp"),
+                    $webapp: path.resolve("webapp"),
                 },
             },
             // Needed for counterpart to work
