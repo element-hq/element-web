59531ea5125b1ae1af0941b78b8dfb545bb23184
David Langley <langley.dave@gmail.com>
2025-08-11 10:35:04 +0100

    Show a blue lock for unencrypted rooms and hide the grey shield for encrypted rooms (#30440)
    * Show a blue lock instead of a grey shield for unencrypted rooms
    
    * Update screenshots and snapshot
    
    * Update snapshots and fix e2e test that used to expect the grey shield
    
    * lint and add tests for shield
    
    * Update more screen shots
    
    * finish unit test for left icon
    
    * Remove unneeded check
    
    * Don't bother adding stray props to E2EIcon for data-testid
    
    * Upate snapshots

--- src/components/views/rooms/E2EIcon.tsx
+++ src/components/views/rooms/E2EIcon.tsx
@@ -74,18 +74,18 @@ const E2EIcon: React.FC<Props> = ({
 
     let content: JSX.Element;
     if (onClick) {
-        content = <AccessibleButton onClick={onClick} className={classes} style={style} />;
+        content = <AccessibleButton onClick={onClick} className={classes} style={style} data-testid="e2e-icon" />;
     } else {
         // Verified and warning icon have a transparent cutout, so add a white background.
         // The normal icon already has the correct shape and size, so reuse that.
         if (status === E2EStatus.Verified || status === E2EStatus.Warning) {
             content = (
-                <div className={classes} style={style}>
+                <div className={classes} style={style} data-testid="e2e-icon">
                     <div className="mx_E2EIcon_normal" />
                 </div>
             );
         } else {
-            content = <div className={classes} style={style} />;
+            content = <div className={classes} style={style} data-testid="e2e-icon" />;
         }
     }
 
