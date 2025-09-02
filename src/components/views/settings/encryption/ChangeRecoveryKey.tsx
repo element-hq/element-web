e8e5902ce5103a4b61313cb704d27effa9b3126e
Will Hunt <will@half-shot.uk>
2025-09-02 08:11:55 +0100

    lint

--- src/components/views/settings/encryption/ChangeRecoveryKey.tsx
+++ src/components/views/settings/encryption/ChangeRecoveryKey.tsx
@@ -5,7 +5,7 @@
  * Please see LICENSE files in the repository root for full details.
  */
 
-import React, { FormEvent, type JSX, type MouseEventHandler, useState } from "react";
+import React, { type JSX, type MouseEventHandler, useState } from "react";
 import {
     Breadcrumb,
     Button,
