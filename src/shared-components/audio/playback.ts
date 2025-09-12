b6710d19c08b3dbfc5ba27cdc1ce2a0b2b47b826
Florian Duros <florianduros@element.io>
2025-09-12 10:24:51 +0200

    Prevent voice message from displaying spurious errors (#30736)
    * fix: avoid to render `AudioPlayerViewModel` when `MAudioBody` is inherited
    
    * fix: avoid `Playback.prepare` to fail when called twice
    
    * fix: add `decoding` to playback type
    
    * refactor: fix circular deps
    
    * refactor: extract `MockedPlayback` from `AudioPlayerViewModel`
    
    * test: add `MAudioBody` basic test
    
    * test: add tests for `MVoiceMessageBody`
    
    * fix: lint

--- src/shared-components/audio/playback.ts
+++ src/shared-components/audio/playback.ts
@@ -7,9 +7,10 @@
 
 /**
  * Represents the possible states of playback.
+ * - "preparing": The audio is being prepared for playback (e.g., loading or buffering).
  * - "decoding": The audio is being decoded and is not ready for playback.
  * - "stopped": The playback has been stopped, with no progress on the timeline.
  * - "paused": The playback is paused, with some progress on the timeline.
  * - "playing": The playback is actively progressing through the timeline.
  */
-export type PlaybackState = "decoding" | "stopped" | "paused" | "playing";
+export type PlaybackState = "decoding" | "stopped" | "paused" | "playing" | "preparing";
