832d7ed43f815ee8799570233bd0406736ad3810
Florian Duros <florian.duros@ormaz.fr>
2025-10-02 17:44:31 +0200

    chore: build shared components as a lib

---
+++ src/shared-components/index.ts
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2025 New Vector Ltd.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only OR GPL-3.0-only OR LicenseRef-Element-Commercial
+ * Please see LICENSE files in the repository root for full details.
+ */
+
+export * from "./audio/AudioPlayerView";
+export * from "./audio/Clock";
+export * from "./audio/PlayPauseButton";
+export * from "./audio/SeekBar";
+export * from "./avatar/AvatarWithDetails";
+export * from "./event-tiles/TextualEventView";
+export * from "./message-body/MediaBody";
+export * from "./pill-input/Pill";
+export * from "./pill-input/PillInput";
+export * from "./rich-list/RichItem";
