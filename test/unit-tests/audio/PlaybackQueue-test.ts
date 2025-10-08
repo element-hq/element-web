e7e7edd4ffd8669a9077e4f45d6490bef36ddd7e
Florian Duros <florian.duros@ormaz.fr>
2025-10-08 11:17:07 +0200

    test: add test

--- test/unit-tests/audio/PlaybackQueue-test.ts
+++ test/unit-tests/audio/PlaybackQueue-test.ts
@@ -7,12 +7,11 @@ Please see LICENSE files in the repository root for full details.
 
 import { type Mocked } from "jest-mock";
 import { type MatrixEvent, type Room } from "matrix-js-sdk/src/matrix";
-import { SimpleObservable } from "matrix-widget-api";
 
 import { PlaybackQueue } from "../../../src/audio/PlaybackQueue";
-import { PlaybackState, type Playback } from "../../../src/audio/Playback";
-import { MockEventEmitter } from "../../test-utils";
+import { type Playback, PlaybackState } from "../../../src/audio/Playback";
 import { UPDATE_EVENT } from "../../../src/stores/AsyncStore";
+import { MockedPlayback } from "./MockedPlayback";
 
 describe("PlaybackQueue", () => {
     let playbackQueue: PlaybackQueue;
@@ -24,40 +23,33 @@ describe("PlaybackQueue", () => {
         playbackQueue = new PlaybackQueue(mockRoom);
     });
 
-    it("does not call skipTo on playback if clock advances to 0s", () => {
+    it.each([
+        [PlaybackState.Playing, true],
+        [PlaybackState.Paused, true],
+        [PlaybackState.Preparing, false],
+        [PlaybackState.Decoding, false],
+        [PlaybackState.Stopped, false],
+    ])("should save (or not) the clock PlayBackState=%s expected=%s", (playbackState, expected) => {
         const mockEvent = {
             getId: jest.fn().mockReturnValue("$foo:bar"),
         } as unknown as Mocked<MatrixEvent>;
-        const mockPlayback = new MockEventEmitter({
-            clockInfo: {
-                liveData: new SimpleObservable<number[]>(),
-            },
-            skipTo: jest.fn(),
-        }) as unknown as Mocked<Playback>;
+        const mockPlayback = new MockedPlayback(playbackState, 0, 0) as unknown as Mocked<Playback>;
 
         // Enqueue
         playbackQueue.unsortedEnqueue(mockEvent, mockPlayback);
 
         // Emit our clockInfo of 0, which will playbackQueue to save the state.
-        mockPlayback.clockInfo.liveData.update([0]);
+        mockPlayback.clockInfo.liveData.update([1]);
 
-        // Fire an update event to say that we have stopped.
-        // Note that Playback really emits an UPDATE_EVENT whenever state changes, the types are lies.
-        mockPlayback.emit(UPDATE_EVENT as any, PlaybackState.Stopped);
-
-        expect(mockPlayback.skipTo).not.toHaveBeenCalled();
+        // @ts-ignore
+        expect(playbackQueue.clockStates.has(mockEvent.getId()!)).toBe(expected);
     });
 
-    it("does call skipTo on playback if clock advances to 0s", () => {
+    it("does call skipTo on playback if clock advances to 1s", () => {
         const mockEvent = {
             getId: jest.fn().mockReturnValue("$foo:bar"),
         } as unknown as Mocked<MatrixEvent>;
-        const mockPlayback = new MockEventEmitter({
-            clockInfo: {
-                liveData: new SimpleObservable<number[]>(),
-            },
-            skipTo: jest.fn(),
-        }) as unknown as Mocked<Playback>;
+        const mockPlayback = new MockedPlayback(PlaybackState.Playing, 0, 0) as unknown as Mocked<Playback>;
 
         // Enqueue
         playbackQueue.unsortedEnqueue(mockEvent, mockPlayback);
