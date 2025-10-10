14700d72df6e92de9aa116862eaf8496cf32afdf
David Baker <dbkr@users.noreply.github.com>
2025-10-10 14:37:20 +0100

    Add richlist

--- packages/shared-components/src/index.ts
+++ packages/shared-components/src/index.ts
@@ -16,6 +16,7 @@ export * from "./message-body/MediaBody";
 export * from "./pill-input/Pill";
 export * from "./pill-input/PillInput";
 export * from "./rich-list/RichItem";
+export * from "./rich-list/RichList";
 export * from "./utils/Box";
 export * from "./utils/Flex";
 
