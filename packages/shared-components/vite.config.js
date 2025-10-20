8e41b137fc0b3d88c38921ac5762836e29c062f8
Florian Duros <florian.duros@ormaz.fr>
2025-10-20 15:56:42 +0200

    fix: exclude counterpart from build

--- packages/shared-components/vite.config.js
+++ packages/shared-components/vite.config.js
@@ -26,13 +26,14 @@ export default defineConfig({
         rollupOptions: {
             // make sure to externalize deps that shouldn't be bundled
             // into your library
-            external: ["react", "react-dom"],
+            external: ["react", "react-dom", "counterpart"],
             output: {
                 // Provide global variables to use in the UMD build
                 // for externalized deps
                 globals: {
                     "react": "react",
                     "react-dom": "ReactDom",
+                    "counterpart": "counterpart",
                 },
             },
         },
