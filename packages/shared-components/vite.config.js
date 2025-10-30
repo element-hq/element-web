6bf10ae8bd9f42564014534f36359f3280398bf4
David Baker <dbkr@users.noreply.github.com>
2025-10-30 16:10:34 +0000

    Avoid bundling compound bits
    Because a) it's silly and b) it means we end up bundling a copy of
    floating-ui too which causes absolute madness with its useDelayGroup
    contexts.

--- packages/shared-components/vite.config.js
+++ packages/shared-components/vite.config.js
@@ -26,7 +26,7 @@ export default defineConfig({
         rollupOptions: {
             // make sure to externalize deps that shouldn't be bundled
             // into your library
-            external: ["react", "react-dom"],
+            external: ["react", "react-dom", "@vector-im/compound-design-tokens", "@vector-im/compound-web"],
             output: {
                 // Provide global variables to use in the UMD build
                 // for externalized deps
