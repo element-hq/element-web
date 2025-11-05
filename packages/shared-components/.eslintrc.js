24aa75954469f01ebcf49cb3d757e6f917c38987
R Midhun Suresh <hi@midhun.dev>
2025-11-05 22:14:59 +0530

    Add license (#31179)

--- packages/shared-components/.eslintrc.js
+++ packages/shared-components/.eslintrc.js
@@ -1,3 +1,10 @@
+/*
+Copyright 2025 Element Creations Ltd.
+
+SPDX-License-Identifier: AGPL-3.0-only OR GPL-3.0-only OR LicenseRef-Element-Commercial
+Please see LICENSE files in the repository root for full details.
+*/
+
 module.exports = {
     root: true,
     plugins: ["matrix-org", "eslint-plugin-react-compiler"],
