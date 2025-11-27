69abef1f1ca662eaa52dedb0256d53847b456568
ElementRobot <releases@riot.im>
2025-11-27 06:22:39 +0000

    [create-pull-request] automated change (#31348)
    Co-authored-by: t3chguy <2403652+t3chguy@users.noreply.github.com>

--- playwright/testcontainers/synapse.ts
+++ playwright/testcontainers/synapse.ts
@@ -7,7 +7,7 @@ Please see LICENSE files in the repository root for full details.
 
 import { SynapseContainer as BaseSynapseContainer } from "@element-hq/element-web-playwright-common/lib/testcontainers";
 
-const TAG = "develop@sha256:115fd1925078b521bbab642df1e4456a65f9193e6353b61aaa4ec5686673ddc4";
+const TAG = "develop@sha256:645cb87b5ac1465af9a64cef6199ad78cd81ac63723bdf6b766b5b12f3ad2e25";
 
 /**
  * SynapseContainer which freezes the docker digest to stabilise tests,
