b8a1b6b23b8d5528f9eec4bb7f543a9f5fd8357f
Skye Elliot <actuallyori@gmail.com>
2025-11-28 11:28:48 +0000

    chore: Remove broken import.

--- packages/shared-components/src/index.ts
+++ packages/shared-components/src/index.ts
@@ -18,7 +18,6 @@ export * from "./pill-input/Pill";
 export * from "./pill-input/PillInput";
 export * from "./rich-list/RichItem";
 export * from "./rich-list/RichList";
-export * from "./room/RoomStatusBarHistoryVisible";
 export * from "./utils/Box";
 export * from "./utils/Flex";
 
