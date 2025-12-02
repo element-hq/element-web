89bfa4047487d3d1d278598fd19d71068d0212ab
Michael Telatynski <7t3chguy@gmail.com>
2025-12-02 09:51:35 +0000

    Iterate

--- modules/restricted-guests/element-web/src/vite-env.d.ts
+++ modules/restricted-guests/element-web/src/vite-env.d.ts
@@ -1,9 +1,8 @@
 /*
-Copyright 2025 New Vector Ltd.
+Copyright 2025 Element Creations Ltd.
 
 SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
 Please see LICENSE files in the repository root for full details.
 */
 
-/// <reference types="vite-plugin-svgr/client" />
 /// <reference types="@arcmantle/vite-plugin-import-css-sheet/client" />
