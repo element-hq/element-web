5432aa4d67972e291865d2738ccf0f494965234f
Michael Telatynski <7t3chguy@gmail.com>
2025-12-09 15:48:03 +0000

    Make CI happier
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- jest.config.ts
+++ jest.config.ts
@@ -43,7 +43,7 @@ const config: Config = {
         "counterpart": "<rootDir>/node_modules/counterpart",
     },
     transformIgnorePatterns: [
-        "/node_modules/(?!(mime|matrix-js-sdk|uuid|p-retry|is-network-error|react-merge-refs)).+$",
+        "/node_modules/(?!(mime|matrix-js-sdk|uuid|p-retry|is-network-error|react-merge-refs|node-fetch|data-uri-to-buffer|fetch-blob|formdata-polyfill)).+$",
     ],
     collectCoverageFrom: [
         "<rootDir>/src/**/*.{js,ts,tsx}",
