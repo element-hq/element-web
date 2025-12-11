d326189abfae03a6fce38001a327c0e7d666cb7d
David Baker <dbkr@users.noreply.github.com>
2025-12-11 14:52:19 +0000

    Remove deviceId property in favour of superclass one

--- src/utils/oidc/TokenRefresher.ts
+++ src/utils/oidc/TokenRefresher.ts
@@ -17,8 +17,6 @@ import { persistAccessTokenInStorage, persistRefreshTokenInStorage } from "../to
  * Stores tokens in the same way as login flow in Lifecycle.
  */
 export class TokenRefresher extends OidcTokenRefresher {
-    private readonly deviceId!: string;
-
     public constructor(
         issuer: string,
         clientId: string,
@@ -28,7 +26,6 @@ export class TokenRefresher extends OidcTokenRefresher {
         private readonly userId: string,
     ) {
         super(issuer, clientId, deviceId, redirectUri, idTokenClaims);
-        this.deviceId = deviceId;
     }
 
     public async persistTokens({ accessToken, refreshToken }: AccessTokens): Promise<void> {
