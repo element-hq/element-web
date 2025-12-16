f872a090c883f7163269b7a493fed7195580721c
Skye Elliot <actuallyori@gmail.com>
2025-12-16 11:41:48 +0000

    docs: Improve documentation for `HistoryVisibleBanner`, second pass.

--- src/components/views/composer/HistoryVisibleBanner.tsx
+++ src/components/views/composer/HistoryVisibleBanner.tsx
@@ -12,8 +12,9 @@ import { type Room } from "matrix-js-sdk/src/matrix";
 import { HistoryVisibleBannerViewModel } from "../../../viewmodels/composer/HistoryVisibleBannerViewModel";
 
 /**
- * Wrapper around {@link HistoryVisibleBannerViewModel} for creation of auto-disposed view model.
- * @param props - See constructor of {@link HistoryVisibleBannerViewModel}
+ * Wrapper around {@link HistoryVisibleBannerViewModel} for the creation of an auto-disposed view model.
+ * @param props.room - The room instance associated with this banner view model.
+ * @param props.threadId - If provided, specifies the ID of the thread currently being viewed in the thread timeline side view, where the banner view is displayed as a child of the message composer.
  */
 export const HistoryVisibleBanner: React.FC<{ room: Room; threadId?: string | null }> = (props) => {
     const vm = useCreateAutoDisposedViewModel(() => new HistoryVisibleBannerViewModel(props));
