02645a5fd4f7b192808c22fc6500377b587151bf
Michael Telatynski <7t3chguy@gmail.com>
2026-01-06 11:02:59 +0000

    Switch from svg masks to svg rendering in BaseCard
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- src/components/views/right_panel/WidgetCard.tsx
+++ src/components/views/right_panel/WidgetCard.tsx
@@ -8,6 +8,7 @@ Please see LICENSE files in the repository root for full details.
 
 import React, { type JSX, useContext, useEffect } from "react";
 import { type Room } from "matrix-js-sdk/src/matrix";
+import { OverflowHorizontalIcon } from "@vector-im/compound-design-tokens/assets/web/icons";
 
 import MatrixClientContext from "../../../contexts/MatrixClientContext";
 import BaseCard from "./BaseCard";
@@ -73,7 +74,9 @@ const WidgetCard: React.FC<IProps> = ({ room, widgetId, onClose }) => {
                 onClick={openMenu}
                 isExpanded={menuDisplayed}
                 label={_t("common|options")}
-            />
+            >
+                <OverflowHorizontalIcon />
+            </ContextMenuButton>
             {contextMenu}
         </div>
     );
