953f00482491ef445cba7ccc9e79e83d297c5ac5
Michael Telatynski <7t3chguy@gmail.com>
2026-01-06 12:43:43 +0000

    Switch from svg masks to svg rendering in NewRoomIntro
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- src/components/views/rooms/NewRoomIntro.tsx
+++ src/components/views/rooms/NewRoomIntro.tsx
@@ -9,7 +9,7 @@ Please see LICENSE files in the repository root for full details.
 import React, { type JSX, useContext } from "react";
 import { EventType, type Room, type User, type MatrixClient } from "matrix-js-sdk/src/matrix";
 import { KnownMembership } from "matrix-js-sdk/src/types";
-import { ErrorSolidIcon } from "@vector-im/compound-design-tokens/assets/web/icons";
+import { ErrorSolidIcon, UserAddIcon } from "@vector-im/compound-design-tokens/assets/web/icons";
 
 import MatrixClientContext from "../../../contexts/MatrixClientContext";
 import DMRoomMap from "../../../utils/DMRoomMap";
@@ -191,6 +191,7 @@ const NewRoomIntro: React.FC = () => {
                             showSpaceInvite(parentSpace!);
                         }}
                     >
+                        <UserAddIcon />
                         {_t("invite|to_space", { spaceName: parentSpace.name })}
                     </AccessibleButton>
                     {room.canInvite(cli.getSafeUserId()) && (
@@ -201,6 +202,7 @@ const NewRoomIntro: React.FC = () => {
                                 defaultDispatcher.dispatch({ action: "view_invite", roomId });
                             }}
                         >
+                            <UserAddIcon />
                             {_t("room|intro|room_invite")}
                         </AccessibleButton>
                     )}
@@ -216,6 +218,7 @@ const NewRoomIntro: React.FC = () => {
                             defaultDispatcher.dispatch({ action: "view_invite", roomId });
                         }}
                     >
+                        <UserAddIcon />
                         {_t("room|invite_this_room")}
                     </AccessibleButton>
                 </div>
