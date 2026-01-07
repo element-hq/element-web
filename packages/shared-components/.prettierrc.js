a5fde3e27ccc038bd40f275513f2bd02925d1b9d
David Baker <dbkr@users.noreply.github.com>
2026-01-07 12:25:34 +0000

    Comment the right .prettierrc

--- packages/shared-components/.prettierrc.js
+++ packages/shared-components/.prettierrc.js
@@ -1,1 +1,5 @@
+// Even though this (at time of writing) is identical Element Web's
+// .prettierrc.js, shared components needs its own because otherwise
+// this refers to element web's copy of eslint-plugin-matrix-org which
+// would require element-web's modules to be installed.
 module.exports = require("eslint-plugin-matrix-org/.prettierrc.js");
