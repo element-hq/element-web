fc6493b6b9f9afdb65f7f9cdc8bb16d1c3ad9b89
R Midhun Suresh <hi@midhun.dev>
2026-01-14 14:20:55 +0530

    Extract base recency sorter class

---
+++ src/stores/room-list-v3/skip-list/sorters/BaseRecencySorter.ts
@@ -0,0 +1,52 @@
+/*
+Copyright 2026 Element Creations Ltd.
+
+SPDX-License-Identifier: AGPL-3.0-only OR GPL-3.0-only OR LicenseRef-Element-Commercial
+Please see LICENSE files in the repository root for full details.
+*/
+
+import type { Room } from "matrix-js-sdk/src/matrix";
+import type { Sorter, SortingAlgorithm } from ".";
+import { getLastTs } from "../../../room-list/algorithms/tag-sorting/RecentAlgorithm";
+
+export abstract class BaseRecencySorter implements Sorter {
+    public constructor(protected myUserId: string) {}
+
+    public sort(rooms: Room[]): Room[] {
+        const tsCache: { [roomId: string]: number } = {};
+        return [...rooms].sort((a, b) => this.comparator(a, b, tsCache));
+    }
+
+    public comparator(roomA: Room, roomB: Room, cache?: any): number {
+        // First check if the rooms are low priority or muted
+        const exceptionalOrdering = this.getScore(roomA) - this.getScore(roomB);
+        if (exceptionalOrdering !== 0) return exceptionalOrdering;
+
+        // Then check recency; recent rooms should be at the top
+        const roomALastTs = this.getTs(roomA, cache);
+        const roomBLastTs = this.getTs(roomB, cache);
+        return roomBLastTs - roomALastTs;
+    }
+
+    private getTs(room: Room, cache?: { [roomId: string]: number }): number {
+        const ts = cache?.[room.roomId] ?? getLastTs(room, this.myUserId);
+        if (cache) {
+            cache[room.roomId] = ts;
+        }
+        return ts;
+    }
+
+    public abstract get type(): SortingAlgorithm;
+
+    /**
+     * Rooms are sorted based on:
+     * - the score of the room
+     * - the timestamp of the last message in that room
+     *
+     * The score takes precedence over the timestamp of the last message. This allows
+     * some rooms to be sorted before/after others regardless of when the last message
+     * was received in that room. Eg: muted rooms can be placed at the bottom of the list
+     * even if they received messages recently.
+     */
+    protected abstract getScore(room: Room): number;
+}
--- src/stores/room-list-v3/skip-list/sorters/RecencySorter.ts
+++ src/stores/room-list-v3/skip-list/sorters/RecencySorter.ts
@@ -7,29 +7,11 @@ Please see LICENSE files in the repository root for full details.
 
 import type { Room } from "matrix-js-sdk/src/matrix";
 import { type Sorter, SortingAlgorithm } from ".";
-import { getLastTs } from "../../../room-list/algorithms/tag-sorting/RecentAlgorithm";
 import { RoomNotificationStateStore } from "../../../notifications/RoomNotificationStateStore";
 import { DefaultTagID } from "../../../room-list/models";
+import { BaseRecencySorter } from "./BaseRecencySorter";
 
-export class RecencySorter implements Sorter {
-    public constructor(private myUserId: string) {}
-
-    public sort(rooms: Room[]): Room[] {
-        const tsCache: { [roomId: string]: number } = {};
-        return [...rooms].sort((a, b) => this.comparator(a, b, tsCache));
-    }
-
-    public comparator(roomA: Room, roomB: Room, cache?: any): number {
-        // First check if the rooms are low priority or muted
-        const exceptionalOrdering = this.getScore(roomA) - this.getScore(roomB);
-        if (exceptionalOrdering !== 0) return exceptionalOrdering;
-
-        // Then check recency; recent rooms should be at the top
-        const roomALastTs = this.getTs(roomA, cache);
-        const roomBLastTs = this.getTs(roomB, cache);
-        return roomBLastTs - roomALastTs;
-    }
-
+export class RecencySorter extends BaseRecencySorter implements Sorter {
     public get type(): SortingAlgorithm.Recency {
         return SortingAlgorithm.Recency;
     }
@@ -45,7 +27,7 @@ export class RecencySorter implements Sorter {
      * - If getScore(A) - getScore(B) < 0, A should come before B
      * - If getScore(A) - getScore(B) = 0, no special ordering needed, just use recency
      */
-    private getScore(room: Room): number {
+    protected getScore(room: Room): number {
         const isLowPriority = !!room.tags[DefaultTagID.LowPriority];
         const isMuted = RoomNotificationStateStore.instance.getRoomState(room).muted;
         // These constants are chosen so that the following order is maintained:
@@ -54,13 +36,5 @@ export class RecencySorter implements Sorter {
         else if (isMuted) return 10;
         else if (isLowPriority) return 2;
         else return 0;
-    }
-
-    private getTs(room: Room, cache?: { [roomId: string]: number }): number {
-        const ts = cache?.[room.roomId] ?? getLastTs(room, this.myUserId);
-        if (cache) {
-            cache[room.roomId] = ts;
-        }
-        return ts;
     }
 }
