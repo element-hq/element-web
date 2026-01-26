11b1fdb25089c10d8af8aa35df25216605734ef6
Skye Elliot <actuallyori@gmail.com>
2026-01-26 12:53:49 +0000

    Revert "Update algorithm for history visible banner. (#31577)"
    This reverts commit ce9c66ba4c25f3de5ceca5d244591d8aa1183ce8.

--- src/viewmodels/composer/HistoryVisibleBannerViewModel.tsx
+++ src/viewmodels/composer/HistoryVisibleBannerViewModel.tsx
@@ -15,21 +15,11 @@ import { HistoryVisibility, RoomStateEvent, type Room } from "matrix-js-sdk/src/
 import SettingsStore from "../../settings/SettingsStore";
 import { SettingLevel } from "../../settings/SettingLevel";
 
-/**
- * A collection of {@link HistoryVisibility} levels that trigger the display of the history visible banner.
- */
-const BANNER_VISIBLE_LEVELS = [HistoryVisibility.Shared, HistoryVisibility.WorldReadable];
-
 interface Props {
     /**
      * The room instance associated with this banner view model.
      */
     room: Room;
-
-    /**
-     * Whether or not the current user is able to send messages in this room.
-     */
-    canSendMessages: boolean;
 
     /**
      * If not null, indicates the ID of the thread currently being viewed in the thread
@@ -76,33 +66,23 @@ export class HistoryVisibleBannerViewModel
 
     /**
      * Computes the latest banner snapshot given the VM's props.
-     * @param props - See {@link Props}.
+     * @param room - The room the banner will be shown in.
+     * @param threadId - The thread ID passed in from the parent {@link MessageComposer}.
      * @returns The latest snapshot. See {@link HistoryVisibleBannerViewSnapshot}.
      */
-    private static readonly computeSnapshot = ({
-        room,
-        canSendMessages,
-        threadId,
-    }: Props): HistoryVisibleBannerViewSnapshot => {
+    private static readonly computeSnapshot = (
+        room: Room,
+        threadId?: string | null,
+    ): HistoryVisibleBannerViewSnapshot => {
         const featureEnabled = SettingsStore.getValue("feature_share_history_on_invite");
         const acknowledged = SettingsStore.getValue("acknowledgedHistoryVisibility", room.roomId);
-        const isHistoryVisible = BANNER_VISIBLE_LEVELS.includes(room.getHistoryVisibility());
 
-        // This implements point 1. of the algorithm described above. In the order below, all
-        // of the following must be true for the banner to display:
-        // - The room history sharing feature must be enabled.
-        // - The room must be encrypted.
-        // - The user must be able to send messages.
-        // - The history must be visible.
-        // - The view should not be part of a thread timeline.
-        // - The user must not have acknowledged the banner.
         return {
             visible:
                 featureEnabled &&
-                room.hasEncryptionStateEvent() &&
-                canSendMessages &&
-                isHistoryVisible &&
                 !threadId &&
+                room.hasEncryptionStateEvent() &&
+                room.getHistoryVisibility() !== HistoryVisibility.Joined &&
                 !acknowledged,
         };
     };
@@ -112,7 +92,7 @@ export class HistoryVisibleBannerViewModel
      * @param props - Properties for this view model. See {@link Props}.
      */
     public constructor(props: Props) {
-        super(props, HistoryVisibleBannerViewModel.computeSnapshot(props));
+        super(props, HistoryVisibleBannerViewModel.computeSnapshot(props.room, props.threadId));
 
         this.disposables.trackListener(props.room, RoomStateEvent.Update, () => this.setSnapshot());
 
@@ -146,7 +126,7 @@ export class HistoryVisibleBannerViewModel
             );
         }
 
-        this.snapshot.set(HistoryVisibleBannerViewModel.computeSnapshot(this.props));
+        this.snapshot.set(HistoryVisibleBannerViewModel.computeSnapshot(this.props.room, this.props.threadId));
     }
 
     /**
