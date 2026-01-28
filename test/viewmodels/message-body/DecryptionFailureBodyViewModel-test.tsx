b79f99008b47c17a303f7490bb48f2f4ebb18613
rbondesson <rickard.bondesson@cgi.com>
2026-01-28 14:26:07 +0100

    Bettter structure on view model

--- test/viewmodels/message-body/DecryptionFailureBodyViewModel-test.tsx
+++ test/viewmodels/message-body/DecryptionFailureBodyViewModel-test.tsx
@@ -35,51 +35,55 @@ describe("DecryptionFailureBodyViewModel", () => {
         });
     });
 
-    const decryptionFailureCodes = Object.entries(DecryptionFailureCode)
-        .filter(([key, value]) => key === value)
-        .map(([key]) => key);
-
-    it.each(decryptionFailureCodes)("should return the snapshot with converted failure reason (%s)", (code) => {
+    it.each([
+        {
+            code: DecryptionFailureCode.HISTORICAL_MESSAGE_BACKUP_UNCONFIGURED,
+            reason: DecryptionFailureReason.HISTORICAL_MESSAGE_BACKUP_UNCONFIGURED,
+        },
+        {
+            code: DecryptionFailureCode.HISTORICAL_MESSAGE_NO_KEY_BACKUP,
+            reason: DecryptionFailureReason.HISTORICAL_MESSAGE_NO_KEY_BACKUP,
+        },
+        {
+            code: DecryptionFailureCode.HISTORICAL_MESSAGE_USER_NOT_JOINED,
+            reason: DecryptionFailureReason.HISTORICAL_MESSAGE_USER_NOT_JOINED,
+        },
+        {
+            code: DecryptionFailureCode.MEGOLM_KEY_WITHHELD,
+            reason: DecryptionFailureReason.UNABLE_TO_DECRYPT,
+        },
+        {
+            code: DecryptionFailureCode.MEGOLM_KEY_WITHHELD_FOR_UNVERIFIED_DEVICE,
+            reason: DecryptionFailureReason.MEGOLM_KEY_WITHHELD_FOR_UNVERIFIED_DEVICE,
+        },
+        {
+            code: DecryptionFailureCode.MEGOLM_UNKNOWN_INBOUND_SESSION_ID,
+            reason: DecryptionFailureReason.UNABLE_TO_DECRYPT,
+        },
+        {
+            code: DecryptionFailureCode.OLM_UNKNOWN_MESSAGE_INDEX,
+            reason: DecryptionFailureReason.UNABLE_TO_DECRYPT,
+        },
+        {
+            code: DecryptionFailureCode.SENDER_IDENTITY_PREVIOUSLY_VERIFIED,
+            reason: DecryptionFailureReason.SENDER_IDENTITY_PREVIOUSLY_VERIFIED,
+        },
+        {
+            code: DecryptionFailureCode.UNKNOWN_ERROR,
+            reason: DecryptionFailureReason.UNABLE_TO_DECRYPT,
+        },
+        {
+            code: DecryptionFailureCode.UNKNOWN_SENDER_DEVICE,
+            reason: DecryptionFailureReason.UNABLE_TO_DECRYPT,
+        },
+        {
+            code: DecryptionFailureCode.UNSIGNED_SENDER_DEVICE,
+            reason: DecryptionFailureReason.UNSIGNED_SENDER_DEVICE,
+        },
+    ])("should return the snapshot with code converted to reason (%s)", ({ code, reason }) => {
         const vm = new DecryptionFailureBodyViewModel({
-            decryptionFailureCode: code as DecryptionFailureCode,
+            decryptionFailureCode: code,
         });
-
-        let reason = DecryptionFailureReason.UNABLE_TO_DECRYPT;
-        switch (code as DecryptionFailureCode) {
-            case DecryptionFailureCode.HISTORICAL_MESSAGE_BACKUP_UNCONFIGURED:
-                reason = DecryptionFailureReason.HISTORICAL_MESSAGE_BACKUP_UNCONFIGURED;
-                break;
-            case DecryptionFailureCode.HISTORICAL_MESSAGE_NO_KEY_BACKUP:
-                reason = DecryptionFailureReason.HISTORICAL_MESSAGE_NO_KEY_BACKUP;
-                break;
-            case DecryptionFailureCode.HISTORICAL_MESSAGE_USER_NOT_JOINED:
-                reason = DecryptionFailureReason.HISTORICAL_MESSAGE_USER_NOT_JOINED;
-                break;
-            case DecryptionFailureCode.MEGOLM_KEY_WITHHELD:
-                reason = DecryptionFailureReason.UNABLE_TO_DECRYPT;
-                break;
-            case DecryptionFailureCode.MEGOLM_KEY_WITHHELD_FOR_UNVERIFIED_DEVICE:
-                reason = DecryptionFailureReason.MEGOLM_KEY_WITHHELD_FOR_UNVERIFIED_DEVICE;
-                break;
-            case DecryptionFailureCode.MEGOLM_UNKNOWN_INBOUND_SESSION_ID:
-                reason = DecryptionFailureReason.UNABLE_TO_DECRYPT;
-                break;
-            case DecryptionFailureCode.OLM_UNKNOWN_MESSAGE_INDEX:
-                reason = DecryptionFailureReason.UNABLE_TO_DECRYPT;
-                break;
-            case DecryptionFailureCode.SENDER_IDENTITY_PREVIOUSLY_VERIFIED:
-                reason = DecryptionFailureReason.SENDER_IDENTITY_PREVIOUSLY_VERIFIED;
-                break;
-            case DecryptionFailureCode.UNKNOWN_ERROR:
-                reason = DecryptionFailureReason.UNABLE_TO_DECRYPT;
-                break;
-            case DecryptionFailureCode.UNKNOWN_SENDER_DEVICE:
-                reason = DecryptionFailureReason.UNABLE_TO_DECRYPT;
-                break;
-            case DecryptionFailureCode.UNSIGNED_SENDER_DEVICE:
-                reason = DecryptionFailureReason.UNSIGNED_SENDER_DEVICE;
-                break;
-        }
 
         expect(vm.getSnapshot().decryptionFailureReason).toBe(reason);
     });
