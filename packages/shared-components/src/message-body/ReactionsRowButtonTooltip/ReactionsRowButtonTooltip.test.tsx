62c7fe54089c9c291dbe8713a6013cbd04127983
Zack <zazi21@student.bth.se>
2026-01-30 12:53:57 +0100

    Refactor ReactionsRowButtonTooltip to shared-components (#31866)
    * Setting up structure for the init refactoring of ReactionsRowButtonTooltip
    
    * implemented example to follow for refactoring to MVVM
    
    * Refactoring of ReactionsRowButtonTooltipView
    
    * updated reactionrowbutton to use our new viewmodel and removed unessecery comments
    
    * Updated children from reactnode to propswithchildren
    
    * removal of children on the vm have it as a props
    
    * implemented constructor into reactionrowbutton to use vm to viewmodel
    
    * Removal of old component
    
    * Added ViewModel Tests for new viewmodel
    
    * Fix issues after merging develop
    
    * Updated import placement for eslint failure CI
    
    * Add tests for ReactionsRowButton ViewModel integration and click handlers to pass coverage
    
    * Added more tests to cover all conditions
    
    * Pass MatrixClient as prop instead of using global; replace expect(true).toBe(true) with not.toThrow()
    
    * Added new snapshot to reflect modifications on tests
    
    * Update images to fit the CI tests
    
    * Optimize reactions tooltip viewmodel updates
    
    * Removal of module.css for reactionbuttontooltip, we dont need it since we dont use any css
    
    * Fixed snapshots to show the tooltip by introducing a boolean to set open to true in Storybook.
    
    * Update snapshots
    
    ---------
    
    Co-authored-by: Michael Telatynski <7t3chguy@gmail.com>

---
+++ packages/shared-components/src/message-body/ReactionsRowButtonTooltip/ReactionsRowButtonTooltip.test.tsx
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2026 Element Creations Ltd.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only OR GPL-3.0-only OR LicenseRef-Element-Commercial
+ * Please see LICENSE files in the repository root for full details.
+ */
+
+import { composeStories } from "@storybook/react-vite";
+import { render } from "@test-utils";
+import React from "react";
+import { describe, it, expect } from "vitest";
+
+import * as stories from "./ReactionsRowButtonTooltip.stories";
+
+const { Default, ManySenders } = composeStories(stories);
+
+describe("ReactionsRowButtonTooltip", () => {
+    it("renders the tooltip with formatted senders and caption", () => {
+        const { container } = render(<Default />);
+        expect(container).toMatchSnapshot();
+    });
+
+    it("renders the tooltip with many senders", () => {
+        const { container } = render(<ManySenders />);
+        expect(container).toMatchSnapshot();
+    });
+});
