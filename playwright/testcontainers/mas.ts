5f4930f976e4933033335c43a0c120d249c30b80
Michael Telatynski <7t3chguy@gmail.com>
2026-02-05 10:57:09 +0000

    Update testcontainers imports
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- playwright/testcontainers/mas.ts
+++ playwright/testcontainers/mas.ts
@@ -8,7 +8,7 @@ Please see LICENSE files in the repository root for full details.
 import {
     MatrixAuthenticationServiceContainer as BaseMatrixAuthenticationServiceContainer,
     type StartedPostgreSqlContainer,
-} from "@element-hq/element-web-playwright-common/lib/testcontainers";
+} from "@element-hq/element-web-playwright-common/lib/testcontainers/index.js";
 
 const TAG = "main@sha256:d0d03f9067c7977807131a9c739c9ed9f081063d1a1c21bee66204e40c44aa50";
 
