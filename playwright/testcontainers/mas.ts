c2f1793bb01cfe8f9a3b2d90bdf509524d2d082f
Michael Telatynski <7t3chguy@gmail.com>
2026-02-09 08:39:11 +0000

    Switch element-web to ESM (#31977)
    * Switch element-web to ESM
    
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>
    
    * Update testcontainers imports
    
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>
    
    * Fix modernizr cjs
    
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>
    
    * Fix modernizr cjs ignore files
    
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>
    
    * Move modernizr sonar exclusion to exclude everything
    
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>
    
    * Update playwright tests for esm compat
    
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>
    
    ---------
    
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- playwright/testcontainers/mas.ts
+++ playwright/testcontainers/mas.ts
@@ -8,7 +8,7 @@ Please see LICENSE files in the repository root for full details.
 import {
     MatrixAuthenticationServiceContainer as BaseMatrixAuthenticationServiceContainer,
     type StartedPostgreSqlContainer,
-} from "@element-hq/element-web-playwright-common/lib/testcontainers";
+} from "@element-hq/element-web-playwright-common/lib/testcontainers/index.js";
 
 const TAG = "main@sha256:294e209b8e3c9dd0347e2baf01c34da9c6b0f27324ebccc5fbd6e221d08d16c3";
 
--- playwright/testcontainers/synapse.ts
+++ playwright/testcontainers/synapse.ts
@@ -5,7 +5,7 @@ SPDX-License-Identifier: AGPL-3.0-only OR GPL-3.0-only OR LicenseRef-Element-Com
 Please see LICENSE files in the repository root for full details.
 */
 
-import { SynapseContainer as BaseSynapseContainer } from "@element-hq/element-web-playwright-common/lib/testcontainers";
+import { SynapseContainer as BaseSynapseContainer } from "@element-hq/element-web-playwright-common/lib/testcontainers/index.js";
 
 const TAG = "develop@sha256:f0de453dbb284112c4bc91be01345f189f218bf51ee09ed565dab295c0a72b44";
 
