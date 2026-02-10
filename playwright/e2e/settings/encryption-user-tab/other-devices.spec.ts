5c3d0fb173689d49cdb49dcce110f866f6043dfd
Skye Elliot <actuallyori@gmail.com>
2026-02-10 10:30:22 +0000

    tests: Ensure local toggle overrides global toggle.

--- playwright/e2e/settings/encryption-user-tab/other-devices.spec.ts
+++ playwright/e2e/settings/encryption-user-tab/other-devices.spec.ts
@@ -132,7 +132,7 @@ test.describe("Other people's devices section in Encryption tab", () => {
         await expect(bobPage.getByText("Decryptable")).toBeVisible();
     });
 
-    test("global blacklist toggle is not affected by local blacklist toggle", async ({
+    test("setting per-room unverified blacklist toggle does not affect other rooms", async ({
         page: alicePage,
         app: aliceElementApp,
         homeserver,
@@ -184,5 +184,70 @@ test.describe("Other people's devices section in Encryption tab", () => {
         // Alice sends a message in the new room, which Bob should be able to decrypt.
         await sendMessageInCurrentRoom(alicePage, "Decryptable");
         await expect(bobPage.getByText("Decryptable")).toBeVisible();
+    });
+
+    test("setting per-room unverified blacklist toggle overrides global toggle", async ({
+        page: alicePage,
+        app: aliceElementApp,
+        homeserver,
+        browser,
+        user: aliceCredentials,
+        util,
+    }, testInfo) => {
+        await aliceElementApp.client.bootstrapCrossSigning(aliceCredentials);
+
+        // Enable blacklist toggle.
+        let dialog = await util.openEncryptionTab();
+        const blacklistToggle = dialog.getByRole("switch", {
+            name: "In encrypted rooms, only send messages to verified users",
+        });
+        await blacklistToggle.scrollIntoViewIfNeeded();
+        await expect(blacklistToggle).toBeVisible();
+        await blacklistToggle.click();
+        await aliceElementApp.settings.closeDialog();
+
+        // Create a second browser instance.
+        const bobCredentials = await homeserver.registerUser(`user_${testInfo.testId}_bob`, "password", "bob");
+        const bobPage = await createNewInstance(browser, bobCredentials, {});
+        const bobElementApp = new ElementAppPage(bobPage);
+        await bobElementApp.client.bootstrapCrossSigning(bobCredentials);
+
+        // Alice creates the room and invite Bob.
+        await createRoom(alicePage, "TestRoom", true);
+        await aliceElementApp.inviteUserToCurrentRoom(bobCredentials.userId);
+
+        // Bob accepts the invite.
+        await bobPage.getByRole("option", { name: "TestRoom" }).click();
+        await bobPage.getByRole("button", { name: "Accept" }).click();
+
+        // Alice configures her client to allow sending to unverified users in this room.
+        dialog = await aliceElementApp.settings.openRoomSettings("Security & Privacy");
+        await dialog.getByRole("switch", { name: "Only send messages to verified users." }).click();
+        await aliceElementApp.settings.closeDialog();
+
+        // Alice sends a message which Bob should be able to decrypt.
+        await sendMessageInCurrentRoom(alicePage, "Decryptable");
+        await expect(bobPage.getByText("Decryptable")).toBeVisible();
+
+        // Alice dismisses key storage warnings, as they now hide the "New conversation" button.
+        await alicePage.getByRole("button", { name: "Dismiss" }).click(); // enable key storage
+        await alicePage.getByRole("button", { name: "Yes, dismiss" }).click(); // enable key storage x2
+
+        // Alice creates a second room and invites Bob.
+        await createRoom(alicePage, "TestRoom2", true);
+        await aliceElementApp.toggleRoomInfoPanel(); // should not be necessary, called in body of below
+        await aliceElementApp.inviteUserToCurrentRoom(bobCredentials.userId);
+
+        // Bob accepts the invite.
+        await bobPage.getByRole("option", { name: "TestRoom2" }).click();
+        await bobPage.getByRole("button", { name: "Accept" }).click();
+
+        // Alice sends a message in the new room, which Bob should not be able to decrypt.
+        await sendMessageInCurrentRoom(alicePage, "Undecryptable");
+        await expect(
+            bobPage.getByText(
+                "The sender has blocked you from receiving this message because your device is unverified",
+            ),
+        ).toBeVisible();
     });
 });
