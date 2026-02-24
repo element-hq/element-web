fa00d54368586f6c77c38d4bc3ecfc03a4fd0a86
David Baker <dbkr@users.noreply.github.com>
2026-02-24 19:10:00 +0100

    Do non-layered builds when running on master (second version)
    Hopefully this will fix the release as it looks like we've introduced
    backwards incompat changes in the js-sdk between the RC and the release,
    which have now broken as we were incorrectly using the develop js-sdk for
    release builds.
    
    Replaces https://github.com/element-hq/element-web/pull/32623

--- scripts/layered.sh
+++ scripts/layered.sh
@@ -20,27 +20,42 @@ pnpm install --frozen-lockfile
 export PR_ORG=element-hq
 export PR_REPO=element-web
 
-# Set up the js-sdk first
-scripts/fetchdep.sh matrix-org matrix-js-sdk develop
-pushd matrix-js-sdk
-[ -n "$JS_SDK_GITHUB_BASE_REF" ] && git fetch --depth 1 origin $JS_SDK_GITHUB_BASE_REF && git checkout $JS_SDK_GITHUB_BASE_REF
-pnpm link
-pnpm install --frozen-lockfile
-popd
+js_sdk_dep=`jq -r '.dependencies["matrix-js-sdk"]' < package.json`
+analytics_events_dep=`jq -r '.dependencies["@matrix-org/analytics-events"]' < package.json`
 
-# Also set up matrix-analytics-events for branch with matching name
-scripts/fetchdep.sh matrix-org matrix-analytics-events
-# We don't pass a default branch so cloning may fail when we are not in a PR
-# This is expected as this project does not share a release cycle but we still branch match it
-if [ -d matrix-analytics-events ]; then
-    pushd matrix-analytics-events
+# Set up the js-sdk first (unless package.json pins a specific version)
+if [ "$js_sdk_dep" = "github:matrix-org/matrix-js-sdk#develop" ]; then
+    scripts/fetchdep.sh matrix-org matrix-js-sdk develop
+    pushd matrix-js-sdk
+    [ -n "$JS_SDK_GITHUB_BASE_REF" ] && git fetch --depth 1 origin $JS_SDK_GITHUB_BASE_REF && git checkout $JS_SDK_GITHUB_BASE_REF
     pnpm link
     pnpm install --frozen-lockfile
-    pnpm build:ts
     popd
+else
+    echo "Skipping matrix-js-sdk fetch and link as package.json pins $js_sdk_dep"
+fi
+
+# Also set up matrix-analytics-events for branch with matching name
+if [ "$analytics_events_dep" = "github:matrix-org/matrix-analytics-events#develop" ]; then
+    scripts/fetchdep.sh matrix-org matrix-analytics-events
+    # We don't pass a default branch so cloning may fail when we are not in a PR
+    # This is expected as this project does not share a release cycle but we still branch match it
+    if [ -d matrix-analytics-events ]; then
+        pushd matrix-analytics-events
+        pnpm link
+        pnpm install --frozen-lockfile
+        pnpm build:ts
+        popd
+    fi
+else
+    echo "Skipping matrix-analytics-events fetch and link as package.json pins $analytics_events_dep"
 fi
 
 # Link the layers into element-web
-pnpm link matrix-js-sdk
-[ -d matrix-analytics-events ] && pnpm link @matrix-org/analytics-events
+if [ "$js_sdk_dep" = "github:matrix-org/matrix-js-sdk#develop" ]; then
+    pnpm link matrix-js-sdk
+fi
+if [ "$analytics_events_dep" = "github:matrix-org/matrix-analytics-events#develop" ] && [ -d matrix-analytics-events ]; then
+    pnpm link @matrix-org/analytics-events
+fi
 pnpm install --frozen-lockfile $@
