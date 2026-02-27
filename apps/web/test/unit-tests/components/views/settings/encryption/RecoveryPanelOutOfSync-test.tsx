ae33ec11cff12abba85d390b0a116fd6946804a5
Andy Balaam <andy.balaam@matrix.org>
2026-02-27 14:23:52 +0000

    Reset key storage if restoring from Recovery encounters the wrong decryption key
    Fixes https://github.com/element-hq/element-web/issues/31793
    
    Depends on https://github.com/matrix-org/matrix-js-sdk/pull/5202
    
    When we try to load the key storage decryption key from Recovery, but we
    find that it does not match the public key of the current key storage
    backup, create a new key storage backup.

--- apps/web/test/unit-tests/components/views/settings/encryption/RecoveryPanelOutOfSync-test.tsx
+++ apps/web/test/unit-tests/components/views/settings/encryption/RecoveryPanelOutOfSync-test.tsx
@@ -11,6 +11,7 @@ import userEvent from "@testing-library/user-event";
 import { mocked } from "jest-mock";
 import { type MatrixClient } from "matrix-js-sdk/src/matrix";
 import { type SecretStorageKeyDescriptionAesV1 } from "matrix-js-sdk/src/secret-storage";
+import { DecryptionKeyDoesNotMatchError } from "matrix-js-sdk/src/crypto-api";
 
 import { RecoveryPanelOutOfSync } from "../../../../../../src/components/views/settings/encryption/RecoveryPanelOutOfSync";
 import { AccessCancelledError, accessSecretStorage } from "../../../../../../src/SecurityManager";
@@ -107,6 +108,32 @@ describe("<RecoveyPanelOutOfSync />", () => {
         expect(matrixClient.getCrypto()!.resetKeyBackup).toHaveBeenCalled();
     });
 
+    it("should reset key backup if decryption key from secret storage does not match backup", async () => {
+        jest.spyOn(DeviceListener.sharedInstance(), "keyStorageOutOfSyncNeedsBackupReset").mockResolvedValue(false);
+
+        const user = userEvent.setup();
+        mocked(accessSecretStorage).mockImplementation(async (func = async (): Promise<void> => {}) => func());
+        mocked(matrixClient.isKeyBackupKeyStored).mockResolvedValue(fakeKeyBackupKey());
+
+        // Given we will fail to load a private key because it doesn't match the
+        // latest backup public key
+        mocked(matrixClient.getCrypto()!.loadSessionBackupPrivateKeyFromSecretStorage).mockRejectedValue(
+            new DecryptionKeyDoesNotMatchError("key no matchy"),
+        );
+
+        const onFinish = jest.fn();
+        renderComponent(onFinish);
+
+        // When we enter the recovery key
+        await user.click(screen.getByRole("button", { name: "Enter recovery key" }));
+        expect(accessSecretStorage).toHaveBeenCalled();
+        expect(onFinish).toHaveBeenCalled();
+
+        // Then we reset backup after attempting to load the key
+        expect(matrixClient.getCrypto()!.loadSessionBackupPrivateKeyFromSecretStorage).toHaveBeenCalled();
+        expect(matrixClient.getCrypto()!.resetKeyBackup).toHaveBeenCalled();
+    });
+
     it("should call onAccessSecretStorageFailed on failure", async () => {
         jest.spyOn(DeviceListener.sharedInstance(), "keyStorageOutOfSyncNeedsBackupReset").mockResolvedValue(true);
 
@@ -120,6 +147,28 @@ describe("<RecoveyPanelOutOfSync />", () => {
 
         await user.click(screen.getByRole("button", { name: "Enter recovery key" }));
         expect(accessSecretStorage).toHaveBeenCalled();
+        expect(onAccessSecretStorageFailed).toHaveBeenCalled();
+    });
+
+    it("should call onAccessSecretStorageFailed when loadSessionBackupPrivateKeyFromSecretStorage fails", async () => {
+        jest.spyOn(DeviceListener.sharedInstance(), "keyStorageOutOfSyncNeedsBackupReset").mockResolvedValue(false);
+
+        const user = userEvent.setup();
+        mocked(accessSecretStorage).mockImplementation(async (func = async (): Promise<void> => {}) => func());
+
+        // Given we will fail to load a private key because of some unexpected error
+        mocked(matrixClient.getCrypto()!.loadSessionBackupPrivateKeyFromSecretStorage).mockRejectedValue(
+            new Error("Unexpected error"),
+        );
+        mocked(matrixClient.isKeyBackupKeyStored).mockResolvedValue(fakeKeyBackupKey());
+
+        const onAccessSecretStorageFailed = jest.fn();
+        renderComponent(jest.fn(), jest.fn(), onAccessSecretStorageFailed);
+
+        // When we enter the recovery key
+        await user.click(screen.getByRole("button", { name: "Enter recovery key" }));
+
+        // Then we handle the error in onAccessSecretStorageFailed
         expect(onAccessSecretStorageFailed).toHaveBeenCalled();
     });
 
