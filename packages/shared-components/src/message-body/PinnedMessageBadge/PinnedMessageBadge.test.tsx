9fa8b34ebe412d864324ad63fbe902d9d44f19f0
Zack <zazi21@student.bth.se>
2026-03-12 14:34:45 +0100

    Move PinnedMessageBadge To Shared Components (#32768)
    * refactor: move pinned message badge into shared-components
    
    * Update lint errors
    
    * Fix for Vitest issue with CI on shared components
    
    * Update related to the pr comments and requests
    
    * Prettier Fix
    
    * Update, remove view since it was just moved and not actually MVVMed
    
    * Removed view suffix since it was only moved and not mvvmed
    
    * Update snapshots
    
    * Removal of unused screenshots
    
    * Added Image that we need

---
+++ packages/shared-components/src/message-body/PinnedMessageBadge/PinnedMessageBadge.test.tsx
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2026 Element Creations Ltd.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only OR GPL-3.0-only OR LicenseRef-Element-Commercial
+ * Please see LICENSE files in the repository root for full details.
+ */
+
+import React from "react";
+import { composeStories } from "@storybook/react-vite";
+import { describe, expect, it } from "vitest";
+import { render } from "@test-utils";
+
+import * as stories from "./PinnedMessageBadge.stories";
+
+const { Default } = composeStories(stories);
+
+describe("PinnedMessageBadge", () => {
+    it("renders the default badge", () => {
+        const { container } = render(<Default />);
+        expect(container).toMatchSnapshot();
+    });
+});
