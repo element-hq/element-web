ace7aad0203ae3b7d620084d31d7a3618cbfdd01
David Baker <dbkr@users.noreply.github.com>
2026-03-13 12:34:32 +0000

    Fix copyright
    Co-authored-by: Florian Duros <florianduros@element.io>

--- apps/web/src/modules/ExtrasApi.ts
+++ apps/web/src/modules/ExtrasApi.ts
@@ -1,5 +1,6 @@
 /*
-Copyright 2025-2026 New Vector Ltd.
+Copyright 2026 Element Creations Ltd.
+Copyright 2025 New Vector Ltd.
 
 SPDX-License-Identifier: AGPL-3.0-only OR GPL-3.0-only OR LicenseRef-Element-Commercial
 Please see LICENSE files in the repository root for full details.
