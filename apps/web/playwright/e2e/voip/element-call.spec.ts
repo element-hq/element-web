ecccc601ca94e793bb2307bf6ed76a1abae5f282
Michael Telatynski <7t3chguy@gmail.com>
2026-03-26 04:39:19 +0000

    Reference files in Playwright more reliably
    Without assuming the cwd

--- apps/web/playwright/e2e/voip/element-call.spec.ts
+++ apps/web/playwright/e2e/voip/element-call.spec.ts
@@ -15,13 +15,14 @@ import { test, expect } from "../../element-web-test";
 import type { Credentials } from "../../plugins/homeserver";
 import { Bot } from "../../pages/bot";
 import { isDendrite } from "../../plugins/homeserver/dendrite";
+import { readSampleFile } from "../../sample-files";
 
 // Load a copy of our fake Element Call app, and the latest widget API.
 // The fake call app does *just* enough to convince Element Web that a call is ongoing
 // and functions like PiP work. It does not actually do anything though, to limit the
 // surface we test.
 const widgetApi = readFile(fileURLToPath(import.meta.resolve("matrix-widget-api/dist/api.min.js")), "utf-8");
-const fakeCallClient = readFile("playwright/sample-files/fake-element-call.html", "utf-8");
+const fakeCallClient = readSampleFile("fake-element-call.html");
 
 function assertCommonCallParameters(
     url: URLSearchParams,
@@ -609,7 +610,7 @@ test.describe("Element Call", () => {
             },
         });
 
-        const fakeCallClientSend = readFile("playwright/sample-files/fake-element-call-with-send.html", "utf-8");
+        const fakeCallClientSend = readSampleFile("fake-element-call-with-send.html");
 
         let charlie: Bot;
         test.use({
