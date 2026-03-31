0f515f581e4b23713c710304c5c2cd6a49ccc52e
Florian Duros <florianduros@element.io>
2026-03-31 20:43:32 +0200

    Room list: add default sections (#32785)
    * feat: add sections to RLSV3
    
    * feat: add sections in vms
    
    * feat: add room list section labs flag
    
    * fix: wrong margin for room list item when in sections
    
    * feat: hide favourites and low priority filters
    
    * fix: crash when changing filter
    
    * feat: support sticky room in sections
    
    * test: update SC snapshot
    
    * test: update SC screenshot
    
    * test: update RLS tests
    
    * test: add tests to RoomListSectionHeaderViewModel
    
    * test: fix existing test in RoomListViewModel
    
    * test: add sections tests for RoomListViewModel
    
    * test: add e2e tests for sections
    
    * fix: incorrect selected room when expanding/collasping a section
    
    * fix: typo in `roomSkipList`
    
    * feat: use one skip list with all filters instead of one list by tag
    
    * chore: put back comment about `roomIndexInSection`
    
    * chore: add missing `readonly`
    
    * chore: add doc about possible undefined value for room item vm

---
+++ apps/web/src/viewmodels/room-list/RoomListSectionHeaderViewModel.ts
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2026 Element Creations Ltd.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only OR GPL-3.0-only OR LicenseRef-Element-Commercial
+ * Please see LICENSE files in the repository root for full details.
+ */
+
+import {
+    BaseViewModel,
+    type RoomListSectionHeaderActions,
+    type RoomListSectionHeaderViewSnapshot,
+} from "@element-hq/web-shared-components";
+
+interface RoomListSectionHeaderViewModelProps {
+    tag: string;
+    title: string;
+    onToggleExpanded: (isExpanded: boolean) => void;
+}
+
+export class RoomListSectionHeaderViewModel
+    extends BaseViewModel<RoomListSectionHeaderViewSnapshot, RoomListSectionHeaderViewModelProps>
+    implements RoomListSectionHeaderActions
+{
+    public constructor(props: RoomListSectionHeaderViewModelProps) {
+        super(props, { id: props.tag, title: props.title, isExpanded: true });
+    }
+
+    public onClick = (): void => {
+        const isExpanded = !this.snapshot.current.isExpanded;
+        this.snapshot.merge({ isExpanded });
+        this.props.onToggleExpanded(isExpanded);
+    };
+
+    /**
+     * Whether the section is currently expanded or not.
+     */
+    public get isExpanded(): boolean {
+        return this.snapshot.current.isExpanded;
+    }
+}
