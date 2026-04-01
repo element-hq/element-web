03902d2453755fa3bea43a053cf9ea7164e24e57
Andy Balaam <andy.balaam@matrix.org>
2026-04-01 14:56:59 +0100

    Close verify toast in room-list-sections test

--- apps/web/playwright/e2e/left-panel/room-list-panel/room-list-sections.spec.ts
+++ apps/web/playwright/e2e/left-panel/room-list-panel/room-list-sections.spec.ts
@@ -45,7 +45,8 @@ test.describe("Room list sections", () => {
     }
 
     test.beforeEach(async ({ page, app, user }) => {
-        // The notification toast is displayed above the search section
+        // The toasts are displayed above the search section
+        await app.closeVerifyToast();
         await app.closeNotificationToast();
 
         // focus the user menu to avoid to have hover decoration
