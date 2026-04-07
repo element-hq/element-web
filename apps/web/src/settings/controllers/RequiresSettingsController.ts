2d7d3dfcef0cfd866ff821fd7247ef2c48354fb7
Half-Shot <will@half-shot.uk>
2026-04-07 10:33:51 +0100

    Add a test

--- apps/web/src/settings/controllers/RequiresSettingsController.ts
+++ apps/web/src/settings/controllers/RequiresSettingsController.ts
@@ -6,7 +6,6 @@ Please see LICENSE files in the repository root for full details.
 */
 
 import SettingController from "./SettingController";
-import { type SettingLevel } from "../SettingLevel";
 import SettingsStore from "../SettingsStore";
 import type { BooleanSettingKey } from "../Settings.tsx";
 
@@ -21,12 +20,7 @@ export default class RequiresSettingsController extends SettingController {
         super();
     }
 
-    public getValueOverride(
-        _level: SettingLevel,
-        _roomId: string,
-        _calculatedValue: any,
-        _calculatedAtLevel: SettingLevel | null,
-    ): any {
+    public getValueOverride(): any {
         if (this.settingDisabled) {
             // per the docs: we force a disabled state when the feature isn't active
             return this.forcedValue;
