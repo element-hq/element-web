1c73e336ea50cf9fa8020a2e21a8f574b7511b2e
R Midhun Suresh <hi@midhun.dev>
2026-04-07 06:16:44 +0530

    Export the view from shared-components package

--- packages/shared-components/src/index.ts
+++ packages/shared-components/src/index.ts
@@ -27,6 +27,7 @@ export * from "./room/timeline/TimelineSeparator";
 export * from "./room/timeline/event-tile/actions/ActionBarView";
 export * from "./room/timeline/event-tile/EventTileView/DisambiguatedProfile";
 export * from "./room/timeline/event-tile/EventTileView/EncryptionEventView";
+export * from "./room/timeline/event-tile/call";
 export * from "./room/timeline/event-tile/EventTileView/EventTileBubble";
 export * from "./room/timeline/event-tile/EventTileView/PinnedMessageBadge";
 export * from "./room/timeline/event-tile/EventTileView/TextualEventView";
