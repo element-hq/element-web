aeabecb09dabe9bc5e3f7c4f80e13d81b238e8fc
Richard van der Hoff <richard@matrix.org>
2026-04-10 12:07:40 +0100

    Fix playwright-server docker image not exiting
    ... by wrapping with tini

--- packages/playwright-common/docker-entrypoint.sh
+++ packages/playwright-common/docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # We use npm here as we used `npm i -g` to install playwright in the Dockerfile
-npm exec -- playwright run-server --port "$PORT" --host 0.0.0.0
+exec npm exec -- /usr/bin/playwright run-server --port "$PORT" --host 0.0.0.0
