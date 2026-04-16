d7f55462940798de139b427a5a9759b4f9f28e78
Florian Duros <florianduros@element.io>
2026-04-16 15:14:10 +0200

    Room list: add custom section creation in SC (#33138)
    * feat: add way to create custom section in SC
    
    * feat: disable custom section in vms
    
    * chore: include design tokens icons in vitest config
    
    * test: update screenshot
    
    * chore: use one react import

--- packages/shared-components/src/room-list/RoomListView/RoomListToast/index.ts
+++ packages/shared-components/src/room-list/RoomListView/RoomListToast/index.ts
@@ -1,13 +1,9 @@
 /*
- * Copyright 2025 Element Creations Ltd.
+ * Copyright 2026 Element Creations Ltd.
  *
  * SPDX-License-Identifier: AGPL-3.0-only OR GPL-3.0-only OR LicenseRef-Element-Commercial
  * Please see LICENSE files in the repository root for full details.
  */
 
-/**
- * Room list container styles
- */
-.roomList {
-    width: 100%;
-}
+export type { ToastType } from "./RoomListToast";
+export { RoomListToast } from "./RoomListToast";
