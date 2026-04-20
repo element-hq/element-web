713de83f3090cfb5d6ba0bd11f1ff9574bdbad2b
Michael Telatynski <7t3chguy@gmail.com>
2026-04-20 16:13:28 +0100

    Wire up element-desktop playwright tests via nx

--- apps/desktop/playwright/docker-entrypoint.sh
+++ apps/desktop/playwright/docker-entrypoint.sh
@@ -8,11 +8,5 @@ sleep 2
 
 export DISPLAY=:99
 
-pnpm install --frozen-lockfile
-pnpm build -l --dir
-
-PLAYWRIGHT_HTML_OPEN=never ELEMENT_DESKTOP_EXECUTABLE="./dist/linux-unpacked/element-desktop" \
-  npx playwright test --update-snapshots --reporter line,html "$1"
-
-# Clean up
-rm -R core qemu_* || exit 0
+PLAYWRIGHT_HTML_OPEN=never CI=1 ELEMENT_DESKTOP_EXECUTABLE="./dist/linux-unpacked/element-desktop" \
+  exec pnpm -C apps/desktop exec playwright test --update-snapshots --reporter line,html "$1"
