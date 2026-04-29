d820bc5bad7dcd653a3d9bb18adf8d288ae72708
Michael Telatynski <7t3chguy@gmail.com>
2026-04-29 13:50:31 +0100

    DRY vite configs using a shared config

--- apps/desktop/vitest.config.ts
+++ apps/desktop/vitest.config.ts
@@ -5,60 +5,20 @@ SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
 Please see LICENSE files in the repository root for full details.
 */
 
-import { defineConfig } from "vitest/config";
-import { type UserConfig } from "vite";
-import { type Reporter } from "vitest/reporters";
-import { env } from "node:process";
+import { defineConfig, mergeConfig } from "vitest/config";
 
-const reporters: NonNullable<UserConfig["test"]>["reporters"] = [["default"]];
+import baseConfig from "../../vite.shared.js";
 
-const slowTestReporter: Reporter = {
-    onTestRunEnd(testModules, unhandledErrors, reason) {
-        const tests = testModules
-            .flatMap((m) => Array.from(m.children.allTests()))
-            .filter((test) => test.diagnostic()?.slow);
-        tests.sort((x, y) => x.diagnostic()!.duration! - y.diagnostic()!.duration!);
-        tests.reverse();
-
-        if (tests.length > 0) {
-            console.warn("Slowest 10 tests:");
-        }
-        for (const t of tests.slice(0, 10)) {
-            console.warn(`${t.module.moduleId} > ${t.fullName}: ${t.diagnostic()?.duration.toFixed(0)}ms`);
-        }
-    },
-};
-
-// if we're running under GHA, enable the GHA & Sonar reporters
-if (env["GITHUB_ACTIONS"] !== undefined) {
-    reporters.push(["github-actions", { silent: false }]);
-    reporters.push([
-        "vitest-sonar-reporter",
-        {
-            outputFile: "coverage/sonar-report.xml",
-            onWritePath: (path): string => `apps/desktop/${path}`,
+export default mergeConfig(
+    baseConfig,
+    defineConfig({
+        test: {
+            coverage: {
+                // The coverage report currently chokes on this file as it doesn't process it as TypeScript
+                exclude: ["src/preload.cts"],
+            },
+            include: ["src/**/*.test.ts"],
         },
-    ]);
-
-    // if we're running against the develop branch, also enable the slow test reporter
-    if (env["GITHUB_REF"] == "refs/heads/develop") {
-        reporters.push(slowTestReporter);
-    }
-}
-
-export default defineConfig({
-    test: {
-        coverage: {
-            provider: "v8",
-            include: ["src/**/*"],
-            // The coverage report currently chokes on this file as it doesn't process it as TypeScript
-            exclude: ["src/preload.cts"],
-            reporter: [["lcov", { projectRoot: "../../" }]],
-        },
-        environment: "node",
-        reporters,
-        globals: true,
-        pool: "threads",
-        include: ["src/**/*.test.ts"],
-    },
-});
+    }),
+    true,
+);
--- packages/module-api/vite.config.ts
+++ packages/module-api/vite.config.ts
@@ -7,57 +7,43 @@ Please see LICENSE files in the repository root for full details.
 
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
-import { defineConfig } from "vite";
+import { defineConfig, mergeConfig } from "vitest/config";
 import dts from "unplugin-dts/vite";
 import externalGlobals from "rollup-plugin-external-globals";
 
 import packageJson from "./package.json" with { type: "json" };
+import baseConfig from "../../vite.shared.js";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-export default defineConfig({
-    build: {
-        lib: {
-            entry: resolve(__dirname, "src/index.ts"),
-            name: "element-web-plugin-engine",
-            fileName: "element-web-plugin-engine",
+export default mergeConfig(
+    baseConfig,
+    defineConfig({
+        build: {
+            lib: {
+                entry: resolve(__dirname, "src/index.ts"),
+                name: "element-web-plugin-engine",
+                fileName: "element-web-plugin-engine",
+            },
+            outDir: "lib",
+            target: "esnext",
+            sourcemap: true,
         },
-        outDir: "lib",
-        target: "esnext",
-        sourcemap: true,
-    },
-    plugins: [
-        dts(),
-        externalGlobals({
-            // Reuse React from the host app
-            react: "window.React",
-        }),
-    ],
-    define: {
-        // We cannot use `process.env.npm_package_version` as when building element-web with module-api set to `workspace`
-        // this would contain the version of element-web rather than that of the module-api.
-        __VERSION__: JSON.stringify(packageJson.version),
-        // Use production mode for the build as it is tested against production builds of Element Web,
-        // this is required for React JSX versions to be compatible.
-        process: { env: { NODE_ENV: "production" } },
-    },
-    test: {
-        coverage: {
-            provider: "v8",
-            include: ["src/**/*"],
-            reporter: [["lcov", { projectRoot: "../../" }]],
-        },
-        reporters: [
-            ["default", { summary: false }],
-            [
-                "vitest-sonar-reporter",
-                {
-                    outputFile: "coverage/sonar-report.xml",
-                    onWritePath(path: string): string {
-                        return `packages/element-web-module-api/${path}`;
-                    },
-                },
-            ],
+        plugins: [
+            dts(),
+            externalGlobals({
+                // Reuse React from the host app
+                react: "window.React",
+            }),
         ],
-    },
-});
+        define: {
+            // We cannot use `process.env.npm_package_version` as when building element-web with module-api set to `workspace`
+            // this would contain the version of element-web rather than that of the module-api.
+            __VERSION__: JSON.stringify(packageJson.version),
+            // Use production mode for the build as it is tested against production builds of Element Web,
+            // this is required for React JSX versions to be compatible.
+            process: { env: { NODE_ENV: "production" } },
+        },
+    }),
+    true,
+);
