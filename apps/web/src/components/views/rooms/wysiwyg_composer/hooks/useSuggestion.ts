1ecd64cff1d376e2c1a4356bb7674a589110f1d2
Will Hunt <2072976+Half-Shot@users.noreply.github.com>
2026-05-01 17:26:42 +0100

    Update useSuggestion.ts

--- apps/web/src/components/views/rooms/wysiwyg_composer/hooks/useSuggestion.ts
+++ apps/web/src/components/views/rooms/wysiwyg_composer/hooks/useSuggestion.ts
@@ -8,8 +8,7 @@ Please see LICENSE files in the repository root for full details.
 
 import { EMOTICON_TO_EMOJI } from "@matrix-org/emojibase-bindings";
 import { type AllowedMentionAttributes, type MappedSuggestion } from "@vector-im/matrix-wysiwyg";
-import { type SyntheticEvent, useState, type SetStateAction } from "react";
-import { logger } from "matrix-js-sdk/src/logger";
+import { type SyntheticEvent, useState } from "react";
 
 import { isNotNull } from "../../../../../Typeguards";
 
