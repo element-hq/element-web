d8d016552e48b1048c8757e557c4acc66850341a
Half-Shot <will@half-shot.uk>
2026-05-12 12:53:06 +0100

    Rewrite Measured to be a functional component

--- apps/web/src/components/views/elements/Measured.tsx
+++ apps/web/src/components/views/elements/Measured.tsx
@@ -1,4 +1,5 @@
 /*
+Copyright (C) 2026 Element Creations Ltd
 Copyright 2024 New Vector Ltd.
 Copyright 2022 The Matrix.org Foundation C.I.C.
 
@@ -6,57 +7,46 @@ SPDX-License-Identifier: AGPL-3.0-only OR GPL-3.0-only OR LicenseRef-Element-Com
 Please see LICENSE files in the repository root for full details.
 */
 
-import React, { type RefObject } from "react";
+import React, { useCallback, useEffect, useRef, type RefObject } from "react";
 
 import UIStore, { UI_EVENTS } from "../../../stores/UIStore";
 
 interface IProps {
     sensor: RefObject<Element | null>;
     breakpoint: number;
-    onMeasurement(narrow: boolean): void;
+    onMeasurement: (narrow: boolean) => void;
 }
 
-export default class Measured extends React.PureComponent<IProps> {
-    private static instanceCount = 0;
-    private readonly instanceId: number;
+let instanceCount = 0;
 
-    public static defaultProps = {
-        breakpoint: 500,
-    };
+export default function Measured({ sensor, breakpoint = 500, onMeasurement }: IProps): null {
+    const instanceIdRef = useRef(instanceCount++);
+    const instanceId = instanceIdRef.current;
 
-    public constructor(props: IProps) {
-        super(props);
+    const onResize = useCallback(
+        (type: UI_EVENTS, entry: ResizeObserverEntry): void => {
+            if (type !== UI_EVENTS.Resize) return;
+            onMeasurement(entry.contentRect.width <= breakpoint);
+        },
+        [onMeasurement, breakpoint],
+    );
 
-        this.instanceId = Measured.instanceCount++;
-    }
+    useEffect(() => {
+        UIStore.instance.on(`Measured${instanceId}`, onResize);
+        return () => {
+            UIStore.instance.off(`Measured${instanceId}`, onResize);
+        };
+    }, [instanceId, onResize]);
 
-    public componentDidMount(): void {
-        UIStore.instance.on(`Measured${this.instanceId}`, this.onResize);
-    }
-
-    public componentDidUpdate(prevProps: Readonly<IProps>): void {
-        const previous = prevProps.sensor.current;
-        const current = this.props.sensor.current;
-        if (previous === current) return;
-        if (previous) {
-            UIStore.instance.stopTrackingElementDimensions(`Measured${this.instanceId}`);
+    useEffect(() => {
+        if (sensor.current) {
+            UIStore.instance.trackElementDimensions(`Measured${instanceId}`, sensor.current);
         }
-        if (current) {
-            UIStore.instance.trackElementDimensions(`Measured${this.instanceId}`, current);
-        }
-    }
 
-    public componentWillUnmount(): void {
-        UIStore.instance.off(`Measured${this.instanceId}`, this.onResize);
-        UIStore.instance.stopTrackingElementDimensions(`Measured${this.instanceId}`);
-    }
+        return () => {
+            UIStore.instance.stopTrackingElementDimensions(`Measured${instanceId}`);
+        };
+    }, [sensor, instanceId]);
 
-    private onResize = (type: UI_EVENTS, entry: ResizeObserverEntry): void => {
-        if (type !== UI_EVENTS.Resize) return;
-        this.props.onMeasurement(entry.contentRect.width <= this.props.breakpoint);
-    };
-
-    public render(): React.ReactNode {
-        return null;
-    }
+    return null;
 }
