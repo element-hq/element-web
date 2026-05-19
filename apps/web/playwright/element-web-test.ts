f96d30cf169ab7482ddf0e1b5fd819e833010d02
rbondesson <rickard.bondesson@cgi.com>
2026-05-19 14:39:52 +0200

    Experimental color matching threshold for playwright

--- apps/web/playwright/element-web-test.ts
+++ apps/web/playwright/element-web-test.ts
@@ -190,7 +190,7 @@ export const expect = baseExpect.extend<Expectations>({
         await baseExpect(receiver).toMatchScreenshot(name, {
             ...options,
             css,
-            threshold: 0.0,
+            threshold: 0.1,
         });
 
         return { pass: true, message: () => "", name: "toMatchScreenshot" };
