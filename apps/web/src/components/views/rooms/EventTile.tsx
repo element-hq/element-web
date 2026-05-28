34c388f760248b921973e3e403f061dbab6c50a1
Zack <zazi21@student.bth.se>
2026-05-28 10:01:39 +0200

    Refactor E2eMessageSharedIcon to MVVM (#33544)
    * Refactor E2eMessageSharedIcon to MVVM
    
    * FIx Prettier and i18n
    
    * Fix prettier
    
    * Remove legacy E2eMessageSharedIcon component
    
    * Cover E2eMessageSharedIcon MVVM updates
    
    * Fix prettier
    
    * Update apps/web/src/components/views/rooms/EventTile.tsx
    
    Co-authored-by: Florian Duros <florian.duros@ormaz.fr>
    
    * Skip unknown E2E shared icon visual
    
    * Track E2E shared icon listener disposal
    
    * Remove E2E shared icon room state guard
    
    * Remove E2E shared icon client resync
    
    * Use built-in disposables for shared icon VM
    
    * Update shared icon VM without React key
    
    ---------
    
    Co-authored-by: Florian Duros <florian.duros@ormaz.fr>

--- apps/web/src/components/views/rooms/EventTile.tsx
+++ apps/web/src/components/views/rooms/EventTile.tsx
@@ -41,6 +41,7 @@ import { CircleIcon, CheckCircleIcon, ThreadsIcon } from "@vector-im/compound-de
 import {
     useCreateAutoDisposedViewModel,
     ActionBarView,
+    E2eMessageSharedIconView,
     MessageTimestampView,
     PinnedMessageBadge,
     ReactionsRowButtonView,
@@ -88,10 +89,10 @@ import { Icon as LateIcon } from "../../../../res/img/sensor.svg";
 import PinningUtils from "../../../utils/PinningUtils";
 import { EventPreview } from "./EventPreview";
 import { E2eStandardPadlockIcon } from "./EventTile/E2eStandardPadlockIcon";
-import { E2eMessageSharedIcon } from "./EventTile/E2eMessageSharedIcon";
 import SettingsStore from "../../../settings/SettingsStore";
 import { CardContext } from "../right_panel/context";
 import { EventTileViewModel } from "../../../viewmodels/room/timeline/event-tile/EventTileViewModel";
+import { E2eMessageSharedIconViewModel } from "../../../viewmodels/room/timeline/event-tile/E2eMessageSharedIconViewModel";
 import {
     getEventTileReceiptState,
     type EventTileReceiptState,
@@ -290,6 +291,50 @@ interface IState {
     isQuoteExpanded?: boolean;
 
     thread: Thread | null;
+}
+
+interface E2eMessageSharedIconWrapperProps {
+    /**
+     * The ID of the room containing the event whose keys were shared.
+     */
+    roomId: string;
+    /**
+     * The ID of the user who shared the keys.
+     */
+    keyForwardingUserId: string;
+}
+
+function E2eMessageSharedIconWrapper({
+    roomId,
+    keyForwardingUserId,
+}: Readonly<E2eMessageSharedIconWrapperProps>): JSX.Element {
+    const client = useMatrixClientContext();
+    const vm = useCreateAutoDisposedViewModel(
+        () =>
+            new E2eMessageSharedIconViewModel({
+                client,
+                roomId,
+                keyForwardingUserId,
+            }),
+    );
+
+    useEffect(() => {
+        vm.setRoomId(roomId);
+    }, [roomId, vm]);
+
+    useEffect(() => {
+        vm.setKeyForwardingUserId(keyForwardingUserId);
+    }, [keyForwardingUserId, vm]);
+
+    return (
+        <E2eMessageSharedIconView
+            vm={vm}
+            className={
+                // Timeline PCSS uses this app class as a layout hook for positioning and layout variants.
+                "mx_EventTile_e2eIcon"
+            }
+        />
+    );
 }
 
 // MUST be rendered within a RoomContext with a set timelineRenderingType
@@ -685,7 +730,7 @@ export class UnwrappedEventTile extends React.Component<EventTileProps, IState> 
                 return null;
             case "messageShared":
                 return (
-                    <E2eMessageSharedIcon
+                    <E2eMessageSharedIconWrapper
                         keyForwardingUserId={e2ePadlockViewState.keyForwardingUserId}
                         roomId={e2ePadlockViewState.roomId}
                     />
