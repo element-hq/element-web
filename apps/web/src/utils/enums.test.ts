6c89dc7291501c47f2367111faa16e3f1d0d9517
Michael Telatynski <7t3chguy@gmail.com>
2026-06-11 11:22:49 +0100

    Use vitest for some EW unit tests

--- apps/web/src/utils/enums.test.ts
+++ apps/web/src/utils/enums.test.ts
@@ -6,7 +6,9 @@ SPDX-License-Identifier: AGPL-3.0-only OR GPL-3.0-only OR LicenseRef-Element-Com
 Please see LICENSE files in the repository root for full details.
 */
 
-import { getEnumValues, isEnumValue } from "../../../src/utils/enums";
+import { describe, it, expect } from "vitest";
+
+import { getEnumValues, isEnumValue } from "./enums";
 
 enum TestStringEnum {
     First = "__first__",
--- apps/web/src/utils/objects.test.ts
+++ apps/web/src/utils/objects.test.ts
@@ -6,6 +6,8 @@ SPDX-License-Identifier: AGPL-3.0-only OR GPL-3.0-only OR LicenseRef-Element-Com
 Please see LICENSE files in the repository root for full details.
 */
 
+import { describe, it, expect } from "vitest";
+
 import {
     objectClone,
     objectDiff,
@@ -14,7 +16,7 @@ import {
     objectKeyChanges,
     objectShallowClone,
     objectWithOnly,
-} from "../../../src/utils/objects";
+} from "./objects";
 
 describe("objects", () => {
     describe("objectExcluding", () => {
