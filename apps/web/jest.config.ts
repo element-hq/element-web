6e5e4684393ddc9950b692c227b721fd0879b3aa
Michael Telatynski <7t3chguy@gmail.com>
2026-06-12 09:05:11 +0100

    Attempt to deflake jest tests

--- apps/web/jest.config.ts
+++ apps/web/jest.config.ts
@@ -52,7 +52,6 @@ const config: Config = {
     ],
     collectCoverageFrom: [
         "<rootDir>/src/**/*.{js,ts,tsx}",
-        "<rootDir>/packages/**/*.{js,ts,tsx}",
         // getSessionLock is piped into a different JS context via stringification, and the coverage functionality is
         // not available in that contest. So, turn off coverage instrumentation for it.
         "!<rootDir>/src/utils/SessionLock.ts",
@@ -62,6 +61,7 @@ const config: Config = {
     coverageReporters: ["text-summary", ["lcov", { projectRoot: "../../" }]],
     prettierPath: null,
     moduleDirectories: ["node_modules", "test/test-utils"],
+    workerIdleMemoryLimit: "512MB",
 };
 
 // if we're running under GHA, enable relevant reporters
