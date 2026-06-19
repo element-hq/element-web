a375ec4affe6f7f006cdf8a486f1ac75931d5940
Michael Telatynski <7t3chguy@gmail.com>
2026-06-19 09:09:03 +0100

    Fix jest config

--- apps/web/jest.config.ts
+++ apps/web/jest.config.ts
@@ -45,6 +45,7 @@ const config: Config = {
         ".*\\?raw": "jest-raw-loader",
         "recorderWorkletFactory": "<rootDir>/__mocks__/empty.js",
         "@vector-im/compound-web": "<rootDir>/node_modules/@vector-im/compound-web",
+        "^vitest$": "<rootDir>/__mocks__/empty.js",
     },
     transformIgnorePatterns: [
         `${path.join(__dirname, "../..")}/node_modules/.pnpm/(?!(mime|uuid|p-retry|is-network-error|react-merge-refs|is-ip|ip-regex|super-regex|function-timeout|time-span|convert-hrtime|clone-regexp|is-regexp|matrix-web-i18n|await-lock|@element-hq/web-shared-components|react-virtuoso|lodash|domutils|domhandler|domelementtype|dom-serializer|entities)).+$`,
