4fb369171590285afb319b64538549546e294ca0
David Baker <dbkr@users.noreply.github.com>
2026-06-23 18:08:30 +0100

    Add the view model

---
+++ apps/web/src/viewmodels/status/SetStatusViewModel.ts
@@ -0,0 +1,78 @@
+/*
+Copyright 2026 Element Creations Ltd.
+
+SPDX-License-Identifier: AGPL-3.0-only OR GPL-3.0-only OR LicenseRef-Element-Commercial
+Please see LICENSE files in the repository root for full details.
+*/
+
+import { type MatrixClient } from "matrix-js-sdk/src/matrix";
+import { logger } from "matrix-js-sdk/src/logger";
+import {
+    BaseViewModel,
+    type SetStatusViewSnapshot,
+    type SetStatusViewActions,
+    type UserStatus,
+} from "@element-hq/web-shared-components";
+
+import { clearUserStatus, setUserStatus } from "../../utils/userStatus";
+import { OwnProfileStore } from "../../stores/OwnProfileStore";
+import dis from "../../dispatcher/dispatcher";
+import { UserTab } from "../../components/views/dialogs/UserTab";
+import { Action } from "../../dispatcher/actions";
+
+export interface SetStatusViewModelProps {
+    /**
+     * The Matrix client instance.
+     */
+    client: MatrixClient;
+}
+
+export class SetStatusViewModel
+    extends BaseViewModel<SetStatusViewSnapshot, SetStatusViewModelProps>
+    implements SetStatusViewActions
+{
+    public constructor(props: SetStatusViewModelProps) {
+        super(props, {
+            userStatus: OwnProfileStore.instance.userStatus,
+        });
+    }
+
+    public setStatus = (userStatus: UserStatus): void => {
+        const oldStatus = this.snapshot.current.userStatus;
+
+        this.snapshot.merge({ userStatus });
+        setUserStatus(this.props.client, userStatus).catch((err) => {
+            this.snapshot.merge({ userStatus: oldStatus });
+            logger.warn("Failed to set user status", err);
+        });
+    };
+
+    public clearStatus = (): void => {
+        const oldStatus = this.snapshot.current.userStatus;
+
+        this.snapshot.merge({ userStatus: undefined });
+        clearUserStatus(this.props.client).catch((err) => {
+            this.snapshot.merge({ userStatus: oldStatus });
+            logger.warn("Failed to clear user status", err);
+        });
+    };
+}
+
+/**
+ * A version of the view model that overrides the click handler to open settings instead.
+ */
+export class UserMenuSetStatusViewModel extends SetStatusViewModel {
+    public constructor(props: SetStatusViewModelProps) {
+        super(props);
+    }
+
+    public onSetStatusClick = (): void => {
+        dis.dispatch({
+            action: Action.ToggleUserMenu,
+        });
+        dis.dispatch({
+            action: Action.ViewUserSettings,
+            initialTabId: UserTab.Account,
+        });
+    };
+}
