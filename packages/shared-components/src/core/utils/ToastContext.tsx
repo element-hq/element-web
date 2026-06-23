da8e81f980758e8fc1ec06b276b4b02b4aa66cbb
Half-Shot <will@half-shot.uk>
2026-06-23 15:36:57 +0100

    lint

--- packages/shared-components/src/core/utils/ToastContext.tsx
+++ packages/shared-components/src/core/utils/ToastContext.tsx
@@ -1,10 +1,10 @@
 /*
-* Copyright 2024 New Vector Ltd.
-* Copyright 2024 The Matrix.org Foundation C.I.C.
-*
-* SPDX-License-Identifier: AGPL-3.0-only OR GPL-3.0-only OR LicenseRef-Element-Commercial
-* Please see LICENSE files in the repository root for full details.
-*/
+ * Copyright 2024 New Vector Ltd.
+ * Copyright 2024 The Matrix.org Foundation C.I.C.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only OR GPL-3.0-only OR LicenseRef-Element-Commercial
+ * Please see LICENSE files in the repository root for full details.
+ */
 
 import { type ReactNode, createContext, useCallback, useContext, useEffect, useState, useMemo } from "react";
 
