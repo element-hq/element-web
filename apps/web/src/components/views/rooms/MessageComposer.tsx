13c147de8b6be7d28afdb1409891d6d59850fb94
Half-Shot <will@half-shot.uk>
2026-06-26 13:11:21 +0100

    hide another one

--- apps/web/src/components/views/rooms/MessageComposer.tsx
+++ apps/web/src/components/views/rooms/MessageComposer.tsx
@@ -152,8 +152,7 @@ export class MessageComposer extends React.Component<IProps, IState> {
             showPollsButton: SettingsStore.getValue("MessageComposerInput.showPollsButton"),
             isWysiwygLabEnabled: isWysiwygLabEnabled,
             isRichTextEnabled: isRichTextEnabled,
-            initialComposerContent: initialComposerContent,
-            hidePreviewsInSentEvent: false,
+            initialComposerContent: initialComposerContent
         };
 
         this.instanceId = instanceCount++;
