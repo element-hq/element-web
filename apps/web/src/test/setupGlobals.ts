4c2ec8bb5c5e52a287a96fd27a362923baa120c0
David Baker <dbkr@users.noreply.github.com>
2026-06-29 15:59:06 +0100

    thank you!
    Co-authored-by: Michael Telatynski <7t3chguy@gmail.com>

--- apps/web/src/test/setupGlobals.ts
+++ apps/web/src/test/setupGlobals.ts
@@ -16,9 +16,9 @@ vi.stubGlobal("AudioContext", function () {
 
 if (globalThis.window === undefined) {
     // We are in a node environment, stub a basic window so singletons work
-    vi.stubGlobal("window", {});
-
-    // Mock this as some code assumes it exists (needs to be done at the top level as
-    // things try to access it before the beforeEach blocks run)
-    window.addEventListener = vi.fn();
+    vi.stubGlobal("window", {
+        // Mock this as some code assumes it exists (needs to be done at the top level as
+        // things try to access it before the beforeEach blocks run)
+        addEventListener: vi.fn(),
+    });
 }
