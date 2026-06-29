c2717f31da323268948d71ed0ea8091aad0bc041
David Baker <dbkr@users.noreply.github.com>
2026-06-29 15:39:49 +0100

    Mock out window.addEventListener globally
    As some code uses it

--- apps/web/src/test/setupTests.ts
+++ apps/web/src/test/setupTests.ts
@@ -5,7 +5,7 @@ SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
 Please see LICENSE files in the repository root for full details.
 */
 
-import { beforeEach, afterEach } from "vitest";
+import { beforeEach, afterEach, vi } from "vitest";
 import fetchMock, { manageFetchMockGlobally } from "@fetch-mock/vitest";
 
 import SdkConfig, { DEFAULTS } from "../SdkConfig";
@@ -13,6 +13,10 @@ import "./setupGlobals.ts";
 import { setupLanguageMock } from "./setupLanguage.ts";
 
 manageFetchMockGlobally();
+
+// Mock this as some code assumes it exists (needs to be done at the top level as
+// things try to access it before the beforeEach blocks run)
+window.addEventListener = vi.fn();
 
 beforeEach(() => {
     // set up fetch API mock
