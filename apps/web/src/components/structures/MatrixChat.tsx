c1981b314eb74482c9c8928a95f388d11ad39bb5
Florian Duros <florian.duros@ormaz.fr>
2026-06-29 18:37:22 +0200

    feat(room-list)!: remove the legacy RoomListStore
    The legacy sublist-based room list UI is gone, so the old
    `stores/room-list` store (Algorithm, sorters, filters, layout store,
    space
    watcher) has no remaining consumers. Delete the directory and its tests.
    
    MatrixChat.forgetRoom no longer calls the legacy `manualRoomUpdate`; the
    new room list store removes the room on the `AfterForgetRoom` dispatch
    that still fires. Drop the `mxRoomListStore`/`mxRoomListLayoutStore`
    globals and the now-dead test imports.

--- apps/web/src/components/structures/MatrixChat.tsx
+++ apps/web/src/components/structures/MatrixChat.tsx
@@ -75,8 +75,6 @@ import { UIFeature } from "../../settings/UIFeature";
 import DialPadModal from "../views/voip/DialPadModal";
 import { showToast as showMobileGuideToast } from "../../toasts/MobileGuideToast";
 import { shouldUseLoginForWelcome } from "../../utils/pages";
-import RoomListStore from "../../stores/room-list/RoomListStore";
-import { RoomUpdateCause } from "../../stores/room-list/models";
 import { ModuleRunner } from "../../modules/ModuleRunner";
 import Spinner from "../views/elements/Spinner";
 import QuestionDialog from "../views/dialogs/QuestionDialog";
@@ -1363,9 +1361,7 @@ export default class MatrixChat extends React.PureComponent<IProps, IState> {
                 }
 
                 if (room) {
-                    // Legacy room list store needs to be told to manually remove this room
-                    RoomListStore.instance.manualRoomUpdate(room, RoomUpdateCause.RoomRemoved);
-                    // New room list store will remove the room on the following dispatch
+                    // The room list store will remove the room on the following dispatch
                     dis.dispatch<AfterForgetRoomPayload>({ action: Action.AfterForgetRoom, room });
                 }
             })
