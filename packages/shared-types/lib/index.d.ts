8a55b02cd7ccb6189ae097ea77d46a4ddac083f3
Michael Telatynski <7t3chguy@gmail.com>
2026-06-30 16:26:08 +0100

    Start consolidating shared types in the monorepo

---
+++ packages/shared-types/lib/index.d.ts
@@ -0,0 +1,9 @@
+/*
+Copyright 2026 Element Creations Ltd.
+
+SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
+Please see LICENSE files in the repository root for full details.
+*/
+
+export type * from "./config.json.d.ts";
+export type * from "./utils.d.ts";
