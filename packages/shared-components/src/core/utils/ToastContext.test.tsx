857267c0549028cdfb138d472535ebb406028680
Half-Shot <will@half-shot.uk>
2026-06-30 10:37:42 +0100

    remove import

--- packages/shared-components/src/core/utils/ToastContext.test.tsx
+++ packages/shared-components/src/core/utils/ToastContext.test.tsx
@@ -7,7 +7,7 @@ Please see LICENSE files in the repository root for full details.
 */
 
 import { describe, it, expect, vitest } from "vitest";
-import { render, waitFor } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import { Toast } from "@vector-im/compound-web";
 import React, { type JSX } from "react";
 
