bc9d82b4ee1eec71aa67090459a3df5ad07d06bb
Michael Telatynski <7t3chguy@gmail.com>
2026-07-01 12:34:26 +0100

    Iterate

--- apps/web/babel.config.cjs
+++ apps/web/babel.config.cjs
@@ -13,7 +13,7 @@ module.exports = {
                 include: ["@babel/plugin-transform-class-properties"],
             },
         ],
-        ["@babel/preset-typescript", { allowDeclareFields: true }],
+        ["@babel/preset-typescript"],
         "@babel/preset-react",
     ],
     plugins: [
@@ -29,7 +29,6 @@ module.exports = {
         // something downstream of babel, at least) fall over.
         "@babel/plugin-transform-logical-assignment-operators",
 
-        "@babel/plugin-syntax-dynamic-import",
         "@babel/plugin-transform-runtime",
         ["@babel/plugin-proposal-decorators", { version: "2023-11" }], // only needed by the js-sdk
         "@babel/plugin-transform-class-static-block", // only needed by the js-sdk for decorators
