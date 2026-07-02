51e6965adf1bbae4b29de2fcb2f733c32e407943
David Baker <dbkr@users.noreply.github.com>
2026-07-02 15:33:32 +0100

    Remove fake indexeddb
    as it seems like the tests didn't actually see the fake indexeddb before
    (somehow) but now do, and are failing because they have that but no postmessage.
    It feels like the right solution is for these tests to not need indexeddb.

--- apps/web/test/unit-tests/components/structures/MatrixChat-test.tsx
+++ apps/web/test/unit-tests/components/structures/MatrixChat-test.tsx
@@ -6,7 +6,6 @@ SPDX-License-Identifier: AGPL-3.0-only OR GPL-3.0-only OR LicenseRef-Element-Com
 Please see LICENSE files in the repository root for full details.
 */
 
-import "fake-indexeddb/auto";
 import React, { type ComponentProps, createRef, type RefObject } from "react";
 import { fireEvent, render, type RenderResult, screen, waitFor, within, act } from "jest-matrix-react";
 import { type Mocked, mocked } from "jest-mock-vitest-adapter";
