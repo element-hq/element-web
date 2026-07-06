c6770898fd888f21549c217e6bd2faf2ded9a3ee
Michael Telatynski <7t3chguy@gmail.com>
2026-07-06 18:16:03 +0100

    Fix bad merge

--- apps/web/src/contexts/SDKContextClass.ts
+++ apps/web/src/contexts/SDKContextClass.ts
@@ -87,16 +87,6 @@ export class SDKContextClass {
         }
     };
 
-    public constructor() {
-        defaultDispatcher.register(this.onDispatch);
-    }
-
-    private onDispatch = (payload: ActionPayload): void => {
-        if (isAction<OnLoggedInPayload>(payload, Action.OnLoggedIn)) {
-            this._client = payload.client;
-        }
-    };
-
     /**
      * Automatically construct stores which need to be created eagerly so they can register with
      * the dispatcher.
