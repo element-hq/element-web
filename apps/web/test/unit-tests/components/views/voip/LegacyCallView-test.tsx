30a953b5a87ca7a4e65854d83c59bccc2a0f681a
Michael Telatynski <7t3chguy@gmail.com>
2026-07-07 08:53:20 +0100

    Improve coverage

--- apps/web/test/unit-tests/components/views/voip/LegacyCallView-test.tsx
+++ apps/web/test/unit-tests/components/views/voip/LegacyCallView-test.tsx
@@ -6,13 +6,13 @@ Please see LICENSE files in the repository root for full details.
 */
 
 import React from "react";
-import { render } from "jest-matrix-react";
+import { render, fireEvent } from "jest-matrix-react";
 import { type MatrixCall } from "matrix-js-sdk/src/matrix";
 import { type CallFeed } from "matrix-js-sdk/src/webrtc/callFeed";
 import { SDPStreamMetadataPurpose } from "matrix-js-sdk/src/webrtc/callEventTypes";
 
 import LegacyCallView from "../../../../../src/components/views/voip/LegacyCallView";
-import { clientAndSDKContextRenderOptions, stubClient } from "../../../../test-utils";
+import { clientAndSDKContextRenderOptions, createTestClient, stubClient } from "../../../../test-utils";
 import DMRoomMap from "../../../../../src/utils/DMRoomMap";
 import { TestSDKContext } from "../../../TestSDKContext.ts";
 
@@ -114,5 +114,97 @@ describe("LegacyCallView", () => {
             clientAndSDKContextRenderOptions(cli, sdkContext),
         );
         expect(container.querySelector(".mx_LegacyCallViewButtons_button_sidebar")).toBeFalsy();
+    });
+
+    it("should allow user to resume held call", async () => {
+        const client = createTestClient();
+        const sdkContext = new TestSDKContext();
+        sdkContext._client = client;
+
+        const call = {
+            roomId: "test-room",
+            on: jest.fn(),
+            removeListener: jest.fn(),
+            getFeeds: jest.fn().mockReturnValue(
+                [{ local: true }, { local: false }, { local: true, screenshare: true }].map(
+                    (x, i) =>
+                        ({
+                            stream: { id: "test-" + i },
+                            addListener: jest.fn(),
+                            removeListener: jest.fn(),
+                            getMember: jest.fn(),
+                            isAudioMuted: jest.fn().mockReturnValue(true),
+                            isVideoMuted: jest.fn().mockReturnValue(true),
+                            isLocal: jest.fn().mockReturnValue(x.local),
+                            purpose: x.screenshare && SDPStreamMetadataPurpose.Screenshare,
+                        }) as unknown as CallFeed,
+                ),
+            ),
+            isLocalOnHold: jest.fn().mockReturnValue(false),
+            isRemoteOnHold: jest.fn().mockReturnValue(true),
+            isMicrophoneMuted: jest.fn().mockReturnValue(true),
+            isLocalVideoMuted: jest.fn().mockReturnValue(true),
+            isScreensharing: jest.fn().mockReturnValue(true),
+            noIncomingFeeds: jest.fn().mockReturnValue(false),
+            opponentSupportsSDPStreamMetadata: jest.fn().mockReturnValue(true),
+            getOpponentMember: jest.fn(),
+        } as unknown as MatrixCall;
+
+        jest.spyOn(sdkContext.legacyCallHandler, "roomIdForCall").mockReturnValue(call.roomId);
+        jest.spyOn(sdkContext.legacyCallHandler, "setActiveCallRoomId");
+
+        const { getByText } = render(
+            <LegacyCallView call={call} sidebarShown />,
+            clientAndSDKContextRenderOptions(client, sdkContext),
+        );
+        fireEvent.click(getByText("Resume"));
+
+        expect(sdkContext.legacyCallHandler.setActiveCallRoomId).toHaveBeenCalledWith(call.roomId);
+    });
+
+    it("should allow user to hangup call", async () => {
+        const client = createTestClient();
+        const sdkContext = new TestSDKContext();
+        sdkContext._client = client;
+
+        const call = {
+            roomId: "test-room",
+            on: jest.fn(),
+            removeListener: jest.fn(),
+            getFeeds: jest.fn().mockReturnValue(
+                [{ local: true }, { local: false }, { local: true, screenshare: true }].map(
+                    (x, i) =>
+                        ({
+                            stream: { id: "test-" + i },
+                            addListener: jest.fn(),
+                            removeListener: jest.fn(),
+                            getMember: jest.fn(),
+                            isAudioMuted: jest.fn().mockReturnValue(true),
+                            isVideoMuted: jest.fn().mockReturnValue(true),
+                            isLocal: jest.fn().mockReturnValue(x.local),
+                            purpose: x.screenshare && SDPStreamMetadataPurpose.Screenshare,
+                        }) as unknown as CallFeed,
+                ),
+            ),
+            isLocalOnHold: jest.fn().mockReturnValue(false),
+            isRemoteOnHold: jest.fn().mockReturnValue(false),
+            isMicrophoneMuted: jest.fn().mockReturnValue(true),
+            isLocalVideoMuted: jest.fn().mockReturnValue(true),
+            isScreensharing: jest.fn().mockReturnValue(true),
+            noIncomingFeeds: jest.fn().mockReturnValue(false),
+            opponentSupportsSDPStreamMetadata: jest.fn().mockReturnValue(true),
+            getOpponentMember: jest.fn(),
+        } as unknown as MatrixCall;
+
+        jest.spyOn(sdkContext.legacyCallHandler, "roomIdForCall").mockReturnValue(call.roomId);
+        jest.spyOn(sdkContext.legacyCallHandler, "hangupOrReject");
+
+        const { getByLabelText } = render(
+            <LegacyCallView call={call} sidebarShown />,
+            clientAndSDKContextRenderOptions(client, sdkContext),
+        );
+        fireEvent.click(getByLabelText("Hangup"));
+
+        expect(sdkContext.legacyCallHandler.hangupOrReject).toHaveBeenCalledWith(call.roomId);
     });
 });
