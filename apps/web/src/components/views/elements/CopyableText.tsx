17d10019a982b5d3e890066eca44f48a553ed2d3
David Baker <dbkr@users.noreply.github.com>
2026-07-10 15:22:25 +0100

    snapshots & add aria-label

--- apps/web/src/components/views/elements/CopyableText.tsx
+++ apps/web/src/components/views/elements/CopyableText.tsx
@@ -44,6 +44,9 @@ export const CopyTextButton: React.FC<Pick<IProps, "getTextToCopy" | "className"
 
     return (
         <IconButton
+            // aria-label as otherwise there's no accessible name when the tooltip
+            // is not visible
+            aria-label={tooltip ?? _t("action|copy")}
             tooltip={tooltip ?? _t("action|copy")}
             onClick={onCopyClickInternal}
             className={className}
