4cf1061476bc2738ed98f7f264c0cdf2699827af
Michael Telatynski <7t3chguy@gmail.com>
2026-07-13 10:08:39 +0100

    Test by augmenting the dehydration test which previously failed using the `user` fixture

--- apps/web/playwright/e2e/crypto/dehydration-mas.spec.ts
+++ apps/web/playwright/e2e/crypto/dehydration-mas.spec.ts
@@ -14,7 +14,7 @@ import {
     logOutOfElement,
     verifyAfterLogin,
 } from "./utils.ts";
-import { registerAccountMas } from "../oidc";
+import { logInAccountMas } from "../oidc";
 import { Bot } from "../../pages/bot.ts";
 
 test.use({
@@ -27,19 +27,12 @@ test.use({
 });
 
 test.describe("Device dehydration, on a MAS-enabled homeserver", () => {
-    test("Can read messages sent while logged out", async ({ mailpitClient, homeserver, page, app }, testInfo) => {
+    test("Can read messages sent while logged out", async ({ homeserver, page, app, user }) => {
         test.slow();
-        const aliceUserId = `alice_${testInfo.testId}`;
-        const alicePassword = "Pa$sW0rD!";
 
-        const recoveryKey =
-            await test.step("Alice registers and sets up recovery => a dehydrated device is created", async () => {
-                await page.goto("/#/login");
-                await page.getByRole("button", { name: "Continue" }).click();
-
-                await registerAccountMas(page, mailpitClient, aliceUserId, `${aliceUserId}@email.com`, alicePassword);
-                return await enableKeyBackup(app);
-            });
+        const recoveryKey = await test.step("Alice sets up recovery => a dehydrated device is created", async () => {
+            return await enableKeyBackup(app);
+        });
 
         const [bob, testRoomId] = await test.step("Bob registers and joins a room with Alice", async () => {
             const bob = new Bot(page, homeserver, { displayName: "Bob" });
@@ -65,8 +58,7 @@ test.describe("Device dehydration, on a MAS-enabled homeserver", () => {
             await page.getByRole("link", { name: "Sign in" }).click();
             await page.getByRole("button", { name: "Continue" }).click();
 
-            await expect(page.getByText("Continue to Element?")).toBeVisible();
-            await page.getByRole("button", { name: "Continue" }).click();
+            await logInAccountMas(page, user.username, user.password);
 
             await verifyAfterLogin(page, recoveryKey);
             await app.viewRoomById(testRoomId);
