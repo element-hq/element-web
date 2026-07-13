fd82c63ba52673a8895984aaa63d8e8d8366ced1
Michael Telatynski <7t3chguy@gmail.com>
2026-07-13 14:21:32 +0100

    Add knip exception

--- knip.ts
+++ knip.ts
@@ -106,6 +106,8 @@ export default {
     ignoreDependencies: [
         // Used by multiple packages, raises a false positive for some reason
         "events",
+        // Used as a workaround for api-extractor not supporting typescript 7.0
+        "@typescript/old",
     ],
     ignoreExportsUsedInFile: true,
     ignoreBinaries: [
