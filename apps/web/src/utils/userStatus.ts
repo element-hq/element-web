57d5c7ddaecb0be53503d0edcafa4d034b7cb2d0
David Baker <dbkr@users.noreply.github.com>
2026-07-15 17:48:21 +0100

    Support for reading m.call status
    (well, the prefixed version)

--- apps/web/src/utils/userStatus.ts
+++ apps/web/src/utils/userStatus.ts
@@ -5,13 +5,22 @@ SPDX-License-Identifier: AGPL-3.0-only OR GPL-3.0-only OR LicenseRef-Element-Com
 Please see LICENSE files in the repository root for full details.
 */
 
-import { type UserStatus } from "@element-hq/web-shared-components";
+import { _td, type UserStatus } from "@element-hq/web-shared-components";
 import { type MatrixClient, MatrixError } from "matrix-js-sdk/src/matrix";
 import { logger } from "matrix-js-sdk/src/logger";
+
+import { _t } from "../languageHandler";
 
 // MSC4426 defines the maximum length of a status to be 256 bytes of UTF-8,
 // so we truncate anything longer than that.
 const MAX_STATUS_TEXT_BYTES = 256;
+
+// We don't use the actual UserStatus type here as we want to translate the string at runtime,
+// so we can make the types reflect the fact it's not ready for human consumption.
+const ON_A_CALL_STATUS = {
+    emoji: "📞",
+    textKey: _td("user_status|on_a_call"),
+};
 
 // Static Intl.Segmenter for grabbing the first grapheme of a user status emoji.
 // We make one and keep it for performance.
@@ -51,8 +60,22 @@ export function validateUserStatus(rawUserStatus: unknown): UserStatus | undefin
 }
 
 /**
- * Fetch the MSC4426 user status of the given user. Returns undefined if the server does not
- * support extended profiles, the user has no (valid) status, or the status could not be fetched.
+ * Takes the raw result from getExtendedProfileProperty for m.call, validates it and
+ * returns true if the user is currently on a call, false otherwise.
+ * @param rawCallStatus
+ */
+function isOnCall(rawCallStatus: unknown): boolean {
+    if (!rawCallStatus || typeof rawCallStatus !== "object") return false;
+    if (!("call_joined_ts" in rawCallStatus) || typeof rawCallStatus.call_joined_ts !== "number") return false;
+    if (rawCallStatus.call_joined_ts > 0) return true;
+
+    return false;
+}
+
+/**
+ * Fetch the MSC4426 user status of the given user, taking into account m.call if present.
+ * Returns undefined if the server does not support extended profiles, the user has no
+ * (valid) status, or the status could not be fetched.
  *
  * @param client The Matrix client to fetch the status with.
  * @param userId The ID of the user whose status is being fetched.
@@ -61,14 +84,31 @@ export async function fetchUserStatus(client: MatrixClient, userId: string): Pro
     if ((await client.doesServerSupportExtendedProfiles()) === false) {
         return undefined;
     }
+
+    let status: UserStatus | undefined;
+
     try {
-        return validateUserStatus(await client.getExtendedProfileProperty(userId, "org.matrix.msc4426.status"));
+        status = validateUserStatus(await client.getExtendedProfileProperty(userId, "org.matrix.msc4426.status"));
     } catch (ex) {
         if (!(ex instanceof MatrixError && ex.errcode === "M_NOT_FOUND")) {
             logger.warn(`Failed to get userStatus for ${userId}`, ex);
         }
         return undefined;
     }
+
+    if (status) return status;
+
+    try {
+        const result = await client.getExtendedProfileProperty(userId, "org.matrix.msc4426.call");
+        if (isOnCall(result)) return { emoji: ON_A_CALL_STATUS.emoji, text: _t(ON_A_CALL_STATUS.textKey) };
+    } catch (ex) {
+        if (ex instanceof MatrixError && ex.errcode === "M_NOT_FOUND") {
+        } else {
+            logger.warn(`Failed to get call status for ${userId}`, ex);
+        }
+    }
+
+    return undefined;
 }
 
 /**
