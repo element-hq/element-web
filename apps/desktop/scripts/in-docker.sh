element-hq/element-web

commit 7408796362964cc836795875c7b0d785a86a1681

Fix element desktop dockerbuild utilities (#33641)
Got broken by monorepo merge

apps/desktop/scripts/in-docker.sh
@@ -21,9 +21,10 @@ docker run --rm -ti \
  --env-file <(env | grep -E '^INDOCKER_' | sed -e 's/^INDOCKER_//') \
  --env ELECTRON_CACHE="/root/.cache/electron" \
  --env ELECTRON_BUILDER_CACHE="/root/.cache/electron-builder" \
- -v ${PWD}:/project \
- -v ${PWD}/docker/node_modules:/project/node_modules \
- -v ${PWD}/docker/.hak:/project/.hak \
+ -v ${PWD}/../../:/project \
+ -v ${PWD}/docker/workspace_node_modules:/project/node_modules \
+ -v ${PWD}/docker/node_modules:/project/apps/desktop/node_modules \
+ -v ${PWD}/docker/.hak:/project/apps/desktop/.hak \
  -v ${PWD}/docker/.gnupg:/root/.gnupg \
  -v ~/.cache/electron:/root/.cache/electron \
  -v ~/.cache/electron-builder:/root/.cache/electron-builder \

apps/desktop/dockerbuild/Dockerfile
@@ -16,8 +16,9 @@ RUN ln -s /usr/bin/python3 /usr/bin/python & ln -s /usr/bin/pip3 /usr/bin/pip
 
 ENV DEBUG_COLORS=true
 ENV FORCE_COLOR=true
+ENV COREPACK_ENABLE_DOWNLOAD_PROMPT=0
 
-WORKDIR /project
+WORKDIR /project/apps/desktop
 
 ARG TARGETOS
 ARG TARGETARCH

commit 0a904aa41ed76cb6307cda4385e45ca02ca9d5ed

Fix desktop docker build script
It will fail with 'workspace_node_modules: no such file or directory' if the
directory does not exist.

This got broken by https://github.com/element-hq/element-web/pull/33641.

apps/desktop/scripts/in-docker.sh
@@ -12,7 +12,11 @@ if [ $? != 0 ]; then
     exit 1
 fi
 
-mkdir -p docker/node_modules docker/.hak docker/.gnupg
+mkdir -p \
+      docker/workspace_node_modules \
+      docker/node_modules \
+      docker/.hak \
+      docker/.gnupg
 
 # Taken from https://www.electron.build/multi-platform-build#docker
 # Pass through any vars prefixed with INDOCKER_, removing the prefix