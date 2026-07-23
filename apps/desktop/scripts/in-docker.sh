element-hq/element-web

commit 7408796362964cc836795875c7b0d785a86a1681

Fix element desktop dockerbuild utilities (#33641)
Got broken by monorepo merge

apps/desktop/scripts/in-docker.sh
@@ -21,9 +21,10 @@ docker run --rm -ti \
  --env-file <(env | grep -E '^INDOCKER_' | sed -e 's/^INDOCKER_//') \
  --env ELECTRON_CACHE="/root/.cache/electron" \
  --env ELECTRON_BUILDER_CACHE="/root/.cache/electron-builder" \
- -v ${PWD}:/project \
- -v ${PWD}/docker/node_modules:/project/node_modules \
- -v ${PWD}/docker/.hak:/project/.hak \
+ -v ${PWD}/../../:/project \
+ -v ${PWD}/docker/workspace_node_modules:/project/node_modules \
+ -v ${PWD}/docker/node_modules:/project/apps/desktop/node_modules \
+ -v ${PWD}/docker/.hak:/project/apps/desktop/.hak \
  -v ${PWD}/docker/.gnupg:/root/.gnupg \
  -v ~/.cache/electron:/root/.cache/electron \
  -v ~/.cache/electron-builder:/root/.cache/electron-builder \

apps/desktop/dockerbuild/Dockerfile
@@ -16,8 +16,9 @@ RUN ln -s /usr/bin/python3 /usr/bin/python & ln -s /usr/bin/pip3 /usr/bin/pip
 
 ENV DEBUG_COLORS=true
 ENV FORCE_COLOR=true
+ENV COREPACK_ENABLE_DOWNLOAD_PROMPT=0
 
-WORKDIR /project
+WORKDIR /project/apps/desktop
 
 ARG TARGETOS
 ARG TARGETARCH

commit 8dc1965d150c6d1490874ac3635638f8b8dc0c64

Fix docs and build script for desktop build (#34395)
* Fix some documentation for element-desktop

Some of these links got broken, or outdated, during the monorepo switch.

* Fix desktop docker build script

It will fail with 'workspace_node_modules: no such file or directory' if the
directory does not exist.

This got broken by https://github.com/element-hq/element-web/pull/33641.

* Clarify native modules doc

Most of this stuff does not apply when using the docker build.

* update snapshot

apps/desktop/scripts/in-docker.sh
@@ -12,7 +12,11 @@ if [ $? != 0 ]; then
     exit 1
 fi
 
-mkdir -p docker/node_modules docker/.hak docker/.gnupg
+mkdir -p \
+      docker/workspace_node_modules \
+      docker/node_modules \
+      docker/.hak \
+      docker/.gnupg
 
 # Taken from https://www.electron.build/multi-platform-build#docker
 # Pass through any vars prefixed with INDOCKER_, removing the prefix

apps/web/src/components/views/settings/EventIndexPanel.tsx
@@ -165,7 +165,7 @@ export default class EventIndexPanel extends React.Component<EmptyObject, IState
             );
         } else if (EventIndexPeg.platformHasSupport() && !EventIndexPeg.supportIsInstalled()) {
             const nativeLink =
-                "https://github.com/element-hq/element-web/blob/develop/apps/desktop/" +
+                "https://github.com/element-hq/element-web/blob/develop/" +
                 "docs/native-node-modules.md#" +
                 "adding-seshat-for-search-in-e2e-encrypted-rooms";
 

apps/web/test/unit-tests/components/views/settings/__snapshots__/EventIndexPanel-test.tsx.snap
@@ -62,7 +62,7 @@ exports[`<EventIndexPanel /> when event indexing is supported but not installed 
       Element is missing some components required for securely caching encrypted messages locally. If you'd like to experiment with this feature, build a custom Element Desktop with 
       <a
         class="mx_ExternalLink"
-        href="https://github.com/element-hq/element-web/blob/develop/apps/desktop/docs/native-node-modules.md#adding-seshat-for-search-in-e2e-encrypted-rooms"
+        href="https://github.com/element-hq/element-web/blob/develop/docs/native-node-modules.md#adding-seshat-for-search-in-e2e-encrypted-rooms"
         rel="noreferrer noopener"
         target="_blank"
       >