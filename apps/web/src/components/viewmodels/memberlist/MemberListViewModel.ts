60532f94db18f97d20c5acbb4d9efdd4e403fa2e
rizzler <chinmayikr0252@gmail.com>
2026-07-27 22:13:59 +0530

    Update MemberListViewModel.ts

--- apps/web/src/components/viewmodels/memberlist/MemberListViewModel.ts
+++ apps/web/src/components/viewmodels/memberlist/MemberListViewModel.ts
@@ -167,7 +167,7 @@ export function useMemberListViewModel(roomId: string): MemberListViewState {
 
                     // Finally add the third party invites
                     for (const invited of threePidInvited) {
-                        const key = invited.threePidInvite!.event.getContent().display_name;
+                        const key = `threePidInvite-${invited.threePidInvite!.event.getStateKey() || invited.threePidInvite!.event.getId()}`;
                         newMemberMap.set(key, invited);
                     }
 
