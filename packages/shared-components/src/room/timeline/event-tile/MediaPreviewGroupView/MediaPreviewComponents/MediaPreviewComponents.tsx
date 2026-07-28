9c977ed85e51fa01b4da51d8bb0bf403006024db
Siriusmart <sirius@siri.ws>
2026-07-28 15:22:34 +0100

    collapsed media preview component

---
+++ packages/shared-components/src/room/timeline/event-tile/MediaPreviewGroupView/MediaPreviewComponents/MediaPreviewComponents.tsx
@@ -0,0 +1,63 @@
+import React, { CSSProperties, JSX } from "react";
+import styles from "./MediaPreviewComponents.module.css";
+import classNames from "classnames";
+import { MediaPreviewEntryButton } from "../MediaPreviewGroupView";
+
+export function Header({ header, headerUrl }: { header: string, headerUrl?: string }): JSX.Element {
+    if (headerUrl === undefined) return <div className={classNames(styles.textHeader, styles.header)}>{header}</div>;
+    else
+        return (
+            <div className={classNames(styles.linkHeader, styles.header)}>
+                <a href={headerUrl} target="_blank">
+                    {header}
+                </a>
+            </div>
+        );
+}
+
+export function Body({ body }: { body: string }): JSX.Element {
+    return <div className={styles.body}>{body}</div>;
+}
+
+export function Footer({ footer }: { footer: string }): JSX.Element {
+    return <div className={styles.footer}>{footer}</div>;
+}
+
+export function TextContent(props: {
+    header: string;
+    headerUrl?: string;
+    body: string;
+    footer?: string;
+}): JSX.Element {
+    return <div className={styles.textContent}>
+        <Header {...props} />
+        <Body {...props} />
+        {props.footer !== undefined && <Footer footer={props.footer} />}
+    </div>
+}
+
+export function Icon({ icon, iconOnClick, iconBg, iconFg, iconBorder }: { icon: JSX.Element, iconOnClick?: () => void, iconBg: string, iconFg: string, iconBorder: string }) {
+    const style: CSSProperties = {
+        border: `1px solid ${iconBorder}`,
+        backgroundColor: iconBg,
+    };
+
+    icon = React.cloneElement(icon, { style: { color: iconFg } });
+
+
+    if (iconOnClick) {
+        return <div className={classNames(styles.icon, styles.iconClickable)} style={style}><button onClick={iconOnClick}>{icon}</button></div>
+    } else {
+        return <div className={styles.icon} style={style}>{icon}</div>
+    }
+}
+
+export function Buttons({ buttons }: { buttons: Array<MediaPreviewEntryButton> }): JSX.Element {
+    return <div className={styles.buttonGroup}>
+        {buttons.map(({ icon, onClick }) => <button className={styles.button} onClick={onClick}>{icon}</button>)}
+    </div>;
+}
+
+export function LeftGroup({ children }: { children: React.ReactNode }): JSX.Element {
+    return <div className={styles.leftGroup}>{children}</div>;
+}
