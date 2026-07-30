26031b17a1f996d9f2fbc9bd514ae1e7718d42f7
Half-Shot <will@half-shot.uk>
2026-07-30 08:51:49 +0100

    fix typo again

--- apps/web/src/modules/Api.ts
+++ apps/web/src/modules/Api.ts
@@ -34,7 +34,7 @@ import { WidgetLifecycleApi } from "./WidgetLifecycleApi.ts";
 import { WidgetApi } from "./WidgetApi.ts";
 import { CustomisationsApi } from "./customisationsApi.ts";
 import { ComposerApi } from "./ComposerApi.ts";
-import { StorageHelperApi } from "./StorageHeleprApi.ts";
+import { StorageHelperApi } from "./StorageHelperApi.ts";
 import { SettingsApi } from "./SettingsApi.ts";
 import defaultDispatcher from "../dispatcher/dispatcher.ts";
 
