0fd76ae9aab14e855153d2c1fe230eb95e1c0ddc
Michael Telatynski <7t3chguy@gmail.com>
2026-08-04 09:06:09 +0100

    Investigate Element Desktop tests

--- apps/desktop/playwright.config.ts
+++ apps/desktop/playwright.config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
     outputDir: "playwright/test-results",
     workers: 1,
     retries: process.env.CI ? 2 : 0,
+    maxFailures: process.env.CI ? 2 : undefined,
     reporter: process.env.CI ? [["blob"], ["github"]] : [["html", { outputFolder: "playwright/html-report" }]],
     snapshotDir: "playwright/snapshots",
     snapshotPathTemplate: "{snapshotDir}/{testFilePath}/{arg}-{platform}{ext}",
