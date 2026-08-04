015fdb77570ef858565e27e938582b95fba4baee
Siriusmart <sirius@siri.ws>
2026-08-04 14:00:32 +0100

    removed sandboxing

--- apps/web/src/components/views/right_panel/PdfViewerCard.tsx
+++ apps/web/src/components/views/right_panel/PdfViewerCard.tsx
@@ -68,7 +68,7 @@ export function PdfViewerCard({ eventId, onClose, room }: Props): JSX.Element | 
                     <Spinner />
                 </div>
             );
-        else card = <iframe src={url} title={_t("right_panel|pdf_viewer|title")} sandbox=""></iframe>;
+        else card = <iframe src={url} title={_t("right_panel|pdf_viewer|title")}></iframe>;
     } else {
         card = (
             <div className="mx_PdfViewerCard_Error">
