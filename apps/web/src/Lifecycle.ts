6ca83985c08d944b202aa84afe814d36faea6ef1
Richard van der Hoff <richard@matrix.org>
2026-08-12 16:30:31 +0100

    fixup! fixup! fixup! Update packages/module-api/src/api/client-creation-management.ts

--- apps/web/src/Lifecycle.ts
+++ apps/web/src/Lifecycle.ts
@@ -845,7 +845,8 @@ async function doSetLoggedIn(
     dis.dispatch<OnLoggedInPayload>({ action: Action.OnLoggedIn, client }, true);
 
     const clientPegOpts: MatrixClientPegAssignOpts = {
-        userVerificationCaCertsPem: ModuleApi.instance.client.creationManagement.userVerificationCaCertsPem ?? undefined,
+        userVerificationCaCertsPem:
+            ModuleApi.instance.client.creationManagement.userVerificationCaCertsPem ?? undefined,
     };
 
     if (credentials.pickleKey) {
