555b75f6cd2aaf38f5dd6e91ffd3861b9327e177
Siriusmart <sirius@siri.ws>
2026-08-12 10:46:04 +0100

    cleaning up PR for review

--- packages/shared-components/src/room/timeline/event-tile/MediaPreviewGroupView/MediaPreviewComponents/MediaPreviewComponents.tsx
+++ packages/shared-components/src/room/timeline/event-tile/MediaPreviewGroupView/MediaPreviewComponents/MediaPreviewComponents.tsx
@@ -63,19 +63,26 @@ export function LeftGroup({ children }: { children: React.ReactNode }): JSX.Elem
     return <div className={styles.leftGroup}>{children}</div>;
 }
 
-interface ValidityState { valid: boolean, src: string };
+interface ValidityState {
+    valid: boolean;
+    src: string;
+}
 
 function useIsValid(check: (src: string) => Promise<boolean>, src: string): ValidityState {
-    let [state, setState]: [ValidityState, React.Dispatch<React.SetStateAction<ValidityState>>] = useState({ valid: false, src } as ValidityState);
+    let [state, setState]: [ValidityState, React.Dispatch<React.SetStateAction<ValidityState>>] = useState({
+        valid: false,
+        src,
+    } as ValidityState);
 
     useEffect(() => {
         let cancelled = false;
         check(src).then((value) => {
-            if (!cancelled)
-                setState({ valid: value, src });
+            if (!cancelled) setState({ valid: value, src });
         });
 
-        return () => { cancelled = true; }
+        return () => {
+            cancelled = true;
+        };
     }, [src]);
 
     return state;
@@ -92,15 +99,19 @@ export function Image({
 }): JSX.Element | null {
     let classes = [styles.image];
 
-    let { valid, src } = useIsValid((src) => new Promise(res => {
-        const img = new window.Image();
-        img.onload = () => res(img.naturalWidth > 0 && img.naturalHeight > 0);
-        img.onerror = (e) => {
-            console.error(`Failed to display image ${src}`, e);
-            res(false)
-        };
-        img.src = src;
-    }), image);
+    let { valid, src } = useIsValid(
+        (src) =>
+            new Promise((res) => {
+                const img = new window.Image();
+                img.onload = () => res(img.naturalWidth > 0 && img.naturalHeight > 0);
+                img.onerror = (e) => {
+                    console.error(`Failed to display image ${src}`, e);
+                    res(false);
+                };
+                img.src = src;
+            }),
+        image,
+    );
 
     if (!valid || src !== image) return null;
 
@@ -141,16 +152,20 @@ export function Video({
 }): JSX.Element | null {
     let classes = [styles.video];
 
-    let { valid, src } = useIsValid((src) => new Promise(res => {
-        const vid = document.createElement("video");
-        vid.preload = "metadata";
-        vid.onloadedmetadata = () => res(vid.videoWidth > 0 && vid.videoHeight > 0);
-        vid.onerror = (e) => {
-            console.error(`Failed to display video ${src}`, e);
-            res(false)
-        };
-        vid.src = src;
-    }), video);
+    let { valid, src } = useIsValid(
+        (src) =>
+            new Promise((res) => {
+                const vid = document.createElement("video");
+                vid.preload = "metadata";
+                vid.onloadedmetadata = () => res(vid.videoWidth > 0 && vid.videoHeight > 0);
+                vid.onerror = (e) => {
+                    console.error(`Failed to display video ${src}`, e);
+                    res(false);
+                };
+                vid.src = src;
+            }),
+        video,
+    );
 
     if (!valid || src !== video) return null;
 
@@ -181,16 +196,20 @@ export function Video({
 }
 
 export function Audio({ audio, audioOnClick }: { audio: string; audioOnClick?: () => void }): JSX.Element | null {
-    let { valid, src } = useIsValid((src) => new Promise(res => {
-        const aud = document.createElement("audio");
-        aud.preload = "metadata";
-        aud.onloadedmetadata = () => res(true);
-        aud.onerror = (e) => {
-            console.error(`Failed to display audio ${src}`, e);
-            res(false)
-        };
-        aud.src = src;
-    }), audio);
+    let { valid, src } = useIsValid(
+        (src) =>
+            new Promise((res) => {
+                const aud = document.createElement("audio");
+                aud.preload = "metadata";
+                aud.onloadedmetadata = () => res(true);
+                aud.onerror = (e) => {
+                    console.error(`Failed to display audio ${src}`, e);
+                    res(false);
+                };
+                aud.src = src;
+            }),
+        audio,
+    );
 
     if (!valid || src !== audio) return null;
 
