9fb57f3f0a13ffd3ddd612390aaf9832d2afac14
rbondesson <rickard.bondesson@cgi.com>
2026-08-17 12:51:05 +0200

    Move styling to shared and clean up unused application overrides

--- packages/shared-components/src/room/timeline/event-tile/EventTileView/EventTileView.stories.tsx
+++ packages/shared-components/src/room/timeline/event-tile/EventTileView/EventTileView.stories.tsx
@@ -160,15 +160,13 @@ const StorySender = ({
 
 const createStorySender = (
     isOwnEvent: boolean,
-    layout: EventLayout,
     showSenderAndAvatar: boolean,
 ): React.ReactElement | undefined => {
     if (!showSenderAndAvatar) return undefined;
 
     const name = isOwnEvent ? "Alice" : "Bob";
     const id = isOwnEvent ? "@alice:example.org" : "@bob:example.org";
-    const className = layout === "irc" ? styles.ircSender : undefined;
-    return <StorySender name={name} id={id} className={className} />;
+    return <StorySender name={name} id={id} />;
 };
 
 const createStoryAvatar = (
@@ -673,7 +671,6 @@ function EventTileViewStoryContent({
         const showSenderAndAvatar = layout === "irc" || !tileState.continuation;
         const sender = createStorySender(
             isOwnEvent,
-            layout,
             showSenderAndAvatar && !tileState.noSender && !tileState.info,
         );
         const avatar = createStoryAvatar(isOwnEvent, layout, showSenderAndAvatar, tileState.info ? "14px" : undefined);
