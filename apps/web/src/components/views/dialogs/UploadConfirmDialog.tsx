c6cbc4bfa2cbd3537c15f0be628c42f86d98530d
Siriusmart <sirius@siri.ws>
2026-08-18 12:20:49 +0100

    updated snapshots

--- apps/web/src/components/views/dialogs/UploadConfirmDialog.tsx
+++ apps/web/src/components/views/dialogs/UploadConfirmDialog.tsx
@@ -115,7 +115,7 @@ export default class UploadConfirmDialog extends React.Component<IProps, IState>
         }
 
         const mimeType = this.props.file.type;
-        let previewContent = UploadConfirmDialog.computePreviewContent({ objectUrl: this.state.objectUrl!, mimeType });
+        const previewContent = UploadConfirmDialog.computePreviewContent({ objectUrl: this.state.objectUrl!, mimeType });
 
         const preview: MediaPreviewGroupEntry = {
             id: this.props.file.name,
