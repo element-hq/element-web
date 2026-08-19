ee0030d5c20c190dd62ced5f68130349176e9f4b
Florian Duros <florian.duros@ormaz.fr>
2026-08-19 14:57:11 +0200

    Avoid to read Settings at every matches of UnreadFilter

--- apps/web/src/stores/room-list-v3/skip-list/filters/UnreadFilter.ts
+++ apps/web/src/stores/room-list-v3/skip-list/filters/UnreadFilter.ts
@@ -8,18 +8,22 @@ import type { Room } from "matrix-js-sdk/src/matrix";
 import { type Filter, FilterEnum } from ".";
 import { RoomNotificationStateStore } from "../../../notifications/RoomNotificationStateStore";
 import { getMarkedUnreadState } from "../../../../utils/notifications";
-import SettingsStore from "../../../../settings/SettingsStore";
 
 export class UnreadFilter implements Filter {
+    /**
+     * Creates a new UnreadFilter.
+     * @param activityIsUnread - If true, the filter will match rooms with any activity (including notifications). If false, it will only match rooms with unread messages.
+     */
+    public constructor(private readonly activityIsUnread: boolean) {}
+
     public matches(room: Room): boolean {
         // If the user marked this room as unread, it's unread
         if (getMarkedUnreadState(room)) {
             return true;
         }
 
-        const activityIsUnread = SettingsStore.getValue("Notifications.activityIsUnread");
         const notifState = RoomNotificationStateStore.instance.getRoomState(room);
-        return activityIsUnread ? notifState.hasAnyNotificationOrActivity : notifState.hasUnreadCount;
+        return this.activityIsUnread ? notifState.hasAnyNotificationOrActivity : notifState.hasUnreadCount;
     }
 
     public get key(): FilterEnum.UnreadFilter {
