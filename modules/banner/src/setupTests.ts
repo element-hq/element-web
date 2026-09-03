d7fd0b9d0fb96307b33936a77505ba4e0761c06e
Michael Telatynski <7t3chguy@gmail.com>
2026-09-03 10:03:26 +0100

    Improve coverage

---
+++ modules/banner/src/setupTests.ts
@@ -0,0 +1,12 @@
+/*
+Copyright 2026 Element Creations Ltd.
+
+SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
+Please see LICENSE files in the repository root for full details.
+*/
+
+import "@testing-library/jest-dom/vitest";
+import { cleanup } from "@testing-library/react";
+import { afterEach } from "vitest";
+
+afterEach(cleanup);
