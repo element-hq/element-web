da496ba7f5fff97758b1c84f3a366965e4224229
Siriusmart <sirius@siri.ws>
2026-09-04 15:14:56 +0100

    moved conform dialog to be a functional component

--- apps/web/src/components/views/dialogs/UploadConfirmDialog.tsx
+++ apps/web/src/components/views/dialogs/UploadConfirmDialog.tsx
@@ -7,7 +7,7 @@ SPDX-License-Identifier: AGPL-3.0-only OR GPL-3.0-only OR LicenseRef-Element-Com
 Please see LICENSE files in the repository root for full details.
 */
 
-import React, { type JSX } from "react";
+import React, { useCallback, type JSX } from "react";
 
 import { _t } from "../../../languageHandler";
 import BaseDialog from "./BaseDialog";
@@ -18,19 +18,17 @@ import {
     type MediaPreviewGroupEntry,
     type MediaPreviewGroupEntryContent,
     MediaPreviewGroupPreview,
+    useCreateAutoDisposedViewModel,
 } from "@element-hq/web-shared-components";
 import { MediaPreviewGroupViewModel } from "../../../viewmodels/message-body/MediaPreviewGroupViewModel";
 
 interface IProps {
     file: File;
-    currentIndex: number;
-    totalFiles: number;
+    /** Defaults to 0. */
+    currentIndex?: number;
+    /** Defaults to 1. */
+    totalFiles?: number;
     onFinished: (uploadConfirmed: boolean, uploadAll?: boolean) => void;
-}
-
-interface IState {
-    objectUrl?: string;
-    mediaPreviewVm?: MediaPreviewGroupViewModel;
 }
 
 const previewableFormats = ["video", "audio", "image"];
@@ -38,136 +36,129 @@ const previewableFormats = ["video", "audio", "image"];
  * previewable formats needs an object URL to be created for the preview
  */
 function formatIsPreviewable(mimetype: string): boolean {
-    return previewableFormats.includes(mimetype.split('/')[0]);
+    return previewableFormats.includes(mimetype.split("/")[0]);
 }
 
-export default class UploadConfirmDialog extends React.Component<IProps, IState> {
-    public static defaultProps: Partial<IProps> = {
-        totalFiles: 1,
-        currentIndex: 0,
-    };
-
-    public constructor(props: IProps) {
-        super(props);
-
-        this.state = {};
-    }
-
-    public componentDidMount(): void {
-        const mimeType = this.props.file.type;
-        const previewContent = this.computePreviewContent(mimeType, this.state.objectUrl!);
-
-        const preview: MediaPreviewGroupEntry = {
-            id: this.props.file.name,
-            header: this.props.file.name,
-            body: fileSize(this.props.file.size),
-            ...attachmentIcon(mimeType),
-            ...previewContent,
+/**
+ * objectUrl should only be undefined if mimetype is text
+ */
+function computePreviewContent(mimeType: string, objectUrl?: string): MediaPreviewGroupEntryContent {
+    if (objectUrl === undefined)
+        return {
+            style: "text",
         };
 
-        const mediaPreviewVm = new MediaPreviewGroupViewModel({ entries: [preview] });
+    switch (mimeType.split("/")[0]) {
+        case "image":
+            return {
+                style: "image",
+                imageSize: "tallbanner",
+                image: objectUrl,
+            };
+        case "video":
+            return {
+                style: "video",
+                videoSize: "tallbanner",
+                video: objectUrl,
+            };
+        case "audio":
+            return {
+                style: "audio",
+                audio: objectUrl,
+            };
+        default:
+            return {
+                style: "text",
+            };
+    }
+}
 
-        if (formatIsPreviewable(this.props.file.type)) {
-            this.setState({
-                // We do not filter the mimetype using getBlobSafeMimeType here as if the user is uploading the file
-                // themselves they should be trusting it enough to open/load it, and it will be rendered into a hidden
-                // canvas for thumbnail generation anyway
-                objectUrl: URL.createObjectURL(this.props.file),
-                mediaPreviewVm,
-            });
-        } else {
-            this.setState({ mediaPreviewVm });
-        }
+/**
+ * Owns the object URL used for the preview so that its lifetime is tied to the lifetime of the
+ * view-model: it is revoked when the view-model is disposed, i.e. when the dialog unmounts.
+ */
+class UploadPreviewViewModel extends MediaPreviewGroupViewModel {
+    public constructor(file: File) {
+        const mimeType = file.type;
+        const objectUrl = formatIsPreviewable(mimeType) ? URL.createObjectURL(file) : undefined;
+
+        const preview: MediaPreviewGroupEntry = {
+            id: file.name,
+            header: file.name,
+            body: fileSize(file.size),
+            ...attachmentIcon(mimeType),
+            ...computePreviewContent(mimeType, objectUrl),
+        };
+
+        super({ entries: [preview] });
+
+        if (objectUrl !== undefined) this.disposables.track(() => URL.revokeObjectURL(objectUrl));
+    }
+}
+
+export default function UploadConfirmDialog({
+    file,
+    currentIndex = 0,
+    totalFiles = 1,
+    onFinished,
+}: IProps): JSX.Element {
+    const vm = useCreateAutoDisposedViewModel(() => new UploadPreviewViewModel(file));
+
+    let title: string;
+    if (totalFiles > 1 && currentIndex !== undefined) {
+        title = _t("upload_file|title_progress", {
+            current: currentIndex + 1,
+            total: totalFiles,
+        });
+    } else {
+        title = _t("upload_file|title");
     }
 
-    public componentWillUnmount(): void {
-        if (this.state.objectUrl) URL.revokeObjectURL(this.state.objectUrl);
-    }
+    const onCancelClick = useCallback((): void => {
+        onFinished(false);
+    }, [onFinished]);
 
-    private onCancelClick = (): void => {
-        this.props.onFinished(false);
-    };
+    const onUploadClick = useCallback((): void => {
+        onFinished(true);
+    }, [onFinished]);
 
-    private onUploadClick = (): void => {
-        this.props.onFinished(true);
-    };
+    const onUploadAllClick = useCallback((): void => {
+        onFinished(true, true);
+    }, [onFinished]);
 
-    private onUploadAllClick = (): void => {
-        this.props.onFinished(true, true);
-    };
-
-    private computePreviewContent(mimeType: string, objectUrl: string): MediaPreviewGroupEntryContent {
-        switch (mimeType.split("/")[0]) {
-            case "image":
-                return {
-                    style: "image",
-                    imageSize: "tallbanner",
-                    image: objectUrl,
-                };
-            case "video":
-                return {
-                    style: "video",
-                    videoSize: "tallbanner",
-                    video: objectUrl,
-                };
-            case "audio":
-                return {
-                    style: "audio",
-                    audio: objectUrl,
-                };
-            default:
-                return {
-                    style: "text",
-                };
-        }
-    }
-
-    public render(): React.ReactNode {
-        let title: string;
-        if (this.props.totalFiles > 1 && this.props.currentIndex !== undefined) {
-            title = _t("upload_file|title_progress", {
-                current: this.props.currentIndex + 1,
-                total: this.props.totalFiles,
-            });
-        } else {
-            title = _t("upload_file|title");
-        }
-
-
-        let uploadAllButton: JSX.Element | undefined;
-        if (this.props.currentIndex + 1 < this.props.totalFiles) {
-            uploadAllButton = (
-                <button onClick={this.onUploadAllClick} className="mx_LegacyDialogButton" type="button">
-                    {_t("upload_file|upload_all_button")}
-                </button>
-            );
-        }
-
-        return (
-            <BaseDialog
-                className="mx_UploadConfirmDialog"
-                fixedWidth={false}
-                onFinished={this.onCancelClick}
-                title={title}
-                contentId="mx_Dialog_content"
-            >
-                <div id="mx_Dialog_content">
-                    <div className="mx_UploadConfirmDialog_previewOuter">
-                        <div className="mx_UploadConfirmDialog_previewInner">
-                            {this.state.mediaPreviewVm && <MediaPreviewGroupPreview vm={this.state.mediaPreviewVm} />}
-                        </div>
-                    </div>
-                </div>
-
-                <DialogButtons
-                    primaryButton={_t("action|upload")}
-                    hasCancel={false}
-                    onPrimaryButtonClick={this.onUploadClick}
-                    focus={true}
-                >
-                    {uploadAllButton}
-                </DialogButtons>
-            </BaseDialog>
+    let uploadAllButton: JSX.Element | undefined;
+    if (currentIndex + 1 < totalFiles) {
+        uploadAllButton = (
+            <button onClick={onUploadAllClick} className="mx_LegacyDialogButton" type="button">
+                {_t("upload_file|upload_all_button")}
+            </button>
         );
     }
+
+    return (
+        <BaseDialog
+            className="mx_UploadConfirmDialog"
+            fixedWidth={false}
+            onFinished={onCancelClick}
+            title={title}
+            contentId="mx_Dialog_content"
+        >
+            <div id="mx_Dialog_content">
+                <div className="mx_UploadConfirmDialog_previewOuter">
+                    <div className="mx_UploadConfirmDialog_previewInner">
+                        <MediaPreviewGroupPreview vm={vm} />
+                    </div>
+                </div>
+            </div>
+
+            <DialogButtons
+                primaryButton={_t("action|upload")}
+                hasCancel={false}
+                onPrimaryButtonClick={onUploadClick}
+                focus={true}
+            >
+                {uploadAllButton}
+            </DialogButtons>
+        </BaseDialog>
+    );
 }
