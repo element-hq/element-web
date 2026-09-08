de8c74e1e397495ab0ab2b005ce34d651a423d5a
Will Hunt <2072976+Half-Shot@users.noreply.github.com>
2026-09-08 20:01:28 +0000

    Module API for overriding URL Preview results (#34877)
    * Add support for mutating URL previews
    
    * Improvements
    
    * Pass in the MatrixEvent being previewed, if relevant.
    
    * And wire it up
    
    * Add a playwright test to cover it.
    
    * Fixup
    
    * cleanup
    
    * Cleanup lint
    
    * fix error
    
    * reduce cognitive load
    
    * lint

---
+++ apps/web/src/modules/models/Event.ts
@@ -0,0 +1,36 @@
+/*
+Copyright 2026 Element Creations Ltd.
+
+SPDX-License-Identifier: AGPL-3.0-only OR GPL-3.0-only OR LicenseRef-Element-Commercial
+Please see LICENSE files in the repository root for full details.
+*/
+
+import type { MatrixEvent } from "matrix-js-sdk/src/matrix";
+import type { MatrixEvent as ModuleMatrixEvent } from "@element-hq/element-web-module-api";
+
+/**
+ * Convert a JS-SDK room MatrixEvent into an object safe for usage with the module API.
+ * @param mxEvent The room event.
+ * @returns A Matrix event if the event contains a truthy eventId, roomId and sender. Otherwise returns null.
+ */
+export function getModuleMatrixEvent(mxEvent: MatrixEvent): ModuleMatrixEvent | null {
+    const eventId = mxEvent.getId();
+    const roomId = mxEvent.getRoomId();
+    const sender = mxEvent.sender;
+    // Typically we wouldn't expect messages without these keys to be rendered
+    // by the timeline, but for the sake of type safety.
+    if (!eventId || !roomId || !sender) {
+        // Not a message event.
+        return null;
+    }
+    return {
+        content: mxEvent.getContent(),
+        eventId,
+        originServerTs: mxEvent.getTs(),
+        roomId,
+        sender: sender.userId,
+        stateKey: mxEvent.getStateKey(),
+        type: mxEvent.getType(),
+        unsigned: mxEvent.getUnsigned(),
+    };
+}
