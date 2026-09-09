4caa5322c2c920606e5383f288bbf5c863bc5d59
Michael Telatynski <7t3chguy@gmail.com>
2026-09-09 14:59:49 +0100

    Migrate batch of Jest tests to Vitest

---
+++ apps/web/src/components/views/location/Marker.test.tsx
@@ -0,0 +1,45 @@
+/*
+Copyright 2024 New Vector Ltd.
+Copyright 2022 The Matrix.org Foundation C.I.C.
+
+SPDX-License-Identifier: AGPL-3.0-only OR GPL-3.0-only OR LicenseRef-Element-Commercial
+Please see LICENSE files in the repository root for full details.
+*/
+
+// @vitest-environment happy-dom
+
+import React from "react";
+import { describe, it, expect } from "vitest";
+import { RoomMember } from "matrix-js-sdk/src/matrix";
+import { getByTestId, render } from "test-utils-rtl";
+
+import Marker from "./Marker";
+
+describe("<Marker />", () => {
+    const defaultProps = {
+        id: "abc123",
+    };
+    const getComponent = (props = {}) => render(<Marker {...defaultProps} {...props} />);
+
+    it("renders with location icon when no room member", () => {
+        const { asFragment } = getComponent();
+        expect(asFragment()).toMatchSnapshot();
+    });
+
+    it("does not try to use member color without room member", () => {
+        const { container } = getComponent({ useMemberColor: true });
+        expect(container.querySelector(".mx_Marker.mx_Marker_defaultColor")).toBeInTheDocument();
+    });
+
+    it("uses member color class", () => {
+        const member = new RoomMember("!room:server", "@user:server");
+        const { container } = getComponent({ useMemberColor: true, roomMember: member });
+        expect(container.querySelector(".mx_Marker.mx_Username_color3")).toBeInTheDocument();
+    });
+
+    it("renders member avatar when roomMember is truthy", () => {
+        const member = new RoomMember("!room:server", "@user:server");
+        const { container } = getComponent({ roomMember: member });
+        expect(getByTestId(container, "avatar-img")).toBeInTheDocument();
+    });
+});
