db1a85d2648f30f3698ed02033e125eb6c025f6b
David Baker <dbkr@users.noreply.github.com>
2026-09-10 18:42:25 +0100

    Actually fix tests
    Don't try to get the oauth client ID unconditionally otherwise
    all our tests have to make sure it's there

--- apps/web/src/Lifecycle.ts
+++ apps/web/src/Lifecycle.ts
@@ -813,7 +813,16 @@ async function doSetLoggedIn(
     // check the session lock just before creating the new client
     checkSessionLock();
 
-    MatrixClientPeg.set(createClientWithCreds(credentials, getStoredOAuthClientId()));
+    let oauthClientId: string | undefined;
+    if (credentials.refreshToken) {
+        try {
+            oauthClientId = getStoredOAuthClientId();
+        } catch (e) {
+            logger.warn("Have a refresh token but no stored OAuth2 client ID: tokens will not be refreshed", e);
+        }
+    }
+
+    MatrixClientPeg.set(createClientWithCreds(credentials, oauthClientId));
     const client = MatrixClientPeg.safeGet();
 
     setSentryUser(credentials.userId);
