26e33326697b51524f6e87c356569e93d481dda0
Skye Elliot <actuallyori@gmail.com>
2026-09-11 12:03:40 +0100

    docs: Add documentation for desktop X.509 configuration

--- packages/shared-types/lib/config.json.d.ts
+++ packages/shared-types/lib/config.json.d.ts
@@ -223,8 +223,25 @@ export interface WebConfigJson {
 export interface DesktopConfigJson extends WebConfigJson {
     web_base_url?: string;
     update_base_url?: string;
+    /**
+     * Configuration for experimental X.509-based identity verification.
+     */
     x509?: {
+        /**
+         * Path to the PKCS#11 library used to interface with our supported hardware keys.
+         *
+         * For Yubikeys, this is generally:
+         * - Linux: `/usr/local/lib/libykcs11.so` (distribution dependent)
+         * - macOS: `/opt/homebrew/lib/libykcs11.dylib` (via Homebrew)
+         * - Windows: `C:\Program Files\Yubico\Yubico PIV Tool\bin\libykcs11.dll`
+         *
+         * Element Desktop Pro likely ships this as part of its build, and so this should be
+         * configured to point there.
+         */
         library_path: string;
+        /**
+         * Overrides the name reported by the PKCS#11 library. This can generally be left unset.
+         */
         library_name?: string;
         /**
          * Directory holding the CA certificates used to build our own chain, plus the user's own leaf
