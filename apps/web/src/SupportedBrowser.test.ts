403fdba3cb645cf5a3f355794c9672d30a8db165
Michael Telatynski <7t3chguy@gmail.com>
2026-09-11 16:05:45 +0100

    Update tests

--- apps/web/src/SupportedBrowser.test.ts
+++ apps/web/src/SupportedBrowser.test.ts
@@ -73,15 +73,15 @@ describe("SupportedBrowser", () => {
         // Safari 26.0 on macOS
         "Mozilla/5.0 (Macintosh; Intel Mac OS X 15_7_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/26.0 Safari/605.1.15",
         // Latest Firefox on macOS Sonoma
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 15.7; rv:152.0) Gecko/20100101 Firefox/152.0",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 15.7; rv:155.0) Gecko/20100101 Firefox/155.0",
         // Latest Edge on Windows
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36 Edg/150.0.4078.65",
         // Latest Edge on macOS
         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36 Edg/150.0.4078.65",
         // Latest Firefox on Windows
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:152.0) Gecko/20100101 Firefox/152.0",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:155.0) Gecko/20100101 Firefox/155.0",
         // Latest Firefox on Linux
-        "Mozilla/5.0 (X11; Linux i686; rv:152.0) Gecko/20100101 Firefox/152.0",
+        "Mozilla/5.0 (X11; Linux i686; rv:155.0) Gecko/20100101 Firefox/155.0",
         // Latest Chrome on Windows
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36",
     ])("should not warn for supported browsers", testUserAgentFactory());
