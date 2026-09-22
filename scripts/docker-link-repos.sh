525f565c712de25eea8060558826a30f37a0e21d
Michael Telatynski <7t3chguy@gmail.com>
2026-09-22 12:06:48 +0000

    Fix docker link repos script (#35133)
    Broken by switching from pnpm link to pnpm pack for installing js-sdk
    
    (cherry picked from commit ffe282a183c33ac0ff40cf88e1998e8baaee5d2e)

--- scripts/docker-link-repos.sh
+++ scripts/docker-link-repos.sh
@@ -7,7 +7,7 @@ set -ex
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 if [[ $USE_CUSTOM_SDKS == false ]] && [[ $BRANCH == 'develop' ]]
 then
-    echo "using develop dependencies for react-sdk and js-sdk"
+    echo "using develop dependency for js-sdk"
     USE_CUSTOM_SDKS=true
     JS_SDK_BRANCH='develop'
 fi
@@ -18,11 +18,11 @@ then
     exit 0
 fi
 
-echo "Linking js-sdk"
-git clone --depth 1 --branch $JS_SDK_BRANCH "$JS_SDK_REPO" js-sdk
-cd js-sdk
-pnpm install
-cd ../
+# Follows process from layered.sh - see that for reasoning as to why we do this rather than just `pnpm link`
+echo "Installing js-sdk"
+git clone --depth 1 --branch $JS_SDK_BRANCH "$JS_SDK_REPO" matrix-js-sdk
+pnpm -C matrix-js-sdk install --frozen-lockfile --ignore-scripts
 
 echo "Setting up element-web with js-sdk package"
-pnpm link ./js-sdk
+pnpm -C matrix-js-sdk pack --out matrix-js-sdk.tgz
+pnpm -C apps/web install $(pwd)/matrix-js-sdk/matrix-js-sdk.tgz
