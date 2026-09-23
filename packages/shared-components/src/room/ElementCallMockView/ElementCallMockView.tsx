f035cc1e96616468f06b58b62fc1a9f4fe6e7e85
Johannes Marbach <n0-0ne+github@mailbox.org>
2026-09-23 10:40:46 +0200

    Format

--- packages/shared-components/src/room/ElementCallMockView/ElementCallMockView.tsx
+++ packages/shared-components/src/room/ElementCallMockView/ElementCallMockView.tsx
@@ -151,7 +151,9 @@ export const ElementCallMockView = ({ vm }: ElementCallMockViewProps): JSX.Eleme
                 ) : (
                     <ul className={styles.members}>
                         {participants.map((participant) => (
-                            <Text as="li" key={participant.id}>{participant.label}</Text>
+                            <Text as="li" key={participant.id}>
+                                {participant.label}
+                            </Text>
                         ))}
                     </ul>
                 )}
